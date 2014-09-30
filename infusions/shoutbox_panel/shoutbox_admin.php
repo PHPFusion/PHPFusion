17486770dd120af9c91bb244e2debc472b255157
hien- <meangczac.chan@gmail.com>
2014-09-30 22:00:53 +0800

    Clean up
    Shoutbox Admin

--- infusions/shoutbox_panel/shoutbox_admin.php
+++ infusions/shoutbox_panel/shoutbox_admin.php
@@ -17,15 +17,12 @@
 +--------------------------------------------------------*/
 require_once "../../maincore.php";
 require_once THEMES."templates/admin_header.php";
-
 include INFUSIONS."shoutbox_panel/infusion_db.php";
 
 // Check if a locale file is available that match the selected locale.
 if (file_exists(INFUSIONS."shoutbox_panel/locale/".LANGUAGE.".php")) {
-	// Load the locale file matching selection.
 	include INFUSIONS."shoutbox_panel/locale/".LANGUAGE.".php";
 } else {
-	// Load the default locale file.
 	include INFUSIONS."shoutbox_panel/locale/English.php";
 }
 
@@ -71,10 +68,8 @@ if (!isset($_GET['page']) || $_GET['page'] != "settings") {
 			if (dbrows($result)) {
 				$data = dbarray($result);
 				opentable($locale['SB_edit_shout']);
-				echo openform('input_form', 'input_form', 'post', FUSION_SELF.$aidlink."&amp;shout_id=".$data['shout_id']."", array('downtime' => 0,
-																																	'notice' => 0));
-				echo form_textarea($locale['SB_message'], 'shout_message', 'shout_message', $data['shout_message'], array('required' => 1,
-																														  'bbcode' => 1));
+				echo openform('input_form', 'input_form', 'post', FUSION_SELF.$aidlink."&amp;shout_id=".$data['shout_id']."", array('downtime' => 0, 'notice' => 0));
+				echo form_textarea($locale['SB_message'], 'shout_message', 'shout_message', $data['shout_message'], array('required' => 1, 'bbcode' => 1));
 				echo form_button($locale['SB_save_shout'], 'saveshout', 'saveshout', $locale['SB_save_shout'], array('class' => 'btn-primary'));
 				echo closeform();
 				closetable();
@@ -150,8 +145,7 @@ if (!isset($_GET['page']) || $_GET['page'] != "settings") {
 	echo $nav;
 	echo openform('shoutbox', 'shoutbox', 'post', FUSION_SELF.$aidlink."&amp;page=settings", array('downtime' => 0));
 	echo "<div class='panel panel-default'>\n<div class='panel-body'>\n";
-	$array = array('90' => "90 ".$locale['SB_days'], '60' => "60 ".$locale['SB_days'], '30' => "30 ".$locale['SB_days'],
-				   '20' => "20 ".$locale['SB_days'], '10' => "10 ".$locale['SB_days'],);
+	$array = array('90' => "90 ".$locale['SB_days'], '60' => "60 ".$locale['SB_days'], '30' => "30 ".$locale['SB_days'], '20' => "20 ".$locale['SB_days'], '10' => "10 ".$locale['SB_days'],);
 	echo form_select($locale['SB_delete_old'], 'num_days', 'num_days', $array, '', array('inline' => 1));
 	echo "<div class='m-t-5 m-b-0'/>\n&nbsp;</div>\n";
 	echo form_button($locale['SB_submit'], 'sb_delete_old', 'sb_delete_old', $locale['SB_submit'], array('class' => 'btn-primary pull-right'));
@@ -160,11 +154,9 @@ if (!isset($_GET['page']) || $_GET['page'] != "settings") {
 	add_to_jquery("
         $('sb_delete_old').bind('click', function() { confirm('".$locale['SB_warning_shouts']."'); return false; });
     ");
-	echo openform('shoutbox2', 'shoutbox2', 'post', FUSION_SELF.$aidlink."&amp;page=settings", array('downtime' => 0,
-																									 'notice' => 0));
+	echo openform('shoutbox2', 'shoutbox2', 'post', FUSION_SELF.$aidlink."&amp;page=settings", array('downtime' => 0, 'notice' => 0));
 	echo "<div class='panel panel-default'>\n<div class='panel-body'>\n";
-	echo form_text($locale['SB_visible_shouts'], 'visible_shouts', 'visible_shouts', $inf_settings['visible_shouts'], array('required' => 1,
-																															'inline' => 1));
+	echo form_text($locale['SB_visible_shouts'], 'visible_shouts', 'visible_shouts', $inf_settings['visible_shouts'], array('required' => 1, 'inline' => 1));
 	$opts = array('1' => $locale['SB_yes'], '0' => $locale['SB_no'],);
 	echo "<div class='m-t-5 m-b-0'/>\n&nbsp;</div>\n";
 	echo form_select($locale['SB_guest_shouts'], 'guest_shouts', 'guest_shouts', $opts, $inf_settings['guest_shouts'], array('inline' => 1));
