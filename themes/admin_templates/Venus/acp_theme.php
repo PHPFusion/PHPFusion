f5cfa0d99c001643ad332ed3528cd2d8a8af1720
Dan C <JoiNNN@users.noreply.github.com>
2014-10-01 22:17:45 +0200

    Open link in a new tab
    Open link in a new tab and let the site whether to use HTTPS or not.

--- themes/admin_templates/Venus/acp_theme.php
+++ themes/admin_templates/Venus/acp_theme.php
@@ -68,7 +68,7 @@ $admin_password = '';
 		echo CONTENT;
 		echo "</div>\n";
 		echo "<div class='m-l-20 display-block'>\n";
-		echo "Venus Admin &copy; ".date("Y")." created by <a href='https://www.php-fusion.co.uk'><strong>PHP-Fusion Inc.</strong></a>\n";
+		echo "Venus Admin &copy; ".date("Y")." created by <a href='http://www.php-fusion.co.uk' target='_blank'><strong>PHP-Fusion Inc.</strong></a>\n";
 		echo showcopyright();
 		echo "</div>\n";
 		echo "</div>\n";
