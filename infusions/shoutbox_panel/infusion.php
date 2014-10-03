c4f1e4e1b1935465adfc5c28236fe382d4e9d2e6
Domi <domi@php-fusion.co.uk>
2014-10-03 08:09:52 +0200

    changed paths, order and author etc
    changed paths, order and author, added description for each action
    naturally Marcus.G, did not write the infusion.php alone. Usually we
    have Nick´s name left there, in this case I put Development Team, it is
    a team effort, built over years how to construct it in these files after
    all.

--- infusions/shoutbox_panel/infusion.php
+++ infusions/shoutbox_panel/infusion.php
@@ -5,7 +5,7 @@
 | https://www.php-fusion.co.uk/
 +--------------------------------------------------------+
 | Filename: infusion.php
-| Author: Marcus Gottschalk (MarcusG)
+| Author: PHP Fusion Development Team
 +--------------------------------------------------------+
 | This program is released as free software under the
 | Affero GPL license. You can redistribute it and/or
@@ -18,7 +18,9 @@
 if (!defined("IN_FUSION")) {
 	die("Access Denied");
 }
+
 include INFUSIONS."shoutbox_panel/infusion_db.php";
+
 // Check if locale file is available matching the current site locale setting.
 if (file_exists(INFUSIONS."shoutbox_panel/locale/".$settings['locale'].".php")) {
 	// Load the locale file matching the current site locale setting.
@@ -27,14 +29,22 @@ if (file_exists(INFUSIONS."shoutbox_panel/locale/".$settings['locale'].".php")) 
 	// Load the infusion's default locale file.
 	include INFUSIONS."shoutbox_panel/locale/English.php";
 }
+
 // Infusion general information
 $inf_title = $locale['SB_title'];
 $inf_description = $locale['SB_desc'];
 $inf_version = "1.00";
-$inf_developer = "PHP Fusion Dev. Team";
+$inf_developer = "PHP Fusion Development Team";
 $inf_email = "";
-$inf_weburl = "http://dev.php-fusion.co.uk";
+$inf_weburl = "https://www.php-fusion.co.uk";
 $inf_folder = "shoutbox_panel"; // The folder in which the infusion resides.
+
+//Administration panel
+$inf_adminpanel[1] = array("title" => $locale['SB_admin1'], "image" => "shout.gif", "panel" => "shoutbox_admin.php","rights" => "S");
+
+//Multilanguage table for Administration
+$inf_mlt[1] = array("title" => $locale['SB_title'], "rights" => "SB");
+
 // Delete any items not required below.
 $inf_newtable[1] = DB_SHOUTBOX." (
     shout_id MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -48,13 +58,14 @@ $inf_newtable[1] = DB_SHOUTBOX." (
     PRIMARY KEY (shout_id),
     KEY shout_datestamp (shout_datestamp)
     ) ENGINE=MyISAM;";
+
+//Infuse insertations
 $inf_insertdbrow[1] = DB_PANELS." (panel_name, panel_filename, panel_content, panel_side, panel_order, panel_type, panel_access, panel_display, panel_status) VALUES('".$locale['SB_title']."', 'shoutbox_panel', '', '4', '3', 'file', '0', '0', '1')";
 $inf_insertdbrow[2] = DB_SETTINGS_INF." (settings_name, settings_value, settings_inf) VALUES('visible_shouts', '5', '".$inf_folder."')";
 $inf_insertdbrow[3] = DB_SETTINGS_INF." (settings_name, settings_value, settings_inf) VALUES('guest_shouts', '0', '".$inf_folder."')";
+
+//Defuse cleaning	
 $inf_droptable[1] = DB_SHOUTBOX;
 $inf_deldbrow[1] = DB_PANELS." WHERE panel_filename='".$inf_folder."'";
 $inf_deldbrow[2] = DB_SETTINGS_INF." WHERE settings_inf='".$inf_folder."'";
-$inf_adminpanel[1] = array("title" => $locale['SB_admin1'], "image" => "shout.gif", "panel" => "shoutbox_admin.php",
-						   "rights" => "S");
-$inf_mlt[1] = array("title" => $locale['SB_title'], "rights" => "SB");
 ?>
