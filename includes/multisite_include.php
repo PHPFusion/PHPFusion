de0c16d1119964148aab5b1706fb9d20110e53ea
Domi <domi@php-fusion.co.uk>
2014-10-09 08:47:09 +0200

    cleanup
    cleanup

--- includes/multisite_include.php
+++ includes/multisite_include.php
@@ -49,9 +49,9 @@ define("DB_NEWS", DB_PREFIX."news");
 define("DB_NEWS_CATS", DB_PREFIX."news_cats");
 define("DB_ONLINE", DB_PREFIX."online");
 define("DB_PANELS", DB_PREFIX."panels");
-define("DB_PERMALINK_REWRITE", DB_PREFIX."permalinks_rewrites"); // rewrite
-define("DB_PERMALINK_METHOD", DB_PREFIX."permalinks_method"); // patten
-define("DB_PERMALINK_ALIAS", DB_PREFIX."permalinks_alias"); // alias open notepad :P
+define("DB_PERMALINK_REWRITE", DB_PREFIX."permalinks_rewrites");
+define("DB_PERMALINK_METHOD", DB_PREFIX."permalinks_method");
+define("DB_PERMALINK_ALIAS", DB_PREFIX."permalinks_alias");
 define("DB_PHOTO_ALBUMS", DB_PREFIX."photo_albums");
 define("DB_PHOTOS", DB_PREFIX."photos");
 define("DB_POLL_VOTES", DB_PREFIX."poll_votes");
