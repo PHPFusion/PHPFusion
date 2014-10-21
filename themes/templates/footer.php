7b66190e59e9fc97729f9338869aedb0c5d807f8
Dan C <JoiNNN@users.noreply.github.com>
2014-10-21 22:27:39 +0200

    Show memory usage
    Just for development purposes added this line of code to give us a hint about the memory used by PHP-Fusion for generating a page.
    The line can be deleted when we push an RC or final release.

--- themes/templates/footer.php
+++ themes/templates/footer.php
@@ -114,4 +114,7 @@ if ($pdo_enabled == "1") {
 	mysql_close($db_connect);
 }
 
+// Shows allocated memory and the actual memory used for generatig a page
+echo "Memory: alloc. ".(memory_get_peak_usage(true)/1024/1024)." MB / used ".number_format((memory_get_peak_usage(false)/1024/1024), 2)." MB";
+
 ?>
