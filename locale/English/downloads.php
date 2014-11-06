728740e5faecdb63d5e27f47aa6b91145c4e66a9
hien- <meangczac.chan@gmail.com>
2014-11-06 10:10:47 +0800

    Increment global locale for Downloads
    New Template

--- locale/English/downloads.php
+++ locale/English/downloads.php
@@ -1,7 +1,7 @@
 <?php
-$locale['400'] = "Downloads";
+$locale['400'] = "Downloads Center";
 // Download File Information
-$locale['410'] = "[NEW]";
+$locale['410'] = "New"; // change case and removed brackets
 $locale['411'] = "License:";
 $locale['412'] = "O/S:";
 $locale['413'] = "Version:";
@@ -9,40 +9,58 @@ $locale['414'] = "Added:";
 $locale['415'] = "Downloads:";
 $locale['416'] = "Download";
 $locale['417'] = "Downloads Home";
-$locale['418'] = "Homepage:";
-$locale['418a'] = "Visit";
+$locale['418'] = "Publishers Homepage"; // changed // deleted 418a
 $locale['419'] = "Screenshot";
-$locale['420'] = "Download title";
+$locale['420'] = "File size"; // changed
 $locale['421'] = "Date";
 $locale['422'] = "Author";
 $locale['423'] = "Version";
 $locale['424'] = "Downloads";
 $locale['425'] = "Comments";
 $locale['426'] = "Rating";
-$locale['427'] = "Created:";
-$locale['428'] = "Info";
+$locale['426a'] = "Average User Rating"; // New
+$locale['427'] = "Editor's Review:"; // changed
+$locale['428'] = "Copyright"; // changed
 $locale['429'] = "Download Stats";
-$locale['429a'] = "Not Available";
+$locale['429a'] = "Not Available"; // defunct
 // Downloads Notices
 $locale['430'] = "No Download Categories defined";
 $locale['431'] = "No Downloads have been added to this Category";
+$locale['432'] = "No Downloads Available";
+
+
 // Statistics
-$locale['440'] = "Downloaded:";
-$locale['441'] = "Most Downloaded:";
-$locale['442'] = "Most Recent:";
-// Search and filters
-$locale['450'] = "Use the filters on the right to find the Downloads you want.";
-$locale['451'] = "View all";
-$locale['452'] = "ID";
-$locale['453'] = "Title";
-$locale['454'] = "Author";
-$locale['455'] = "Downloads";
-$locale['456'] = "Date";
-$locale['457'] = "Ascending";
-$locale['458'] = "Descending";
-$locale['459'] = "Apply";
-$locale['460'] = "Search Downloads:";
-$locale['461'] = "Search";
-$locale['462'] = "Category:";
-$locale['463'] = "Order by:";
+$locale['440'] = "Downloaded"; // removed double dots
+$locale['441'] = "Most Downloaded"; // removed double dots
+$locale['442'] = "Most Recent"; // removed double dots
+$locale['443'] = "Latest Additions"; // new
+$locale['444'] = "Highest Ratings"; // new
+
+// Side nav bar
+$locale['445'] = 'Browse by Category';
+$locale['446'] = 'Browse by Rates';
+
+// Filter titles
+$locale['comments'] = 'Most Popular Downloads';
+$locale['recent'] = 'Latest Released Downloads';
+$locale['download'] = 'Most Popular Downloads';
+$locale['ratings'] = 'Top User Pick Downloads';
+
+// Search and filters - New change
+$locale['450'] = "Sort";
+$locale['451'] = "Show all Results";
+$locale['452'] = "Most Commented";
+$locale['453'] = "Date Added";
+$locale['454'] = "Downloads Title";
+$locale['455'] = "Highest Rating";
+$locale['456'] = "Total Downloads";
+
+$locale['457'] = "--";
+$locale['458'] = "Added";
+$locale['459'] = "Rate this item";
+$locale['460'] = "Search Downloads"; // removed hyphens
+$locale['461'] = "Download Now"; // reused
+$locale['462'] = "Download Information";
+$locale['463'] = "Be the first to rate this download";
+$locale['464'] = "Visit Website";
 ?>
