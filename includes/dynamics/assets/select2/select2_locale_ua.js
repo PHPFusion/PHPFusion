b3b83c3f601bbf3308afeaa5a78826f3d49e24d8
hien- <meangczac.chan@gmail.com>
2014-11-08 00:41:21 +0800

    Fix Ukranian Locale

--- includes/dynamics/assets/select2/select2_locale_ua.js
+++ includes/dynamics/assets/select2/select2_locale_ua.js
@@ -12,11 +12,11 @@
         },
         formatInputTooShort: function (input, min) {
             var n = min - input.length, s = ["", "и", "ів"], p = [2, 0, 1, 1, 1, 2];
-            return "Введіть буль ласка ще " + n + " символ" + s[ (n % 100 > 4 && n % 100 <= 20) ? 2 : p[Math.min(n % 10, 5)] ];
+            return "Введіть, будь ласка ще " + n + " символ" + s[ (n % 100 > 4 && n % 100 <= 20) ? 2 : p[Math.min(n % 10, 5)] ];
         },
         formatInputTooLong: function (input, max) {
             var n = input.length - max, s = ["", "и", "ів"], p = [2, 0, 1, 1, 1, 2];
-            return "Введіть буль ласка на " + n + " символ" + s[ (n % 100 > 4 && n % 100 <= 20) ? 2 : p[Math.min(n % 10, 5)] ] + " менше";
+            return "Введіть, будь ласка на " + n + " символ" + s[ (n % 100 > 4 && n % 100 <= 20) ? 2 : p[Math.min(n % 10, 5)] ] + " менше";
         },
         formatSelectionTooBig: function (limit) {
             var s = ["", "и", "ів"], p = [2, 0, 1, 1, 1, 2];
