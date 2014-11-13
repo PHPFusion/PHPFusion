fa709650a011808e98275c602418d5db7cf0f473
Domi <domi@php-fusion.co.uk>
2014-11-13 02:52:48 +0100

    updated to work with permalinks
    updated to work with permalinks

--- infusions/rss_feeds_panel/feeds/rss_forums.php
+++ infusions/rss_feeds_panel/feeds/rss_forums.php
@@ -16,7 +16,7 @@
 | copyright header is strictly prohibited without
 | written permission from the original author(s).
 +--------------------------------------------------------*/
-require_once "../../../maincore.php";
+require_once dirname(__FILE__)."../../../../maincore.php";
 
 header('Content-Type: application/rss+xml; charset='.$locale['charset'].'');
 
