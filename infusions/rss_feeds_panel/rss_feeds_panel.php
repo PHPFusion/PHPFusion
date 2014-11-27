5ebd3235a70f29148961cf21f63f553927904dd7
Domi <domi@php-fusion.co.uk>
2014-11-27 06:19:02 +0100

    Added Blog to RSS feed
    Added Blog to RSS feed

--- infusions/rss_feeds_panel/rss_feeds_panel.php
+++ infusions/rss_feeds_panel/rss_feeds_panel.php
@@ -27,6 +27,7 @@ if (file_exists(INFUSIONS."rss_feeds_panel/locale/".LANGUAGE.".php")) {
 openside($locale['rss009']);
 echo "<p style='text-align: center;'>\n";
 echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_news.php'><img class='img-responsive' src='".INFUSIONS."rss_feeds_panel/images/rss_news.gif' alt='".$locale['rss004']."'></a>\n";
+echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_blog.php'><img class='img-responsive' src='".INFUSIONS."rss_feeds_panel/images/rss_blog.gif' alt='".$locale['rss000']."'></a>\n";
 echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_articles.php'><img class='img-responsive' src='".INFUSIONS."rss_feeds_panel/images/rss_articles.gif' alt='".$locale['rss002']."'></a>\n";
 echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_downloads.php'><img class='img-responsive' src='".INFUSIONS."rss_feeds_panel/images/rss_downloads.gif' alt='".$locale['rss005']."'></a>\n";
 echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_weblinks.php'><img class='img-responsive' src='".INFUSIONS."rss_feeds_panel/images/rss_weblinks.gif' alt='".$locale['rss006']."'></a>\n";
