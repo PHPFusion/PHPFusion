6ebd6f456a47845c8379349b6fb3d840117d7469
Takács Ákos <rimelek@rimelek.hu>
2014-12-05 22:28:49 +0100

    refactored PDO database functions - fixed calculation of query time - turned off emulation of prepare - thrown exceptions are handled and logged in the database - dbresult does not fetch all records just seeking to the required one - removed unneeded init command in dbconnect - some renamed variables - IDE supported PHPdoc comments

--- includes/db_handlers/pdo_functions_include.php
+++ includes/db_handlers/pdo_functions_include.php
@@ -22,19 +22,29 @@ if (!defined("IN_FUSION")) { die("Access Denied"); }
 $pdo = NULL;
 
 // MySQL database functions
+
+/**
+ * Send a database query
+ * 
+ * @global PDO $pdo
+ * @global int $mysql_queries_count
+ * @global array $mysql_queries_time
+ * @param string $query SQL 
+ * @return \PDOStatement or FALSE on error
+ */
 function dbquery($query) {
 	global $pdo, $mysql_queries_count, $mysql_queries_time;
-	$mysql_queries_count++;
-	$query_time = get_microtime();
-	$result = $pdo->prepare($query);
-	$query_time = round((get_microtime()-$query_time), 7);
-	$mysql_queries_time[$mysql_queries_count] = array($query_time, $query);
-	if (!$result) {
-		print_r($result->errorInfo());
-		return FALSE;
-	} else {
+	$start_time = get_microtime();
+	try {
+		$result = $pdo->prepare($query);
 		$result->execute();
+		$query_time = round((get_microtime()-$start_time), 7);
+		$mysql_queries_time[++$mysql_queries_count] = array($query_time, $query);
 		return $result;
+	} catch (PDOException $e) {
+		trigger_error($e->getMessage(), E_USER_ERROR);
+		echo $e;
+		return FALSE;
 	}
 }
 
@@ -48,59 +58,98 @@ function dbquery_exec($query) {
 	return $result;
 }
 
+/**
+ * Count the number of rows in a table filtered by conditions
+ * 
+ * @global \PDO $pdo
+ * @global int $mysql_queries_count
+ * @global array $mysql_queries_time
+ * @param string $field Parenthesized field name
+ * @param string $table Table name
+ * @param string $conditions conditions after "where"
+ * @return boolean
+ */
 function dbcount($field, $table, $conditions = "") {
 	global $pdo, $mysql_queries_count, $mysql_queries_time;
-	$mysql_queries_count++;
+	
 	$cond = ($conditions ? " WHERE ".$conditions : "");
-	$query_time = get_microtime();
-	$result = $pdo->prepare("SELECT COUNT".$field." FROM ".$table.$cond);
-	$query_time = substr((get_microtime()-$query_time), 0, 7);
-	$mysql_queries_time[$mysql_queries_count] = array($query_time, "SELECT COUNT".$field." FROM ".$table.$cond);
-	if (!$result) {
-		print_r($result->errorInfo());
+	$start_time = get_microtime();
+	$sql = "SELECT COUNT".$field." FROM ".$table.$cond;
+	try {
+		$statement = $pdo->prepare($sql);
+		$statement->execute();
+		$query_time = round((get_microtime()-$start_time), 7);
+		$mysql_queries_time[++$mysql_queries_count] = array($query_time, $sql);
+		return $statement->fetchColumn();
+	} catch (PDOException $e) {
+		trigger_error($e->getMessage(), E_USER_ERROR);
+		echo $e;
 		return FALSE;
-	} else {
-		$result->execute();
-		return $result->fetchColumn();
 	}
 }
 
-function dbresult($query, $row) {
-	global $pdo, $mysql_queries_count, $mysql_queries_time;
-	$query_time = get_microtime();
-	$data = $query->fetchAll();
-	$query_time = substr((get_microtime()-$query_time), 0, 7);
-	$mysql_queries_time[$mysql_queries_count] = array($query_time, $query);
-	if (!$query) {
-		print_r($query->errorInfo());
-		return FALSE;
-	} else {
-		$result = $query->getColumnMeta(0);
-		return $data[$row][$result['name']];
+/**
+ * Fetch the first column of a specific row
+ * 
+ * @param \PDOStatement $statement
+ * @param int $row
+ * @return mixed
+ */
+function dbresult($statement, $row) {
+	//seek
+	for ($i=0; $i<$row; $i++) {
+		$statement->fetchColumn();
 	}
+	//returns false when an error occurs
+	return $statement->fetchColumn();
 }
 
-function dbrows($query) {
-	return $query->rowCount();
+/**
+ * Count the number of affected rows by the given query 
+ * 
+ * @param \PDOStatement $statement
+ * @return int
+ */
+function dbrows($statement) {
+	return $statement->rowCount();
 }
 
-function dbarray($query) {
-	global $pdo;
-	$query->setFetchMode(PDO::FETCH_ASSOC);
-	return $query->fetch();
+/**
+ * Fetch one row as an associative array
+ * 
+ * @param \PDOStatement $statement
+ * @return array Associative array
+ */
+function dbarray($statement) {
+	$statement->setFetchMode(PDO::FETCH_ASSOC);
+	return $statement->fetch();
 }
 
-function dbarraynum($query) {
-	global $pdo;
-	$query->setFetchMode(PDO::FETCH_NUM);
-	return $query->fetch();
+/**
+ * Fetch one row as a numeric array
+ * 
+ * @param \PDOStatement $statement
+ * @return array Numeric array
+ */
+function dbarraynum($statement) {
+	$statement->setFetchMode(PDO::FETCH_NUM);
+	return $statement->fetch();
 }
 
+/**
+ * Connect to the database
+ * 
+ * @param string $db_host
+ * @param string $db_user
+ * @param string $db_pass
+ * @param string $db_name
+ */
 function dbconnect($db_host, $db_user, $db_pass, $db_name) {
 	global $pdo;
 	try {
-	$pdo = new PDO("mysql:host=".$db_host.";dbname=".$db_name.";charset=utf8", $db_user, $db_pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$pdo = new PDO("mysql:host=".$db_host.";dbname=".$db_name.";charset=utf8", $db_user, $db_pass);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
 	} catch (PDOException $error) {
 		die("<strong>Unable to select MySQL database</strong><br />".$error->getMessage());
 	}
