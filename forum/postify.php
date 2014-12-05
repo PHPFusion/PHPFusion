7ded6097d5dfff18e915b97d816d5b38a88dc840
Vitalij <v.chubatyj@ukr.net>
2014-12-05 11:36:35 +0200

    Forum locale merging
    Locale files for forum merged into one and moved to /forum.php

--- forum/postify.php
+++ forum/postify.php
@@ -17,7 +17,7 @@
 +--------------------------------------------------------*/
 require_once "../maincore.php";
 require_once THEMES."templates/header.php";
-include LOCALE.LOCALESET."forum/post.php";
+include LOCALE.LOCALESET."forum.php";
 add_to_title($locale['global_204']);
 if (!isset($_GET['forum_id']) || !isnum($_GET['forum_id'])) {
 	redirect("index.php");
@@ -26,17 +26,17 @@ if (!isset($_GET['error']) || !isnum($_GET['error']) || $_GET['error'] == 0 || $
 	$_GET['error'] = 0;
 	$errorb = "";
 } elseif ($_GET['error'] == 1) {
-	$errorb = $locale['440a'];
+	$errorb = $locale['forum_0540'];
 } elseif ($_GET['error'] == 2) {
-	$errorb = $locale['440b'];
+	$errorb = $locale['forum_0541'];
 } elseif ($_GET['error'] == 3) {
-	$errorb = $locale['441'];
+	$errorb = $locale['forum_0542'];
 } elseif ($_GET['error'] == 4) {
-	$errorb = $locale['450'];
+	$errorb = $locale['forum_0551'];
 } elseif ($_GET['error'] == 5) {
-	$errorb = $locale['454'];
+	$errorb = $locale['forum_0555'];
 } elseif ($_GET['error'] == 6) {
-	$errorb = sprintf($locale['455'], $settings['forum_edit_timelimit']);
+	$errorb = sprintf($locale['forum_0556'], $settings['forum_edit_timelimit']);
 }
 $valid_get = array("on", "off", "new", "reply", "edit");
 if (!iMEMBER || !in_array($_GET['post'], $valid_get)) {
@@ -55,52 +55,52 @@ if (($_GET['post'] == "on" || $_GET['post'] == "off") && $settings['thread_notif
 		if (checkgroup($data['forum_access'])) {
 			add_to_head("<meta http-equiv='refresh' content='2; url=".FORUM."viewthread.php?forum_id=".$_GET['forum_id']."&amp;thread_id=".$_GET['thread_id']."' />\n");
 			$output = TRUE;
-			opentable($locale['451']);
+			opentable($locale['forum_0552']);
 			echo "<div class='alert alert-info' style='text-align:center'><br />\n";
 			if ($_GET['post'] == "on" && !dbcount("(thread_id)", DB_THREAD_NOTIFY, "thread_id='".$_GET['thread_id']."' AND notify_user='".$userdata['user_id']."'")) {
 				$result = dbquery("INSERT INTO ".DB_THREAD_NOTIFY." (thread_id, notify_datestamp, notify_user, notify_status) VALUES('".$_GET['thread_id']."', '".time()."', '".$userdata['user_id']."', '1')");
-				echo $locale['452']."<br /><br />\n";
+				echo $locale['forum_0553']."<br /><br />\n";
 			} elseif (isset($_GET['post']) && $_GET['post'] == 'off') {
 				$result = dbquery("DELETE FROM ".DB_THREAD_NOTIFY." WHERE thread_id='".$_GET['thread_id']."' AND notify_user='".$userdata['user_id']."'");
-				echo $locale['453']."<br /><br />\n";
+				echo $locale['forum_0554']."<br /><br />\n";
 			}
-			echo "<a href='".FORUM."viewthread.php?forum_id=".$_GET['forum_id']."&amp;thread_id=".$_GET['thread_id']."'>".$locale['447']."</a> ::\n";
-			echo "<a href='".FORUM."viewforum.php?forum_id=".$_GET['forum_id']."'>".$locale['448']."</a> ::\n";
-			echo "<a href='".FORUM."index.php'>".$locale['449']."</a><br /><br />\n</div>\n";
+			echo "<a href='".FORUM."viewthread.php?forum_id=".$_GET['forum_id']."&amp;thread_id=".$_GET['thread_id']."'>".$locale['forum_0548']."</a> ::\n";
+			echo "<a href='".FORUM."viewforum.php?forum_id=".$_GET['forum_id']."'>".$locale['forum_0549']."</a> ::\n";
+			echo "<a href='".FORUM."index.php'>".$locale['forum_0550']."</a><br /><br />\n</div>\n";
 			closetable();
 		}
 	}
 	if (!$output) redirect("index.php");
 } elseif ($_GET['post'] == "new") {
-	add_to_title($locale['global_201'].$locale['401']);
-	opentable($locale['401']);
+	add_to_title($locale['global_201'].$locale['forum_0501']);
+	opentable($locale['forum_0501']);
 	echo "<div class='alert ".($errorb ? "alert-warning" : "alert-info")."' style='text-align:center'><br />\n";
 	if ($errorb) {
 		echo $errorb."<br /><br />\n";
 	} else {
-		echo $locale['442']."<br /><br />\n";
+		echo $locale['forum_0543']."<br /><br />\n";
 	}
 	if ($_GET['error'] < 3) {
 		if (!isset($_GET['thread_id']) || !isnum($_GET['thread_id'])) {
 			redirect("index.php");
 		}
-		echo "<a href='".FORUM."viewthread.php?thread_id=".$_GET['thread_id']."'>".$locale['447']."</a> ::\n";
+		echo "<a href='".FORUM."viewthread.php?thread_id=".$_GET['thread_id']."'>".$locale['forum_0548']."</a> ::\n";
 		add_to_head("<meta http-equiv='refresh' content='2; url=".FORUM."viewthread.php?thread_id=".$_GET['thread_id']."' />\n");
 	}
-	echo "<a href='".FORUM."viewforum.php?forum_id=".$_GET['forum_id']."'>".$locale['448']."</a> ::\n";
-	echo "<a href='index.php'>".$locale['449']."</a><br /><br /></div>\n";
+	echo "<a href='".FORUM."viewforum.php?forum_id=".$_GET['forum_id']."'>".$locale['forum_0549']."</a> ::\n";
+	echo "<a href='index.php'>".$locale['forum_0550']."</a><br /><br /></div>\n";
 	closetable();
 } elseif ($_GET['post'] == "reply") {
 	if (!isset($_GET['thread_id']) || !isnum($_GET['thread_id'])) {
 		redirect("index.php");
 	}
-	add_to_title($locale['global_201'].$locale['403']);
-	opentable($locale['403']);
+	add_to_title($locale['global_201'].$locale['forum_0503']);
+	opentable($locale['forum_0503']);
 	echo "<div class='alert ".($errorb ? "alert-warning" : "alert-info")."' style='text-align:center'><br />\n";
 	if ($errorb) {
 		echo $errorb."<br /><br />\n";
 	} else {
-		echo $locale['443']."<br /><br />\n";
+		echo $locale['forum_0544']."<br /><br />\n";
 	}
 	if ($_GET['error'] < "2") {
 		if (!isset($_GET['post_id']) || !isnum($_GET['post_id'])) {
@@ -134,8 +134,8 @@ if (($_GET['post'] == "on" || $_GET['post'] == "off") && $settings['thread_notif
 							if ($data2['forum_access'] == 0 || in_array($data2['forum_access'], explode(".", $data['user_level'].".".$data['user_groups']))) {
 								$message_el1 = array("{USERNAME}", "{THREAD_SUBJECT}", "{THREAD_URL}");
 								$message_el2 = array($data['user_name'], $data2['thread_subject'], $link);
-								$message_subject = str_replace("{THREAD_SUBJECT}", $data2['thread_subject'], $locale['550']);
-								$message_content = str_replace($message_el1, $message_el2, $locale['551']);
+								$message_subject = str_replace("{THREAD_SUBJECT}", $data2['thread_subject'], $locale['forum_0660']);
+								$message_content = str_replace($message_el1, $message_el2, $locale['forum_0661']);
 								sendemail($data['user_name'], $data['user_email'], $settings['siteusername'], $settings['siteemail'], $message_subject, $message_content);
 							}
 						}
@@ -145,8 +145,8 @@ if (($_GET['post'] == "on" || $_GET['post'] == "off") && $settings['thread_notif
 						if ($data2['forum_access'] == 0 || in_array($data2['forum_access'], explode(".", $data['user_level'].".".$data['user_groups']))) {
 							$message_el1 = array("{USERNAME}", "{THREAD_SUBJECT}", "{THREAD_URL}");
 							$message_el2 = array($data['user_name'], $data2['thread_subject'], $link);
-							$message_subject = str_replace("{THREAD_SUBJECT}", $data2['thread_subject'], $locale['550']);
-							$message_content = str_replace($message_el1, $message_el2, $locale['551']);
+							$message_subject = str_replace("{THREAD_SUBJECT}", $data2['thread_subject'], $locale['forum_0660']);
+							$message_content = str_replace($message_el1, $message_el2, $locale['forum_0661']);
 							sendemail($data['user_name'], $data['user_email'], $settings['siteusername'], $settings['siteemail'], $message_subject, $message_content);
 						}
 					}
@@ -154,34 +154,34 @@ if (($_GET['post'] == "on" || $_GET['post'] == "off") && $settings['thread_notif
 				$result = dbquery("UPDATE ".DB_THREAD_NOTIFY." SET notify_status='0' WHERE thread_id='".$_GET['thread_id']."' AND notify_user!='".$userdata['user_id']."'");
 			}
 		}
-		echo "<a href='".FORUM."viewthread.php?thread_id=".$_GET['thread_id']."&amp;pid=".$_GET['post_id']."#post_".$_GET['post_id']."'>".$locale['447']."</a> ::\n";
+		echo "<a href='".FORUM."viewthread.php?thread_id=".$_GET['thread_id']."&amp;pid=".$_GET['post_id']."#post_".$_GET['post_id']."'>".$locale['forum_0548']."</a> ::\n";
 	} else {
 		if (!isset($_GET['thread_id']) || !isnum($_GET['thread_id'])) {
 			redirect("index.php");
 		}
 		$data = dbarray(dbquery("SELECT post_id FROM ".DB_POSTS." WHERE thread_id='".$_GET['thread_id']."' ORDER BY post_id DESC"));
 		add_to_head("<meta http-equiv='refresh' content='4; url=".FORUM."viewthread.php?thread_id=".$_GET['thread_id']."&amp;pid=".$data['post_id']."#post_".$data['post_id']."' />\n");
-		echo "<a href='".FORUM."viewthread.php?thread_id=".$_GET['thread_id']."&amp;pid=".$data['post_id']."#post_".$data['post_id']."'>".$locale['447']."</a> ::\n";
+		echo "<a href='".FORUM."viewthread.php?thread_id=".$_GET['thread_id']."&amp;pid=".$data['post_id']."#post_".$data['post_id']."'>".$locale['forum_0548']."</a> ::\n";
 	}
-	echo "<a href='".FORUM."viewforum.php?forum_id=".$_GET['forum_id']."'>".$locale['448']."</a> ::\n";
-	echo "<a href='index.php'>".$locale['449']."</a><br /><br />\n</div>\n";
+	echo "<a href='".FORUM."viewforum.php?forum_id=".$_GET['forum_id']."'>".$locale['forum_0549']."</a> ::\n";
+	echo "<a href='index.php'>".$locale['forum_0550']."</a><br /><br />\n</div>\n";
 	closetable();
 } elseif ($_GET['post'] == "edit") {
 	if (!isset($_GET['thread_id']) || !isnum($_GET['thread_id'])) {
 		redirect("index.php");
 	}
-	add_to_title($locale['global_201'].$locale['409']);
+	add_to_title($locale['global_201'].$locale['forum_0508']);
 	add_to_head("<meta http-equiv='refresh' content='2; url=".FORUM."viewthread.php?thread_id=".$_GET['thread_id']."&amp;pid=".$_GET['post_id']."#post_".$_GET['post_id']."' />\n");
-	opentable($locale['409']);
+	opentable($locale['forum_0508']);
 	echo "<div class='alert ".($errorb ? 'alert-warning' : 'alert-info')."' style='text-align:center'><br />\n";
 	if ($errorb) {
 		echo $errorb."<br /><br />\n";
 	} else {
-		echo $locale['446']."<br /><br />\n";
+		echo $locale['forum_0547']."<br /><br />\n";
 	}
-	echo "<a href='".FORUM."viewthread.php?thread_id=".$_GET['thread_id']."&amp;pid=".$_GET['post_id']."#post_".$_GET['post_id']."'>".$locale['447']."</a> ::\n";
-	echo "<a href='".FORUM."viewforum.php?forum_id=".$_GET['forum_id']."'>".$locale['448']."</a> ::\n";
-	echo "<a href='".FORUM."index.php'>".$locale['449']."</a><br /><br />\n</div>\n";
+	echo "<a href='".FORUM."viewthread.php?thread_id=".$_GET['thread_id']."&amp;pid=".$_GET['post_id']."#post_".$_GET['post_id']."'>".$locale['forum_0548']."</a> ::\n";
+	echo "<a href='".FORUM."viewforum.php?forum_id=".$_GET['forum_id']."'>".$locale['forum_0549']."</a> ::\n";
+	echo "<a href='".FORUM."index.php'>".$locale['forum_0550']."</a><br /><br />\n</div>\n";
 	closetable();
 }
 require_once THEMES."templates/footer.php";
--- themes/templates/global/forum.forms.php
+++ themes/templates/global/forum.forms.php
@@ -29,11 +29,11 @@ function postform($data) {
 
 	echo "<!--pre_postnewthread-->";
 	if ($data['edit']) {
-		opentable($locale['408']);
+		opentable($locale['forum_0507']);
 	} elseif ($data['reply']) {
-		opentable($locale['403']);
+		opentable($locale['forum_0503']);
 	} else {
-		opentable($locale['401']);
+		opentable($locale['forum_0501']);
 	}
 
 	$formaction = $settings['site_seo'] == 1 ? FUSION_ROOT : '';
@@ -48,30 +48,30 @@ function postform($data) {
 	echo openform('input_form', 'input_form', 'post', $formaction, array('enctype'=>1, 'downtime'=>0));
 	if ($data['edit'] or $data['reply'])  {
 		if ($data['reply']) {
-			echo "<h4 class='m-b-20'>".$locale['1000'].$data['thread_subject']."</h4>\n ".form_hidden('', 'thread_subject', 'thread_subject', $data['thread_subject']);
+			echo "<h4 class='m-b-20'>".$locale['forum_2000'].$data['thread_subject']."</h4>\n ".form_hidden('', 'thread_subject', 'thread_subject', $data['thread_subject']);
 		} else {
 			echo $data['first_post'] == $_GET['post_id'] ?
-				form_text($locale['460'], 'thread_subject', 'thread_subject', $data['thread_subject'], array('required' => 1, 'placeholder'=>$locale['1001'], 'error_text' => '', 'class'=>'m-t-20 m-b-20')) :
-				"<h4 class='m-b-20'>".$locale['1002'].$data['thread_subject']."</h4>\n ".form_hidden('', 'thread_subject', 'thread_subject', $data['thread_subject']);
+				form_text($locale['forum_0600'], 'thread_subject', 'thread_subject', $data['thread_subject'], array('required' => 1, 'placeholder'=>$locale['forum_2001'], 'error_text' => '', 'class'=>'m-t-20 m-b-20')) :
+				"<h4 class='m-b-20'>".$locale['forum_2002'].$data['thread_subject']."</h4>\n ".form_hidden('', 'thread_subject', 'thread_subject', $data['thread_subject']);
 		}
 	} else {
-		echo form_text($locale['460'], 'thread_subject', 'thread_subject', $data['thread_subject'], array('required' => 1, 'placeholder'=>$locale['1001'], 'error_text' => '', 'class'=>'m-t-20 m-b-20'));
+		echo form_text($locale['forum_0600'], 'thread_subject', 'thread_subject', $data['thread_subject'], array('required' => 1, 'placeholder'=>$locale['forum_2001'], 'error_text' => '', 'class'=>'m-t-20 m-b-20'));
 	}
-	echo form_textarea($locale['461'], 'post_message', 'post_message', $data['post_message'], array('required' => 1, 'error_text' => '', 'autosize'=>1, 'no_resize'=>1, 'preview'=>1, 'form_name'=>'input_form', 'bbcode' => 1));
-	echo $data['edit'] ? form_text($locale['474'], 'post_editreason', 'post_editreason', $data['post_editreason'], array('placeholder'=>'Edit reasons', 'error_text' => '', 'class'=>'m-t-20 m-b-20')) : '';
+	echo form_textarea($locale['forum_0601'], 'post_message', 'post_message', $data['post_message'], array('required' => 1, 'error_text' => '', 'autosize'=>1, 'no_resize'=>1, 'preview'=>1, 'form_name'=>'input_form', 'bbcode' => 1));
+	echo $data['edit'] ? form_text($locale['forum_0611'], 'post_editreason', 'post_editreason', $data['post_editreason'], array('placeholder'=>'Edit reasons', 'error_text' => '', 'class'=>'m-t-20 m-b-20')) : '';
 
 	// Tabs
-	$tab_title['title'][0] = $locale['463'];
+	$tab_title['title'][0] = $locale['forum_0602'];
 	$tab_title['id'][0] = 'postopts';
 	$tab_title['icon'][0] = '';
 
 	if ($info['forum_attach'] && checkgroup($info['forum_attach'])) {
-		$tab_title['title'][1] = $locale['464'];
+		$tab_title['title'][1] = $locale['forum_0557'];
 		$tab_title['id'][1] = 'attach';
 		$tab_title['icon'][1] = '';
 	}
 	if ($info['forum_poll'] && checkgroup($info['forum_poll']) && ($data['edit'] or $data['new'])) {
-		$tab_title['title'][2] = !$data['edit'] ? 'Add Poll' : $locale['468'];
+		$tab_title['title'][2] = !$data['edit'] ? 'Add Poll' : $locale['forum_0603'];
 		$tab_title['id'][2] = 'pollopts';
 		$tab_title['icon'][2] = '';
 	}
@@ -82,23 +82,23 @@ function postform($data) {
 	echo opentab($tab_title, $tab_active, 'newthreadopts');
 	echo opentabbody($tab_title['title'][0], 'postopts', $tab_active);
 	if ($data['edit']) {
-		echo form_checkbox($locale['484'], 'delete', 'delete', '', array('class'=>'m-b-0'));
+		echo form_checkbox($locale['forum_0624'], 'delete', 'delete', '', array('class'=>'m-b-0'));
 		echo "<hr class='m-t-5 m-b-10'>\n";
 	}
-	echo form_checkbox($locale['482'], 'post_smileys', 'post_smileys', $data['post_smileys'], array('class'=>'m-b-0'));
+	echo form_checkbox($locale['forum_0622'], 'post_smileys', 'post_smileys', $data['post_smileys'], array('class'=>'m-b-0'));
 	if ((iMOD || iSUPERADMIN) && !$data['reply']) {
-		echo form_checkbox($locale['480'], 'thread_sticky', 'thread_sticky', $data['thread_sticky'], array('class'=>'m-b-0'));
-		echo form_checkbox($locale['481'], 'thread_locked', 'thread_locked', $data['thread_locked'], array('class'=>'m-b-0'));
+		echo form_checkbox($locale['forum_0620'], 'thread_sticky', 'thread_sticky', $data['thread_sticky'], array('class'=>'m-b-0'));
+		echo form_checkbox($locale['forum_0621'], 'thread_locked', 'thread_locked', $data['thread_locked'], array('class'=>'m-b-0'));
 		if ($data['edit']) {
-			echo form_checkbox($locale['487'], 'hide_edit', 'hide_edit', '', array('class'=>'m-b-0'));
-			echo form_checkbox($locale['488'], 'post_locked', 'post_locked', $data['post_locked'], array('class'=>'m-b-0'));
+			echo form_checkbox($locale['forum_0627'], 'hide_edit', 'hide_edit', '', array('class'=>'m-b-0'));
+			echo form_checkbox($locale['forum_0628'], 'post_locked', 'post_locked', $data['post_locked'], array('class'=>'m-b-0'));
 		}
 	}
 	if (array_key_exists("user_sig", $userdata) && $userdata['user_sig']) {
-		echo form_checkbox($locale['483'], 'post_showsig', 'post_showsig', $data['post_showsig'], array('class'=>'m-b-0'));
+		echo form_checkbox($locale['forum_0623'], 'post_showsig', 'post_showsig', $data['post_showsig'], array('class'=>'m-b-0'));
 	}
 	if ($settings['thread_notify'] && !$data['edit']) {
-		echo form_checkbox($locale['486'], 'notify_me', 'notify_me', $data['notify_me'], array('class'=>'m-b-0'));
+		echo form_checkbox($locale['forum_0626'], 'notify_me', 'notify_me', $data['notify_me'], array('class'=>'m-b-0'));
 	}
 	echo closetabbody();
 
@@ -108,13 +108,13 @@ function postform($data) {
 		if (isset($info['attachment']) && !empty($info['attachment'])) {
 			$i = 0;
 			foreach($info['attachment'] as $attach_id => $attach_name) {
-				echo "<label><input type='checkbox' name='delete_attach_".$attach_id."' value='1' /> ".$locale['485']."</label>\n";
+				echo "<label><input type='checkbox' name='delete_attach_".$attach_id."' value='1' /> ".$locale['forum_0625']."</label>\n";
 				echo "<a href='".FORUM."attachments/".$attach_name."'>".$attach_name."</a> [".parsebytesize(filesize(FORUM."attachments/".$attach_name))."]\n";
 				echo "<br/>\n";
 				$i++;
 			}
 		}
-		echo "<div class='m-b-10'>".sprintf($locale['466'], parsebytesize($settings['attachmax']), str_replace(',', ' ', $settings['attachtypes']), $settings['attachmax_count'])."</div>\n";
+		echo "<div class='m-b-10'>".sprintf($locale['forum_0559'], parsebytesize($settings['attachmax']), str_replace(',', ' ', $settings['attachtypes']), $settings['attachmax_count'])."</div>\n";
 		echo "<input id='my_file_element' type='file' name='file_1' class='textbox' style='width:200px;' />\n";
 		echo "<div class='m-t-10' id='files_list'></div>\n";
 		echo "<script>\n";
@@ -131,17 +131,17 @@ function postform($data) {
 	if ($info['forum_poll'] && checkgroup($info['forum_poll']) && ($data['edit'] or $data['new'])) {
 		echo opentabbody($tab_title['title'][2], 'pollopts', $tab_active);
 		echo "<div class='clearfix'>\n";
-		echo form_text($locale['469'], 'forum_poll_title', 'forum_poll_title', $data['forum_poll_title'], array('max_length' => 255, 'placeholder'=>'Enter a Poll Title', 'inline'=>1));
+		echo form_text($locale['forum_0604'], 'forum_poll_title', 'forum_poll_title', $data['forum_poll_title'], array('max_length' => 255, 'placeholder'=>'Enter a Poll Title', 'inline'=>1));
 		echo form_hidden('', 'thread_poll', 'thread_poll', $data['thread_poll']);
 		$i = 1;
 		if (isset($data['poll_opts']) && !empty($data['poll_opts'])) {
 			foreach ($data['poll_opts'] as $poll_option) {
 
-				echo form_text($locale['470'].' '.$i, "poll_options[$i]", "poll_options[$i]", $poll_option, array('max_length' => 255, 'placeholder'=>'Poll Options', 'inline'=>1, 'class'=>'m-b-0'));
+				echo form_text($locale['forum_0605'].' '.$i, "poll_options[$i]", "poll_options[$i]", $poll_option, array('max_length' => 255, 'placeholder'=>'Poll Options', 'inline'=>1, 'class'=>'m-b-0'));
 				if ($data['edit']) {
 					echo "<div class='col-xs-12 col-sm-offset-3 m-t-5'>\n";
-					echo form_button($locale['472'], "update_poll_option[$i]", "update_poll_option[$i]", $locale['472'], array('class'=>'btn-xs btn-default m-r-10'));
-					echo form_button($locale['473'], "delete_poll_option[$i]", "delete_poll_option[$i]", $locale['473'],  array('class'=>'btn-xs btn-default m-r-10'));
+					echo form_button($locale['forum_0609'], "update_poll_option[$i]", "update_poll_option[$i]", $locale['forum_0609'], array('class'=>'btn-xs btn-default m-r-10'));
+					echo form_button($locale['forum_0610'], "delete_poll_option[$i]", "delete_poll_option[$i]", $locale['forum_0610'],  array('class'=>'btn-xs btn-default m-r-10'));
 					echo "</div>\n";
 				}
 				echo "<hr/>";
@@ -149,13 +149,13 @@ function postform($data) {
 				if ($i == count($data['poll_opts'])) {
 					if ($data['edit']) {
 						$i++;
-						echo form_text($locale['470'].' '.$i, "poll_options[$i]", "poll_options[$i]", '', array('max_length'=>255, 'placeholder'=>'Poll Options', 'inline'=>1, 'class'=>'m-b-0'));
+						echo form_text($locale['forum_0605'].' '.$i, "poll_options[$i]", "poll_options[$i]", '', array('max_length'=>255, 'placeholder'=>'Poll Options', 'inline'=>1, 'class'=>'m-b-0'));
 					}
 					echo "<div class='col-xs-12 col-sm-offset-3 m-b-10'>\n";
-					echo form_button($locale['471'], 'add_poll_option', 'add_poll_option', $locale['471'], array('class' => 'btn-default btn-sm m-r-10', 'icon'=>'entypo plus-circled'));
+					echo form_button($locale['forum_0608'], 'add_poll_option', 'add_poll_option', $locale['forum_0608'], array('class' => 'btn-default btn-sm m-r-10', 'icon'=>'entypo plus-circled'));
 					if ($data['edit']) {
-						echo form_button($locale['472'], 'update_poll_title', 'update_poll_title', $locale['472'], array('class'=>'btn-default btn-sm m-r-10'));
-						echo form_button($locale['473'], 'delete_poll', 'delete_poll', $locale['473'], array('class'=>'btn-default btn-sm m-r-10'));
+						echo form_button($locale['forum_0609'], 'update_poll_title', 'update_poll_title', $locale['forum_0609'], array('class'=>'btn-default btn-sm m-r-10'));
+						echo form_button($locale['forum_0610'], 'delete_poll', 'delete_poll', $locale['forum_0610'], array('class'=>'btn-default btn-sm m-r-10'));
 					}
 					echo "</div>\n";
 				}
@@ -163,10 +163,10 @@ function postform($data) {
 			}
 
 		} else {
-			echo form_text($locale['470a'], "poll_options[1]", "poll_options[1]", '', array('max_length' => 255, 'placeholder'=>'Poll Options', 'inline'=>1));
-			echo form_text($locale['470b'], "poll_options[2]", "poll_options[2]", '', array('max_length' => 255, 'placeholder'=>'Poll Options', 'inline'=>1));
+			echo form_text($locale['forum_0606'], "poll_options[1]", "poll_options[1]", '', array('max_length' => 255, 'placeholder'=>'Poll Options', 'inline'=>1));
+			echo form_text($locale['forum_0607'], "poll_options[2]", "poll_options[2]", '', array('max_length' => 255, 'placeholder'=>'Poll Options', 'inline'=>1));
 			echo "<div class='col-xs-12 col-sm-offset-3'>\n";
-			echo form_button($locale['471'], 'add_poll_option', 'add_poll_option', $locale['471'], array('class' => 'btn-default btn-sm'));
+			echo form_button($locale['forum_0608'], 'add_poll_option', 'add_poll_option', $locale['forum_0608'], array('class' => 'btn-default btn-sm'));
 			echo "</div>\n";
 		}
 		echo "</div>\n";
@@ -177,14 +177,14 @@ function postform($data) {
 	echo "<hr/>\n";
 	echo "<div class='m-b-20'>\n";
 	echo form_hidden('', 'forum_id', 'forum_id', $data['forum_id']);
-	echo form_button($data['edit'] ? $locale['405'] : $locale['400'], 'previewpost', 'previewpost', $data['edit'] ? $locale['405'] : $locale['400'], array('class' => 'btn-default btn-sm m-r-10'));
+	echo form_button($data['edit'] ? $locale['forum_0505'] : $locale['forum_0500'], 'previewpost', 'previewpost', $data['edit'] ? $locale['forum_0505'] : $locale['forum_0500'], array('class' => 'btn-default btn-sm m-r-10'));
 	echo form_button($locale['cancel'], 'cancel', 'cancel', $locale['cancel'], array('class' => 'btn-default pull-right btn-sm m-l-10'));
 	if ($data['edit']) {
-		echo form_button($locale['409'], "savechanges", "savechanges", $locale['409'], array('class'=>'btn-primary pull-right btn-sm'));
+		echo form_button($locale['forum_0508'], "savechanges", "savechanges", $locale['forum_0508'], array('class'=>'btn-primary pull-right btn-sm'));
 	} elseif ($data['reply']) {
-		echo form_button($locale['404'], 'postreply', 'postreply', $locale['404'], array('class'=>'btn-primary pull-right btn-sm'));
+		echo form_button($locale['forum_0504'], 'postreply', 'postreply', $locale['forum_0504'], array('class'=>'btn-primary pull-right btn-sm'));
 	} else {
-		echo form_button($locale['401'], 'postnewthread', 'postnewthread', $locale['401'], array('class' => 'btn-primary pull-right btn-sm'));
+		echo form_button($locale['forum_0501'], 'postnewthread', 'postnewthread', $locale['forum_0501'], array('class' => 'btn-primary pull-right btn-sm'));
 	}
 	echo "</div>\n";
 	echo closeform();
@@ -203,9 +203,9 @@ function postform($data) {
 			if (dbrows($result)) {
 				$title = "";
 				if ($settings['forum_last_posts_reply'] == "1") {
-					$title = $locale['431'];
+					$title = $locale['forum_0525'];
 				} else {
-					$title = sprintf($locale['432'], $settings['forum_last_posts_reply']);
+					$title = sprintf($locale['forum_0526'], $settings['forum_last_posts_reply']);
 				}
 				opentable($title);
 				//echo "<div style='max-height:350px;overflow:auto;'>\n";
@@ -220,9 +220,9 @@ function postform($data) {
 					echo "<tr>\n<td class='tbl2 forum_thread_user_name' style='width:10%'><!--forum_thread_user_name-->".profile_link($data['user_id'], $data['user_name'], $data['user_status'])."</td>\n";
 					echo "<td class='tbl2 forum_thread_post_date'>\n";
 					echo "<div style='float:right' class='small'>\n";
-					echo $i.($i == $settings['forum_last_posts_reply'] ? " (".$locale['431'].")" : "");
+					echo $i.($i == $settings['forum_last_posts_reply'] ? " (".$locale['forum_0525'].")" : "");
 					echo "</div>\n";
-					echo "<div class='small'>".$locale['426'].showdate("forumdate", $data['post_datestamp'])."</div>\n";
+					echo "<div class='small'>".$locale['forum_0524'].showdate("forumdate", $data['post_datestamp'])."</div>\n";
 					echo "</td>\n";
 					echo "</tr>\n<tr>\n<td valign='top' class='tbl2 forum_thread_user_info' style='width:10%'>\n";
 					echo display_avatar($data, '50px');
@@ -244,7 +244,7 @@ if (!function_exists('post_preview')) {
 	function post_preview($data) {
 		global $locale, $userdata, $can_poll;
 
-		echo openmodal('preview', $locale['400']);
+		echo openmodal('preview', $locale['forum_0500']);
 		echo "<div class='clearfix'>\n";
 		echo "<div class='pull-left m-r-10 text-center' style='width:100px'>\n".display_avatar($userdata, '50px', '', '', 'img-rounded m-0')." <br/>
 		<span class='strong display-inline-block m-t-10'>".profile_link($userdata['user_id'], $userdata['user_name'], $userdata['user_status'])."</span><br/>\n
@@ -252,7 +252,7 @@ if (!function_exists('post_preview')) {
 		</div>\n";
 		echo "<div class='overflow-hide'>\n";
 		echo "<h4 class='m-b-10'>".$data['thread_subject']."</h4>\n";
-		echo "<span>".$locale['426']." ".showdate('forumdate', time())."</span>\n<hr class='m-t-10 m-b-10'/>";
+		echo "<span>".$locale['forum_0524']." ".showdate('forumdate', time())."</span>\n<hr class='m-t-10 m-b-10'/>";
 		echo "<p>\n".$data['preview_message']."</p>\n";
 		// poll.
 
@@ -269,7 +269,7 @@ if (!function_exists('post_preview')) {
 			}
 			echo "</ul>\n";
 			echo "<hr class='m-t-10 m-b-10'/>\n";
-			echo form_button($locale['1010'], 'vote', 'vote', 'vote', array('class'=>'btn btn-sm btn-primary m-l-20 ', 'deactivate'=>1));
+			echo form_button($locale['forum_2010'], 'vote', 'vote', 'vote', array('class'=>'btn btn-sm btn-primary m-l-20 ', 'deactivate'=>1));
 			echo "</div>\n";
 			echo "</div>\n";
 		}
