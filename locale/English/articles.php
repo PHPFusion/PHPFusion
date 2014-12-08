30d0b7cca8802b5c4429f3e34fc10f371e8a1fe1
hien- <meangczac.chan@gmail.com>
2014-12-08 19:52:06 +0800

    Articles 9.00
    Full MVC export.

--- locale/English/articles.php
+++ locale/English/articles.php
@@ -3,6 +3,4 @@ $locale['400'] = "Articles";
 $locale['401'] = "No Article Categories defined";
 $locale['402'] = "NEW";
 $locale['403'] = "No Articles have been added to this Category";
-$locale['404'] = "Articles Home";
-$locale['405'] = "Articles Hierarchy";
 ?>
