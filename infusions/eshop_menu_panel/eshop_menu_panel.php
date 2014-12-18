6322971c5fe7a1e666bdfb40282d83c7bd3779ab
Domi <domi@php-fusion.co.uk>
2014-12-18 12:09:17 +0100

    Customizable JQtree menu panel with PHP function
    Customizable JQtree menu panel with PHP function

--- infusions/eshop_menu_panel/eshop_menu_panel.php
+++ infusions/eshop_menu_panel/eshop_menu_panel.php
@@ -1,11 +1,20 @@
 <?php
-/*--------------------------------------------------------------+
-| PHP-Fusion Content Management System 				|
-| Copyright © 2002 - 2008 Nick Jones 				|
-| http://www.php-fusion.co.uk/ 					|
-+---------------------------------------------------------------+
-| Author: Joakim Falk (Domi) 					|
-+--------------------------------------------------------------*/
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| http://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: eshop_menu_panel.php
+| Author: J.Falk (Domi)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
 if (!defined("IN_FUSION")) { die("Access Denied"); }
 
 // Check if a locale file is available that match the selected locale.
