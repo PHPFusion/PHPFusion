69e6b93faed7b18567b6983364ab7cececd862cc
Creatium <jankauskas.edmundas@gmail.com>
2014-12-20 20:12:44 +0200

    Update to Lithuanian locale

--- locale/Lithuanian/admin/downloads.php
+++ locale/Lithuanian/admin/downloads.php
@@ -1,75 +1,106 @@
 <?php
 // Downloads titles
-$locale['400'] = "Pridėti siuntinį";
-$locale['400b'] = "Siuntiniai";
-$locale['401'] = "Tvarkyti siuntinį";
-$locale['402'] = "Esami siuntiniai";
+$locale['download_0000'] = "Esami siuntiniai";
+$locale['download_0001'] = "Siuntiniai";
+$locale['download_0002'] = "Pridėti siuntinį";
+$locale['download_0003'] = "Tvarkyti siuntinį";
+// Download Category titles
+$locale['download_0020'] = "Current Download Categories";
+$locale['download_0021'] = "Edit Download Category";
+$locale['download_0022'] = "Add Download Category";
 // Downloads messages
-$locale['410'] = "Siuntinys pridėtas";
-$locale['411'] = "Siuntinys atnaujintas";
-$locale['412'] = "Siuntinys ištrintas";
-$locale['413'] = "Buvo rastos šios problemos:";
-$locale['414'] = "Neteisingas failo vardas.";
-$locale['415'] = "Failas turi būti mažesnis nei %s.";  
-$locale['415a'] = "Nuotrauka turi sverti mažiau nei %s.";
-$locale['415b'] = "Nuotrauka turi būti mažesnė nei %s.";	  
-$locale['416'] = "Failas turi būti vieno iš šių tipų: %s.";
-$locale['417'] = "Siuntinio pavadinimas negali būti tuščias.";	
-$locale['416a'] = "Nuotrauka turi būti vieno iš šių failų tipo: %s.";
-$locale['418'] = "Siuntinio failas ar adresas negal būti tuščias.";
-$locale['419'] = "Siuntinio trumpasis aprašymas negali būti tuščias.";
+$locale['download_0100'] = "Siuntinys pridėtas";
+$locale['download_0101'] = "Siuntinys atnaujintas";
+$locale['download_0102'] = "Siuntinys ištrintas";
+
+$locale['download_0103'] = "Buvo rastos šios problemos:";
+$locale['download_0104'] = "Neteisingas failo vardas.";
+$locale['download_0105'] = "Failas turi būti mažesnis nei %s.";
+$locale['download_0106'] = "Nuotrauka turi sverti mažiau nei %s.";
+$locale['download_0107'] = "Nuotrauka turi būti mažesnė nei %s.";
+$locale['download_0108'] = "Failas turi būti vieno iš šių tipų: %s.";
+$locale['download_0109'] = "Nuotrauka turi būti vieno iš šių failų tipo: %s.";
+$locale['download_0110'] = "Siuntinio pavadinimas negali būti tuščias.";
+$locale['download_0111'] = "Siuntinio failas ar adresas negal būti tuščias.";
+$locale['download_0112'] = "Siuntinio trumpasis aprašymas negali būti tuščias.";
+$locale['download_0113'] = "Nežinoma klaida";
 
 
-
-
-
-$locale['419a'] = "Nežinoma klaida";
+// Download Category messages
+$locale['download_0150'] = "Siuntinių kategorija pridėta";
+$locale['download_0151'] = "Siuntinių kategorija atnaujinta";
+$locale['download_0152'] = "Siuntinių kategorija negali būti atnaujinta";
+$locale['download_0153'] = "Kategorijoje yra siuntinių arba subkategorijų";
+$locale['download_0154'] = "Siuntinių kategorija ištrinta";
 // Downloads form
-$locale['420'] = "Pavadinimas:";
-$locale['421'] = "Aprašymas:";
-$locale['421b'] = "Trumpasis aprašymas:";
-$locale['421c'] = "Raktažodžiai (atskirti kableliu):";
-$locale['421d'] = "Siuntinio šaltinis (bent vienas privalomas):";
-$locale['422'] = "URL:";
-$locale['423'] = "Kategorija:";
-$locale['424'] = "Licencija:";
-$locale['425'] = "O/S:";
-$locale['426'] = "Versija:";
-$locale['427'] = "Failo dydis:";
-$locale['428'] = "Išsaugoti siuntinį";
-$locale['429'] = "Atnaujinti įdėjimo datą";
-$locale['430'] = "Atsisiųsti failą";
-$locale['430b'] = "Siuntinio nuoroda";
-$locale['431'] = "Trinti";
-$locale['432'] = "Skaičiuoti failo dydį";
-$locale['433'] = "Maks. failo dydis: %s / Leidžiami plėtiniai: %s";
-$locale['433b'] = "Maksimalus failo dydis: %s / Leidžiami failų tipai: %s / Maksimalus plotis: %spx, Maksimalus aukštis: %spx";
-$locale['434'] = "Nuotrauka:";
-$locale['435'] = "Web nuoroda:";
-$locale['436'] = "Autorinės teisės:";
-$locale['437'] = "Komentarai:";
-$locale['438'] = "Reitingai:";
-$locale['439'] = "Atšaukti";
+$locale['download_0200'] = "Pavadinimas:";
+$locale['download_0201'] = "Aprašymas:";
+$locale['download_0202'] = "Trumpasis aprašymas:";
+
+
+$locale['download_0203'] = "Raktažodžiai (atskirti kableliu):";
+$locale['download_0204'] = "Siuntinio šaltinis (bent vienas privalomas):";
+$locale['download_0205'] = "Matomumas";
+$locale['download_0206'] = "URL:";
+$locale['download_0207'] = "Kategorija:";
+$locale['download_0208'] = "Licencija:";
+$locale['download_0209'] = "O/S:";
+$locale['download_0210'] = "Versija:";
+$locale['download_0211'] = "Failo dydis:";
+
+
+$locale['download_0212'] = "Išsaugoti siuntinį";
+$locale['download_0213'] = "Atnaujinti įdėjimo datą";
+$locale['download_0214'] = "Atsisiųsti failą";
+$locale['download_0215'] = "Siuntinio nuoroda";
+$locale['download_0216'] = "Trinti";
+
+
+
+$locale['download_0217'] = "Skaičiuoti failo dydį";
+$locale['download_0218'] = "Maks. failo dydis: %s / Leidžiami plėtiniai: %s";
+$locale['download_0219'] = "Maksimalus failo dydis: %s / Leidžiami failų tipai: %s / Maksimalus plotis: %spx, Maksimalus aukštis: %spx";
+$locale['download_0220'] = "Nuotrauka:";
+$locale['download_0221'] = "Kūrėjų nuoroda:";
+$locale['download_0222'] = "Autorinės teisės:";
+$locale['download_0223'] = "Įjungti komentarus";
+$locale['download_0224'] = "Įjungti reitingus";
+$locale['download_0225'] = "Atšaukti";
 // Current Downloads
-$locale['440'] = "Siuntimas [paspausti patikrinimui]";
-$locale['441'] = "Pasirinkimas";
-$locale['442'] = "Redaguoti";
-$locale['443'] = "Trinti";
+$locale['download_0226'] = "Siuntimas [paspausti patikrinimui]";
+$locale['download_0227'] = "Nustatymai";
+
 // No Downloads/Download Categories Defined
-$locale['450'] = "Nėra siuntinių šioje kategorijoje";
-$locale['451'] = "Siuntinių kategorijos nesukurtos";
-$locale['452'] = "Jūs turite sukurti nors vieną kategoriją, kad būtų galima pridėti siuntimą.";
-
-
-
-$locale['453'] = "Spauskite čia";
-$locale['454'] = " eiti prie siuntimų kategorijų";
+$locale['download_0250'] = "Nėra siuntinių šioje kategorijoje";
+$locale['download_0251'] = "Siuntinių kategorijos nesukurtos";
+$locale['download_0252'] = "Jūs turite sukurti nors vieną kategoriją, kad būtų galima pridėti siuntimą.";
+$locale['download_0253'] = "Spauskite čia";
+$locale['download_0254'] = " eiti prie siuntimų kategorijų";
 
 // Popup Error/Warning Messages
-$locale['460'] = "Ištrinti šį siuntinį?";
-$locale['461'] = "%s yra išjungti visiems dalykams.<br />Šis nustatymas pradės veikti, jeigu tai bus įjungta.";
+$locale['download_0255'] = "Ištrinti šį siuntinį?";
+$locale['download_0256'] = "%s yra išjungti visiems dalykams.<br />Šis nustatymas pradės veikti, jeigu tai bus įjungta.";
 
-$locale['462'] = "Komentarai";
-$locale['463'] = "Reitingai";
-$locale['464'] = $locale['462']." ir ".$locale['463'];
+// Download Category form
+$locale['download_0300'] = "Kategorijos pavadinimas:";
+$locale['download_0301'] = "Kategorijos aprašymas:";
+$locale['download_0302'] = "Kategorijos eilė:";
+$locale['download_0303'] = "Siuntinio ID";
+$locale['download_0304'] = "Siuntinio pavadinimas";
+
+$locale['download_0305'] = "Siuntinio data";
+$locale['download_0306'] = "Didėjančia tvarka";
+$locale['download_0307'] = "Mažėjančia tvarka";
+$locale['download_0308'] = "Kategorija priklauso:";
+$locale['download_0309'] = "Išsaugot kategoriją";
+// Current Download Categories
+$locale['download_0310'] = "Kategorija";
+$locale['download_0311'] = "Prieiga";
+$locale['download_0312'] = "Nustatymai";
+$locale['download_0313'] = "Šioje kategorijoje nėra siuntinių";
+// Delete Download category
+$locale['download_0350'] = "Ištrinti šią siuntinių kategoriją?";
+// Error Messages
+$locale['download_0351'] = "Prašom įvesti kategorijos pavadinimą.";
+$locale['download_0352'] = "Tokia kategorija jau yra sukurta.";
 ?>
--- locale/Lithuanian/admin/news.php
+++ locale/Lithuanian/admin/news.php
@@ -1,55 +1,79 @@
 <?php
 // News titles
-$locale['400'] = "Esamos naujienos";
-$locale['401'] = "Pridėti naujieną";
-$locale['402'] = "Redagiuoti naujieną";
+$locale['news_0000'] = "Esamos naujienos";
+$locale['news_0001'] = "Naujiena";
+$locale['news_0002'] = "Pridėti naujieną";
+$locale['news_0003'] = "Redagiuoti naujieną";
+// News Categories titles
+$locale['news_0020'] = "Esamos naujienų kategorijos";
+$locale['news_0021'] = "Redaguoti naujienų kategoriją";
+$locale['news_0022'] = "Pridėti naujienų kategoriją";
+
 // News messages
-$locale['410'] = "Naujiena pridėta";
-$locale['411'] = "Naujiena atnaujinta";
-$locale['412'] = "Naujiena ištrinta";
-$locale['413'] = "Blogas paveikslėlio failo vardas.";
-$locale['414'] = "Paveikslėlis turi būti mažesnis nei %s.";
-$locale['415'] = "Paveikslėlis turi būti GIF, JPEG arba PNG plėtinio.";
-$locale['416'] = "Paveikslėlis neturi būti didesnis negu %s pikselių.";
-$locale['417'] = "Nežinoma klaida";
+$locale['news_0100'] = "Naujiena pridėta";
+$locale['news_0101'] = "Naujiena atnaujinta";
+$locale['news_0102'] = "Naujiena ištrinta";
+$locale['news_0103'] = "Blogas paveikslėlio failo vardas.";
+
+$locale['news_0104'] = "Paveikslėlis turi būti mažesnis nei %s.";
+$locale['news_0105'] = "Paveikslėlis turi būti vieno iš šių plėtinių: %s.";
+$locale['news_0106'] = "Paveikslėlis neturi būti didesnis negu %s pikselių.";
+$locale['news_0107'] = "Nežinoma klaida";
+// News Categories messages
+$locale['news_0150'] = "Naujienų kategorija pridėta";
+$locale['news_0151'] = "Naujienų kategorija atnaujinta";
+$locale['news_0152'] = "Naujienų kategorija negali būti ištrinta";
+$locale['news_0153'] = "Kategorijoje yra naujienų arba subkategorijų";
+$locale['news_0154'] = "Naujienų kategorija ištrinta";
 
 // News form
-$locale['420'] = "Redaguoti";
-$locale['421'] = "Ištrinti";
-$locale['422'] = "Pavadinimas:";
-$locale['423'] = "Kategorija:";
-$locale['424'] = "- Nė viena -";
-$locale['425'] = "Naujiena:";
-$locale['426'] = "Naujienos tesinys:";
-$locale['427'] = "Pradžios data:";
-$locale['428'] = "Pabaigos data:";
-$locale['429'] = "(Nebūtina)";
-$locale['430'] = "Gali matyti:";
-$locale['431'] = "išsaugoti kaip ruošinį";
-$locale['432'] = "Padaryti šią naujieną svarbia";
-$locale['433'] = "Automatinis eilučių laužymas";
-$locale['434'] = "Leisti komentarus";
-$locale['435'] = "Leisti reitingus";
-$locale['436'] = "Peržiūrėti naujieną";
-$locale['437'] = "Išsaugoti naujieną";
-$locale['438'] = "[RUOŠINYS]";
-$locale['439'] = "Naujienų paveiksliukas";
-$locale['440'] = "Maks. failo dydis: %s / Leidžiami plėtiniai: GIF, JPEG or PNG.";
-$locale['441'] = "Puslapio perskyrimas";
-$locale['442'] = "Pasirinkti naujienų paveiksliuko lygiavimą";
-$locale['443'] = "Raktažodžiai (atskirti kableliu)";
+$locale['news_0200'] = "Pavadinimas";
+$locale['news_0201'] = "Kategorija";
+$locale['news_0202'] = "Be kategorijos";
+$locale['news_0203'] = "Naujiena";
+
+$locale['news_0204'] = "Naujienos tesinys";
+$locale['news_0205'] = "Raktažodžiai (atskirti kableliu)";
+$locale['news_0206'] = "Pradžios data";
+
+$locale['news_0207'] = "Pabaigos data";
+$locale['news_0208'] = "(Pasirinktinai)";
+$locale['news_0209'] = "Gali matyti";
+$locale['news_0210'] = "Išsaugoti kaip ruošinį";
+$locale['news_0211'] = "Padaryti šią naujieną svarbia";
+$locale['news_0212'] = "Automatinis eilučių laužymas";
+$locale['news_0213'] = "Leisti komentarus";
+$locale['news_0214'] = "Leisti reitingus";
+$locale['news_0215'] = "Ruošinys";
+$locale['news_0216'] = "Naujienų paveiksliukas";
+$locale['news_0217'] = "Maks. failo dydis: %s / Leidžiami plėtiniai: GIF, JPEG or PNG.";
+$locale['news_0218'] = "Pasirinkti naujienų paveiksliuko lygiavimą";
+$locale['news_0219'] = "Puslapio perskyrimas";
+$locale['news_0240'] = "Peržiūrėti naujieną";
+$locale['news_0241'] = "Išsaugoti naujieną";
 // Popup Error/Warning Messages
-$locale['450'] = "Prašome įrašyti naujienos pavadinimą";
-$locale['451'] = "Ištrinti šią naujieną?";
-$locale['451b'] = "Ištrinti šią naujienų kategoriją?";
-$locale['452'] = "%s yra išjungti visiems dalykams.<br />Šis nustatymas pradės veiks, jeigu tai bus įjungta.";
-$locale['453'] = "Komentarai";
-$locale['454'] = "Reitingai";
-$locale['455'] = $locale['453']." ir ".$locale['454'];
-$locale['456'] = 'Šioje kategorijoje nėra naujienų.';
-$locale['457'] = "Prašom nurodyti raktažodžius";
-//Alignment
-$locale['left'] = "Kairė";
-$locale['center'] = "Centras";
-$locale['right'] = "Dešinė";
+$locale['news_0250'] = "Prašome įrašyti naujienos pavadinimą";
+$locale['news_0251'] = "Ištrinti šią naujieną?";
+$locale['news_0252'] = "Ištrinti šią naujienų kategoriją?";
+$locale['news_0253'] = "%s yra išjungti visiems dalykams.<br />Šis nustatymas pradės veiks, jeigu tai bus įjungta.";
+$locale['news_0254'] = 'Šioje kategorijoje nėra naujienų.';
+$locale['news_0255'] = "Prašom nurodyti raktažodžius";
+// News Categories form
+
+
+$locale['news_0300'] = "Kategorijos pavadinimas:";
+$locale['news_0301'] = "Kategorijos paveiksliukas:";
+
+$locale['news_0302'] = "Išsaugoti kategoriją";
+$locale['news_0303'] = "Nėra nustatytų naujienų kategorijų";
+$locale['news_0304'] = "Spauskite čia, kad atsisiųsti kategorijų paveiksliukus";
+$locale['news_0305'] = "Priklauso";
+
+
+// Popup Error/Warning Messages
+$locale['news_0350'] = "Ištrinti šią naujienų kategoriją?";
+// Error Messages
+$locale['news_0351'] = "Prašom įvesti kategorijos pavadinimą.";
+$locale['news_0352'] = "Tokia kategorija jau yra sukurta.";
+
 ?>
