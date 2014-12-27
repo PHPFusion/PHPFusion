c965bc309fb0dd7e657fb52aa982bf56606b4b2b
hien- <meangczac.chan@gmail.com>
2014-12-27 23:10:17 +0800

    Malay Locale update

--- locale/Malay/forum.php
+++ locale/Malay/forum.php
@@ -9,7 +9,7 @@ $locale['forum_0005'] = "Tiada Pos Balasan";
 $locale['forum_0006'] = "Dimulakan oleh ";
 $locale['forum_0007'] = "Diuruskan oleh: ";
 $locale['forum_0008'] = "Direktori";
-$locale['forum_0009'] = "Paparkan pos dari sebelumnya";
+$locale['forum_0009'] = "Paparkan Pos dari sebelumnya";
 $locale['forum_0010'] = "Halaman Utama Forum Perbincangan";
 // View Forum
 $locale['forum_0050'] = "Kunjungan Forum";
@@ -20,8 +20,8 @@ $locale['forum_0054'] = "Balasan";
 $locale['forum_0055'] = "Halaman: ";
 $locale['forum_0056'] = "Tiada Utusan Perbincangan dimulakan";
 // View Forum Moderator options
-$locale['forum_0080'] = "Pilih Semua";
-$locale['forum_0081'] = "Bubuh Semua";
+$locale['forum_0080'] = "Pilih Semua Pilihan";
+$locale['forum_0081'] = "Gugur Semua Pilihan";
 $locale['forum_0082'] = "Padamkan Utusan";
 $locale['forum_0083'] = "Padamkan utusan yang dipilih?";
 // Forum Guide/Privelages
@@ -57,7 +57,7 @@ $locale['forum_0173'] = "Pratonton Balasan";
 $locale['forum_0174'] = "Berhenti mengikut perbincangan ini";
 $locale['forum_0175'] = "Ikut perbincangan ini";
 $locale['forum_0176'] = "Pindah Pos";
-$locale['forum_0177'] = "Padam pos yang dipilih?";
+$locale['forum_0177'] = "Padam Pos yang dipilih?";
 $locale['forum_0178'] = "Cetak Utusan Perbincangan";
 $locale['forum_0179'] = "Cetak Pos Balasan";
 // Moderator Options
@@ -110,13 +110,16 @@ $locale['forum_0300'] = "Pindah Pos";
 $locale['forum_0301'] = "Sila pilih forum baru";
 $locale['forum_0302'] = "Seterusnya";
 $locale['forum_0303'] = "Silah pilih utusan baru";
-$locale['forum_0304'] = "Pindah pos";
-$locale['forum_0305'] = "Anda hendak pindah pos pertama dalam utusan perbincangan ini!<br />Pos pertama <strong>tidak akan dipindah atau akan dipadamkan</strong> dalam perbincangan ini kerana masih terdapat balasan dalam utusan ini.";
-$locale['forum_0306'] = "Anda hendak pindah pos pertama dalam utusan perbincangan ini!<br />Pos pertama <strong>akan dipindah </strong> dan utusan perbincangan ini akan dipadamkan.";
-$locale['forum_0307'] = "Tiada pos untuk dipindah.";
+$locale['forum_0304'] = "Pindah Pos";
+$locale['forum_0305'] = "Anda hendak pindah Pos pertama dalam utusan perbincangan ini!<br />Pos pertama <strong>tidak akan dipindah atau akan dipadamkan</strong> dalam perbincangan ini kerana masih terdapat balasan dalam utusan ini.";
+$locale['forum_0306'] = "Anda hendak pindah Pos pertama dalam utusan perbincangan ini!<br />Pos pertama <strong>akan dipindah </strong> dan utusan perbincangan ini akan dipadamkan.";
+$locale['forum_0307'] = "Tiada Pos untuk dipindah.";
 $locale['forum_0308'] = "Tiada utusan dalam Forum ini";
-$locale['forum_0309'] = "Kembali ke Utusan Forum.";
-$locale['forum_0310'] = "Tiada Utusan lain untuk pindah pos ini.";
+$locale['forum_0309'] = $locale['forum_0240'];
+
+
+
+$locale['forum_0310'] = "Tiada Utusan lain untuk pindah Pos ini.";
 // Viewforum icons
 $locale['forum_0311'] = "Utusan Panas";
 $locale['forum_0312'] = "Fail dilampirkan";
@@ -133,7 +136,7 @@ $locale['forum_0328'] = 'Tiada forum disenaraikan.';
 $locale['forum_0340'] = 'Soalan';
 $locale['forum_0341'] = 'Tajuk';
 
-$locale['forum_0350'] = " Peraturan Forum:";
+$locale['forum_0350'] = "Peraturan Forum:";
 $locale['forum_0351'] = "Subforums";
 $locale['forum_0352'] = "Pengumuman";
 
@@ -169,56 +172,53 @@ $locale['forum_0391'] = 'Masa Pos';
 $locale['forum_0395'] = 'Pilih Forum';
 $locale['forum_0396'] = 'Pastikan Pilihan Forum';
 $locale['forum_0397'] = "Tiada forum disenaraikan.";
-// phew...
-// Post Titles
-$locale['forum_0500'] = "Preview Thread";
-$locale['forum_0501'] = "Post Thread";
-$locale['forum_0502'] = "Preview Reply";
-$locale['forum_0503'] = "Reply to Thread";
-$locale['forum_0504'] = "Post Reply";
-$locale['forum_0505'] = "Preview Changes";
-$locale['forum_0506'] = "Delete Post";
-$locale['forum_0507'] = "Edit Post";
-$locale['forum_0508'] = "Save Changes";
-// Post Preview
-$locale['forum_0520'] = "No Message, Post will be rejected if you do not include a Message";
-$locale['forum_0521'] = "Posts:";
-$locale['forum_0522'] = "Location:";
-$locale['forum_0523'] = "Joined:";
-$locale['forum_0524'] = "Posted on ";
-$locale['forum_0525'] = "Latest post";
-$locale['forum_0526'] = "Latest %s posts";
+$locale['forum_0500'] = "Pratonton Utusan";
+$locale['forum_0501'] = "Pos Utusan";
+$locale['forum_0502'] = "Pratonton Mesej Balasan";
+$locale['forum_0503'] = "Balas kepada Utusan";
+$locale['forum_0504'] = "Pos Balasan";
+$locale['forum_0505'] = "Pratonton Mesej Penyuntingan";
+$locale['forum_0506'] = "Padam Pos";
+$locale['forum_0507'] = "Sunting Pos";
+$locale['forum_0508'] = "Rekod Penyuntingan Balasan";
+$locale['forum_0520'] = "Tiada Mesej. Post akan digagalkan jika tiada isi kandungan dalam Mesej.";
+$locale['forum_0521'] = "Pos:";
+$locale['forum_0522'] = "Lokasi:";
+$locale['forum_0523'] = "Daftar Sejak:";
+$locale['forum_0524'] = "Diposkan pada ";
+$locale['forum_0525'] = "Pos Terkini";
+$locale['forum_0526'] = "Pos %s terkini";
 // Post Error/Success
-$locale['forum_0540'] = "Attachment file type not allowed.";
-$locale['forum_0541'] = "Invalid attachment filename or filesize.";
-$locale['forum_0542'] = "Error: You did not specify a Subject and/or Message";
-$locale['forum_0543'] = "Your Thread has been Posted";
-$locale['forum_0544'] = "Your Reply has been Posted";
-$locale['forum_0545'] = "The Thread has been deleted";
-$locale['forum_0546'] = "The Post has been deleted";
-$locale['forum_0547'] = "Your Post has been updated";
-$locale['forum_0548'] = "Return to Thread";
-$locale['forum_0549'] = "Return to Forum";
-$locale['forum_0550'] = "Return to Forum Index";
-$locale['forum_0551'] = "Error: Your cookie session has expired, please login and repost";
-$locale['forum_0552'] = "Track Thread";
-$locale['forum_0553'] = "You are now tracking this thread";
-$locale['forum_0554'] = "You are no longer tracking this thread";
-$locale['forum_0555'] = "This post is locked. Contact the moderator for further information.";
-$locale['forum_0556'] = "You may only edit a post for %d minute(s) after initial submission.";
+$locale['forum_0540'] = "Lampiran tidak dibenarkan.";
+$locale['forum_0541'] = "Nama fail atau saiz fail lampiran tidak layak.";
+$locale['forum_0542'] = "Kesalahan: Anda tidak mengisi butir Tajuk Topik dan/atau Mesej.";
+$locale['forum_0543'] = "Utusan anda telah diposkan";
+$locale['forum_0544'] = "Balasan anda telah diposkan";
+$locale['forum_0545'] = "Utusan anda telah dipadamkan";
+$locale['forum_0546'] = "Pos anda telah dipadamkan";
+$locale['forum_0547'] = "Pos anda telah dikemaskinikan";
+$locale['forum_0548'] = "Kembali ke Utusan Perbincangan";
+$locale['forum_0549'] = $locale['forum_0240'];
+$locale['forum_0550'] = "Kembali ke Halaman Utama Forum";
+$locale['forum_0551'] = "Kesalahan: Cookie pelayar anda telah luput. Sila log masuk dan buat Pos sekali lagi.";
+$locale['forum_0552'] = "Ikut Utusan Perbincangan";
+$locale['forum_0553'] = "Anda telah mengikut utusan perbincangan ini";
+$locale['forum_0554'] = "Anda telah tidak mengikut utusan perbincangan ini";
+$locale['forum_0555'] = "Pos ini dikunci. Sila hubungi pengurus forum ini untuk keterangan lanjut.";
+$locale['forum_0556'] = "Anda hanya dibenarkan untuk sunting post anda dalam %d minit selepas masa Pos.";
 
-$locale['forum_0557'] = "Attachment";
+$locale['forum_0557'] = "Lampiran";
 $locale['forum_0558'] = " (Optional)";
-$locale['forum_0559'] = "Max. filesize: %s / Allowed filetypes: %s / Max. number of attachments: %s";
+$locale['forum_0559'] = "Max. Size Fail: %s / Lampiran jenis fail: %s / Max. Lampiran dibenarkan: %s";
 
-$locale['forum_0570'] = "Photo Max size exceeded. You can only upload up to ";
-$locale['forum_0571'] = 'Image not uploaded';
-$locale['forum_0572'] = " Posts deleted";
-$locale['forum_0573'] = " Threads deleted";
-$locale['forum_0574'] = " Attachments deleted";
-$locale['forum_0575'] = " Polls deleted";
-$locale['forum_0576'] = " Polls Votes deleted";
-$locale['forum_0577'] = " Polls Options deleted";
+$locale['forum_0570'] = "Fail gambar saiz maksimum terlanjur. Anda hanya dibenarkan untuk muat naik sebanyak ";
+$locale['forum_0571'] = 'Gambar tidak di muat naik';
+$locale['forum_0572'] = " Pos dipadamkan";
+$locale['forum_0573'] = " Utusan dipadamkan";
+$locale['forum_0574'] = " Lampiran digugurkan";
+$locale['forum_0575'] = " Undian digugurkan";
+$locale['forum_0576'] = " Undi dipadam";
+$locale['forum_0577'] = " Pengaturan Undian digugurkan";
 $locale['forum_0580'] = " Wipe records for Developer Use ";
 $locale['forum_0581'] = "Current Mode is --- ";
 $locale['forum_0582'] = "EDIT MODE";
@@ -231,91 +231,82 @@ $locale['forum_0593'] = 'Flush Attachments';
 $locale['forum_0595'] = 'Flush Poll';
 
 // Post Form
-$locale['forum_0600'] = "Subject";
-$locale['forum_0601'] = "Message";
-$locale['forum_0602'] = "Options";
-$locale['forum_0603'] = "Edit Poll";
-$locale['forum_0604'] = "Poll Title";
-$locale['forum_0605'] = "Poll Option";
+$locale['forum_0600'] = "Tajuk Topik";
+$locale['forum_0601'] = "Mesej";
+$locale['forum_0602'] = "Pengaturan";
+$locale['forum_0603'] = "Sunting Undian";
+$locale['forum_0604'] = "Sunting Tajuk Undian";
+$locale['forum_0605'] = "Tukar Pengaturan Undian";
 $locale['forum_0606'] = $locale['forum_0605']." 1";
 $locale['forum_0607'] = $locale['forum_0605']." 2";
-$locale['forum_0608'] = "Add Option";
-$locale['forum_0609'] = "Update";
-$locale['forum_0610'] = "Delete";
-$locale['forum_0611'] = "Edit Reason";
+$locale['forum_0608'] = "Tambah Pengaturan";
+$locale['forum_0609'] = "Kemaskini";
+$locale['forum_0610'] = "Padam";
+$locale['forum_0611'] = "Sebab Disunting";
 // Post Form Options
-$locale['forum_0620'] = "Make this Thread Sticky";
-$locale['forum_0621'] = "Lock this Thread";
-$locale['forum_0622'] = "Disable Smileys in this Post";
-$locale['forum_0623'] = "Show My Signature in this Post";
-$locale['forum_0624'] = "Delete this Post";
-$locale['forum_0625'] = "Delete attachment -";
-$locale['forum_0626'] = "Notify me when a reply is posted";
-$locale['forum_0627'] = "Hide Edit";
-$locale['forum_0628'] = "Lock Post";
-
+$locale['forum_0620'] = "Lekitkan Utusan ini";
+$locale['forum_0621'] = "Kunci Utusan ini";
+$locale['forum_0622'] = "Jangan mengguna Smileys di Pos ini";
+$locale['forum_0623'] = "Paparkan Tandatangan saya di Pos ini";
+$locale['forum_0624'] = "Padam Pos ini";
+$locale['forum_0625'] = "Gugurkan Lampiran -";
+$locale['forum_0626'] = $locale['forum_0171'];
+$locale['forum_0627'] = "Sembunyi Sunting";
+$locale['forum_0628'] = "Kunci Pos";
 // Forum Post Merger
-$locale['forum_0640'] = "Merged on";
-
+$locale['forum_0640'] = "Digabung pada";
 // Search Forum Form
-
 $locale['forum_0650'] = 'Flood control nice message.';
-
 // Forum Notification Email
-$locale['forum_0660'] = "Thread Reply Notification - {THREAD_SUBJECT}";
-$locale['forum_0661'] = "Hello {USERNAME},
-
-A reply has been posted in the forum thread '{THREAD_SUBJECT}' which you are tracking at ".$settings['sitename'].". You can use the following link to view the reply:
-
+$locale['forum_0660'] = "Makluman Balasan Pos Utusan Perbincangan - {THREAD_SUBJECT}";
+$locale['forum_0661'] = "Salam Sejahtera {USERNAME},
+Anda dihubungi kerana ada pos balasan di forum perbincangan '{THREAD_SUBJECT}' yang diikuti oleh anda di ".$settings['sitename'].". Anda boleh mengguna pautan berikut untuk mengunjungi balasan tersebut di:
 {THREAD_URL}
-
-If you no longer wish to watch this thread you can click the 'Stop tracking this thread' link located at the top of the thread.
-
-Regards,
+Jika anda tidak ingin lagi mengikuti utusan perbincangan ini, sila klik pada `".$locale['forum_0174']."` di atas halaman Utusan ini.
+Kirim Salam,
 ".$settings['siteusername'].".";
 
 // Delete Thread
-$locale['forum_0700'] = "Delete Thread";
-$locale['forum_0701'] = "The Thread has been deleted.";
-$locale['forum_0702'] = "Return to Forum";
-$locale['forum_0703'] = "Return to Forum Index";
-$locale['forum_0704'] = "Are you sure you want to delete this Thread?";
+$locale['forum_0700'] = "Padam Utusan";
+$locale['forum_0701'] = "Utusan ini telah dipadamkan.";
+$locale['forum_0702'] = $locale['forum_0240'];
+$locale['forum_0703'] = $locale['forum_0550'];
+$locale['forum_0704'] = 'Adakah anda hendak padamkan Utusan perbincangan ini?';
 // Lock Thread
-$locale['forum_0710'] = "Lock Thread";
-$locale['forum_0711'] = "The Thread has been locked.";
+$locale['forum_0710'] = "Kunci Utusan";
+$locale['forum_0711'] = "Utusan telah dikuncikan.";
 // Unlock Thread
-$locale['forum_0720'] = "Unlock Thread";
-$locale['forum_0721'] = "The Thread has been unlocked.";
+$locale['forum_0720'] = "Buka Utusan";
+$locale['forum_0721'] = "Utusan telah dibukakan.";
 // Make Thread Sticky
-$locale['forum_0730'] = "Make Thread Sticky";
-$locale['forum_0731'] = "The Thread has been made sticky.";
+$locale['forum_0730'] = "Lekitkan Utusan ini";
+$locale['forum_0731'] = "Utusan ini telah dilekitkan.";
 // Make Thread Non-Sticky
-$locale['forum_0740'] = "Make Thread Non-Sticky";
-$locale['forum_0741'] = "The Thread has been made non-sticky.";
+$locale['forum_0740'] = $locale['forum_0205'];
+$locale['forum_0741'] = 'Utusan ini telah dilepaskan dari melekit.';
 // Move Thread
-$locale['forum_0750'] = "Move Thread";
-$locale['forum_0751'] = "Move Thread to:";
-$locale['forum_0752'] = "The Thread has been moved successfully.";
+$locale['forum_0750'] = 'Pindah Utusan';
+$locale['forum_0751'] = "Pindah Utusan ke:";
+$locale['forum_0752'] = "Pindahan Utusan berjaya.";
 // Renew Thread
-$locale['forum_0758'] = "Renew Thread";
-$locale['forum_0759'] = "The Thread has been renewed.";
-
+$locale['forum_0758'] = $locale['forum_0207'];
+$locale['forum_0759'] = 'Utusan ini telah diperbaharui';
 // forum.forms.php
-$locale['forum_2000'] = "Topic: ";
-$locale['forum_2001'] = 'What is on your mind?';
-$locale['forum_2002'] = "Re: ";
-$locale['forum_2010'] = 'Cast Vote';
+$locale['forum_2000'] = "Tajuk Topik: ";
+$locale['forum_2001'] = 'Apakah yang mahu disoalkan?';
+$locale['forum_2002'] = "Tajuk: ";
+$locale['forum_2010'] = 'Undi';
 
 // Period for search & filter
-$locale['forum_p000'] = 'Today';
-$locale['forum_p001'] = "1 Day";
-$locale['forum_p002'] = '2 Days';
-$locale['forum_p007'] = '1 Week';
-$locale['forum_p014'] = '2 Weeks';
-$locale['forum_p030'] = '1 Month';
-$locale['forum_p060'] = '2 Months';
-$locale['forum_p090'] = '3 Months';
-$locale['forum_p180'] = '6 Months';
-$locale['forum_p365'] = '1 Year';
-$locale['forum_p999'] = "All Results";
+$locale['forum_p000'] = 'Hari Ini';
+$locale['forum_p001'] = "1 Hari";
+$locale['forum_p002'] = '2 Hari';
+$locale['forum_p007'] = '1 Minggu';
+$locale['forum_p014'] = '2 Minggu';
+$locale['forum_p030'] = '1 Bulan';
+$locale['forum_p060'] = '2 Bulan';
+$locale['forum_p090'] = '3 Bulan';
+$locale['forum_p180'] = '6 Bulan';
+$locale['forum_p365'] = '1 Tahun';
+$locale['forum_p999'] = "Semua";
 ?>
