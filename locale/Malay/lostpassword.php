7abd07838802d4924c456ac7372ed5f528adb828
hien- <meangczac.chan@gmail.com>
2014-12-27 23:24:35 +0800

    Malay locale update

--- locale/Malay/lostpassword.php
+++ locale/Malay/lostpassword.php
@@ -1,25 +1,23 @@
 <?php
-$locale['400'] = "Lost Password";
-$locale['401'] = "A confirmation email has been sent to your email address.";
-$locale['402'] = "Your new password has been sent to your email address.";
-$locale['403'] = "Return to Main Site";
-$locale['404'] = "The email address you specified could not be found.";
-$locale['405'] = "The email address you specified is invalid.";
-$locale['406'] = "Please try again.";
-$locale['407'] = "Please enter the email address your account is registered with below.<br />
-A new password will be created and sent to this address.";
-$locale['408'] = "Send Password";
-$locale['409'] = "New password for ";
-$locale['410'] = "Hello [USER_NAME],\n
-You have or someone has requested a new password to access your ".$settings['sitename']." account.\n
-To change your password please click the following link:\n
+$locale['400'] = "Lupa kata aluan";
+$locale['401'] = "Emel pengesahan telah dihantar ke alamat emel anda.";
+$locale['402'] = "Kata Aluan telah dihantar ke alamat emel anda.";
+$locale['403'] = "Kembali ke Laman Utama";
+$locale['404'] = "Alamat emel anda tidak dalam senarai.";
+$locale['405'] = "Alamat emel anda tidak sah.";
+$locale['406'] = "Sila ulangi lagi.";
+$locale['407'] = 'Sila isikan alamat emel yang didaftarkan kepada akaun anda di bawah.<br />Kami akan menghantar kata aluan baru kepada alamat emel anda.';
+$locale['408'] = "Kirimkan Kata Aluan Baru";
+$locale['409'] = "Kata Aluan baru kepada ";
+$locale['410'] = "Salam Sejahtera kepada [USER_NAME],\nAnda atau sesiapa telah meminta kata aluan baru untuk akaun anda di ".$settings['sitename']."\n
+Untuk menukarkan kata aluan, anda perlu klik di pautan web berikut:\n
 [NEW_PASS_LINK]\n
-Regards,
+Kiriman Salam,
 ".$settings['siteusername'];
-$locale['411'] = "Hello [USER_NAME],\n
-Your new password to access your ".$settings['sitename']." account is:\n
+$locale['411'] = "Salam Sejahtera kepada [USER_NAME],\n
+Kata alauan anda untuk log masuk ke akaun anda di ".$settings['sitename']." ialah:\n
 [NEW_PASS]\n
-Regards,
+Daripada,
 ".$settings['siteusername'];
-$locale['412'] = "There was an error, please try again.";
+$locale['412'] = "Kesalahan didapati, sila ulangi lagi.";
 ?>
--- locale/Malay/members.php
+++ locale/Malay/members.php
@@ -1,12 +1,12 @@
 <?php
-$locale['400'] = "Members List";
-$locale['401'] = "User Name";
-$locale['402'] = "User Type";
-$locale['403'] = "There are no user names beginning with ";
-$locale['404'] = "Show All";
-$locale['405'] = "User groups";
-$locale['406'] = "Not registered to any group";
-$locale['407'] = "Access to all groups";
-$locale['408'] = "Search for member:";
-$locale['409'] = "Search";
+$locale['400'] = "Senarai Ahli";
+$locale['401'] = "Nama User";
+$locale['402'] = "Jenis User";
+$locale['403'] = "Tiada nama user yang bermulai dengan ";
+$locale['404'] = "Tampilkan Semua";
+$locale['405'] = "Kumpulan User";
+$locale['406'] = "Tidak didaftarkan kepada apa-apa kumpulan User";
+$locale['407'] = "Akses ke Kumpulan User";
+$locale['408'] = "Semakan Keahlian:";
+$locale['409'] = "Cari";
 ?>
