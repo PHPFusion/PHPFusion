8ee1e85af2df115e529176c4d4299d4506a43041
Takács Ákos <rimelek@rimelek.hu>
2015-01-06 13:47:06 +0100

    PHPDoc comments in OutputHandler

--- includes/output_handling_include.php
+++ includes/output_handling_include.php
@@ -18,27 +18,89 @@
 namespace PHPFusion {
 	//TODO: PHPDoc comments
 	class OutputHandler {
+		/**
+		 * Associative array of meta tags
+		 *
+		 * @var string[]
+		 */
 		private static $pageMeta = array();
+		
+		/**
+		 * The title in the "title" tag
+		 *
+		 * @var string
+		 */
 		private static $pageTitle = "";
+		
+		/**
+		 * PHP code to execute using eval and regexps to replace anything in the output
+		 *
+		 * @var string
+		 */
 		private static $pageReplacements = "";
+		
+		/**
+		 * PHP code to execute using eval replace anything in the output
+		 *
+		 * @var string
+		 */
 		private static $outputHandlers = "";
 		
+		/**
+		 * Additional tags to the html head
+		 *
+		 * @var string 
+		 */
 		public static $pageHeadTags = "";
+		
+		/**
+		 * Additional contents to the footer
+		 *
+		 * @var string
+		 */
 		public static $pageFooterTags = "";
+		
+		/**
+		 * Additional javascripts
+		 *
+		 * @var string
+		 */
 		public static $jqueryTags = "";
 		
+		/**
+		 * Set the new title of the page
+		 * 
+		 * @param string $title
+		 */
 		public static function setTitle($title = "") {
 			self::$pageTitle = $title;
 		}
 		
+		/**
+		 * Append something to the title of the page
+		 * 
+		 * @param type $addition
+		 */
 		public static function addToTitle($addition = "") {
 			self::$pageTitle .= preg_replace("/".$GLOBALS['locale']['global_200']."/", '', $addition, 1);
 		}
 		
+		/**
+		 * Set a meta tag by name
+		 * 
+		 * @param string $name
+		 * @param string $content
+		 */
 		public static function setMeta($name, $content = "") {
 			self::$pageMeta[$name] = $content;
 		}
 		
+		/**
+		 * Append something to a meta tag
+		 * 
+		 * @param string $name
+		 * @param string $addition
+		 */
 		public static function addToMeta($name, $addition = "") {
 			$settings = \fusion_get_settings();
 			if (empty(self::$pageMeta)) {
@@ -52,28 +114,55 @@ namespace PHPFusion {
 			}
 		}
 		
+		/**
+		 * Add content to the html head
+		 * 
+		 * @param string $tag
+		 */
 		public static function addToHead($tag = "") {
 			if (!\stristr(self::$pageHeadTags, $tag)) {
 				self::$pageHeadTags .= $tag."\n";
 			}
 		}
 		
+		/**
+		 * Add content to the footer
+		 * 
+		 * @param string $tag
+		 */
 		public static function addToFooter($tag = "") {
 			if (!stristr(self::$pageFooterTags, $tag)) {
 				self::$pageFooterTags .= $tag."\n";
 			}
 		}
 		
+		/**
+		 * Replace something in the output using regexp
+		 * 
+		 * @param string $target Regexp pattern without delimiters
+		 * @param string $replace The new content
+		 * @param string $modifiers Regexp modifiers
+		 */
 		public static function replaceInOutput($target, $replace, $modifiers = "") {
 			self::$pageReplacements .= "\$output = preg_replace('^$target^$modifiers', '$replace', \$output);";
 		}
 		
+		/**
+		 * Add a new output handler function
+		 * 
+		 * @param string $name The name of the output handler function
+		 */
 		public static function addHandler($name) {
 			if (!empty($name)) {
 				self::$outputHandlers .= "\$output = $name(\$output);";
 			}
 		}
 		
+		/**
+		 * Add handler to the $permalink object
+		 * 
+		 * @param string $name
+		 */
 		public static function addPermalinkHandler($name) {
 			//TODO: test it
 			$settings = \fusion_get_settings();
@@ -82,10 +171,21 @@ namespace PHPFusion {
 			}
 		}
 		
+		/**
+		 * Add javascript source code to the output
+		 * 
+		 * @param string $tag
+		 */
 		public static function addToJQuery ($tag = "") {
 			self::$jqueryTags .= $tag;
 		}
 		
+		/**
+		 * Add a link to the breadcrumb
+		 * 
+		 * @global array $breadcrumbs
+		 * @param array $link Keys: link, title
+		 */
 		public static function addToBreadCrumbs(array $link = array()) {
 			//TODO: remove global variable
 			global $breadcrumbs;
@@ -94,6 +194,13 @@ namespace PHPFusion {
 			}
 		}
 		
+		/**
+		 * Execute the output handlers
+		 * 
+		 * @global \PermalinksDisplay $permalink
+		 * @param string $output
+		 * @return string
+		 */
 		public static function handleOutput($output) {
 			//TODO: remove global variables
 			global $permalink;
@@ -126,51 +233,116 @@ namespace PHPFusion {
 }
 namespace {
 	use PHPFusion\OutputHandler;
+	/**
+	 * Set the new title of the page
+	 * 
+	 * @param string $title
+	 */
 	function set_title($title = "") {
 		OutputHandler::setTitle($title);
 	}
 
+	/**
+	 * Append something to the title of the page
+	 * 
+	 * @param type $addition
+	 */
 	function add_to_title($addition = "") {
 		OutputHandler::addToTitle($addition);
 	}
 
+	/**
+	 * Set a meta tag by name
+	 * 
+	 * @param string $name
+	 * @param string $content
+	 */
 	function set_meta($name, $content = "") {
 		OutputHandler::setMeta($name, $content);
 	}
 
+	/**
+	 * Append something to a meta tag
+	 * 
+	 * @param string $name
+	 * @param string $addition
+	 */
 	function add_to_meta($name, $addition = "") {
 		OutputHandler::addToMeta($name, $addition);
 	}
 
+	/**
+	 * Add content to the html head
+	 * 
+	 * @param string $tag
+	 */
 	function add_to_head($tag = "") {
 		OutputHandler::addToHead($tag);
 	}
 
+	/**
+	 * Add content to the footer
+	 * 
+	 * @param string $tag
+	 */
 	function add_to_footer($tag = "") {
 		OutputHandler::addToFooter($tag);
 	}
 
+	/**
+	 * Replace something in the output using regexp
+	 * 
+	 * @param string $target Regexp pattern without delimiters
+	 * @param string $replace The new content
+	 * @param string $modifiers Regexp modifiers
+	 */
 	function replace_in_output($target, $replace, $modifiers = "") {
 		OutputHandler::replaceInOutput($target, $replace, $modifiers);
 	}
 
+	/**
+	 * Add a new output handler function
+	 * 
+	 * @param string $name The name of the output handler function
+	 */
 	function add_handler($name) {
 		OutputHandler::addHandler($name);
 	}
 
+	/**
+	 * Add handler to the $permalink object
+	 * 
+	 * @param string $name
+	 */
 	function add_permalink_handler($name) {
 		OutputHandler::addPermalinkHandler($name);
 	}
 
+	/**
+	 * Execute the output handlers
+	 * 
+	 * @param string $output
+	 * @return string
+	 */
 	function handle_output($output) {
 		return OutputHandler::handleOutput($output);
 	}
 
+	/**
+	 * Add javascript source code to the output
+	 * 
+	 * @param string $tag
+	 */
 	function add_to_jquery($tag = "") {
 		OutputHandler::addToJQuery($tag);
 	}
-
-	// Add links to breadcrumbs array
+	
+	/**
+	 * Add a link to the breadcrumb
+	 * 
+	 * @global array $breadcrumbs
+	 * @param array $link Keys: link, title
+	 */
 	function add_to_breadcrumbs(array $link=array()) {
 		OutputHandler::addToBreadCrumbs($link);
 	}
