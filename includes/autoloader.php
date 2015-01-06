4895855291a24612df94dddc4a00d7a3e7f8a3e8
Takács Ákos <rimelek@rimelek.hu>
2015-01-06 18:30:17 +0100

    simple, temporary autoloader for the core classes

---
+++ includes/autoloader.php
@@ -0,0 +1,51 @@
+<?php
+
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------*
+| Filename: includes/autoloader.php
+| Author: Takács Ákos (Rimelek)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+
+/*
+ * Temporary Autoloader for only core classes until we implement a real 
+ * PSR compatible file structure for the classes
+ */
+
+spl_autoload_register(function ($className) {
+	$baseDir = __DIR__.'/classes/';
+
+	$extensions = array(
+		'class.php', 'php'
+	);
+	$classNameComponents = explode('\\', $className);
+	$count = count($classNameComponents);
+	if ($count > 1) {
+		if($classNameComponents[0] !== 'PHPFusion') {
+			//The autoloader only for PHPFusion classes
+			return;
+		}
+		array_shift($classNameComponents);
+	}
+	$path = implode('/', $classNameComponents);
+	
+	foreach ($extensions as $extension) {
+		$fullPath = $baseDir.$path.'.'.$extension;
+		if (is_file($fullPath)) {
+			require $fullPath;
+			break;
+		}
+	}
+	
+});
+
