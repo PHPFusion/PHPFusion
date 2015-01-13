483c60cfb8ba7e524e8a5a55b18fb3e782f65544
Takács Ákos <rimelek@rimelek.hu>
2015-01-14 00:35:54 +0100

    fixed reading of error_file. It could not show the last line. The loop condition was wrong. Since I was here, I refactored the file and made shorter code where I could. The most of the HTMLs are not inside a PHP string, so the IDE can inform us if the HTML contains a bug. Some incorrect HTML code are fixed.

--- administration/errors.php
+++ administration/errors.php
@@ -19,7 +19,6 @@ require_once "../maincore.php";
 if (!checkrights("ERRO") || !defined("iAUTH") || !isset($_GET['aid']) || $_GET['aid'] != iAUTH) {
 	die("Acces Denied");
 }
-require_once THEMES."templates/admin_header.php";
 include LOCALE.LOCALESET."admin/errors.php";
 add_to_head("<link rel='stylesheet' href='".THEMES."templates/errors.css' type='text/css' media='all' />");
 // Setting maximum number of folders for an URL
@@ -69,173 +68,183 @@ function printCode($source_code, $starting_line, $error_line = "") {
 	}
 	return "<table class='err_tbl-border center' cellspacing='0' cellpadding='0'>".$formatted_code."</table>";
 }
+$error_status = filter_input(INPUT_POST, 'error_status', FILTER_VALIDATE_INT, array('min_range' => 0, 'max_range' => 2));
+$posted_error_id = filter_input(INPUT_POST, 'error_id', FILTER_VALIDATE_INT);
+$delete_status = filter_input(INPUT_POST, 'delete_status', FILTER_VALIDATE_INT, array('min_range' => 0, 'max_range' => 2));
+$rowstart = filter_input(INPUT_GET, 'rowstart', FILTER_VALIDATE_INT) ? : 0;
+$error_id = filter_input(INPUT_GET, 'error_id', FILTER_VALIDATE_INT);
 
-if (isset($_POST['error_status']) && isnum($_POST['error_status']) && isset($_POST['error_id']) && isnum($_POST['error_id'])
-) {
-	$result = dbquery("UPDATE ".DB_ERRORS." SET error_status='".$_POST['error_status']."'
-		WHERE error_id='".$_POST['error_id']."'");
+if (is_integer($error_status) && $posted_error_id) {
+	dbquery("UPDATE ".DB_ERRORS." SET error_status='".$error_status."' WHERE error_id='".$posted_error_id."'");
 }
-if (isset($_POST['delete_entries']) && isset($_POST['delete_status']) && isnum($_POST['delete_status'])) {
-	$result = dbquery("DELETE FROM ".DB_ERRORS." WHERE error_status='".$_POST['delete_status']."'");
+if (isset($_POST['delete_entries']) && is_integer($delete_status)) {
+	dbquery("DELETE FROM ".DB_ERRORS." WHERE error_status='".$_POST['delete_status']."'");
 }
+
+$errors = array();
+$result = dbquery("SELECT * FROM ".DB_ERRORS." ORDER BY error_timestamp DESC LIMIT ".$rowstart.",20");
+while ($data = dbarray($result)) {
+	$errors[] = $data;
+}
+$rows = $errors ? dbcount('(error_id)', DB_ERRORS) : 0;
+
+require_once THEMES."templates/admin_header.php";
 opentable($locale['400']);
-$rows = dbcount("(error_id)", DB_ERRORS);
-if (!isset($_GET['rowstart']) || !isnum($_GET['rowstart'])) {
-	$_GET['rowstart'] = 0;
-}
-if ($rows != 0) {
-	$i = 0;
-	$result = dbquery("SELECT * FROM ".DB_ERRORS." ORDER BY error_timestamp DESC
-		LIMIT ".$_GET['rowstart'].",20");
-	echo "<a name='top'></a>\n<table cellpadding='0' cellspacing='1' class='tbl-border center' style='width:90%;'>\n";
-	echo "<tr>\n";
-	echo "<td class='tbl1' colspan='4' style='text-align:center;'>";
-	echo "<form name='delete_form' action='".FUSION_SELF.$aidlink."' method='post'>";
-	echo "".$locale['440']." <select name='delete_status' class='textbox'>";
-	echo "<option>---</option>\n";
-	echo "<option value='0'>".$locale['450']."</option>\n";
-	echo "<option value='1'>".$locale['451']."</option>\n";
-	echo "<option value='2'>".$locale['452']."</option>\n";
-	echo "</select>\n<input type='submit' class='button' name='delete_entries' value='".$locale['453']."' style='margin-left:5px;' />";
-	echo "</form>\n";
-	echo "</td>\n";
-	echo "</tr>\n<tr>\n";
-	echo "<td class='tbl2' style='font-weight:bold;'>".$locale['410']."</td>\n";
-	echo "<td class='tbl2' style='font-weight:bold;width:5%;'>".$locale['413']."</td>\n";
-	echo "<td class='tbl2' style='text-align:center;width:5%;font-weight:bold;'>".$locale['414']."</td>\n";
-	echo "</tr>\n";
-	while ($data = dbarray($result)) {
-		$row_color = ($i%2 == 0 ? "tbl1" : "tbl2");
-		echo "<tr>\n";
-		echo "<td class='".$row_color."'>\n";
-		echo "<a href='".FUSION_SELF.$aidlink."&amp;rowstart=".$_GET['rowstart']."&amp;error_id=".$data['error_id']."#file' title='".stripslashes($data['error_file'])."'>";
-		echo getMaxFolders(stripslashes($data['error_file']), 2)."</a><br />\n";
-		echo "<span class='small2'>".$data['error_message']." ".$locale['415']." ".$data['error_line']."</span>";
-		echo "</td>\n";
-		echo "<td class='".$row_color."' style='white-space:nowrap;'>".showdate("longdate", $data['error_timestamp'])."</td>\n";
-		echo "<td class='".$row_color."' style='white-space:nowrap;'>\n";
-		echo "<form action='".FUSION_SELF.$aidlink."&amp;rowstart=".$_GET['rowstart']."' method='post'>";
-		echo "<input type='hidden' name='error_id' value='".$data['error_id']."' />";
-		echo "<select name='error_status' class='textbox' onchange='this.form.submit();'>";
-		echo "<option value='0'".($data['error_status'] == 0 ? " selected='selected'" : "").">".$locale['450']."</option>\n";
-		echo "<option value='1'".($data['error_status'] == 1 ? " selected='selected'" : "").">".$locale['451']."</option>\n";
-		echo "<option value='2'".($data['error_status'] == 2 ? " selected='selected'" : "").">".$locale['452']."</option>\n";
-		echo "</select>\n<input type='submit' class='button change_status' value='".$locale['453']."' style='margin-left:5px;' /></form>\n";
-		echo "</td>\n";
-		echo "</tr>\n";
-		$i++;
-	}
-	echo "</table>\n";
-} else {
-	echo "<div style='text-align:center'><br />\n".$locale['418']."<br /><br />\n</div>\n";
-}
-if ($rows > 20) {
-	echo "<div style='margin-top:5px;text-align:center;'>\n".makepagenav($_GET['rowstart'], 20, $rows, 3, FUSION_SELF.$aidlink."&amp;")."\n</div>\n";
-}
+if ($errors) : ?>
+	<a name='top'></a>
+	<table cellpadding='0' cellspacing='1' class='tbl-border center' style='width:90%;'>
+		<tr>
+			<td class='tbl1' colspan='3' style='text-align:center;'>
+				<form name='delete_form' action='<?php echo FUSION_SELF.$aidlink ?>' method='post'>
+					<?php echo $locale['440'] ?> 
+					<select name='delete_status' class='textbox'>
+						<option>---</option>
+						<option value='0'><?php echo $locale['450'] ?></option>
+						<option value='1'><?php echo $locale['451'] ?></option>
+						<option value='2'><?php echo $locale['452'] ?></option>
+					</select>
+					<input type='submit' class='button' name='delete_entries' value='<?php echo $locale['453'] ?>' style='margin-left:5px;' />
+				</form>
+			</td>
+		</tr>
+		<tr>
+			<td class='tbl2' style='font-weight:bold;'><?php echo $locale['410'] ?></td>
+			<td class='tbl2' style='font-weight:bold;width:5%;'><?php echo $locale['413'] ?></td>
+			<td class='tbl2' style='text-align:center;width:5%;font-weight:bold;'><?php echo $locale['414'] ?></td>
+		</tr>
+	<?php foreach ($errors as $i => $data) {
+		$row_color = ($i%2 == 0 ? "tbl1" : "tbl2"); ?>
+		<tr>
+			<td class='<?php echo $row_color ?>'>
+				<a href='<?php echo FUSION_SELF.$aidlink."&amp;rowstart=".$rowstart."&amp;error_id=".$data['error_id'] ?>#file' title='<?php echo stripslashes($data['error_file']) ?>'>
+					<?php echo getMaxFolders(stripslashes($data['error_file']), 2) ?></a><br />
+				<span class='small2'><?php echo $data['error_message']." ".$locale['415']." ".$data['error_line'] ?></span>
+			</td>
+			<td class='<?php echo $row_color ?>' style='white-space:nowrap;'><?php echo showdate("longdate", $data['error_timestamp']) ?></td>
+			<td class='<?php echo $row_color ?>' style='white-space:nowrap;'>
+				<form action='<?php echo FUSION_SELF.$aidlink."&amp;rowstart=".$rowstart ?>' method='post'>
+					<input type='hidden' name='error_id' value='<?php echo $data['error_id'] ?>' />
+					<select name='error_status' class='textbox' onchange='this.form.submit();'>
+						<option value='0' <?php echo ($data['error_status'] == 0 ? "selected='selected'" : "") ?>><?php echo $locale['450'] ?></option>
+						<option value='1' <?php echo ($data['error_status'] == 1 ? "selected='selected'" : "") ?>><?php echo $locale['451'] ?></option>
+						<option value='2' <?php echo ($data['error_status'] == 2 ? "selected='selected'" : "") ?>><?php echo $locale['452'] ?></option>
+					</select>
+					<input type='submit' class='button change_status' value='<?php echo $locale['453'] ?>' style='margin-left:5px;' />
+				</form>
+			</td>
+		</tr>
+		<?php
+	} ?>
+	</table>
+<?php else : ?>
+	<div style='text-align:center'><br />
+		<?php echo $locale['418'] ?><br /><br />
+	</div>
+<?php endif;
+if ($rows > 20) : ?>
+	<div style='margin-top:5px;text-align:center;'><?php echo makepagenav($rowstart, 20, $rows, 3, FUSION_SELF.$aidlink."&amp;") ?></div>
+<?php endif;
 closetable();
-if (isset($_GET['error_id']) && isnum($_GET['error_id'])) {
-	$result = dbquery("SELECT * FROM ".DB_ERRORS." WHERE error_id='".$_GET['error_id']."' LIMIT 1");
-	if (dbrows($result) == 0) {
+if ($error_id) {
+	$data = dbarray(dbquery("SELECT * FROM ".DB_ERRORS." WHERE error_id='".$error_id."' LIMIT 1"));
+	if (!$data) {
 		redirect(FUSION_SELF.$aidlink);
 	}
-	$data = dbarray($result);
 	$thisFileContent = file($data['error_file']);
-	$line_start = "";
-	$line_end = "";
-	if (isset($data['error_line']) && isnum($data['error_line'])) {
-		$line_start = $data['error_line']-10;
-	} else {
-		$line_start = 1;
-	}
-	if (isset($data['error_line']) && isnum($data['error_line'])) {
-		if (($data['error_line']+10) <= count($thisFileContent)) {
-			$line_end = $data['error_line']+10;
-		} else {
-			$line_end = count($thisFileContent);
-		}
-	} else {
-		$line_end = count($thisFileContent);
-	}
+	$line_start = max($data['error_line']-10, 1);
+	$line_end = min($data['error_line']+10, count($thisFileContent));
 	opentable($locale['401']." ".getMaxFolders(stripslashes($data['error_file']), 3));
-	$output = "";
-	for ($i = ($line_start-1); $i < ($line_end-1); $i++) {
-		$output .= $thisFileContent[$i];
-	}
-	echo "<table cellpadding='0' cellspacing='1' class='tbl-border center' style='border-collapse:collapse;width:90%;'>\n";
-	echo "<tr>\n";
-	echo "<td colspan='4' class='tbl2'><a name='file'></a>\n<strong>".$locale['420']."</strong></td>\n";
-	echo "</tr>\n<tr>\n";
-	echo "<td class='tbl2 err_tbl-error' style='width:5%;white-space:nowrap;'>".$locale['410'].":</td>\n";
-	echo "<td class='tbl1 err_tbl-error'>".$data['error_message']."</td>\n";
-	echo "<td class='tbl2 err_tbl-error' style='width:5%;white-space:nowrap;'>".$locale['415']."</td>\n";
-	echo "<td class='tbl1 err_tbl-error'>".$data['error_line']."</td>\n";
-	echo "</tr>\n<tr>\n";
-	echo "<td class='tbl2' style='width:5%;white-space:nowrap;'>".$locale['419'].":</td>\n";
-	echo "<td class='tbl1'><strong>".getMaxFolders(stripslashes($data['error_file']), 3)."</strong></td>\n";
-	echo "<td class='tbl2' style='width:5%;white-space:nowrap;'>".$locale['411'].":</td>\n";
-	echo "<td class='tbl1'>";
-	echo "<a href='".FUSION_SELF.$aidlink."&amp;rowstart=".$_GET['rowstart']."&amp;error_id=".$data['error_id']."#page' title='".$data['error_page']."'>";
-	echo getMaxFolders($data['error_page'], 3)."</a></td>\n";
-	echo "</tr>\n<tr>\n";
-	echo "<td class='tbl2' style='width:5%;white-space:nowrap;'>".$locale['412']."-".$locale['416']."</td>\n";
-	echo "<td class='tbl1'>".$data['error_user_level']."</td>\n";
-	echo "<td class='tbl2' style='width:5%;white-space:nowrap;'>".$locale['417']."</td>\n";
-	echo "<td class='tbl1'>".$data['error_user_ip']."</td>\n";
-	echo "</tr>\n<tr>\n";
-	echo "<td class='tbl2' style='width:5%;white-space:nowrap;'>".$locale['413'].":</td>\n";
-	echo "<td class='tbl1'>".showdate("longdate", $data['error_timestamp'])."</td>\n";
-	echo "<td class='tbl2' style='width:5%;white-space:nowrap;'>".$locale['414'].":</td>\n";
-	echo "<td class='tbl1'>";
-	echo "<form action='".FUSION_SELF.$aidlink."&amp;rowstart=".$_GET['rowstart']."&amp;error_id=".$data['error_id']."#file' method='post'>";
-	echo "<input type='hidden' name='error_id' value='".$data['error_id']."' />";
-	echo "<select name='error_status' class='textbox' onchange='this.form.submit();'>";
-	echo "<option value='0'".($data['error_status'] == 0 ? " selected='selected'" : "").">".$locale['450']."</option>\n";
-	echo "<option value='1'".($data['error_status'] == 1 ? " selected='selected'" : "").">".$locale['451']."</option>\n";
-	echo "<option value='2'".($data['error_status'] == 2 ? " selected='selected'" : "").">".$locale['452']."</option>\n";
-	echo "</select>\n<input type='submit' class='button change_status' value='".$locale['453']."' style='margin-left:5px;' /></form>\n";
-	echo "</td>\n";
-	echo "</tr>\n<tr>\n";
-	echo "<td colspan='4' class='tbl1' style='text-align:center;font-weight:bold;'>";
-	echo "<hr />\n<a href='#top' title='".$locale['422']."'>".$locale['422']."</a>\n";
-	echo "</td>\n";
-	echo "</tr>\n<tr>\n";
-	echo "<td colspan='4' class='tbl2'><strong>".$locale['421']."</strong> (".$locale['415']." ".$line_start." - ".$line_end.")</td>\n";
-	echo "</tr>\n<tr>\n";
-	echo "<td colspan='4'><div style='max-height:600px;overflow:auto;'>".printCode($output, $line_start, $data['error_line'])."</div>\n</td>\n";
-	echo "</tr>\n";
-	$thisPageContent = file(BASEDIR.substr($data['error_page'], strlen(fusion_get_settings('site_path'))));
-	$output = "";
-	for ($i = 0; $i < count($thisPageContent); $i++) {
-		$output .= $thisPageContent[$i];
-	}
-	echo "<tr>\n";
-	echo "<td colspan='4' class='tbl1' style='text-align:center;font-weight:bold;'>";
-	echo "<hr />\n<a href='#top' title='".$locale['422']."'>".$locale['422']."</a>\n";
-	echo "</td>\n";
-	echo "</tr>\n<tr>\n";
-	echo "<td colspan='4' class='tbl2'><a name='page'></a>\n";
-	echo "<strong>".$locale['411'].": ".getMaxFolders($data['error_page'], 2)."</strong></td>\n";
-	echo "</tr>\n<tr>\n";
-	echo "<td colspan='4'><div style='max-height:500px;overflow:auto;'>".printCode($output, "1")."</div>\n</td>\n";
-	echo "</tr>\n";
-	echo "</table>\n";
-	echo "<div style='margin-top:5px;text-align:center;font-weight:bold;'>\n";
-	echo "<a href='#top' title='".$locale['422']."'>".$locale['422']."</a>\n";
-	echo "</div>";
+	$output = implode("", array_slice($thisFileContent, $line_start-1, $line_end - $line_start + 1));
+	$pageContent = file_get_contents(BASEDIR.substr($data['error_page'], strlen(fusion_get_settings('site_path'))));
+	?>
+	<table cellpadding='0' cellspacing='1' class='tbl-border center' style='border-collapse:collapse;width:90%;'>
+		<tr>
+			<td colspan='4' class='tbl2'><a name='file'></a><strong><?php echo $locale['420'] ?></strong></td>
+		</tr>
+		<tr>
+			<td class='tbl2 err_tbl-error' style='width:5%;white-space:nowrap;'><?php echo $locale['410'] ?></td>
+			<td class='tbl1 err_tbl-error'><?php echo $data['error_message'] ?></td>
+			<td class='tbl2 err_tbl-error' style='width:5%;white-space:nowrap;'><?php echo $locale['415'] ?></td>
+			<td class='tbl1 err_tbl-error'><?php echo $data['error_line'] ?></td>
+		</tr>
+		<tr>
+			<td class='tbl2' style='width:5%;white-space:nowrap;'><?php echo $locale['419'] ?></td>
+			<td class='tbl1'><strong><?php echo getMaxFolders(stripslashes($data['error_file']), 3) ?></strong></td>
+			<td class='tbl2' style='width:5%;white-space:nowrap;'><?php echo $locale['411'] ?>:</td>
+			<td class='tbl1'>
+				<a href='<?php echo FUSION_SELF.$aidlink."&amp;rowstart=".$rowstart."&amp;error_id=".$data['error_id'] ?>#page' title='<?php echo $data['error_page'] ?>'>
+					<?php echo getMaxFolders($data['error_page'], 3) ?></a>
+			</td>
+		</tr>
+		<tr>
+			<td class='tbl2' style='width:5%;white-space:nowrap;'><?php echo $locale['412']."-".$locale['416'] ?></td>
+			<td class='tbl1'><?php echo $data['error_user_level'] ?></td>
+			<td class='tbl2' style='width:5%;white-space:nowrap;'><?php echo $locale['417'] ?></td>
+			<td class='tbl1'><?php echo $data['error_user_ip'] ?></td>
+		</tr>
+		<tr>
+			<td class='tbl2' style='width:5%;white-space:nowrap;'><?php echo $locale['413'] ?>:</td>
+			<td class='tbl1'><?php echo showdate("longdate", $data['error_timestamp']) ?></td>
+			<td class='tbl2' style='width:5%;white-space:nowrap;'><?php echo $locale['414'] ?>:</td>
+			<td class='tbl1'>
+				<form action='<?php echo FUSION_SELF.$aidlink."&amp;rowstart=".$rowstart."&amp;error_id=".$data['error_id'] ?>#file' method='post'>
+					<input type='hidden' name='error_id' value='<?php echo $data['error_id'] ?>' />
+					<select name='error_status' class='textbox' onchange='this.form.submit();'>
+						<option value='0' <?php echo ($data['error_status'] == 0 ? "selected='selected'" : "") ?>><?php echo $locale['450'] ?></option>
+						<option value='1' <?php echo ($data['error_status'] == 1 ? "selected='selected'" : "") ?>><?php echo $locale['451'] ?></option>
+						<option value='2' <?php echo ($data['error_status'] == 2 ? " selected='selected'" : "") ?>><?php echo $locale['452'] ?></option>
+					</select>
+					<input type='submit' class='button change_status' value='<?php echo $locale['453'] ?>' style='margin-left:5px;' />
+				</form>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='4' class='tbl1' style='text-align:center;font-weight:bold;'>
+				<hr />
+				<a href='#top' title='<?php echo $locale['422'] ?>'><?php echo $locale['422'] ?></a>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='4' class='tbl2'><strong><?php echo $locale['421'] ?></strong> (<?php echo $locale['415']." ".$line_start." - ".$line_end ?>)</td>
+		</tr>
+		<tr>
+			<td colspan='4'><div style='max-height:600px;overflow:auto;'><?php echo printCode($output, $line_start, $data['error_line']) ?></div>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='4' class='tbl1' style='text-align:center;font-weight:bold;'>
+				<hr />
+				<a href='#top' title='<?php echo $locale['422'] ?>'><?php echo $locale['422'] ?></a>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='4' class='tbl2'><a name='page'></a>
+				<strong><?php echo $locale['411'] ?>: <?php echo getMaxFolders($data['error_page'], 2) ?></strong></td>
+		</tr>
+		<tr>
+			<td colspan='4'><div style='max-height:500px;overflow:auto;'><?php echo printCode($pageContent, "1") ?></div>
+			</td>
+		</tr>
+	</table>
+	<div style='margin-top:5px;text-align:center;font-weight:bold;'>
+		<a href='#top' title='<?php echo $locale['422'] ?>'><?php echo $locale['422'] ?></a>
+	</div>
+	<?php
 	closetable();
 }
 // Show the "Apply"-button only when javascript is disabled"
-echo "<script language='JavaScript' type='text/javascript'>\n";
-echo "/* <![CDATA[ */\n";
-echo "jQuery(document).ready(function() {
+?>
+<script type='text/javascript'>
+/* <![CDATA[ */
+jQuery(document).ready(function() {
 	jQuery('.change_status').hide();
 
 	jQuery('a[href=#top]').click(function(){
 		jQuery('html, body').animate({scrollTop:0}, 'slow');
 		return false;
 	});
-});";
-echo "/* ]]>*/\n";
-echo "</script>\n";
+});
+/* ]]>*/
+</script>
+<?php
 require_once THEMES."templates/footer.php";
-?>
