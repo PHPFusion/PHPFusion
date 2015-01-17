eb24dfd4b6d4954fe4689742bddd3a87b10fe158
Takács Ákos <rimelek@rimelek.hu>
2015-01-17 03:00:02 +0100

    Removed unnecessary assignmet in while loops in the footer.php. Don't assign the result of dbquery to a variable for nothing! And be careful, because you can override the result of the previous query and the loop will stop after one step as it happened in this case.

--- themes/templates/footer.php
+++ themes/templates/footer.php
@@ -31,17 +31,17 @@ if (!defined('ADMIN_PANEL')) {
 
 // Cron Job (6 MIN)
 if ($settings['cronjob_hour'] < (time()-360)) {
-	$result = dbquery("DELETE FROM ".DB_FLOOD_CONTROL." WHERE flood_timestamp < '".(time()-360)."'");
-	$result = dbquery("DELETE FROM ".DB_CAPTCHA." WHERE captcha_datestamp < '".(time()-360)."'");
-	$result = dbquery("DELETE FROM ".DB_USERS." WHERE user_joined='0' AND user_ip='0.0.0.0' and user_level='103'");
-	$result = dbquery("UPDATE ".DB_SETTINGS." SET settings_value='".time()."' WHERE settings_name='cronjob_hour'");
+	dbquery("DELETE FROM ".DB_FLOOD_CONTROL." WHERE flood_timestamp < '".(time()-360)."'");
+	dbquery("DELETE FROM ".DB_CAPTCHA." WHERE captcha_datestamp < '".(time()-360)."'");
+	dbquery("DELETE FROM ".DB_USERS." WHERE user_joined='0' AND user_ip='0.0.0.0' and user_level='103'");
+	dbquery("UPDATE ".DB_SETTINGS." SET settings_value='".time()."' WHERE settings_name='cronjob_hour'");
 }
 // Cron Job (24 HOUR)
 if ($settings['cronjob_day'] < (time()-86400)) {
 	$new_time = time();
-	$result = dbquery("DELETE FROM ".DB_THREAD_NOTIFY." WHERE notify_datestamp < '".(time()-1209600)."'");
-	$result = dbquery("DELETE FROM ".DB_NEW_USERS." WHERE user_datestamp < '".(time()-86400)."'");
-	$result = dbquery("DELETE FROM ".DB_EMAIL_VERIFY." WHERE user_datestamp < '".(time()-86400)."'");
+	dbquery("DELETE FROM ".DB_THREAD_NOTIFY." WHERE notify_datestamp < '".(time()-1209600)."'");
+	dbquery("DELETE FROM ".DB_NEW_USERS." WHERE user_datestamp < '".(time()-86400)."'");
+	dbquery("DELETE FROM ".DB_EMAIL_VERIFY." WHERE user_datestamp < '".(time()-86400)."'");
 	$usr_inactive = dbcount("(user_id)", DB_USERS, "user_status='3' AND user_actiontime!='0' AND user_actiontime < '".time()."'");
 	if ($usr_inactive) {
 		require_once INCLUDES."sendmail_include.php";
@@ -49,7 +49,7 @@ if ($settings['cronjob_day'] < (time()-86400)) {
 			WHERE user_status='3' AND user_actiontime!='0' AND user_actiontime < '".time()."'
 			LIMIT 10");
 		while ($data = dbarray($result)) {
-			$result2 = dbquery("UPDATE ".DB_USERS." SET user_status='0', user_actiontime='0' WHERE user_id='".$data['user_id']."'");
+			dbquery("UPDATE ".DB_USERS." SET user_status='0', user_actiontime='0' WHERE user_id='".$data['user_id']."'");
 			$subject = $locale['global_451'];
 			$message = str_replace("USER_NAME", $data['user_name'], $locale['global_452']);
 			$message = str_replace("LOST_PASSWORD", $settings['siteurl']."lostpassword.php", $message);
@@ -66,28 +66,28 @@ if ($settings['cronjob_day'] < (time()-86400)) {
 			LIMIT 10");
 		if ($settings['deactivation_action'] == 0) {
 			while ($data = dbarray($result)) {
-				$result = dbquery("UPDATE ".DB_USERS." SET user_actiontime='0', user_status='6' WHERE user_id='".$data['user_id']."'");
+				dbquery("UPDATE ".DB_USERS." SET user_actiontime='0', user_status='6' WHERE user_id='".$data['user_id']."'");
 			}
 		} else {
 			while ($data = dbarray($result)) {
-				$result = dbquery("DELETE FROM ".DB_USERS." WHERE user_id='".$data['user_id']."'");
-				$result = dbquery("DELETE FROM ".DB_ARTICLES." WHERE article_name='".$data['user_id']."'");
-				$result = dbquery("DELETE FROM ".DB_COMMENTS." WHERE comment_name='".$data['user_id']."'");
-				$result = dbquery("DELETE FROM ".DB_MESSAGES." WHERE message_to='".$data['user_id']."' OR message_from='".$data['user_id']."'");
-				$result = dbquery("DELETE FROM ".DB_NEWS." WHERE news_name='".$data['user_id']."'");
-				$result = dbquery("DELETE FROM ".DB_POLL_VOTES." WHERE vote_user='".$data['user_id']."'");
-				$result = dbquery("DELETE FROM ".DB_RATINGS." WHERE rating_user='".$data['user_id']."'");
-				$result = dbquery("DELETE FROM ".DB_SUSPENDS." WHERE suspended_user='".$data['user_id']."'");
-				$result = dbquery("DELETE FROM ".DB_THREADS." WHERE thread_author='".$data['user_id']."'");
-				$result = dbquery("DELETE FROM ".DB_POSTS." WHERE post_author='".$data['user_id']."'");
-				$result = dbquery("DELETE FROM ".DB_THREAD_NOTIFY." WHERE notify_user='".$data['user_id']."'");
+				dbquery("DELETE FROM ".DB_USERS." WHERE user_id='".$data['user_id']."'");
+				dbquery("DELETE FROM ".DB_ARTICLES." WHERE article_name='".$data['user_id']."'");
+				dbquery("DELETE FROM ".DB_COMMENTS." WHERE comment_name='".$data['user_id']."'");
+				dbquery("DELETE FROM ".DB_MESSAGES." WHERE message_to='".$data['user_id']."' OR message_from='".$data['user_id']."'");
+				dbquery("DELETE FROM ".DB_NEWS." WHERE news_name='".$data['user_id']."'");
+				dbquery("DELETE FROM ".DB_POLL_VOTES." WHERE vote_user='".$data['user_id']."'");
+				dbquery("DELETE FROM ".DB_RATINGS." WHERE rating_user='".$data['user_id']."'");
+				dbquery("DELETE FROM ".DB_SUSPENDS." WHERE suspended_user='".$data['user_id']."'");
+				dbquery("DELETE FROM ".DB_THREADS." WHERE thread_author='".$data['user_id']."'");
+				dbquery("DELETE FROM ".DB_POSTS." WHERE post_author='".$data['user_id']."'");
+				dbquery("DELETE FROM ".DB_THREAD_NOTIFY." WHERE notify_user='".$data['user_id']."'");
 			}
 		}
 		if ($usr_deactivate > 10) {
 			$new_time = $settings['cronjob_day'];
 		}
 	}
-	$result = dbquery("UPDATE ".DB_SETTINGS." SET settings_value='".$new_time."' WHERE settings_name='cronjob_day'");
+	dbquery("UPDATE ".DB_SETTINGS." SET settings_value='".$new_time."' WHERE settings_name='cronjob_day'");
 }
 // Error handling
 if (iADMIN && checkrights("ERRO") && count($_errorHandler) > 0) {
@@ -123,14 +123,6 @@ if (ob_get_length() !== FALSE) {
 	ob_end_flush();
 }
 
-/* //We don't need to close the connection manually at the very end of the script
- * //The PHP will close the opened resourses itself
-if ($pdo_enabled == "1") {
-	$pdo = NULL;
-} else {
-	mysql_close($db_connect);
-}
-*/
 // Shows allocated memory and the actual memory used for generatig a page
 //echo "Memory: alloc. ".(memory_get_peak_usage(true)/1024/1024)." MB / used ".number_format((memory_get_peak_usage(false)/1024/1024), 2)." MB";
 //var_dump($_SESSION);
