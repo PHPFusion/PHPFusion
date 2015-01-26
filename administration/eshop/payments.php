20232a04ed202dbd129d21fee2537b1d8c1dd4b0
hien- <meangczac.chan@gmail.com>
2015-01-27 01:33:44 +0800

    Payments Progress

--- administration/eshop/payments.php
+++ administration/eshop/payments.php
@@ -21,9 +21,14 @@ if (isset($_GET['payid']) && !isnum($_GET['payid'])) die("Denied");
 
 class eShop_payment {
 	private $data = array(
+		'pid' => 0,
 		'method'=>'',
+		'payment_image' => '',
+		'active' => 1,
 		'surcharge'=>'',
 		'cfile'=>'',
+		'description' => '',
+		'code' => '',
 	);
 	private $formaction = '';
 	private $max_rowstart = 0;
@@ -33,18 +38,18 @@ class eShop_payment {
 		global $aidlink;
 		define("PAYMENT_DIR", BASEDIR."eshop/paymentimgs/");
 		$_GET['action'] = isset($_GET['action']) ? $_GET['action'] : '';
-		$this->max_rowstart = dbcount("(cuid)", DB_ESHOP_CUSTOMERS);
+		$this->max_rowstart = dbcount("(pid)", DB_ESHOP_PAYMENTS);
 
 		switch($_GET['action']) {
 			case 'edit':
-				$this->formaction = FUSION_SELF.$aidlink."&amp;a_page=customers&amp;action=edit&amp;section=customerform&amp;cuid=".$_GET['cuid'];
+				$this->formaction = FUSION_SELF.$aidlink."&amp;a_page=payments&amp;action=edit&amp;section=paymentform&amp;pid=".$_GET['pid'];
 				$this->data = self::payment_data();
 				break;
 			case 'delete':
-				self::delete_payment($_GET['cuid']);
+				self::delete_payment($_GET['pid']);
 				break;
 			default:
-				$this->formaction = FUSION_SELF.$aidlink."&amp;a_page=customers&amp;section=customerform";
+				$this->formaction = FUSION_SELF.$aidlink."&amp;a_page=payments&amp;section=paymentform";
 		}
 	}
 
@@ -57,6 +62,13 @@ class eShop_payment {
 		return $payment_list;
 	}
 
+	static function get_activeOpts() {
+		global $locale;
+		return array(
+			'1'=> $locale['yes'],
+			'0'=> $locale['no']
+		);
+	}
 
 	static function get_paymentFile() {
 		$payment_cfile = array();
@@ -70,46 +82,85 @@ class eShop_payment {
 	}
 
 	private function payment_data() {
-		$result = dbquery("SELECT * FROM ".DB_ESHOP_CUSTOMERS." WHERE cuid='".$_GET['cuid']."'");
+		$result = dbquery("SELECT * FROM ".DB_ESHOP_PAYMENTS." WHERE pid='".$_GET['pid']."'");
 		if (dbrows($result)>0) {
 			return dbarray($result);
 		}
 		return array();
 	}
 
-	static function delete_payment($cuid) {
+	static function delete_payment($pid) {
 		global $aidlink;
-		if (isnum($cuid)) {
-			if (self::verify_customer($cuid)) {
-				dbquery("DELETE FROM ".DB_ESHOP_CUSTOMERS." WHERE cuid='".intval($cuid)."'");
-				redirect(FUSION_SELF.$aidlink."&amp;a_page=customers");
+		if (isnum($pid)) {
+			if (self::verify_payment($pid)) {
+				dbquery("DELETE FROM ".DB_ESHOP_PAYMENTS." WHERE pid='".intval($pid)."'");
+				redirect(FUSION_SELF.$aidlink."&amp;a_page=payments");
 			}
 		}
 	}
 
 	static function verify_payment($id) {
 		if (isnum($id)) {
-			return dbcount("(cuid)", DB_ESHOP_CUSTOMERS." c INNER JOIN ".DB_USERS." u on c.cuid=u.user_id", "cuid='".intval($id)."'");
+			return dbcount("(pid)", DB_ESHOP_PAYMENTS, "pid='".intval($id)."'");
 		}
 		return false;
 	}
 
-	public function payment_listing() {
-
+	private function set_paymentdb() {
+		global $aidlink;
+		if (isset($_POST['save_payment'])) {
+			$this->data['pid'] = isset($_POST['pid']) ? form_sanitizer($_POST['pid'], '0', 'pid') : 0;
+			$this->data['method'] = isset($_POST['method']) ? form_sanitizer($_POST['method'], '', 'method') : '';
+			$this->data['payment_image'] = isset($_POST['payment_image']) ? form_sanitizer($_POST['payment_image'], '', 'payment_image') : '';
+			$this->data['active'] = isset($_POST['active']) ? form_sanitizer($_POST['active'], '0', 'active') : 0;
+			$this->data['surcharge'] = isset($_POST['surcharge']) ? form_sanitizer($_POST['surcharge'], '', 'surcharge') : '';
+			$this->data['cfile'] = isset($_POST['cfile']) ? form_sanitizer($_POST['cfile'], '', 'cfile') : '';
+			$this->data['description'] = isset($_POST['description']) ? form_sanitizer($_POST['description'], '') : '';
+			$this->data['code'] = isset($_POST['code']) ? form_sanitizer($_POST['code'], '', 'code') : '';
+			if (self::verify_payment($this->data['pid'])) { // this is update
+				// find the category
+				dbquery_insert(DB_ESHOP_PAYMENTS, $this->data, 'update');
+				if (!defined("FUSION_NULL")) redirect("".FUSION_SELF.$aidlink."&amp;a_page=payments&status=su");
+			} else { // this is save
+				dbquery_insert(DB_ESHOP_PAYMENTS, $this->data, 'save');
+				if (!defined("FUSION_NULL")) redirect("".FUSION_SELF.$aidlink."&amp;a_page=payments&status=sn");
+			}
+		}
 	}
 
+	public function payment_listing() {
+		global $locale, $aidlink;
+		echo "<div class='m-t-20'>\n";
+		echo "<table class='table table-responsive'><tr>
+		<th>".$locale['ESHPPMTS116']."</th>\n
+		<th>".$locale['ESHPPMTS117']."</th>\n
+		</tr>\n";
+		$result = dbquery("SELECT * FROM ".DB_ESHOP_PAYMENTS." ORDER BY method ASC, method LIMIT ".$_GET['rowstart'].",25");
+		if (dbrows($result)>0) {
+			while ($data = dbarray($result)) {
+				echo "<tr>";
+				echo "<td><a href='".FUSION_SELF.$aidlink."&amp;a_page=payments&amp;action=edit&amp;pid=".$data['pid']."'><b>".$data['method']."</b></a></td>\n";
+				echo "<td><a href='".FUSION_SELF.$aidlink."&amp;a_page=payments&amp;action=delete&amp;pid=".$data['pid']."' onClick='return confirmdelete();'>".$locale['delete']."</a></td>\n";
+				echo "</tr>";
+			}
+		} else {
+			echo "<tr><td colspan='2' class='text-center'><div class='alert alert-warning m-t-10'>".$locale['ESHPPMTS118']."</div></td></tr>\n";
+		}
+		echo "</table>\n";
+		echo "<div align='center' style='margin-top:5px;'>".makePageNav($_GET['rowstart'],25,$this->max_rowstart,3,FUSION_SELF.$aidlink."&amp;a_page=payments&amp;")."\n</div>\n";
+		echo "</div>n";
+	}
 
 	public function add_payment_form() {
 		global $locale;
 		echo "<div class='m-t-20'>\n";
 		echo openform('paymentform', 'ipaymentform', 'post', $this->formaction);
-
 		echo "<div class='row'>\n";
 		echo "<div class='col-xs-12 col-sm-8'>\n";
 		openside('');
 		echo thumbnail(PAYMENT_DIR.$this->payment_image, '70px');
 		echo "<div class='overflow-hide p-l-15'>\n";
-		echo form_select('Payment Type', 'payment_image', 'payment_image',  self::get_paymentOpts(), '');
+		echo form_select('Payment Type', 'payment_image', 'payment_image',  self::get_paymentOpts(), $this->data['payment_image']);
 		add_to_jquery("
 		$('#payment_image').bind('change', function(e) {
 			$('.thumb > img').prop('src', '".PAYMENT_DIR."'+ $(this).val());
@@ -117,63 +168,30 @@ class eShop_payment {
 		");
 		echo "</div>\n";
 		closeside();
-
 		openside('');
+		echo form_hidden('', 'pid', 'pid', $this->data['pid']);
 		echo form_text($locale['ESHPPMTS100'], 'method', 'method', $this->data['method'], array('inline'=>1, 'required'=>1, 'tip'=>$locale['ESHPPMTS101']));
 		echo form_text($locale['ESHPPMTS102'], 'surcharge', 'surcharge', $this->data['surcharge'], array('inline'=>1, 'required'=>1, 'tip'=>$locale['ESHPPMTS103']));
 		echo form_select($locale['ESHPPMTS104'], 'cfile', 'cfile', self::get_paymentFile(), $this->data['cfile'], array('inline'=>1, 'required'=>1, 'tip'=>$locale['ESHPPMTS106']));
 		closeside();
-
+		openside('');
+		echo form_textarea($locale['ESHPPMTS107'], 'description', 'description', $this->data['description'], array('inline'=>1, 'autosize'=>1, 'tip'=>$locale['ESHPPMTS108']));
+		closeside();
+		openside('');
+		echo form_textarea($locale['ESHPPMTS113'], 'code', 'code', $this->data['code'], array('tip'=>$locale['ESHPPMTS114'], 'placeholder'=>'<?php .... ?>', 'html'=>1, 'preview'=>1, 'form_name'=>'paymentform', 'autosize'=>1));
+		closeside();
 		echo "</div>\n";
 		echo "<div class='col-xs-12 col-sm-4'>\n";
+		openside('');
+		echo form_select($locale['ESHPPMTS109'], 'active', 'active', self::get_activeOpts(), $this->data['active'], array('tip'=>$locale['ESHPPMTS112']));
+		echo form_button($locale['save'], 'save_payment', 'save_payment2', $locale['save'], array('class'=>'btn-primary'));
+		closeside();
 		echo "</div>\n";
+		echo "</div>\n";
+		echo form_button($locale['save'], 'save_payment', 'save_payment', $locale['save'], array('class'=>'btn-primary'));
 		echo closeform();
 		echo "</div>\n";
-
-
-
-		/*
-		 * echo "<form name='inputform' method='post' action='$formaction'>
-<div style='float:left;width:50%'>
-<table align='left' cellspacing='0' width='100%' cellpadding='0' class='tbl'>
-
-
-echo "<tr><td align='left' valign='top'>".$locale['ESHPPMTS107']."</td><td align='left'><textarea name='description' cols='30' rows='4' class='textbox'>$description</textarea>
-<a href='javascript:;' class='info'><span>
-".$locale['ESHPPMTS108']."
-</span><img src='".BASEDIR."eshop/img/helper.png' height='25' border='0' alt='' style='vertical-align:top;' /></a>
-</td></tr>";
-
-echo "<tr><td align='left'>".$locale['ESHPPMTS109']."</td><td align='left'><select name='active' class='textbox'>
-    <option value='1'".($active == "1" ? " selected" : "").">".$locale['ESHPPMTS110']."</option>
-    <option value='0'".($active == "0" ? " selected" : "").">".$locale['ESHPPMTS111']."</option>
-    </select>
-	<a href='javascript:;' class='info'><span>
-	".$locale['ESHPPMTS112']."
-	</span><img src='".BASEDIR."eshop/img/helper.png' height='25' border='0' alt='' style='vertical-align:middle;' /></a>
-	</td></tr>";
-echo "</table></div>";
-
-    require_once INCLUDES."html_buttons_include.php";
-    echo "<div style='float:right;width:50%;'><table align='left' cellspacing='0' width='100%' cellpadding='0' class='tbl'>";
-	echo "<tr><td width='100%' class='tbl'>".$locale['ESHPPMTS113']."
-	<a href='javascript:;' class='info'><span>
-	".$locale['ESHPPMTS114']."
-	</span><img src='".BASEDIR."eshop/img/helper.png' height='25' border='0' alt='' style='vertical-align:middle;' /></a>
-	<br /><textarea name='code' cols='45' rows='15' class='textbox' style='width:98%'>".$code."</textarea></td>\n";
-	echo "</tr>\n<tr>\n";
-	echo "<td class='tbl'>\n";
-	echo "<input type='button' value='&lt;?php?&gt;' class='button' onclick=\"addText('code', '&lt;?php\\n', '\\n?&gt;');\" />\n";
-	echo "<input type='button' value='&lt;p&gt;' class='button' onclick=\"addText('code', '&lt;p&gt;', '&lt;/p&gt;');\" />\n";
-	echo "<input type='button' value='&lt;br /&gt;' class='button' onclick=\"insertText('code', '&lt;br /&gt;');\" />\n";
-	    echo display_html("inputform", "code", true)."</td>\n";
-	echo "</tr>\n";
-echo "</table></div>";
-echo "<div class='clear'></div>";
-echo "<center><input type='submit'name='save_payment' value='".$locale['ESHPPMTS115']."' class='button'></center></form>\n";
-		 */
 	}
-
 }
 
 
@@ -197,78 +215,3 @@ if (isset($_GET['section']) && $_GET['section'] == 'paymentform') {
 }
 
 
-
-
-
-
-if (isset($_GET['step']) && $_GET['step'] == "delete") {
-$result = dbquery("DELETE FROM ".DB_ESHOP_PAYMENTS." WHERE pid='".$_GET['payid']."'");
-} 
-if (isset($_POST['save_payment'])) {
-	$method = stripinput($_POST['method']);
-	$description = stripinput($_POST['description']);
-	$payment_image = stripinput($_POST['payment_image']);
-	$surcharge = stripinput($_POST['surcharge']);
-	$code = addslashes($_POST['code']);
-	$cfile = stripinput($_POST['cfile']);
-	$active = stripinput($_POST['active']);
-
-if (isset($_GET['step']) && $_GET['step'] == "edit") {
-$result = dbquery("UPDATE ".DB_ESHOP_PAYMENTS." SET method='$method',description='$description',image='$payment_image',surcharge='$surcharge',code='$code',cfile='$cfile',active='$active' WHERE pid ='".$_GET['payid']."'");
-} else {
-$result = dbquery("INSERT INTO ".DB_ESHOP_PAYMENTS." (pid,method,description,image,surcharge,code,cfile,active) VALUES('','$method','$description','$payment_image','$surcharge','$code','$cfile','$active')");
-}
-redirect(FUSION_SELF.$aidlink."&amp;a_page=payments");
-}
-
-if (isset($_GET['step']) && $_GET['step'] == "edit") {
-	$data = dbarray(dbquery("SELECT * FROM ".DB_ESHOP_PAYMENTS." WHERE pid='".$_GET['payid']."'"));
-	$pid = $data['pid'];
-	$method = $data['method'];
-	$description = $data['description'];
-	$payment_image = $data['image'];
-	$surcharge = $data['surcharge'];
-	$code = stripslashes($data['code']);
-	$cfile = $data['cfile'];
-	$active = $data['active'];
-	$formaction = FUSION_SELF.$aidlink."&amp;a_page=payments&amp;step=edit&amp;payid=".$data['pid'];
-} else {
-	$pid = "";
-	$method = "";
-	$description = "";
-	$payment_image = "";
-	$surcharge = "";
-	$code = "";
-	$cfile = "";
-	$active = "";
-	$payment_image = "invoice.png";
-	$formaction = FUSION_SELF.$aidlink."&amp;a_page=payments";
-}
-
-
-echo "<div class='clear'></div>";
-echo "<hr />";
-
-$result = dbquery("SELECT * FROM ".DB_ESHOP_PAYMENTS."");
-$rows = dbrows($result);
-if ($rows != 0) {
-
-echo "<br /><table align='center' cellspacing='4' cellpadding='0' class='tbl-border' width='99%'><tr>
-<td class='tbl2' align='center' width='1%'><b>".$locale['ESHPPMTS116']."</b></td>
-<td class='tbl2' align='center' width='1%'><b>".$locale['ESHPPMTS117']."</b></td>
-</tr>\n";
-
-
-
-
-$result = dbquery("SELECT * FROM ".DB_ESHOP_PAYMENTS." ORDER BY method ASC, method LIMIT ".$_GET['rowstart'].",25");
-while ($data = dbarray($result)) {
-echo "<tr style='height:20px;' onMouseOver=\"this.className='tbl2'\" onMouseOut=\"this.className='tbl1'\">";
-echo "<td align='center' width='1%'><a href='".FUSION_SELF.$aidlink."&amp;a_page=payments&amp;step=edit&amp;payid=".$data['pid']."'><b>".$data['method']."</b></a></td>\n";
-echo "<td align='center' width='1%'><a href='".FUSION_SELF.$aidlink."&amp;a_page=payments&amp;step=delete&amp;payid=".$data['pid']."' onClick='return confirmdelete();'><img src='".BASEDIR."eshop/img/remove.png' border='0' height='20' style='vertical-align:middle;'  alt='' /></a></td></tr>";
-} 
-echo "</table>\n";
-echo "<div align='center' style='margin-top:5px;'>".makePageNav($_GET['rowstart'],25,$rows,3,FUSION_SELF.$aidlink."&amp;payments&amp;")."\n</div>\n";
-} else {
-echo "<div class='admin-message'>".$locale['ESHPPMTS118']."</div>\n";
-}
