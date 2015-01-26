e629de0d4ff8a5d9c31c24f6233d4aa5b58a673d
dialektika <dialektika@proline.in.ua>
2015-01-27 01:20:12 +0200

    Update ex_navigation_panel.php

--- infusions/ex_navigation_panel/ex_navigation_panel.php
+++ infusions/ex_navigation_panel/ex_navigation_panel.php
@@ -5,7 +5,7 @@
 | http://www.php-fusion.co.uk/
 +--------------------------------------------------------+
 | Filename: ex_navigation_panel.php
-| Author: Nick Jones (Digitanium)
+| Author: Stas Beh (dialektika)
 +--------------------------------------------------------+
 | This program is released as free software under the
 | Affero GPL license. You can redistribute it and/or
