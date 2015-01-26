5e39448e65f3334419307747035dc5988e7fac80
dialektika <dialektika@proline.in.ua>
2015-01-27 01:20:52 +0200

    Update infusion.php

--- infusions/ex_navigation_panel/infusion.php
+++ infusions/ex_navigation_panel/infusion.php
@@ -5,7 +5,7 @@
 | https://www.php-fusion.co.uk/
 +--------------------------------------------------------+
 | Filename: infusion.php
-| Author: PHP Fusion Development Team
+| Author: Stas Beh (dialektika)
 +--------------------------------------------------------+
 | This program is released as free software under the
 | Affero GPL license. You can redistribute it and/or
