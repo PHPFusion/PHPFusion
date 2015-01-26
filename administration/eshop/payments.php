07cb6b506bb1885d9f7d4de0a090bc5f1c0e11f6
hien- <meangczac.chan@gmail.com>
2015-01-26 18:32:05 +0800

    Payment Progress.
    UF have problem again. need to fix.

--- administration/eshop/payments.php
+++ administration/eshop/payments.php
@@ -20,11 +20,32 @@ if (isset($_GET['payid']) && !isnum($_GET['payid'])) die("Denied");
 
 
 class eShop_payment {
-	private $data = array();
+	private $data = array(
+		'method'=>'',
+		'surcharge'=>'',
+		'cfile'=>'',
+	);
+	private $formaction = '';
+	private $max_rowstart = 0;
+	private $payment_image = 'paypal.png';
 
 	public function __construct() {
+		global $aidlink;
 		define("PAYMENT_DIR", BASEDIR."eshop/paymentimgs/");
+		$_GET['action'] = isset($_GET['action']) ? $_GET['action'] : '';
+		$this->max_rowstart = dbcount("(cuid)", DB_ESHOP_CUSTOMERS);
 
+		switch($_GET['action']) {
+			case 'edit':
+				$this->formaction = FUSION_SELF.$aidlink."&amp;a_page=customers&amp;action=edit&amp;section=customerform&amp;cuid=".$_GET['cuid'];
+				$this->data = self::payment_data();
+				break;
+			case 'delete':
+				self::delete_payment($_GET['cuid']);
+				break;
+			default:
+				$this->formaction = FUSION_SELF.$aidlink."&amp;a_page=customers&amp;section=customerform";
+		}
 	}
 
 	static function get_paymentOpts() {
@@ -37,44 +58,85 @@ class eShop_payment {
 	}
 
 
-	private function payment_form() {
+	static function get_paymentFile() {
+		$payment_cfile = array();
+		if (file_exists('../paymentscripts')) {
+			$payment_dir = makefilelist("../paymentscripts/",  ".|..|index.php", true, "files");
+			foreach($payment_dir as $paymentfile) {
+				$payment_cfile[$paymentfile] = $paymentfile;
+			}
+		}
+		return $payment_cfile;
+	}
 
+	private function payment_data() {
+		$result = dbquery("SELECT * FROM ".DB_ESHOP_CUSTOMERS." WHERE cuid='".$_GET['cuid']."'");
+		if (dbrows($result)>0) {
+			return dbarray($result);
+		}
+		return array();
+	}
+
+	static function delete_payment($cuid) {
+		global $aidlink;
+		if (isnum($cuid)) {
+			if (self::verify_customer($cuid)) {
+				dbquery("DELETE FROM ".DB_ESHOP_CUSTOMERS." WHERE cuid='".intval($cuid)."'");
+				redirect(FUSION_SELF.$aidlink."&amp;a_page=customers");
+			}
+		}
+	}
+
+	static function verify_payment($id) {
+		if (isnum($id)) {
+			return dbcount("(cuid)", DB_ESHOP_CUSTOMERS." c INNER JOIN ".DB_USERS." u on c.cuid=u.user_id", "cuid='".intval($id)."'");
+		}
+		return false;
+	}
+
+	public function payment_listing() {
+
+	}
+
+
+	public function add_payment_form() {
+		global $locale;
 		echo "<div class='m-t-20'>\n";
+		echo openform('paymentform', 'ipaymentform', 'post', $this->formaction);
+
+		echo "<div class='row'>\n";
+		echo "<div class='col-xs-12 col-sm-8'>\n";
+		openside('');
+		echo thumbnail(PAYMENT_DIR.$this->payment_image, '70px');
+		echo "<div class='overflow-hide p-l-15'>\n";
+		echo form_select('Payment Type', 'payment_image', 'payment_image',  self::get_paymentOpts(), '');
+		add_to_jquery("
+		$('#payment_image').bind('change', function(e) {
+			$('.thumb > img').prop('src', '".PAYMENT_DIR."'+ $(this).val());
+		});
+		");
+		echo "</div>\n";
+		closeside();
+
+		openside('');
+		echo form_text($locale['ESHPPMTS100'], 'method', 'method', $this->data['method'], array('inline'=>1, 'required'=>1, 'tip'=>$locale['ESHPPMTS101']));
+		echo form_text($locale['ESHPPMTS102'], 'surcharge', 'surcharge', $this->data['surcharge'], array('inline'=>1, 'required'=>1, 'tip'=>$locale['ESHPPMTS103']));
+		echo form_select($locale['ESHPPMTS104'], 'cfile', 'cfile', self::get_paymentFile(), $this->data['cfile'], array('inline'=>1, 'required'=>1, 'tip'=>$locale['ESHPPMTS106']));
+		closeside();
 
 		echo "</div>\n";
+		echo "<div class='col-xs-12 col-sm-4'>\n";
+		echo "</div>\n";
+		echo closeform();
+		echo "</div>\n";
+
+
+
 		/*
 		 * echo "<form name='inputform' method='post' action='$formaction'>
 <div style='float:left;width:50%'>
 <table align='left' cellspacing='0' width='100%' cellpadding='0' class='tbl'>
-<tr><td width='1%' align='left'><img src='".PAYMENT_DIR.($payment_image!=''?$payment_image:"")."' width='70' name='payment_image_preview' alt='' valign='middle' /></td>
-<td width='1%' align='left'><select name='payment_image' class='textbox' style='width:200px;' onChange=\"document.payment_image_preview.src = '".PAYMENT_DIR."' + document.inputform.payment_image.options[document.inputform.payment_image.selectedIndex].value;\">
-<option value='".$payment_image."' ".($payment_image == "$payment_image" ? " selected" : "").">".$payment_image."</option>
-$payment_list</select></td></tr>
-<tr><td width='1%' align='left'>".$locale['ESHPPMTS100']."</td><td width='1%' align='left'><input type='text' name='method' value='$method' class='textbox' style='width:190px;'>
-	<a href='javascript:;' class='info'><span>
-	".$locale['ESHPPMTS101']."
-	</span><img src='".BASEDIR."eshop/img/helper.png' height='25' border='0' alt='' style='vertical-align:middle;' /></a>
-</td></tr>";
 
-echo "<tr><td width='1%' align='left'>".$locale['ESHPPMTS102']."</td><td width='1%' align='left'><input type='text' name='surcharge' value='$surcharge' class='textbox' style='width:50px;'> ".$settings['eshop_currency']."
-	<a href='javascript:;' class='info'><span>
-	".$locale['ESHPPMTS103']."
-	</span><img src='".BASEDIR."eshop/img/helper.png' height='25' border='0' alt='' style='vertical-align:middle;' /></a>
-</td></tr>";
-
-echo "<tr><td align='left'>".$locale['ESHPPMTS104']."</td><td>";
-    $payment_dir = makefilelist("../paymentscripts/",  ".|..|index.php", true, "files");
-  echo "<select name='cfile' class='textbox' style='width:180px;' >";
-  echo "<option value=''>".$locale['ESHPPMTS105']."</option>";
-     foreach($payment_dir as $paymentfile){
-  echo "<option value='$paymentfile' ".($paymentfile == "$cfile" ? " selected" : "").">$paymentfile</option>";
- }
-echo "</select>
-
-<a href='javascript:;' class='info'><span>
-".$locale['ESHPPMTS106']."
-</span><img src='".BASEDIR."eshop/img/helper.png' height='25' border='0' alt='' style='vertical-align:middle;' /></a>
-</td></tr>";
 
 echo "<tr><td align='left' valign='top'>".$locale['ESHPPMTS107']."</td><td align='left'><textarea name='description' cols='30' rows='4' class='textbox'>$description</textarea>
 <a href='javascript:;' class='info'><span>
@@ -116,6 +178,23 @@ echo "<center><input type='submit'name='save_payment' value='".$locale['ESHPPMTS
 
 
 $payment = new eShop_payment();
+$edit = (isset($_GET['action']) && $_GET['action'] == 'edit') ? $customer->verify_payment($_GET['cuid']) : 0;
+$tab_title['title'][] = 'Current Payment Method'; //$locale['ESHPCUPNS100'];
+$tab_title['id'][] = 'payment';
+$tab_title['icon'][] = '';
+$tab_title['title'][] =  $edit ? 'Edit Payment Method' : 'Add Payment Method'; // $locale['ESHPCUPNS115'] : $locale['ESHPCUPNS114'];
+$tab_title['id'][] = 'paymentform';
+$tab_title['icon'][] = $edit ? "fa fa-pencil m-r-10" : 'fa fa-plus-square m-r-10';
+$tab_active = tab_active($tab_title, $edit ? 1 : 0, 1, 1);
+echo opentab($tab_title, $tab_active, 'id', FUSION_SELF.$aidlink."&amp;a_page=payments");
+echo opentabbody($tab_title['title'][0], 'payment', $tab_active, 1);
+$payment->payment_listing();
+echo closetabbody();
+if (isset($_GET['section']) && $_GET['section'] == 'paymentform') {
+	echo opentabbody($tab_title['title'][1], 'paymentform', $tab_active, 1);
+	$payment->add_payment_form();
+	echo closetabbody();
+}
 
 
 
