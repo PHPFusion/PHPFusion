f427866c059708ebabb74137ba4b3d99b712c81f
Vitalij <v.chubatyj@ukr.net>
2015-02-03 00:18:04 +0200

    Removed double slashes
    There were "images//favicons" entries in page head (in source). Fixed -
    repeated slashes are removed.

--- themes/templates/render_functions.php
+++ themes/templates/render_functions.php
@@ -82,19 +82,19 @@ if (!function_exists('render_favicons')) {
 		if (file_exists($folder)) {
 			return "
 			<link rel='apple-touch-icon' sizes='57x57' href='".$folder."favicons/apple-touch-icon-57x57.png'/>
-			<link rel='apple-touch-icon' sizes='114x114' href='".$folder."/favicons/apple-touch-icon-114x114.png'/>
-			<link rel='apple-touch-icon' sizes='72x72' href='".$folder."/favicons/apple-touch-icon-72x72.png'/>
-			<link rel='apple-touch-icon' sizes='144x144' href='".$folder."/favicons/apple-touch-icon-144x144.png'/>
-			<link rel='apple-touch-icon' sizes='60x60' href='".$folder."/favicons/apple-touch-icon-60x60.png'/>
-			<link rel='apple-touch-icon' sizes='120x120' href='".$folder."/favicons/apple-touch-icon-120x120.png'/>
-			<link rel='apple-touch-icon' sizes='76x76' href='".$folder."/favicons/apple-touch-icon-76x76.png'/>
-			<link rel='shortcut icon' href='".$folder."/favicons/favicon.ico'/>
-			<link rel='icon' type='image/png' href='".$folder."/favicons/favicon-96x96.png' sizes='96x96'/>
-			<link rel='icon' type='image/png' href='".$folder."/favicons/favicon-16x16.png' sizes='16x16'/>
-			<link rel='icon' type='image/png' href='".$folder."/favicons/favicon-32x32.png' sizes='32x32'/>
+			<link rel='apple-touch-icon' sizes='114x114' href='".$folder."favicons/apple-touch-icon-114x114.png'/>
+			<link rel='apple-touch-icon' sizes='72x72' href='".$folder."favicons/apple-touch-icon-72x72.png'/>
+			<link rel='apple-touch-icon' sizes='144x144' href='".$folder."favicons/apple-touch-icon-144x144.png'/>
+			<link rel='apple-touch-icon' sizes='60x60' href='".$folder."favicons/apple-touch-icon-60x60.png'/>
+			<link rel='apple-touch-icon' sizes='120x120' href='".$folder."favicons/apple-touch-icon-120x120.png'/>
+			<link rel='apple-touch-icon' sizes='76x76' href='".$folder."favicons/apple-touch-icon-76x76.png'/>
+			<link rel='shortcut icon' href='".$folder."favicons/favicon.ico'/>
+			<link rel='icon' type='image/png' href='".$folder."favicons/favicon-96x96.png' sizes='96x96'/>
+			<link rel='icon' type='image/png' href='".$folder."favicons/favicon-16x16.png' sizes='16x16'/>
+			<link rel='icon' type='image/png' href='".$folder."favicons/favicon-32x32.png' sizes='32x32'/>
 			<meta name='msapplication-TileColor' content='#2d7793'/>
-			<meta name='msapplication-TileImage' content='".$folder."/favicons/mstile-144x144.png'/>
-			<meta name='msapplication-config' content='".$folder."/favicons/browserconfig.xml'/>
+			<meta name='msapplication-TileImage' content='".$folder."favicons/mstile-144x144.png'/>
+			<meta name='msapplication-config' content='".$folder."favicons/browserconfig.xml'/>
 			";
 		}
 	}
