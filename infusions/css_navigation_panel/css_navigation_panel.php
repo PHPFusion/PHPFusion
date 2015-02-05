8690940d9f390d5cc089bbbb6374dc3a0ac2015d
Vitalij <v.chubatyj@ukr.net>
2015-02-05 17:42:34 +0200

    New CSS Navigation panel
    New panel with subitems support

--- infusions/css_navigation_panel/css_navigation_panel.php
+++ infusions/css_navigation_panel/css_navigation_panel.php
@@ -6,6 +6,7 @@
 +--------------------------------------------------------+
 | Filename: navigation_panel.php
 | Author: Nick Jones (Digitanium)
+| Co-author: Chubatyj Vitalij (Rizado)
 +--------------------------------------------------------+
 | This program is released as free software under the
 | Affero GPL license. You can redistribute it and/or
@@ -16,59 +17,68 @@
 | written permission from the original author(s).
 +--------------------------------------------------------*/
 if (!defined("IN_FUSION")) { die("Access Denied"); }
-$list_open = FALSE;
 openside($locale['global_001']);
-$result = dbquery("SELECT link_name, link_url, link_window, link_visibility FROM ".DB_SITE_LINKS." WHERE
-	".(multilang_table("SL") ? " link_language='".LANGUAGE."' AND" : "")." link_position in (1, 2) ORDER BY link_order");
-if (dbrows($result)) {
-	$i = 0;
-	echo "<div id='navigation'>\n";
-	while ($data = dbarray($result)) {
-		$li_class = "";
-		$i++;
-		if (checkgroup($data['link_visibility'])) {
-			if ($data['link_name'] != "---" && $data['link_url'] == "---") {
-				if ($list_open) {
-					echo "</ul>\n";
-					$list_open = FALSE;
-				}
-				echo "<h2>".parseubb($data['link_name'], "b|i|u|color|img")."</h2>\n";
-			} else if ($data['link_name'] == "---" && $data['link_url'] == "---") {
-				if ($list_open) {
-					echo "</ul>\n";
-					$list_open = FALSE;
-				}
-				echo "<hr class='side-hr' />\n";
-			} else {
-				if (!$list_open) {
-					echo "<ul>\n";
-					$list_open = TRUE;
-				}
-				$link_target = ($data['link_window'] == "1" ? " target='_blank'" : "");
-				if ($i == 1) {
-					$li_class = "first-link";
-				}
-				if (START_PAGE == $data['link_url']) {
-					$li_class .= ($li_class ? " " : "")."current-link";
-				}
-				if (preg_match("!^(ht|f)tp(s)?://!i", $data['link_url'])) {
-					echo "<li".($li_class ? " class='".$li_class."'" : "").">\n";
-					echo "<a href='".$data['link_url']."'".$link_target." class='side'>".THEME_BULLET."\n";
-					echo "<span>".parseubb($data['link_name'], "b|i|u|color|img")."</span></a></li>\n";
-				} else {
-					echo "<li".($li_class ? " class='".$li_class."'" : "").">\n";
-					echo "<a href='".BASEDIR.$data['link_url']."'".$link_target." class='side'>".THEME_BULLET."\n";
-					echo "<span>".parseubb($data['link_name'], "b|i|u|color|img")."</span></a></li>\n";
-				}
+
+function showsidelinks(array $options = array(), $id = 0) {
+	global $userdata;
+	static $data = array();
+	$settings = fusion_get_settings();
+	$acclevel = isset($userdata['user_level']) ? $userdata['user_level'] : 0;
+	$res = &$res;
+	if (empty($data)) {
+		$data = dbquery_tree_full(DB_SITE_LINKS, "link_id", "link_cat", "WHERE link_position <= 2".(multilang_table("SL") ? " AND link_language='".LANGUAGE."'" : "")." AND link_visibility <= '$acclevel' ORDER BY link_cat, link_order");
+	}
+
+	$res .= "<ul>\n";
+
+	foreach($data[$id] as $link_id => $link_data) {
+		$li_class = $class;
+		if ($link_data['link_name'] != "---" && $link_data['link_name'] != "===") {
+			$link_target = ($data['link_window'] == "1" ? " target='_blank'" : "");
+			if (START_PAGE == $link_data['link_url']) {
+				$li_class .= ($li_class ? " " : "")."current-link";
 			}
+			if (preg_match("!^(ht|f)tp(s)?://!i", $link_data['link_url'])) {
+				$itemlink = $link_data['link_url'];
+			} else {
+				$itemlink = BASEDIR.$link_data['link_url'];
+			}
+			$res .= "<li".($li_class ? " class='".$li_class."'" : "")."><a href='".$itemlink."'".$link_target.">".$link_data['link_name']."</a>";
+			if (isset($data[$link_id])) {
+				$res .= showsidelinks($options, $link_data['link_id']);
+			}
+			$res .= "</li>\n";
+		} elseif ($link_data['link_cat'] > 0) {
+			echo "<li class='divider'></li>";
 		}
 	}
-	if ($list_open) {
-		echo "</ul>\n";
-	}
-	echo "</div>\n";
-} else {
-	echo $locale['global_002'];
+
+	$res .= "</ul>\n";
+
+	return $res;
 }
+
+echo "<table width='100%' cellpadding='0' cellspacing='0'>\n<tr>\n<td>\n";
+echo "<div class='sidecssmenu'>\n";
+
+echo showsidelinks();
+
+echo "</div>\n";
+echo "</td>\n</tr>\n</table>\n";
+
+echo "<script type='text/javascript'>
+	$(document).ready(function(){
+    $('.sidecssmenu ul li').hover(
+        function() {
+            $(this).find('ul:first').slideDown();
+        },
+        function() {            
+            $(this).find('ul:first').slideUp('fast'); 
+        }
+    );
+    $('.sidecssmenu li:has(ul)').find('a:first').append(' »');
+	});
+	</script>\n";
+
 closeside();
 ?>
