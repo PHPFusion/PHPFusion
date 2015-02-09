a035e78099625b406921ea4daa9ab443f210f0d3
hien- <meangczac.chan@gmail.com>
2015-02-09 15:31:37 +0800

    Move SiteLinks Admin to class

--- includes/classes/PHPFusion/SiteLinks.php
+++ includes/classes/PHPFusion/SiteLinks.php
@@ -1,30 +1,8 @@
 <?php
-/*-------------------------------------------------------+
-| PHP-Fusion Content Management System
-| Copyright (C) PHP-Fusion Inc
-| https://www.php-fusion.co.uk/
-+--------------------------------------------------------+
-| Filename: site_links.php
-| Author: Frederick MC Chan (Hien)
-+--------------------------------------------------------+
-| This program is released as free software under the
-| Affero GPL license. You can redistribute it and/or
-| modify it under the terms of this license which you
-| can read by viewing the included agpl.txt or online
-| at www.gnu.org/licenses/agpl.html. Removal of this
-| copyright header is strictly prohibited without
-| written permission from the original author(s).
-+--------------------------------------------------------*/
-require_once "../maincore.php";
 
-if (!checkrights("SL") || !defined("iAUTH") || !isset($_GET['aid']) || $_GET['aid'] != iAUTH) { redirect("../index.php"); }
-require_once THEMES."templates/admin_header.php";
-include LOCALE.LOCALESET."admin/sitelinks.php";
+namespace PHPFusion;
 
-/**
- * Class SiteLinks_Admin
- */
-class SiteLinks_Admin {
+class SiteLinks {
 
 	private $data = array(
 		'link_id' => 0,
@@ -258,7 +236,7 @@ class SiteLinks_Admin {
 	 * @param $link_id
 	 * @return bool|string
 	 */
-	static function verify_edit($link_id) {
+	public static function verify_edit($link_id) {
 		if (isnum($link_id)) {
 			return dbcount("(link_id)", DB_SITE_LINKS, "link_id='".intval($link_id)."'");
 		}
@@ -450,7 +428,7 @@ class SiteLinks_Admin {
 								  'disable_opts' => $this->data['link_id'],
 								  'hide_disabled' => 1
 							  ),
-			DB_SITE_LINKS, "link_name", "link_id", "link_cat");
+							  DB_SITE_LINKS, "link_name", "link_id", "link_cat");
 		echo form_select($locale['global_ML100'], 'link_language', 'link_languages', $this->language_opts, $this->data['link_language'], array('placeholder' => $locale['choose'], 'width'=>'100%'));
 		echo form_select($locale['SL_0022'], 'link_visibility', 'link_visibilitys', self::getVisibility(), $this->data['link_visibility'], array('placeholder' => $locale['choose'], 'width'=>'100%'));
 		echo form_checkbox($locale['SL_0028'], 'link_window', 'link_windows', $this->data['link_window']);
@@ -462,28 +440,3 @@ class SiteLinks_Admin {
 		echo "</div>\n";
 	}
 }
-
-$site_links = new SiteLinks_Admin();
-$edit = isset($_GET['link_id']) ? $site_links->verify_edit($_GET['link_id']) : 0;
-$master_title['title'][] = $locale['SL_0001'];
-$master_title['id'][] = 'links';
-$master_title['icon'][] = '';
-$master_title['title'][] = $edit ? $locale['SL_0011'] : $locale['SL_0010'];
-$master_title['id'][] = 'nform';
-$master_title['icon'][] = $edit ? "fa fa-pencil m-r-10" : 'fa fa-plus-square m-r-10';
-$tab_active = tab_active($master_title, $edit ?  1 : 0, 1);
-opentable($locale['SL_0012']);
-echo "<div id='info'></div>\n";
-echo opentab($master_title, $tab_active, 'link', FUSION_SELF);
-echo opentabbody($master_title['title'][0], 'links', $tab_active, 1);
-$site_links->menu_listing();
-echo closetabbody();
-if (isset($_GET['section']) && $_GET['section'] == 'nform') {
-	echo opentabbody($master_title['title'][1], 'nform', $tab_active, 1);
-	$site_links->menu_form();
-	echo closetabbody();
-}
-echo closetab();
-closetable();
-
-require_once THEMES."templates/footer.php";
