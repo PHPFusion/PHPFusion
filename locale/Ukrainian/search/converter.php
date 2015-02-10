bcc8bfde5354a5d44004d63de7dcbb2e187a5374
kot2007 <koot2007@gmail.com>
2015-02-10 14:14:07 +0200

    Locale (search\converter.php) sync

--- locale/Ukrainian/search/converter.php
+++ locale/Ukrainian/search/converter.php
@@ -1,6 +1,6 @@
 <?php
 $filename_locale['articles.php'] = "Статті";
-$filename_locale['blog.php'] = "Blog";
+$filename_locale['blog.php'] = "Блог";
 $filename_locale['custompages.php'] = "Сторінки";
 $filename_locale['downloads.php'] = "Завантаження";
 $filename_locale['faqs.php'] = "ЧАПи";
