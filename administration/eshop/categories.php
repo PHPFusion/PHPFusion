0247a2c7c6e47e31ad6b7aba649f78dbc10637fa
hien- <meangczac.chan@gmail.com>
2015-03-03 10:28:19 +0800

    Fix Tab

--- administration/eshop/categories.php
+++ administration/eshop/categories.php
@@ -18,15 +18,18 @@
 if (!defined("IN_FUSION")) die("Access Denied");
 
 $category = new PHPFusion\Eshop\Admin\ProductCategories(); // load constructs
-$edit = (isset($_GET['action']) && $_GET['action'] == 'edit') ? $category->verify_cat_edit($_GET['cid']) : 0;
 // build a new interface
 $tab_title['title'][] = $locale['ESHPCATS099'];
 $tab_title['id'][] = 'listcat';
 $tab_title['icon'][] = '';
+
+$edit = (isset($_GET['action']) && $_GET['action'] == 'edit') && $category->verify_cat_edit($_GET['cid']) ? $_GET['section'] : 'listcat';
+
 $tab_title['title'][] = $edit ? $locale['ESHPCATS139'] : $locale['ESHPCATS140'];
 $tab_title['id'][] = 'catform';
 $tab_title['icon'][] = $edit ? "fa fa-pencil m-r-10" : 'fa fa-plus-square m-r-10';
-$tab_active = tab_active($tab_title, $edit ? 1 : 0, 1, 1);
+
+$tab_active = tab_active($tab_title, $edit, 1);
 $category->getMessage();
 echo opentab($tab_title, $tab_active, 'id', FUSION_SELF.$aidlink."&amp;a_page=categories");
 echo opentabbody($tab_title['title'][0], 'listcat', $tab_active, 1);
