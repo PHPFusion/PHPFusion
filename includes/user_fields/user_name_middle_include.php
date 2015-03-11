de1aa01d8123115398d931180b206a9692f9af05
Vitalij <v.chubatyj@ukr.net>
2015-03-11 16:00:44 +0200

    Some new user fields

---
+++ includes/user_fields/user_name_middle_include.php
@@ -0,0 +1,28 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| http://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: user_name_middle_include.php
+| Author: Chubatyj Vitalij (Rizado)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+if (!defined("IN_FUSION")) {
+	die("Access Denied");
+}
+if ($profile_method == "input") {
+	$options += array('inline'=>1, 'max_length'=>20, 'max_width'=>'200px');
+	$user_fields = form_text($locale['uf_name_middle'], 'user_name_middle', 'user_name_middle', $field_value, $options);
+} elseif ($profile_method == "display") {
+	if ($field_value) {
+		$user_fields = array('title'=>$locale['uf_name_middle'], 'value'=>$field_value);
+	}
+}
---
+++ includes/user_fields/user_phone_mobile_include.php
@@ -0,0 +1,28 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| http://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: user_phone_mobile_include.php
+| Author: Chubatyj Vitalij (Rizado)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+if (!defined("IN_FUSION")) {
+	die("Access Denied");
+}
+if ($profile_method == "input") {
+	$options += array('inline'=>1, 'max_length'=>20, 'max_width'=>'200px');
+	$user_fields = form_text($locale['uf_phone_mobile'], 'user_phone_mobile', 'user_phone_mobile', $field_value, $options);
+} elseif ($profile_method == "display") {
+	if ($field_value) {
+		$user_fields = array('title'=>$locale['uf_phone_mobile'], 'value'=>$field_value);
+	}
+}
