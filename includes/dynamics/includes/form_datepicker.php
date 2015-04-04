b789f59b4c152dad324928a921f36a7e1b77eba8
Takács Ákos <rimelek@rimelek.hu>
2015-04-04 17:48:09 +0200

    incomplete documentation of datepicker + some fixes

--- includes/dynamics/includes/form_datepicker.php
+++ includes/dynamics/includes/form_datepicker.php
@@ -17,6 +17,49 @@
 +--------------------------------------------------------*/
 //credits: eternicode @ http://bootstrap-datepicker.readthedocs.org/en/latest/
 //http://bootstrap-datepicker.readthedocs.org/en/release/options.html
+
+/**
+ * Input to save date using datepicker
+ *
+ * TODO: Document safemode
+ * TODO: Document fieldicon_off
+ *
+ * @param $input_name
+ * @param string $label
+ * @param string $input_value
+ * @param array $options
+ * 	<ul>
+ * 		<li><strong>input_id</strong> (string): $input name by default.
+ * 			The value of attribute id of input.</li>
+ * 		<li><strong>type</strong> (string): timestamp by default.
+ * 			Valid types:
+ * 			<ul>
+ * 				<li>date: The date will be saved as mysql date.</li>
+ * 				<li>timestamp: A timestamp will be saved as an integer</li>
+ * 			</ul>
+ * 		</li>
+ * 		<li><strong>date_format</strong> (string): dd-mm-yyyy by default.
+ * 			Date format for datepicker plugin.</li>
+ *		<li><strong>required</strong> (boolean): FALSE by default</li>
+ * 		<li><strong>deactivate</strong> (boolean): FALSE by default.
+ * 			You can pass TRUE and turn off the javascript datepicker plugin</li>
+ * 		<li><strong>width</strong> (string): 250px by default.
+ * 			A valid value for CSS width</li>
+ * 		<li><strong>class</strong> (string): Empty string by default.
+ * 		The value of attribute class of the input.</li>
+ * 		<li><strong>inline</strong> (boolean): FALSE by default.
+ * 			TRUE if the input should be an inline element.</li>
+ * 		<li><strong>error_text</strong> (string): empty string by default.
+ * 			An error message</li>
+ * 		<li><strong>safemode</strong> (boolean): FALSE by default. </li>
+ * 		<li><strong>icon</strong> (string): Empty string by default.
+ * 			The CSS class of the glyphicon. </li>
+ * 		<li><strong>week_start</strong> (int): 0 by default.
+ * 			An integer between 0 and 6. It is the same as
+ * 			the attribute weekStart of datepicker.</li>
+ * 	</ul>
+ * @return string
+ */
 function form_datepicker($input_name, $label = '', $input_value = '', array $options = array()) {
 	global $defender, $locale;
 	if (!defined('DATEPICKER')) {
@@ -45,20 +88,20 @@ function form_datepicker($input_name, $label = '', $input_value = '', array $opt
 		'icon' => '',
 		'date_format' => 'dd-mm-yyyy',
 		'fieldicon_off' => FALSE,
-		'type' => 'datestamp',
+		'type' => 'timestamp',
 		'week_start' => fusion_get_settings('week_start')
 	);
 	$options += $default_options;
 	if (!$options['width']){
 		$options['width'] = $default_options['width'];
 	}
-	if (!in_array($options['type'], array('date', 'datestamp'))) {
+	if (!in_array($options['type'], array('date', 'timestamp'))) {
 		$options['type'] = $default_options['type'];
 	}
 	$options['week_start'] = (int) $options['week_start'];
 	$input_id = $options['input_id'] ? : $default_options['input_id'];
 	$html = "<div id='$input_id-field' class='form-group ".$options['class']." ".($options['icon'] ? 'has-feedback' : '')."'>\n";
-	$html .= ($label) ? "<label class='control-label ".($options['inline'] ? "col-xs-12 col-sm-3 col-md-3 col-lg-3 p-l-0" : '')."' for='$input_id'>$label ".($options['required'] == 1 ? "<span class='required'>*</span>" : '')."</label>\n" : '';
+	$html .= ($label) ? "<label class='control-label ".($options['inline'] ? "col-xs-12 col-sm-3 col-md-3 col-lg-3 p-l-0" : '')."' for='$input_id'>$label ".($options['required'] ? "<span class='required'>*</span>" : '')."</label>\n" : '';
 	$html .= $options['inline'] ? "<div class='col-xs-12 col-sm-9 col-md-9 col-lg-9'>\n" : "";
 	$html .= "<div class='input-group date' ".($options['width'] ? "style='width:".$options['width'].";'" : '').">\n";
 	$html .= "<input type='text' name='".$input_name."' id='".$input_id."' value='".$input_value."' class='form-control textbox' placeholder='".$options['placeholder']."' />\n";
@@ -79,7 +122,7 @@ function form_datepicker($input_name, $label = '', $input_value = '', array $opt
 			 'safemode' 	=> 	$options['safemode'],
 			 'error_text'	=> 	$options['error_text']
 		 ));
-	if ($options['deactivate'] !== 1) {
+	if (!$options['deactivate']) {
 		add_to_jquery("
         $('#$input_id-field .input-group.date').datepicker({
         format: '".$options['date_format']."',
