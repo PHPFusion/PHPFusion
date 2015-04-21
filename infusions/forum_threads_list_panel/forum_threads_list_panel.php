d0993bf21b206829b85be3cbe0092b3660d6c920
Domi <domi@php-fusion.co.uk>
2015-04-21 16:20:44 +0200

    LAFT should work now
    LAFT should work now

--- infusions/forum_threads_list_panel/forum_threads_list_panel.php
+++ infusions/forum_threads_list_panel/forum_threads_list_panel.php
@@ -22,40 +22,38 @@ if (!isset($lastvisited) || !isnum($lastvisited)) {
 	$lastvisited = time();
 }
 
-$data = dbarray(dbquery("SELECT	f.forum_id, f.forum_cat, f.forum_name, f.forum_description, f.forum_mods, f.forum_lastpost, f.forum_postcount,
-	f.forum_threadcount, f.forum_lastuser, f.forum_access, f2.forum_name AS forum_cat_name, f2.forum_description AS forum_cat_description,
-	t.thread_id, t.thread_lastpost, t.thread_lastpostid, t.thread_subject,
+$data = dbarray(dbquery("SELECT f.*, f2.forum_name AS forum_cat_name,
+	t.thread_id, t.thread_lastpost,t.thread_lastuser, t.thread_lastpostid, t.thread_subject,
 	u.user_id, u.user_name, u.user_status, u.user_avatar
 	FROM ".DB_FORUMS." f
 	LEFT JOIN ".DB_FORUMS." f2 ON f.forum_cat = f2.forum_id
-	LEFT JOIN ".DB_FORUM_THREADS." t ON f.forum_lastpostid = t.thread_lastpostid AND f.forum_id = t.forum_id
+	LEFT JOIN ".DB_FORUM_THREADS." t ON f.forum_lastpostid = t.thread_lastpostid
 	LEFT JOIN ".DB_USERS." u ON f.forum_lastuser = u.user_id
 	".(multilang_table("FO") ? "WHERE f2.forum_language='".LANGUAGE."' AND" : "WHERE")." ".groupaccess('f.forum_access')." AND f.forum_type!='1' AND f.forum_type!='3'
 	GROUP BY thread_id ORDER BY t.thread_lastpost LIMIT ".$settings['numofthreads'].""));
 
 $timeframe = empty($data['thread_lastpost']) ? 0 : $data['thread_lastpost'];
 
-$result = dbquery("SELECT	f.forum_id, f.forum_cat, f.forum_name, f.forum_description, f.forum_mods, f.forum_lastpost, f.forum_postcount,
-	f.forum_threadcount, f.forum_lastuser, f.forum_access, f2.forum_name AS forum_cat_name, f2.forum_description AS forum_cat_description,
-	t.thread_id, t.thread_lastpost, t.thread_lastpostid, t.thread_subject, t.thread_postcount, t.thread_views, t.thread_lastuser, t.thread_poll, 
+$result = dbquery("SELECT f.*, f2.forum_name AS forum_cat_name,
+	t.thread_id, t.thread_lastpost, t.thread_lastpostid, t.thread_lastuser, t.thread_poll, t.thread_views, t.thread_postcount, t.thread_subject,
 	u.user_id, u.user_name, u.user_status, u.user_avatar
 	FROM ".DB_FORUMS." f
 	LEFT JOIN ".DB_FORUMS." f2 ON f.forum_cat = f2.forum_id
-	LEFT JOIN ".DB_FORUM_THREADS." t ON f.forum_lastpostid = t.thread_lastpostid AND f.forum_id = t.forum_id
+	LEFT JOIN ".DB_FORUM_THREADS." t ON f.forum_lastpostid = t.thread_lastpostid
 	LEFT JOIN ".DB_USERS." u ON f.forum_lastuser = u.user_id
-	".(multilang_table("FO") ? "WHERE f2.forum_language='".LANGUAGE."' AND" : "WHERE")." ".groupaccess('f.forum_access')." AND f.forum_type!='1' AND f.forum_type!='3' AND t.thread_lastpost >= ".$timeframe." AND t.thread_hidden='0'
+	".(multilang_table("FO") ? "WHERE f.forum_language='".LANGUAGE."' AND" : "WHERE")." ".groupaccess('f2.forum_access')." AND t.thread_lastpost >= ".$timeframe."
 	GROUP BY thread_id ORDER BY t.thread_lastpost LIMIT ".$settings['numofthreads']."");
-
+	
 if (dbrows($result)) {
-	$i = 0;
-	opentable($locale['global_040']);
-	echo "<table cellpadding='0' cellspacing='1' width='100%' class='tbl-border'>\n<tr>\n";
-	echo "<td class='tbl2'>&nbsp;</td>\n";
-	echo "<td width='100%' class='tbl2'><strong>".$locale['global_044']."</strong></td>\n";
-	echo "<td width='1%' class='tbl2' style='text-align:center;white-space:nowrap'><strong>".$locale['global_045']."</strong></td>\n";
-	echo "<td width='1%' class='tbl2' style='text-align:center;white-space:nowrap'><strong>".$locale['global_046']."</strong></td>\n";
-	echo "<td width='1%' class='tbl2' style='text-align:center;white-space:nowrap'><strong>".$locale['global_047']."</strong></td>\n";
-	echo "</tr>\n";
+$i = 0;
+opentable($locale['global_040']);
+echo "<table cellpadding='0' cellspacing='1' width='100%' class='tbl-border'>\n<tr>\n";
+echo "<td class='tbl2'>&nbsp;</td>\n";
+echo "<td width='100%' class='tbl2'><strong>".$locale['global_044']."</strong></td>\n";
+echo "<td width='1%' class='tbl2' style='text-align:center;white-space:nowrap'><strong>".$locale['global_045']."</strong></td>\n";
+echo "<td width='1%' class='tbl2' style='text-align:center;white-space:nowrap'><strong>".$locale['global_046']."</strong></td>\n";
+echo "<td width='1%' class='tbl2' style='text-align:center;white-space:nowrap'><strong>".$locale['global_047']."</strong></td>\n";
+echo "</tr>\n";
 	while ($data = dbarray($result)) {
 		$row_color = ($i%2 == 0 ? "tbl1" : "tbl2");
 		echo "<tr>\n<td class='".$row_color."'>";
@@ -82,16 +80,17 @@ if (dbrows($result)) {
 		echo "</tr>\n";
 		$i++;
 	}
-	echo "</table>\n";
-	if (iMEMBER) {
-		echo "<div class='tbl1' style='text-align:center'><a href='".INFUSIONS."forum_threads_list_panel/my_threads.php'>".$locale['global_041']."</a> ::\n";
-		echo "<a href='".INFUSIONS."forum_threads_list_panel/my_posts.php'>".$locale['global_042']."</a> ::\n";
-		echo "<a href='".INFUSIONS."forum_threads_list_panel/new_posts.php'>".$locale['global_043']."</a>";
-		if ($settings['thread_notify']) {
-			echo " ::\n<a href='".INFUSIONS."forum_threads_list_panel/my_tracked_threads.php'>".$locale['global_056']."</a>";
-		}
-		echo "</div>\n";
+echo "</table>\n";
+
+if (iMEMBER) {
+	echo "<div class='tbl1' style='text-align:center'><a href='".INFUSIONS."forum_threads_list_panel/my_threads.php'>".$locale['global_041']."</a> ::\n";
+	echo "<a href='".INFUSIONS."forum_threads_list_panel/my_posts.php'>".$locale['global_042']."</a> ::\n";
+	echo "<a href='".INFUSIONS."forum_threads_list_panel/new_posts.php'>".$locale['global_043']."</a>";
+	if ($settings['thread_notify']) {
+		echo " ::\n<a href='".INFUSIONS."forum_threads_list_panel/my_tracked_threads.php'>".$locale['global_056']."</a>";
 	}
-	closetable();
+echo "</div>\n";
+}
+closetable();
 }
 ?>
