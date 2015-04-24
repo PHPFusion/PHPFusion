dd256495c048b48f1a33c0aabcb4211835c8de80
Takács Ákos <rimelek@rimelek.hu>
2015-04-24 21:51:12 +0200

    fixed forum image upload (url does not work yet)

--- includes/infusions_include.php
+++ includes/infusions_include.php
@@ -230,6 +230,9 @@ if (!function_exists('upload_file')) {
 // Upload image function
 if (!function_exists('upload_image')) {
 	function upload_image($source_image, $target_name = "", $target_folder = IMAGES, $target_width = "1800", $target_height = "1600", $max_size = "150000", $delete_original = FALSE, $thumb1 = TRUE, $thumb2 = TRUE, $thumb1_ratio = 0, $thumb1_folder = IMAGES, $thumb1_suffix = "_t1", $thumb1_width = "100", $thumb1_height = "100", $thumb2_ratio = 0, $thumb2_folder = IMAGES, $thumb2_suffix = "_t2", $thumb2_width = "400", $thumb2_height = "300", $query = "") {
+		if (strlen($target_folder) > 0 && substr($target_folder, -1) !== '/') {
+			$target_folder .= '/';
+		}
 		if (is_uploaded_file($_FILES[$source_image]['tmp_name'])) {
 			$image = $_FILES[$source_image];
 			if ($target_name != "" && !preg_match("/[^a-zA-Z0-9_-]/", $target_name)) {
