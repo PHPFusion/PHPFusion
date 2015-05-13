6c33a3aa56a990890b6f509119aa5431404fc2e3
Domi <domi@php-fusion.co.uk>
2015-05-13 13:29:41 +0200

    Added faq
    Added faq

---
+++ includes/rewrites/faq_rewrite_include.php
@@ -0,0 +1,30 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| File Category: Core Rewrite Modules
+| Author: Hien (Frederick MC Chan)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+if (!defined("IN_FUSION")) { die("Access Denied"); }
+
+$regex = array("%faq_cat_id%" => "([0-9]+)", "%faq_cat_name%" => "([0-9a-zA-Z._\W]+)");
+
+$pattern = array("Frequently_asked_Questions" => "faq.php",
+				 "faq-cat/%faq_cat_id%/%faq_cat_name%" => "faq.php?cat_id=%faq_cat_id%"
+				 );
+
+$dir_path = ROOT;
+$dbname = DB_FAQ_CATS;
+$dbid = array("%faq_cat_id%" => "faq_cat_id");
+$dbinfo = array("%faq_cat_name%" => "faq_cat_name");
+?>
