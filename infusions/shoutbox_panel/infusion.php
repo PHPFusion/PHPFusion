9e6ecbb2c769f7dd7797dd01ae63d02c2fb1e023
Domi <domi@php-fusion.co.uk>
2015-05-18 10:39:53 +0200

    Completed with some updated fields, added defaults.
    Completed with some updated fields, added defaults as to provide
    examples.

--- infusions/shoutbox_panel/infusion.php
+++ infusions/shoutbox_panel/infusion.php
@@ -37,11 +37,25 @@ $inf_email = "";
 $inf_weburl = "https://www.php-fusion.co.uk";
 $inf_folder = "shoutbox_panel"; // The folder in which the infusion resides.
 
+//Uncomment to enable site link creation
+/*
+$inf_sitelink[1] = array(
+	"title" => "test",
+	"url" => "filename.php",
+	"icon" => "icon.png",
+	"position" => "2",
+	"visibility" => "0");
+*/
+
 //Administration panel
-$inf_adminpanel[1] = array("title" => $locale['SB_admin1'], "image" => "shout.gif", "panel" => "shoutbox_admin.php","rights" => "S");
+$inf_adminpanel[1] = array("title" => $locale['SB_admin1'], 
+						   "image" => "shout.gif", 
+						   "panel" => "shoutbox_admin.php",
+						   "rights" => "S");
 
 //Multilanguage table for Administration
-$inf_mlt[1] = array("title" => $locale['SB_title'], "rights" => "SB");
+$inf_mlt[1] = array("title" => $locale['SB_title'], 
+					"rights" => "SB");
 
 // Delete any items not required below.
 $inf_newtable[1] = DB_SHOUTBOX." (
@@ -58,7 +72,7 @@ $inf_newtable[1] = DB_SHOUTBOX." (
     ) ENGINE=MyISAM DEFAULT CHARSET=UTF8 COLLATE=utf8_unicode_ci";
 
 //Infuse insertations
-$inf_insertdbrow[1] = DB_PANELS." (panel_name, panel_filename, panel_content, panel_side, panel_order, panel_type, panel_access, panel_display, panel_status) VALUES('".$locale['SB_title']."', 'shoutbox_panel', '', '4', '3', 'file', '0', '1', '1')";
+$inf_insertdbrow[1] = DB_PANELS." (panel_name, panel_filename, panel_content, panel_side, panel_order, panel_type, panel_access, panel_display, panel_status, panel_url_list, panel_restriction) VALUES('".$locale['SB_title']."', 'shoutbox_panel', '', '4', '3', 'file', '0', '1', '1', '', '')";
 $inf_insertdbrow[2] = DB_SETTINGS_INF." (settings_name, settings_value, settings_inf) VALUES('visible_shouts', '5', '".$inf_folder."')";
 $inf_insertdbrow[3] = DB_SETTINGS_INF." (settings_name, settings_value, settings_inf) VALUES('guest_shouts', '0', '".$inf_folder."')";
 
