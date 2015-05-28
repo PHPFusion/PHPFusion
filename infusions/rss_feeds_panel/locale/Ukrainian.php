d25935ac25be1affc45e22c1a44610c734c35a6c
kot2007 <koot2007@gmail.com>
2015-05-28 12:40:10 +0300

    Locale sync

---
+++ infusions/rss_feeds_panel/locale/Ukrainian.php
@@ -0,0 +1,10 @@
+<?php
+$locale['rss000'] = " - Блог";
+$locale['rss001'] = " - Форум";
+$locale['rss002'] = " - Статті";
+$locale['rss003'] = " - Завантаження";
+$locale['rss004'] = " - Новини";
+$locale['rss005'] = " - Посилання";
+$locale['rss007'] = "в";
+$locale['rss008'] = "Дані для RSS відсутні";
+$locale['rss009'] = "Стрічка RSS";
