9d15e9a1b2a99139ce9c7a66c05d26834e7e60c2
Vitalij <v.chubatyj@ukr.net>
2015-06-01 16:55:21 +0300

    Russian locale sync

--- infusions/member_poll_panel/locale/Russian/member_poll_panel.php
+++ infusions/member_poll_panel/locale/Russian/member_poll_panel.php
@@ -1,16 +1,16 @@
 <?php
 // Make these more unique to the panel at some point
-$locale['global_130'] = "Member Polls";
-$locale['global_131'] = "Submit Vote";
-$locale['global_132'] = "You must login to vote.";
-$locale['global_133'] = "Vote";
-$locale['global_134'] = "Votes";
-$locale['global_135'] = "Votes: ";
-$locale['global_136'] = "Started: ";
-$locale['global_137'] = "Ended: ";
-$locale['global_138'] = "Polls Archive";
-$locale['global_139'] = "Please select a poll to view from the list:";
-$locale['global_140'] = "View";
-$locale['global_141'] = "View Poll";
-$locale['global_142'] = "There are no polls defined.";
-$locale['global_143'] = "Ratings";
+$locale['global_130'] = "Опрос пользователей";
+$locale['global_131'] = "Проголосовать";
+$locale['global_132'] = "Войдите для голосования.";
+$locale['global_133'] = "Голос";
+$locale['global_134'] = "Голосов";
+$locale['global_135'] = "Голосов: ";
+$locale['global_136'] = "начало: ";
+$locale['global_137'] = "Окончание: ";
+$locale['global_138'] = "Архив опросов";
+$locale['global_139'] = "Пожалуйста, выберите опрос из списка для просмотра:";
+$locale['global_140'] = "Просмотр";
+$locale['global_141'] = "Смотреть опрос";
+$locale['global_142'] = "Опросы не созданы.";
+$locale['global_143'] = "Оценки.";
--- locale/Russian/global.php
+++ locale/Russian/global.php
@@ -215,21 +215,7 @@ $locale['UM101'] = "Изменить язык";
 $locale['UM098'] = "Входящие сообщения:";
 $locale['UM099'] = "Отправленные сообщения:";
 $locale['UM100'] = "Архив сообщений:";
-// Poll
-$locale['global_130'] = "Опрос пользователей";
-$locale['global_131'] = "Проголосовать";
-$locale['global_132'] = "Войдите для голосования.";
-$locale['global_133'] = "Голос";
-$locale['global_134'] = "Голосов";
-$locale['global_135'] = "Голосов: ";
-$locale['global_136'] = "начало: ";
-$locale['global_137'] = "Окончание: ";
-$locale['global_138'] = "Архив опросов";
-$locale['global_139'] = "Пожалуйста, выберите опрос из списка для просмотра:";
-$locale['global_140'] = "Просмотр";
-$locale['global_141'] = "Смотреть опрос";
-$locale['global_142'] = "Опросы не созданы.";
-$locale['global_143'] = "Оценки.";
+
 // Keywords and Meta
 $locale['tags'] = "Тэги";
 // Captcha
