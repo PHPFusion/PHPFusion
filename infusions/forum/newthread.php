540f626b0cafb4f71595fce4fb4b440134d9cf5a
Domi <domi@php-fusion.co.uk>
2015-06-22 09:40:10 +0200

    Fixed redirect notice
    Fixed redirect notice

--- infusions/forum/newthread.php
+++ infusions/forum/newthread.php
@@ -146,7 +146,8 @@ if (iMEMBER && PHPFusion\Forums\Functions::verify_forum($_GET['forum_id'])) {
 					}
 				}
 				$error = defined("FUSION_NULL") ? '1' : '0';
-				redirect("postify.php?post=new&error=$error&amp;forum_id=".intval($post_data['forum_id'])."&amp;parent_id=".intval($post_data['forum_cat'])."&amp;forum_branch=".intval($post_data['forum_branch'])."&amp;thread_id=".intval($post_data['thread_id'].""));
+				//redirect("postify.php?post=new&error=$error&amp;forum_id=".intval($post_data['forum_id'])."&amp;parent_id=".intval($post_data['forum_cat'])."&amp;forum_branch=".intval($post_data['forum_branch'])."&amp;thread_id=".intval($post_data['thread_id'].""));
+				redirect("postify.php?post=new&error=$error&amp;forum_id=".intval($post_data['forum_id'])."&amp;forum_branch=".intval($post_data['forum_branch'])."&amp;thread_id=".intval($post_data['thread_id'].""));
 			}
 		}
 
