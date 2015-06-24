8e0ffdfa6e844d46063117018b95437ebac8aa8c
hien- <meangczac.chan@gmail.com>
2015-06-24 15:26:27 +0800

    Septenary Responsive Menu Grid Fix.

--- themes/Septenary/includes/content.php
+++ themes/Septenary/includes/content.php
@@ -23,13 +23,14 @@ if (!defined("IN_FUSION")) { die("Access Denied"); }
 open_grid('section-3', 1);
 echo AU_CENTER ? "<div class='au-content'>".AU_CENTER."</div>\n" : '';
 echo "<div class='row'>\n";
-echo "<div class='hidden-xs col-sm-3 col-md-3 col-lg-3 leftbar'>\n";
-echo RIGHT.LEFT;
-echo "</div>\n";
-echo "<div class='col-xs-12 col-sm-9 col-md-9 col-lg-9 main-content'>\n";
+if (LEFT !=='' or RIGHT !=='') {
+	echo "<div class='hidden-xs col-sm-3 col-md-3 col-lg-3 leftbar'>\n";
+	echo RIGHT.LEFT;
+	echo "</div>\n";
+}
+echo "<div class='".how_to_calculate_bootstrap_span()."main-content'>\n";
 // Get all notices, we also include notices that are meant to be displayed on all pages
-$notices = getNotices(array('all', FUSION_SELF));
-echo renderNotices($notices);
+echo renderNotices(getNotices(array('all', FUSION_SELF)));
 echo U_CENTER;
 echo CONTENT;
 echo L_CENTER;
@@ -37,3 +38,39 @@ echo "</div>\n";
 echo BL_CENTER ? "<div class='bl-content'>".BL_CENTER."</div>\n" : '';
 echo "</div>\n";
 close_grid(1);
+
+/**
+ * You can have many formula to it..
+ * @return string
+ */
+function how_to_calculate_bootstrap_span() {
+
+	$default_side_span_sm = 3; // <---- change this to change the sidebar width on tablet
+	$default_side_span_md = 3; //<--- change this to change the sidebar width on laptop
+	$default_side_span_lg = 3; // <---- change this to change the sidebar width on desktop
+	$how_many_sides_are_visible = 0;
+
+	if (defined('LEFT') && LEFT !=='') $how_many_sides_are_visible++;
+	if (defined('RIGHT') && RIGHT !=='') $how_many_sides_are_visible++;
+
+	if ($how_many_sides_are_visible > 0) {
+		$span =  array(
+			'col-xs-' => 12,
+			'col-sm-' => 12-($how_many_sides_are_visible*$default_side_span_sm),
+			'col-md-' => 12-($how_many_sides_are_visible*$default_side_span_md),
+			'col-lg-' => 12-($how_many_sides_are_visible*$default_side_span_lg),
+		);
+	} else {
+		$span = array(
+			'col-xs-' => 12,
+			'col-sm-' => 12,
+			'col-md-' => 12,
+			'col-lg-' => 12,
+		);
+	}
+	$css = '';
+	foreach($span as $css_class => $css_value) {
+		$css .= "".$css_class.$css_value." ";
+	}
+	return $css;
+}
