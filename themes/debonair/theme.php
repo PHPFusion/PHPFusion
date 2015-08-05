fcbdee4ae289505f7e9bc5372b2da7541490d799
hien- <meangczac.chan@gmail.com>
2015-08-05 20:17:39 +0800

    Debonair - Logo, Button tweak

--- themes/debonair/theme.php
+++ themes/debonair/theme.php
@@ -56,7 +56,7 @@ function render_page($license = FALSE) {
 	echo "<div class='body-inner-wrap'>\n";
 	// start header ----
 	echo "<header class='clearfix m-t-10'>
-		<a class='logo' href='".BASEDIR."index.php'><img src='".THEME."images/logo.gif' alt='Web 2.0 Business Template'/></a>
+		<a class='logo' href='".BASEDIR."index.php'><img src='".IMAGES."php-fusion-logo.png' alt='PHP-Fusion'/></a>
 		<div class='tagline'>Super Clean Web 2.0 Business Template</div>\n";
 	echo "<div class='call-tag'>\n";
 	if (iADMIN) {
