1493b51774ef85b4772f5f90d8390fd33f896ce0
Domi <domi@php-fusion.co.uk>
2015-08-26 12:44:12 +0200

    new weblinks submitpage
    new weblinks submitpage

--- submit.php
+++ submit.php
@@ -37,46 +37,7 @@ if (!$sum or empty($modules[$stype])) {
 	redirect("index.php");
 
 } elseif ($stype === "l") {
-	if (isset($_POST['submit_link'])) {
-		$submit_info['link_category'] = form_sanitizer($_POST['link_category'], '', 'link_category');
-		$submit_info['link_name'] = form_sanitizer($_POST['link_name'], '', 'link_name');
-		$submit_info['link_url'] = form_sanitizer($_POST['link_url'], '', 'link_url');
-		$submit_info['link_description'] = form_sanitizer($_POST['link_description'], '', 'link_description');
-		if (!defined("FUSION_NULL")) {
-			$result = dbquery("INSERT INTO ".DB_SUBMISSIONS." (submit_type, submit_user, submit_datestamp, submit_criteria) VALUES ('l', '".$userdata['user_id']."', '".time()."', '".addslashes(serialize($submit_info))."')");
-			add_to_title($locale['global_200'].$locale['400']);
-			opentable($locale['400']);
-			echo "<div style='text-align:center'><br />\n".$locale['410']."<br /><br />\n";
-			echo "<a href='submit.php?stype=l'>".$locale['411']."</a><br /><br />\n";
-			echo "<a href='index.php'>".$locale['412']."</a><br /><br />\n</div>\n";
-			closetable();
-		}
-	}
-	add_to_title($locale['global_200'].$locale['400']);
-	opentable($locale['400']);
-	$result = dbquery("SELECT weblink_cat_id, weblink_cat_name FROM ".DB_WEBLINK_CATS." ".(multilang_table("WL") ? "WHERE weblink_cat_language='".LANGUAGE."'" : "")." ORDER BY weblink_cat_name");
-	if (dbrows($result) > 0) {
-		$opts = array();
-		while ($data = dbarray($result)) {
-			$opts[$data['weblink_cat_id']] = $data['weblink_cat_name'];
-		}
-		echo "<div class='panel panel-default tbl-border'>\n<div class='panel-body'>\n";
-		echo "<div class='alert alert-info m-b-20 submission-guidelines'>".$locale['420']."</div>\n";
-		echo openform('submit_form', 'post', ($settings['site_seo'] ? FUSION_ROOT : '').BASEDIR."submit.php?stype=l", array('max_tokens' => 1));
-		echo form_select('link_category', $locale['421'], isset($_POST['link_category']) ? $_POST['link_category'] : '', array("options" => $opts,
-			"required" => TRUE));
-		echo form_text('link_name', $locale['422'], isset($_POST['link_name']) ? $_POST['link_name'] : '', array("required" => TRUE));
-		echo form_text('link_url', $locale['423'], isset($_POST['link_url']) ? $_POST['link_url'] : '', array("required" => TRUE,
-			'placeholder' => 'http://'));
-		echo form_text('link_description', $locale['424'], isset($_POST['link_description']) ? $_POST['link_description'] : '', array("required" => TRUE,
-			'max_length' => '200'));
-		echo form_button('submit_link', $locale['425'], $locale['425'], array('class' => 'btn-primary'));
-		echo closeform();
-		echo "</div>\n</div>\n";
-	} else {
-		echo "<div class='well' style='text-align:center'><br />\n".$locale['551']."<br /><br />\n</div>\n";
-	}
-	closetable();
+	include INFUSIONS."weblinks/weblinks_submit.php";
 } elseif ($stype === "n") {
 	include INFUSIONS."news/news_submit.php";
 } elseif ($stype === "b") {
