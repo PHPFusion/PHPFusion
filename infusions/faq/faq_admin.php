40fedd9d8b4f3bb04bc7c7fadd0bc17444a2e792
hien- <meangczac.chan@gmail.com>
2015-08-28 15:39:08 +0800

    FAQ locale - (Faq 3/3)

--- infusions/faq/faq_admin.php
+++ infusions/faq/faq_admin.php
@@ -28,46 +28,6 @@ $data = array(
 	"faq_cat_description" => "",
 	"faq_cat_language" => LANGUAGE,
 );
-$locale['faq_0100'] = "FAQs";
-$locale['faq_0101'] = "Currrent FAQs";
-$locale['faq_0102'] = "FAQ Category";
-$locale['faq_0102b'] = "Add FAQ";
-$locale['faq_0102c'] = "Edit FAQ";
-$locale['faq_0102d'] = "Edit FAQ Category";
-$locale['faq_0103'] = "Category Name";
-$locale['faq_0104'] = "Questions Count";
-$locale['faq_0105'] = "Category Id";
-$locale['faq_0106'] = "Options";
-$locale['faq_0107'] = "Edit";
-$locale['faq_0108'] = "Delete";
-$locale['faq_0109'] = "Delete this FAQ Category?";
-$locale['faq_0110'] = "Question:";
-$locale['faq_0111'] = "Answer:";
-$locale['faq_0112'] = "Delete this Question?";
-$locale['faq_0113'] = "No Frequently Asked Question defined";
-$locale['faq_0114'] = "Listing %d of total %d FAQs entries";
-$locale['faq_0115'] = "Listing %d of total %d Categories";
-
-
-// Faq Category form
-$locale['faq_0200'] = "Category Name";
-$locale['faq_0201'] = "Fill in category name";
-$locale['faq_0202'] = "Category Description";
-$locale['faq_0203'] = "Save Category";
-$locale['faq_0204'] = "FAQ Category saved";
-$locale['faq_0205'] = "FAQ Category updated";
-$locale['faq_0206'] = "FAQ Category deleted";
-$locale['faq_0207'] = "FAQ Category cannot be deleted because there are %d questions in this category";
-// Faq Form
-$locale['faq_0300'] = "Category";
-$locale['faq_0301'] = "Question";
-$locale['faq_0302'] = "Answer";
-$locale['faq_0303'] = "Save FAQ";
-$locale['faq_0304'] = "FAQ is not available currently because there are no FAQ Category defined. Please click <a href='%s'>here</a> to add a FAQ category";
-$locale['faq_0305'] = "FAQ added";
-$locale['faq_0306'] = "FAQ updated";
-$locale['faq_0307'] = "FAQ deleted";
-
 $faq_edit = isset($_GET['action']) && $_GET['action'] == "edit" && isset($_GET['faq_id']) && isnum($_GET['faq_id']) ? TRUE : FALSE;
 $faqCat_edit = isset($_GET['action']) && $_GET['action'] == "edit" && isset($_GET['cat_id']) && isnum($_GET['cat_id']) ? TRUE : FALSE;
 opentable($locale['faq_0100']);
