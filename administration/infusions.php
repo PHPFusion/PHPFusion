d3a9f5487d339132f7bf96144743b1b83e9ab5ce
hien- <meangczac.chan@gmail.com>
2015-09-01 13:44:26 +0800

    Admin Page Locked - set min and max admin_page range. Unlocks in future revisions

--- administration/infusions.php
+++ administration/infusions.php
@@ -141,7 +141,7 @@ if (isset($_POST['infuse']) && isset($_POST['infusion'])) {
 				$error = 0;
 				foreach ($inf_adminpanel as $item) {
 					$inf_admin_image = ($item['image'] ? : "infusion_panel.gif");
-					$item_page = ($item['page'] && isnum($item['page'])) ? $item['page'] : 5;
+					$item_page = ($item['page'] && isnum($item['page']) && $item['page'] > 0 && $item['page'] <=5) ? $item['page'] : 5;
 					if (!dbcount("(admin_id)", DB_ADMIN, "admin_rights='".$item['rights']."'")) {
 						dbquery("INSERT INTO ".DB_ADMIN." (admin_rights, admin_image, admin_title, admin_link, admin_page) VALUES ('".$item['rights']."', '".$inf_admin_image."', '".$item['title']."', '".INFUSIONS.$inf_folder."/".$item['panel']."', '".$item['page']."')");
 						$result = dbquery("SELECT user_id, user_rights FROM ".DB_USERS." WHERE user_level=".USER_LEVEL_SUPER_ADMIN);
