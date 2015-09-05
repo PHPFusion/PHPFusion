8067dc33208191ec78709ccf4afc9679e95aac0d
hien- <meangczac.chan@gmail.com>
2015-09-06 00:21:11 +0800

    Removed the need for table-content. (opentabbody function)

--- administration/site_links.php
+++ administration/site_links.php
@@ -16,10 +16,9 @@
 | written permission from the original author(s).
 +--------------------------------------------------------*/
 require_once "../maincore.php";
-pageAccess('SL');
+pageAccess("SL");
 require_once THEMES."templates/admin_header.php";
 include LOCALE.LOCALESET."admin/sitelinks.php";
-
 $site_links = new \PHPFusion\SiteLinks();
 $edit = isset($_GET['link_id']) ? $site_links->verify_edit($_GET['link_id']) : 0;
 $master_title['title'][] = $locale['SL_0001'];
@@ -32,13 +31,10 @@ $tab_active = tab_active($master_title, $edit ?  1 : 0, 1);
 opentable($locale['SL_0012']);
 echo "<div id='info'></div>\n";
 echo opentab($master_title, $tab_active, 'link', FUSION_SELF);
-echo opentabbody($master_title['title'][0], 'links', $tab_active, 1);
-$site_links->menu_listing();
-echo closetabbody();
-if (isset($_GET['section']) && $_GET['section'] == 'nform') {
-	echo opentabbody($master_title['title'][1], 'nform', $tab_active, 1);
+if (isset($_GET['section']) && $_GET['section'] == "nform") {
 	$site_links->menu_form();
-	echo closetabbody();
+} else {
+	$site_links->menu_listing();
 }
 echo closetab();
 closetable();
