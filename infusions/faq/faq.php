ca7c83386af502e3300be254483091a018c9cba5
hien- <meangczac.chan@gmail.com>
2015-09-06 10:02:07 +0800

    FAQ - Complete Template

--- infusions/faq/faq.php
+++ infusions/faq/faq.php
@@ -17,55 +17,29 @@
 +--------------------------------------------------------*/
 require_once file_exists('maincore.php') ? 'maincore.php' : __DIR__."/../../maincore.php";
 require_once THEMES."templates/header.php";
-
 include INFUSIONS."faq/locale/".LOCALESET."faq.php";
 include "templates/faq.php";
-
 add_to_title($locale['global_203']);
-
 if (isset($_GET['cat_id']) && isnum($_GET['cat_id'])) {
-
-	if ($data = dbarray(dbquery("SELECT faq_cat_name,faq_cat_language FROM ".DB_FAQ_CATS." ".(multilang_table("FQ") ? "WHERE faq_cat_language='".LANGUAGE."' AND" : "WHERE")." faq_cat_id='".intval($_GET['cat_id'])."'"))) {
+	$result = dbquery("SELECT *	FROM ".DB_FAQ_CATS." ".(multilang_table("FQ") ? "WHERE faq_cat_language='".LANGUAGE."' AND" : "WHERE")." faq_cat_id='".intval($_GET['cat_id'])."'");
+	if (dbrows($result) > 0) {
+		$data = dbarray($result);
 		add_to_title($locale['global_201'].$data['faq_cat_name']);
-		opentable($locale['401'].": ".$data['faq_cat_name']);
-		echo "<table cellpadding='0' cellspacing='1' width='100%'>\n<tr>\n";
-		echo "<td class='tbl2'>\n<a href='".INFUSIONS."faq/faq.php'>".$locale['400']."</a> &gt;";
-		echo "<a href='".INFUSIONS."faq/faq.php?cat_id=".$_GET['cat_id']."'>".$data['faq_cat_name']."</a></td>\n";
-		echo "</tr>\n</table>\n";
-		$rows = dbcount("(faq_id)", DB_FAQS, "faq_cat_id='".$_GET['cat_id']."'");
-		if ($rows) {
-			$i = 0;
-			$ii = 1;
-			$columns = 4;
-			$faq_content = "";
-			echo "<table cellpadding='0' cellspacing='0' width='100%'>\n<tr>\n";
-			$result = dbquery("SELECT faq_id, faq_question, faq_answer from ".DB_FAQS." WHERE faq_cat_id='".$_GET['cat_id']."' ORDER BY faq_question");
-			$numrows = dbrows($result);
+		$data['faq_link'] = INFUSIONS."faq/faq.php?cat_id=".$data['faq_cat_id'];
+		$info = $data;
+		if (dbcount("(faq_id)", DB_FAQS, "faq_cat_id='".intval($_GET['cat_id'])."'")) {
+			$result = dbquery("SELECT faq_id, faq_question, faq_answer from ".DB_FAQS." WHERE faq_cat_id='".intval($_GET['cat_id'])."' ORDER BY faq_question");
 			while ($data = dbarray($result)) {
-				if ($i != 0 && ($i%$columns == 0)) {
-					echo "</tr>\n<tr>\n";
-				}
-				echo "<td class='tbl1' width='25%'><a href='".INFUSIONS."faq/faq.php#faq_".$data['faq_id']."'>".$data['faq_question']."</a></td>";
-				$faq_content .= "<div class='".($ii%2 == 0 ? "tbl1" : "tbl2")."' style='display:block; padding:10px 5px'>\n";
-				$faq_content .= "<a id='faq_".$data['faq_id']."'></a><strong>".$data['faq_question']."</strong><br />\n".nl2br(html_entity_decode(stripslashes($data['faq_answer'])));
-				$faq_content .= "<br style='clear:both' /><a href='".INFUSIONS."faq/faq.php#content'><span class='small'>".$locale['402']."</span></a><br />\n";
-				$faq_content .= "</div>\n";
-				$i++;
-				$ii++;
+				$info['items'][$data['faq_id']] = $data;
 			}
-			echo "</tr>\n</table>\n";
-			echo "<div style='margin:5px'></div>\n";
-			echo "<div class='tbl-border' style='padding:1px'>\n".$faq_content."</div>\n";
-			closetable();
 		} else {
-			echo $locale['411']."\n";
-			closetable();
+			$info['nofaq_items'] = $locale['411'];
 		}
+		render_faq_item($info);
 	} else {
 		redirect(FUSION_SELF);
 	}
 } else {
-
 	$result = dbquery("
 				SELECT fc.faq_cat_id, fc.faq_cat_name, fc.faq_cat_description, fc.faq_cat_language,
 				count(f.faq_id) 'faq_count'
@@ -76,7 +50,7 @@ if (isset($_GET['cat_id']) && isnum($_GET['cat_id'])) {
 	 			ORDER BY faq_cat_name
 	 			");
 	$info['faq_title'] = $locale['400'];
-	if (dbrows($result)>0) {
+	if (dbrows($result) > 0) {
 		while ($data = dbarray($result)) {
 			$data['faq_link'] = INFUSIONS."faq/faq.php?cat_id=".$data['faq_cat_id'];
 			$info['items'][$data['faq_cat_id']] = $data;
--- infusions/faq/templates/faq.php
+++ infusions/faq/templates/faq.php
@@ -1,11 +1,27 @@
 <?php
-
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: templates/faq.php
+| Author: PHP-Fusion Development Team
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+// Main View
 function render_faq_main($info) {
 	opentable($info['faq_title']);
 	echo "<!--pre_faq_idx-->\n";
 	if (!empty($info['items']) && count($info['items'])) {
 		echo "<div class='list-group'>\n";
-		foreach($info['items'] as $data) {
+		foreach ($info['items'] as $data) {
 			echo "<div class='list-group-item'>\n";
 			echo "<h4 style='width:100%'><a href='".$data['faq_link']."'>".$data['faq_cat_name']."</a><span class='badge pull-right'>".$data['faq_count']."</span></h4>\n";
 			if ($data['faq_cat_description']) {
@@ -16,6 +32,37 @@ function render_faq_main($info) {
 		echo "</div>\n";
 	} else {
 		echo "<div class='well text-center'>".$info['nofaqs']."</div>\n";
+	}
+	closetable();
+}
+// Category View
+function render_faq_item($info) {
+	global $locale;
+	echo "<span id='content'></span>\n";
+	opentable($locale['401'].": ".$info['faq_cat_name']);
+	echo "<a href='".INFUSIONS."faq/faq.php'>".$locale['400']."</a> &gt; <a href='".$info['faq_link']."'>".$info['faq_cat_name']."</a>\n";
+	if (!empty($info['nofaq_items'])) {
+		echo "<div class='well text-center m-t-20'>".$info['nofaq_items']."</div>\n";
+	} else {
+		echo "<div class='row m-t-20'>\n";
+		echo "<div class='col-xs-12 col-sm-3'>\n";
+		if (!empty($info['items'])) {
+			echo "<ul>\n";
+			foreach ($info['items'] as $data) {
+				echo "<li><a href='".FUSION_REQUEST."#faq_".$data['faq_id']."'>".$data['faq_question']."</a></li>\n";
+			}
+			echo "</ul>\n";
+		}
+		echo "</div>\n";
+		echo "<div class='col-xs-12 col-sm-9'>\n";
+		foreach ($info['items'] as $data) {
+			echo "<a class='pull-right btn btn-xs btn-default' href='".FUSION_REQUEST."#content'><i class='fa fa-arrow-up'></i> ".$locale['402']."</a>\n";
+			echo "<h4 id='faq_".$data['faq_id']."'>".$data['faq_question']."</h4>\n";
+			echo nl2br(html_entity_decode(stripslashes($data['faq_answer'])));
+			echo "<hr/>\n";
+		}
+		echo "</div>\n";
+		echo "</div>\n";
 	}
 	closetable();
 }
