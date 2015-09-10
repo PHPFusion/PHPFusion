82aa85c86e50e23c8ffe32115dd72e738a366093
hien- <meangczac.chan@gmail.com>
2015-09-10 14:11:10 +0800

    Weblink - Git one time

--- infusions/weblinks/templates/weblinks.php
+++ infusions/weblinks/templates/weblinks.php
@@ -15,8 +15,8 @@
 | copyright header is strictly prohibited without
 | written permission from the original author(s).
 +--------------------------------------------------------*/
+// This is new code
 if (!defined("IN_FUSION")) { die("Access Denied"); }
-
 if (!function_exists('render_weblinks_item')) {
 	function render_weblinks_item($info) {
 		global $locale;
