eae5200018ac24b777c71faf265f55b74d3744bd
Björn Dahlgren <bjorn@dahlgren.at>
2015-10-02 00:53:18 +0200

    Install infusions step had incorrect variable used for inserting rows. Seems to be copypasted from administration page.

--- install/index.php
+++ install/index.php
@@ -734,7 +734,7 @@ switch (INSTALLATION_STEP) {
 						}
 						if ($inf['insertdbrow'] && is_array($inf['insertdbrow'])) {
 							$last_id = 0;
-							foreach ($inf_insertdbrow as $insertdbrow) {
+							foreach ($inf['insertdbrow'] as $insertdbrow) {
 								if (stristr($insertdbrow, "{last_id}") && !empty($last_id)) {
 									dbquery("INSERT INTO ".str_replace("{last_id}", $last_id, $insertdbrow));
 								} else {
