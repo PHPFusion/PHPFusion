c0b5ff683ab909b15b1b51d51c06015b3aaa718d
hien- <meangczac.chan@gmail.com>
2015-10-12 10:44:22 +0800

    Checkbox Jquery for child_of replacement - use $options['options']

--- includes/dynamics/includes/form_checkbox.php
+++ includes/dynamics/includes/form_checkbox.php
@@ -17,7 +17,6 @@
 +--------------------------------------------------------*/
 function form_checkbox($input_name, $label = '', $input_value = '0', array $options = array()) {
 	global $defender, $locale;
-
 	$options += array(
 		'input_id' => !empty($options['input_id']) ? $options['input_id'] : $input_name,
 		'class' => !empty($options['class']) ? $options['class'] : '',
@@ -36,9 +35,6 @@ function form_checkbox($input_name, $label = '', $input_value = '0', array $opti
 		'value' => !empty($options['value']) && $options['value'] ? $options['value'] : 1,
 		'tip' => !empty($options['tip']) ? "title='".$options['tip']."'" : '',
 		"reverse_label" => !empty($options['reverse_label']) ? true : false,
-		// If this checkbox would be marked as a child of another checkbox
-		// it would get disabled when the main checkbox is unchecked. Not completed yet.
-		'child_of' => !empty($options['child_of']) ? $options['child_of'] : '',
 	);
 	if ($options['toggle'] && !defined("BOOTSTRAP_SWITCH_ASSETS")) {
 		define("BOOTSTRAP_SWITCH_ASSETS", TRUE);
@@ -56,6 +52,19 @@ function form_checkbox($input_name, $label = '', $input_value = '0', array $opti
 		$options['toggle'] = "";
 		$input_value = explode(",", $input_value); // require key to value
 		$input_value = array_combine(array_keys($options['options']), $input_value);
+		if (!empty($label)) {
+			add_to_jquery("
+			$('#".$options['input_id']."-field > .control-label').bind('click', function() {
+				var checked_status = $(this).data('checked');
+				$('#".$options['input_id']."-field input:checkbox').prop('checked', $(this).data('checked'));
+				if ($(this).data('checked') == '1') {
+					$(this).data('checked', 0);
+				} else {
+					$(this).data('checked', 1);
+				}
+			});
+			");
+		}
 	} else {
 		$switch_class = $options['toggle'] ? "is-bootstrap-switch" : "";
 		$on_label = $options['toggle_text'][1];
@@ -69,50 +78,28 @@ function form_checkbox($input_name, $label = '', $input_value = '0', array $opti
 	$checkbox = $options['inline'] ? "<div class='col-xs-12 col-sm-9 col-md-9 col-lg-9'>\n" : "\n";
 	if (!empty($options['options']) && is_array($options['options'])) {
 		foreach($options['options'] as $key => $value) {
-			$checkbox .= "<input id='".$options['input_id']."-$key' style='vertical-align: middle' ".($options['child_of'] ? "data-child-of='".$options['child_of']."'" : "")." name='$input_name' value='$value' type='".$options['type']."' ".($options['disabled'] ? 'disabled' : '')." ".($input_value[$key] == '1' ? 'checked' : '')." /> <label class='control-label m-r-10' for='".$options['input_id']."-$key'>".$value."</label>\n";
+			$checkbox .= "<input id='".$options['input_id']."-$key' style='vertical-align: middle' name='$input_name' value='$value' type='".$options['type']."' ".($options['disabled'] ? 'disabled' : '')." ".($input_value[$key] == '1' ? 'checked' : '')." /> <label class='control-label m-r-10' for='".$options['input_id']."-$key'>".$value."</label>\n";
 		}
 	} else {
-		$checkbox .= "<input id='".$options['input_id']."' ".($options['toggle'] ? "data-on-text='".$on_label."' data-off-text='".$off_label."'" : "")." style='margin: 0;vertical-align: middle' ".($options['child_of'] ? "data-child-of='".$options['child_of']."'" : "")." name='$input_name' value='".$options['value']."' type='checkbox' ".($options['disabled'] ? 'disabled' : '')." ".($input_value == '1' ? 'checked' : '')." />\n";
+		$checkbox .= "<input id='".$options['input_id']."' ".($options['toggle'] ? "data-on-text='".$on_label."' data-off-text='".$off_label."'" : "")." style='margin: 0;vertical-align: middle' name='$input_name' value='".$options['value']."' type='checkbox' ".($options['disabled'] ? 'disabled' : '')." ".($input_value == '1' ? 'checked' : '')." />\n";
 	}
 	$checkbox .= $defender->inputHasError($input_name) ? "<div id='".$options['input_id']."-help' class='label label-danger p-5 display-inline-block'>".$options['error_text']."</div>" : "";
 	$checkbox .= $options['inline'] ? "</div>\n" : "";
-
 	$html = "<div id='".$options['input_id']."-field' class='$switch_class $error_class form-group clearfix ".$options['class']."'>\n";
-	$html .= "<label class='control-label ".($options['inline'] ? "col-xs-12 col-sm-3 col-md-3 col-lg-3 p-l-0" : '')."' for='".$options['input_id']."'>\n";
+	$html .= (!empty($label)) ? "<label class='control-label ".($options['inline'] ? "col-xs-12 col-sm-3 col-md-3 col-lg-3 p-l-0" : '')."' data-checked='".(!empty($input_value) ? "1" : "0")."'  for='".$options['input_id']."'>\n" : "";
 	$html .= ($options['reverse_label'] == true) ? $checkbox : "";
-	$html .= "$label ".($options['required'] == 1 ? "<span class='required'>*</span>" : '')." ".($options['tip'] ? "<i class='pointer fa fa-question-circle' title='".$options['tip']."'></i>" : '')."</label>\n";
+	$html .= (!empty($label)) ? "$label ".($options['required'] == 1 ? "<span class='required'>*</span>" : '')." ".($options['tip'] ? "<i class='pointer fa fa-question-circle' title='".$options['tip']."'></i>" : '')."</label>\n" : "";
 	$html .= ($options['reverse_label'] == false) ? $checkbox : "";
 	$html .= "</div>\n";
 
-	$defender->add_field_session(array(
-									 'input_name' => $input_name,
-									 'title' => $title,
-									 'id' => $options['input_id'],
-									 'type' => $options['type'],
-									 'child_of' => $options['child_of'],
-									 'required' => $options['required'],
-								 ));
-	// Experimental stuff
-	if (!empty($options['child_of']) && !defined($input_name.'_JS')) {
-		define($input_name.'_JS', TRUE);
-		add_to_jquery("
-		$('#".$options['child_of']."').each(function() {
-			if (this.checked) {
-				$('input[data-child-of=".$options['child_of']."]').removeAttr('disabled');
-			} else {
-				$('input[data-child-of=".$options['child_of']."]').attr('disabled', 'disabled');
-			}
-		});
-
-		$('#".$options['child_of']."').change(function(){
-			if (this.checked) {
-				$('input[data-child-of=".$options['child_of']."]').removeAttr('disabled');
-			} else {
-				$('input[data-child-of=".$options['child_of']."]').attr('disabled', 'disabled');
-			}
-		});
-	
-		");
-	}
+	$defender->add_field_session(
+		array(
+			 'input_name' => $input_name,
+			 'title' => $title,
+			 'id' => $options['input_id'],
+			 'type' => $options['type'],
+			 'child_of' => $options['child_of'],
+			 'required' => $options['required'],
+		));
 	return $html;
 }
