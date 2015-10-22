f57358f0bb1557b0bf79a7d1711f01f336423949
Talocha <mateofds@hotmail.com>
2015-10-22 12:31:59 +0200

    Create custom_pages.php

---
+++ locale/Spanish/custom_pages.php
@@ -0,0 +1,5 @@
+<?php
+$locale['400'] = "<span style='color:#ff0000;'><b>Â¡Acceso Denegado!</b></span><br />No tienes permiso para ver esta pÃ¡gina.";
+$locale['401'] = "Error Interno";
+$locale['402'] = "No se ha podido obtener el contenido de la pÃ¡gina.";
+$locale['403'] = "AtrÃ¡s";?>
