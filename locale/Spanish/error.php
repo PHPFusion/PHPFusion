864710497070b66706a33551119a990ca744822a
Talocha <mateofds@hotmail.com>
2015-10-22 13:41:00 +0200

    Create error.php

---
+++ locale/Spanish/error.php
@@ -0,0 +1,7 @@
+<?php
+$locale['err401'] = "Acceso no autorizado a esta pÃ¡gina!";
+$locale['err403'] = "Acceso prohibido a esta pÃ¡gina!";
+$locale['err404'] = "Esta pÃ¡gina no se encuentra en este servidor.";
+$locale['err500'] = "Error interno del servidor.";
+$locale['errunk'] = "Error desconocido";
+$locale['errret'] = "Volver al sitio Principal";
