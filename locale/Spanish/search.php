d540a1ad037d7e1269198e7317a73a0db3d1009d
Talocha <mateofds@hotmail.com>
2015-10-23 14:10:46 +0200

    Create search.php

---
+++ locale/Spanish/search.php
@@ -0,0 +1,47 @@
+<?php
+$locale['400'] = "Buscar en ".fusion_get_settings('sitename');
+$locale['401'] = "Buscar:";
+$locale['402'] = "Buscar";
+$locale['403'] = "Cualquiera de estas palabras";
+$locale['404'] = "Todas estas palabras";
+$locale['405'] = "Buscar en:";
+$locale['406'] = "Opciones:";
+$locale['407'] = "Todo el Sitio";
+$locale['408'] = "Resultados de la BÃºsqueda";
+//addition date
+$locale['420'] = "Buscar desde:";
+$locale['421'] = "Cualquier fecha";
+$locale['422'] = "Ãšltimo dÃ­a";
+$locale['423'] = "Ãšltima semana";
+$locale['424'] = "Ãšltimas dos semanas";
+$locale['425'] = "Ãšltimo mes";
+$locale['426'] = "Ãšltimos tres meses";
+$locale['427'] = "Ãšltimos seis meses";
+//where
+$locale['430'] = "En el tÃ­tulo y el mensaje";
+$locale['431'] = "SÃ³lo en el mensaje";
+$locale['432'] = "SÃ³lo en el tÃ­tulo";
+//sort by
+$locale['440'] = "Ordenar por:";
+$locale['441'] = "Fecha";
+$locale['442'] = "TÃ­tulo";
+$locale['443'] = "Autor";
+//
+$locale['450'] = "Descendente";
+$locale['451'] = "Ascendente";
+//characters in results
+$locale['460'] = "Mostrar los:";
+$locale['461'] = "Primeros caracteres";
+$locale['462'] = "Todo el texto";
+//No results
+$locale['500'] = "No se han encontrado resultados.";
+$locale['501'] = "El texto a buscar debe tener, como mÃ­nimo, 3 caracteres de longitud.";
+$locale['502'] = "Las cadenas de bÃºsqueda con menos de tres caracteres (%s) son ignoradas en la bÃºsqueda.";
+//Global search
+$locale['510'] = "Resultados en la bÃºsqueda global del sitio";
+//
+$locale['520'] = "Resultado";
+$locale['521'] = "Resultados";
+$locale['522'] = "segÃºn el criterio de bÃºsqueda";
+//more than 100 results
+$locale['530'] = "Debes precisar mÃ¡s tu bÃºsqueda. La consulta ha devuelto %s resultados. SÃ³lo se muestran los 100 primeros.";
