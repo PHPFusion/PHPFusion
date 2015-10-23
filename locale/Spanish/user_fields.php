42237d8e7b9b97c79cd7c5bfd10784c9bf935ee5
Talocha <mateofds@hotmail.com>
2015-10-23 19:36:54 +0200

    Create user_fields.php

---
+++ locale/Spanish/user_fields.php
@@ -0,0 +1,159 @@
+<?php
+// Items shown in profiles
+$locale['u040'] = "Fecha de Registro";
+$locale['u041'] = "Ãšltima Visita";
+$locale['u042'] = "Nunca";
+$locale['u043'] = "Enviar un Mensaje Privado";
+$locale['u044'] = "InformaciÃ³n de Contacto";
+$locale['u045'] = "Otra InformaciÃ³n";
+$locale['u046'] = "Opciones";
+$locale['u047'] = "EstadÃ­sticas";
+$locale['u048'] = "InformaciÃ³n de AdministraciÃ³n";
+$locale['u049'] = "DirecciÃ³n IP";
+$locale['u050'] = "Sin Definir";
+$locale['u051'] = "Â¿Ocultar el Email?";
+$locale['u052'] = " SÃ­ ";
+$locale['u053'] = " No";
+$locale['u054'] = "Ver el Registro de SuspensiÃ³n";
+$locale['u055'] = "Estado del Usuario:";
+$locale['u056'] = "Motivo";
+$locale['u057'] = "Grupos de Usuarios";
+$locale['u058'] = "Opciones de AdministraciÃ³n";
+$locale['u059'] = "AÃ±adir";
+$locale['u060'] = "Â¿AÃ±adir este usuario al grupo seleccionado?";
+$locale['u061'] = "AÃ±adir al Grupo";
+$locale['u062'] = "Avatar del Usuario";
+$locale['u063'] = "Nivel del Usuario";
+$locale['u064'] = "Email";
+$locale['u066'] = "Fecha de Registro";
+$locale['u067'] = "Ãšltima Visita";
+$locale['u068'] = "Nombre de Usuario";
+$locale['u069'] = "Editar";
+$locale['u070'] = "Expulsar";
+$locale['u071'] = "Suspender";
+$locale['u072'] = "Borrar";
+$locale['u073'] = "Â¿Borrar este usuario?";
+// Profile and register
+$locale['u100'] = "Para cambiar tu contraseÃ±a o tu direcciÃ³n de email<br />debes indicar tu contraseÃ±a actual.";
+$locale['u101'] = "Registro";
+$locale['u102'] = "Editar Perfil";
+$locale['u103'] = "Perfil";
+$locale['u104'] = "Perfil de";
+$locale['u105'] = "Actualizar Perfil";
+
+// New UF 1.20
+$locale['uf_100'] = "Configuración de la cuenta general";
+$locale['uf_101'] = "Ajustes información de usuario";
+$locale['uf_102'] = "Ajustes Avatar de Usuario";
+$locale['uf_103'] = "General";
+$locale['uf_104'] = "Información de usuario";
+$locale['uf_105'] = "Foto de perfil";
+$locale['uf_106'] = "Información básica";
+$locale['uf_107'] = "%s no se tiene información disponible del usuario.";
+$locale['uf_108'] = "No hay campos de usuario definidos";
+
+// View User Groups
+$locale['u110'] = "Ver Grupo de Usuarios";
+$locale['u111'] = "%u Usuario";
+$locale['u112'] = "%u Usuarios";
+$locale['u113'] = "Nombre de Usuario";
+$locale['u114'] = "Tipo de Usuario";
+// User name and email
+$locale['u120'] = "El nombre de usuario contiene caracteres no vÃ¡lidos.";
+$locale['u121'] = "El nombre de usuario que has elegido ya estÃ¡ registrado.";
+$locale['u122'] = "El nombre de usuario no puede dejarse vacÃ­o.";
+$locale['u123'] = "La direcciÃ³n de email que has indicado parece no ser vÃ¡lida.";
+$locale['u124'] = "Tu direcciÃ³n de email o el dominio de tu email parece estar expulsado.";
+$locale['u125'] = "La direcciÃ³n de email que has indicado ya estÃ¡ registrada.";
+$locale['u126'] = "La direcciÃ³n de email no puede dejarse vacÃ­a.";
+$locale['u127'] = "Nombre de Usuario";
+$locale['u128'] = "DirecciÃ³n de Email";
+$locale['u129'] = "InformaciÃ³n de la Cuenta";
+// Passwords
+$locale['u130'] = "ContraseÃ±as de AdministraciÃ³n";
+$locale['u131'] = "ContraseÃ±a de Administrador";
+$locale['u132'] = "ContraseÃ±as de Usuario";
+$locale['u133'] = "ContraseÃ±a de Usuario";
+$locale['u134'] = "Nueva ContraseÃ±a de Usuario";
+$locale['u134a'] = "Establecer contraseÃ±a";
+$locale['u134b'] = "Repita la contraseÃ±a";
+$locale['u135a'] = "ContraseÃ±a actual";
+$locale['u135b'] = "Establecer nueva contraseÃ±a";
+$locale['u135c'] = "Repita la nueva contraseÃ±a";
+$locale['u136'] = "La contraseÃ±a no puede dejarse vacÃ­a.";
+$locale['u137'] = "La contraseÃ±a actual de administrador no puede dejarse vacÃ­a.";
+$locale['u138'] = "La contraseÃ±a actual de usuario no puede dejarse vacÃ­a.";
+$locale['u139'] = "La contraseÃ±a que has indicado no coincide con tu contraseÃ±a actual de usuario.";
+$locale['u140'] = "La contraseÃ±a que has indicado no coincide con tu contraseÃ±a actual de administrador.";
+$locale['u141'] = " no puede ser la misma que ";
+$locale['u142'] = " es demasiado corta o contiene caracteres no vÃ¡lidos.";
+$locale['u143'] = " no coincide.";
+$locale['u143a'] = " no puede dejarse vacÃ­a.";
+$locale['u144'] = "Nueva ContraseÃ±a de Administrador";
+$locale['u144a'] = "ContraseÃ±a de administrador actual";
+$locale['u145'] = "Confirmar la ContraseÃ±a";
+$locale['u146'] = " no puede ser la misma que tu actual ";
+$locale['u147'] = "Las contraseÃ±as deben tener entre 8 y 64 caracteres de longitud.<br />Se admiten letras (a-z), nÃºmeros (0-9) y los sÃ­mbolos: @!#$%&\/()=-_?+*.,:;";
+$locale['u148'] = "Las nuevas contraseÃ±as de usuario no son iguales.";
+$locale['u148a'] = "Las nuevas contraseÃ±as de administrador no son iguales.";
+$locale['u149'] = "No has indicado tu actual contraseÃ±a de usuario o no es vÃ¡lida.";
+$locale['u149a'] = "No has indicado tu actual contraseÃ±a de administrador o no es vÃ¡lida.";
+$locale['u149b'] = "Su contraseÃ±a de acceso actual no ha sido especificada o no es vÃ¡lida.<br />No se puede establecer la contraseÃ±a de administrador sin la contraseÃ±a de acceso correcta.";
+// Email actiation
+$locale['u150'] = "Tu registro estÃ¡ casi completo. RecibirÃ¡s un email conteniendo tus datos de inicio de sesiÃ³n, asÃ­ como un enlace para verificar tu cuenta.";
+$locale['u151'] = "Bienvenida y ActivaciÃ³n en ".fusion_get_settings('sitename');
+$locale['u152'] = "Hola USER_NAME,\n
+Te damos la bienvenida a ".fusion_get_settings('sitename').". AquÃ­ tienes tus datos de usuario:\n
+Nombre de Usuario: USER_NAME
+ContraseÃ±a: USER_PASSWORD\n
+Ahora debes activar tu cuenta por medio del siguiente enlace: ACTIVATION_LINK\n\n
+Saludos,
+".fusion_get_settings('sitename');
+$locale['u153'] = "No se ha podido enviar el email de activaciÃ³n.";
+$locale['u154'] = "Contacta con el <a href='".BASEDIR."contact.php'>Administrador del Portal.</a>.";
+$locale['u155'] = "Activar la Cuenta";
+$locale['u156'] = "Para cambiar tu direcciÃ³n de email, indica tu contraseÃ±a.";
+// Success / Fail
+$locale['u160'] = "Registro Completo";
+$locale['u161'] = "Ahora puedes iniciar sesiÃ³n.";
+$locale['u162'] = "El Administrador activarÃ¡ tu cuenta lo antes posible.";
+$locale['u163'] = "El perfil ha sido actualizado correctamente.";
+$locale['u164'] = "Ha fallado la actualizaciÃ³n";
+
+//$locale['u165'] = "Registration failed";
+$locale['u167'] = "por lo siguiente:";
+$locale['u168'] = "Vuelve a intentarlo.";
+$locale['u169'] = "Perfil Actualizado";
+$locale['u170'] = "Registro Correcto";
+$locale['u171'] = "Tu cuenta ha sido verificada.";
+$locale['u172'] = "Miembro aÃ±adido correctamente.";
+$locale['u173'] = "Volver a la GestiÃ³n de Usuarios.";
+$locale['u174'] = "AÃ±adir Otro Miembro.";
+// Avatar upload
+$locale['u180'] = "Hay un error cargando imagen de avatar";
+
+
+$locale['u184'] = "TamaÃ±o MÃ¡x. Archivo: %s / Dimensiones MÃ¡x.: %u x %u pÃ­xeles";
+$locale['u185'] = "Avatar";
+$locale['u186'] = "Pulsa el botÃ³n Examinar para transferir una imagen";
+$locale['u187'] = "Borrar";
+// Captcha and terms
+$locale['u190'] = "CÃ³digo de ValidaciÃ³n";
+$locale['u191'] = "Introduce el CÃ³digo de ValidaciÃ³n";
+$locale['u192'] = "Condiciones del Registro";
+$locale['u193'] = "He leÃ­do las <a href='".BASEDIR."print.php?type=T' target='_blank'>Condiciones del Registro</a> y estoy de acuerdo con ellas.";
+$locale['u194'] = "CÃ³digo de validaciÃ³n incorrecto.";
+$locale['u195'] = "El cÃ³digo de validaciÃ³n no puede dejarse vacÃ­o.";
+// E-mail Change Confirmation
+$locale['u200'] = "Ha sido enviado un email de verificaciÃ³n a tu nueva direcciÃ³n de email (%s).";
+$locale['u201'] = "Tu direcciÃ³n de email serÃ¡ cambiada cuando pulses el enlace contenido en el email de verificaciÃ³n.";
+$locale['u202'] = "VerificaciÃ³n de Email - ".fusion_get_settings('sitename');
+$locale['u203'] = "Hola [USER_NAME],\n
+Ha sido solicitada la configuraciÃ³n de esta direcciÃ³n de email en una cuenta de nuestro sitio web.\n
+Si realmente quieres cambiar tu direcciÃ³n de email, pulsa el siguiente enlace:\n
+[EMAIL_VERIFY_LINK]\n
+Nota: tienes que haber iniciado sesiÃ³n para proceder.\n
+Saludos,
+".fusion_get_settings('siteusername')."
+".fusion_get_settings('sitename');
+
