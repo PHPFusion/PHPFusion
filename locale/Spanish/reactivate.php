ccce2d51984ce494f2949a6e1bc0b071030bafd7
Talocha <mateofds@hotmail.com>
2015-10-23 14:04:07 +0200

    Create reactivate.php

---
+++ locale/Spanish/reactivate.php
@@ -0,0 +1,17 @@
+<?php
+// Error messages
+$locale['500'] = "Se ha producido un error";
+$locale['501'] = "El enlace de reactivaciÃ³n que has usado ya no es vÃ¡lido.<br /><br />
+Contacta con el administrador del sitio en <a href='mailto:".fusion_get_settings('siteemail')."'>".fusion_get_settings('siteemail')."</a> si deseas solicitar la reactivaciÃ³n manual.";
+$locale['502'] = "El enlace de reactivaciÃ³n que has usado no es vÃ¡lido.<br /><br />
+Contacta con el administrador del sitio en <a href='mailto:".fusion_get_settings('siteemail')."'>".fusion_get_settings('siteemail')."</a> si deseas solicitar la reactivaciÃ³n manual.";
+$locale['503'] = "El enlace de reactivaciÃ³n que has usado no ha podido reactivar tu cuenta.<br />
+QuizÃ¡ tu cuenta ya haya sido reactivada y, en ese caso, deberÃ­as poder <a href='".fusion_get_settings('siteurl')."login.php'>iniciar sesiÃ³n aquÃ­</a>.<br /><br />
+Si ahora no puedes iniciar sesiÃ³n, contacta con el administrador del sitio en <a href='mailto:".fusion_get_settings('siteemail')."'>".fusion_get_settings('siteemail')."</a> si deseas solicitar la reactivaciÃ³n manual.";
+// Send confirmation mail
+$locale['504'] = "Cuenta Reactivada en ".fusion_get_settings('sitename');
+$locale['505'] = "Hola [USER_NAME],\n
+Tu cuenta en ".fusion_get_settings('sitename')." ha sido reactivada. Esperamos poder verte mÃ¡s a menudo en el sitio.\n\n
+Saludos,\n\n
+".fusion_get_settings('siteusername');
+$locale['506'] = "Reactivada por el usuario.";
