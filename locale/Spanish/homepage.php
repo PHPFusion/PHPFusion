6116e18651b43e55e983ee58cd7d31b24809aa72
Talocha <mateofds@hotmail.com>
2015-10-23 09:13:31 +0200

    Create homepage.php

---
+++ locale/Spanish/homepage.php
@@ -0,0 +1,28 @@
+<?php
+// block titles
+$locale['home_0000'] = "Ãšltimas Noticias";
+$locale['home_0001'] = "Ãšltimos ArtÃ­culos";
+$locale['home_0002'] = "Ãšltimos Blogs";
+$locale['home_0003'] = "Ãšltimas Descargas";
+
+$locale['submit_0000'] = '%s Noticias';
+$locale['submit_0001'] = '%s ArtÃ­culos';
+$locale['submit_0002'] = '%s Descargas';
+$locale['submit_0003'] = '%s Fotos';
+$locale['submit_0004'] = '%s Enlaces';
+$locale['submit_0005'] = '%s Blog';
+
+// no content messages
+$locale['home_0050'] = "No Hay Noticias para mostrar";
+$locale['home_0051'] = "No hay artÃ­culos para mostrar";
+$locale['home_0052'] = "No hay blogs para mostrar";
+$locale['home_0053'] = "No hay Descargas para mostrar";
+
+// common text
+$locale['home_0100'] = "Los mÃ³dulos de contenido no instalados";
+$locale['home_0101'] = "Instale al menos un mÃ³dulo de contenido de texto o cambiar la pÃ¡gina de inicio en configuraciÃ³n principal.";
+
+$locale['home_0102'] = "No categorizados";
+
+$locale['home_0105'] = "Publicado por ";
+$locale['home_0106'] = " en ";
