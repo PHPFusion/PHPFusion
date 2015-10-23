ebc2496a8b0ded843270451a9fa1b2012a0ff4c6
Talocha <mateofds@hotmail.com>
2015-10-23 09:48:13 +0200

    Create lostpassword.php

---
+++ locale/Spanish/lostpassword.php
@@ -0,0 +1,24 @@
+<?php
+$locale['400'] = "Nueva ContraseÃ±a";
+$locale['401'] = "Se ha enviado un correo de confirmaciÃ³n a tu direcciÃ³n de email.";
+$locale['402'] = "Tu nueva contraseÃ±a ha sido enviada a tu direcciÃ³n de email.";
+$locale['403'] = "Volver al Sitio";
+$locale['404'] = "No se ha podido encontrar la direcciÃ³n de email que has indicado.";
+$locale['405'] = "La direcciÃ³n de email que has indicado no es vÃ¡lida.";
+$locale['406'] = "Vuelve a intentarlo.";
+$locale['407'] = "Indica la direcciÃ³n de email con la que registraste tu cuenta.<br />
+Se crearÃ¡ una nueva contraseÃ±a y se enviarÃ¡ a esa direcciÃ³n.";
+$locale['408'] = "Enviar ContraseÃ±a";
+$locale['409'] = "Nueva ContraseÃ±a para ";
+$locale['410'] = "Hola [USER_NAME].\n
+Ha sido solicitada una nueva contraseÃ±a para acceder a tu cuenta de ".fusion_get_settings('sitename').".\n
+Para cambiar su contraseÃ±a, por favor haga clic en el siguiente enlace:\n
+[NEW_PASS_LINK]\n
+Saludos,
+".fusion_get_settings('siteusername');
+$locale['411'] = "Hola [USER_NAME].\n
+Tu nueva contraseÃ±a para acceder a tu cuenta de ".fusion_get_settings('sitename')." es:\n
+[NEW_PASS]\n
+Saludos,
+".fusion_get_settings('siteusername');
+$locale['412'] = "Se ha producido un error. Vuelve a intentarlo.";
