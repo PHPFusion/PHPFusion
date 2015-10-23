4d96d51d0a3199e21622df63e9b5453052eca378
Talocha <mateofds@hotmail.com>
2015-10-23 22:51:34 +0200

    Create admins.php

---
+++ locale/Spanish/admin/admins.php
@@ -0,0 +1,46 @@
+<?php
+// Administrators messages
+$locale['400'] = "Administrador aÃ±adido";
+$locale['401'] = "Administrador actualizado";
+$locale['402'] = "Administrador eliminado";
+// Search/Add Administrator
+$locale['410'] = "AÃ±adir Administradores";
+$locale['411'] = "Usa el siguiente formulario para buscar y aÃ±adir Administradores.";
+$locale['412'] = "ID de Usuario";
+$locale['413'] = "Nombre de Usuario";
+$locale['414'] = "Buscar";
+$locale['415'] = "Aplicar todos los permisos";
+$locale['416'] = "Hacer Super Administrador";
+$locale['417'] = "AÃ±adir";
+$locale['418'] = "No se han encontrado usuarios.";
+$locale['419'] = "Nueva bÃºsqueda";
+// Current Administrators
+$locale['420'] = "Administradores Existentes";
+$locale['421'] = "Nombre de Usuario";
+$locale['422'] = "Nivel de AdministraciÃ³n";
+$locale['423'] = "Opciones";
+$locale['424'] = "Administrador Principal";
+$locale['425'] = "No se han asignado permisos";
+$locale['426'] = "Editar";
+$locale['427'] = "Eliminar";
+// Edit Admin Rights
+$locale['440'] = "Permisos de AdministraciÃ³n";
+$locale['441'] = "AdministraciÃ³n de Contenidos";
+$locale['442'] = "AdministraciÃ³n de Usuarios";
+$locale['443'] = "AdministraciÃ³n del Sistema";
+$locale['444'] = "Infusiones";
+$locale['445'] = "Marcar Todos";
+$locale['446'] = "Desmarcar Todos";
+$locale['447'] = "ContraseÃ±a de Administrador";
+$locale['448'] = "Guardar Permisos de AdministraciÃ³n";
+$locale['449'] = "ConfiguraciÃ³n";
+$locale['450'] = "Comprobar Seguridad";
+// Popup Error/Warning Messages
+$locale['460'] = "Â¿Borrar este Administrador?";
+$locale['461'] = "Â¿AÃ±adir este Administrador?";
+$locale['462'] = "Estos derechos de administraciÃ³n pueden suponer un riesgo potencial de seguridad";
+// Remove Admin confirmation
+$locale['470'] = "Borrar Administrador";
+$locale['471'] = "Para eliminar este Administrador, debes indicar tu contraseÃ±a de administrador:";
+$locale['472'] = "Confirmar";
+$locale['473'] = "Cancelar";
