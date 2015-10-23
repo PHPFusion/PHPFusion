7c872ae97a1b99c4bf279276b5f371b83d077b4c
Talocha <mateofds@hotmail.com>
2015-10-23 12:15:25 +0200

    Create print.php

---
+++ locale/Spanish/print.php
@@ -0,0 +1,16 @@
+<?php
+//news, articles
+$locale['400'] = "Publicado por ";
+$locale['401'] = " el ";
+$locale['402'] = "Noticia";
+$locale['403'] = "Blog extendido";
+//forums
+$locale['500'] = "TÃ­tulo del Tema:";
+$locale['501'] = "Publicado por ";
+$locale['502'] = " el ";
+$locale['503'] = "Editado por ";
+//License Agreement
+$locale['600'] = "Condiciones del Registro";
+$locale['601'] = "Ãšltima ActualizaciÃ³n:";
+//Privacy Policy
+$locale['700'] = "PolÃ­tica de privacidad";
