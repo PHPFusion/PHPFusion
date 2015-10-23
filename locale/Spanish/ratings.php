398bd93bb19354ee071f00adcbb1391c307dd136
Talocha <mateofds@hotmail.com>
2015-10-23 12:22:41 +0200

    Create ratings.php

---
+++ locale/Spanish/ratings.php
@@ -0,0 +1,18 @@
+<?php
+$locale['r100'] = "Valoraciones";
+$locale['r101'] = "No hay valoraciones.";
+$locale['r102'] = "Eliminar";
+$locale['r103'] = "Enviar";
+$locale['r104'] = "La valoraciÃ³n sÃ³lo estÃ¡ disponible para miembros.<br /><br /><a href='".BASEDIR."login.php'>Inicia sesiÃ³n</a>".(fusion_get_settings('enable_registration') ? " o <a href='".BASEDIR."register.php'>regÃ­strate</a>" : "")." para votar.";
+$locale['r105'] = "Tu valoraciÃ³n fue <strong>%s</strong> el %s";
+$locale['r106'] = "Tu valoraciÃ³n es";
+$locale['r107'] = "Elija ValoraciÃ³n:";
+$locale['r108'] = "No hay votos";
+$locale['r109'] = "Voto";
+$locale['r110'] = "Votos";
+// Rating Options
+$locale['r120'] = "Â¡Excelente!";
+$locale['r121'] = "Muy Bien";
+$locale['r122'] = "Bien";
+$locale['r123'] = "Normal";
+$locale['r124'] = "Pobre";
