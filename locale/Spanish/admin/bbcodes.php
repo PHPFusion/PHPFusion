834c7fafdc2522408b5c37ed503db46e8d93a175
Talocha <mateofds@hotmail.com>
2015-10-25 11:32:48 +0100

    Create bbcodes.php

---
+++ locale/Spanish/admin/bbcodes.php
@@ -0,0 +1,25 @@
+<?php
+$locale['400'] = "AdministraciÃ³n de CÃ³digos BB";
+$locale['401'] = "Comprobar los CÃ³digos BB";
+// Enabled BB Codes
+$locale['402'] = "CÃ³digos BB Activados";
+$locale['403'] = "Nombre";
+$locale['404'] = "Icono";
+$locale['405'] = "DescripciÃ³n";
+$locale['406'] = "Uso";
+$locale['407'] = "Orden";
+$locale['408'] = "Bajar";
+$locale['409'] = "Subir";
+$locale['410'] = "Desactivar";
+$locale['411'] = "No hay cÃ³digos BB activados.";
+// Disabled BB Codes
+$locale['413'] = "CÃ³digos BB Desactivados";
+$locale['414'] = "Activar";
+$locale['415'] = "Nueva LÃ­nea";
+$locale['416'] = "No hay cÃ³digos BB desactivados.";
+// Test BB Codes
+$locale['417'] = "Resultado de la ComprobaciÃ³n de los CÃ³digos BB";
+$locale['418'] = "Desactivar Smileys en la comprobaciÃ³n";
+$locale['418a'] = "Mensaje de prueba";
+$locale['418b'] = "Por favor, introduzca un mensaje a probar BBcodes.";
+
