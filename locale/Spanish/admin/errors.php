7900ca1d591120210761655dd81f4b693c80642d
Talocha <mateofds@hotmail.com>
2015-10-25 11:39:20 +0100

    Create errors.php

---
+++ locale/Spanish/admin/errors.php
@@ -0,0 +1,26 @@
+<?php
+// Titles
+$locale['400'] = "Registro de Errores";
+$locale['401'] = "Examinar el archivo";
+// Errors table
+$locale['410'] = "Error";
+$locale['411'] = "En PÃ¡gina";
+$locale['412'] = "Usuario";
+$locale['413'] = "Fecha";
+$locale['414'] = "Estado";
+$locale['415'] = "LÃ­nea:";
+$locale['416'] = "Nivel:";
+$locale['417'] = "IP:";
+$locale['418'] = "No hay errores registrados.";
+$locale['419'] = "Archivo";
+$locale['420'] = "Detalles";
+$locale['421'] = "CÃ³digo Fuente";
+$locale['422'] = "Voler Arriba";
+// Delete form
+$locale['440'] = "Borrar todas las entrada del estado";
+// Status form
+$locale['450'] = "Nuevo";
+$locale['451'] = "Resuelto";
+$locale['452'] = "Ignorado";
+$locale['453'] = "Aplicar";
+$locale['454'] = "Severidad";
