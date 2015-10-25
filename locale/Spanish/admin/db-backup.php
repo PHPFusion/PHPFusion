7a84facd92a2c57c0e586c795d50c40a2f68edf0
Talocha <mateofds@hotmail.com>
2015-10-25 11:37:02 +0100

    Create db-backup.php

---
+++ locale/Spanish/admin/db-backup.php
@@ -0,0 +1,47 @@
+<?php
+// Database Restore
+$locale['400'] = "RestauraciÃ³n de la Base de Datos";
+$locale['401'] = "Error";
+$locale['402'] = "El formato del archivo de copia de seguridad no es vÃ¡lido";
+$locale['403'] = "Cerrar";
+// Backup File Information
+$locale['410'] = "InformaciÃ³n del Archivo de Copia de Seguridad";
+$locale['411'] = "InformaciÃ³n del Archivo de RestauraciÃ³n";
+$locale['412'] = "Nombre del Archivo:";
+$locale['413'] = "Fecha:";
+$locale['414'] = "Nombre de la BD:";
+$locale['415'] = "Prefijo de las Tablas:";
+$locale['416'] = "Tablas:";
+$locale['417'] = "Ver";
+$locale['418'] = "Cancelar";
+$locale['419'] = "Tablas";
+// Database Restore
+$locale['430'] = "Opciones de RestauraciÃ³n";
+$locale['431'] = "Nombre del Archivo:";
+$locale['432'] = "Fecha:";
+$locale['433'] = "Crear Tablas:";
+$locale['434'] = "Rellenar Tablas:";
+$locale['435'] = "Seleccionar:";
+$locale['436'] = "Todas";
+$locale['437'] = "Ninguna";
+$locale['438'] = "Restaurar";
+$locale['439'] = "Cancelar";
+$locale['440'] = "Tipos de Archivo Admitidos:";
+// Database Backup
+$locale['450'] = "Copia de Seguridad de la Base de Datos";
+$locale['451'] = "InformaciÃ³n de la Base de Datos";
+$locale['452'] = "TamaÃ±o Total de las Tablas:";
+$locale['453'] = "TamaÃ±o de la Tablas del NÃºcleo:";
+$locale['454'] = "Opciones de la Copia de Seguridad:";
+$locale['455'] = "Tipo de Copia de Seguridad:";
+$locale['456'] = "(Comprimida)";
+$locale['457'] = "Tablas de la Base de Datos";
+$locale['458'] = "NÃºcleo";
+$locale['459'] = "Hacer Copia";
+$locale['460'] = "ContraseÃ±a de Administrador:";
+$locale['460b'] = "Por favor, introduzca su contraseÃ±a de administrador";
+$locale['461'] = "InformaciÃ³n Obligatoria";
+// Backup List
+$locale['480'] = "Restaurar Copia de Seguridad de la Base de Datos";
+$locale['481'] = "Nombre del Archivo:";
+$locale['481b'] = "Por favor, introduzca un nombre de archivo";
