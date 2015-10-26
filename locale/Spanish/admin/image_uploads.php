a714d4bcbc0c28059bf7c9f06dd79c768ca1d288
Talocha <mateofds@hotmail.com>
2015-10-26 10:59:04 +0100

    Create image_uploads.php

---
+++ locale/Spanish/admin/image_uploads.php
@@ -0,0 +1,29 @@
+<?php
+// Delete Image
+$locale['400'] = "Borrar Imagen";
+$locale['401'] = "Imagen borrada";
+$locale['402'] = "Volver a la AdministraciÃ³n de ImÃ¡genes";
+$locale['403'] = "Volver a AdministraciÃ³n";
+// Upload Image
+$locale['420'] = "Transferir";
+$locale['421'] = "Nombre del Archivo:";
+$locale['422'] = "ImÃ¡genes Principales";
+$locale['423'] = "ImÃ¡genes de ArtÃ­culos";
+$locale['424'] = "ImÃ¡genes de Noticias";
+$locale['425'] = "SÃ³lo se pueden transferir imÃ¡genes en formato GIF, JPG o PNG";
+$locale['426'] = "Imagen transferida";
+$locale['427'] = "ImÃ¡genes de CategorÃ­as de Noticias";
+$locale['428'] = "Blog ImÃ¡genes";
+$locale['429'] = "Blog CategorÃ­as ImÃ¡genes";
+// View Image
+$locale['440'] = "Ver Imagen";
+$locale['441'] = "Formato de archivo desconocido. No se puede mostrar la imagen.";
+$locale['442'] = "Borrar";
+// Image Management
+$locale['460'] = "ImÃ¡genes Existentes";
+$locale['461'] = "Ver";
+$locale['462'] = "Borrar";
+$locale['463'] = "Este directorio estÃ¡ vacÃ­o";
+$locale['464'] = "Actualizar la lista de imÃ¡genes para el editor TinyMCE";
+// Popup Error/Warning Messages
+$locale['470'] = "Â¿Borrar esta imagen?";
