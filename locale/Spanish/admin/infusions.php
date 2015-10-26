b9afb3a106b902cb84e80ae23936135613dc211a
Talocha <mateofds@hotmail.com>
2015-10-26 11:03:10 +0100

    Create infusions.php

---
+++ locale/Spanish/admin/infusions.php
@@ -0,0 +1,25 @@
+<?php
+// Infusion Management
+$locale['400'] = "Infusiones";
+$locale['401'] = "Instalar";
+$locale['402'] = "Esta infusiÃ³n estÃ¡ instalada";
+$locale['403'] = "Esta infusiÃ³n no puede ser instalada";
+$locale['404'] = "Infusiones Instaladas";
+$locale['405'] = "TÃ­tulo";
+$locale['406'] = "VersiÃ³n";
+$locale['407'] = "Autor";
+$locale['408'] = "Contacto";
+$locale['409'] = "Email";
+$locale['410'] = "Web";
+$locale['411'] = "Desinstalar";
+$locale['412'] = "Â¿Desinstalar esta infusiÃ³n?";
+$locale['413'] = "Clave:";
+$locale['414'] = "No Instalada";
+$locale['415'] = "Instalada";
+$locale['416'] = "ActualizaciÃ³n Disponible";
+$locale['417'] = "No hay infusiones disponibles.";
+$locale['418'] = "Estado";
+$locale['419'] = "Opciones";
+$locale['420'] = "VersiÃ³n";
+$locale['421'] = "Autor";
+$locale['422'] = "Descargar mÃ¡s Complementos";
