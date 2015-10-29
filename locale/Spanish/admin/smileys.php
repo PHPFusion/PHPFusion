53589a3cb50b9bd0af597dda297e0167e83d7ad4
Talocha <mateofds@hotmail.com>
2015-10-29 19:17:30 +0100

    Create smileys.php

---
+++ locale/Spanish/admin/smileys.php
@@ -0,0 +1,32 @@
+<?php
+// Smiley titles
+$locale['400'] = "Smileys Existentes";
+$locale['401'] = "AÃ±adir Smiley";
+$locale['402'] = "Editar Smiley";
+$locale['403'] = "Smileys";
+// Smiley messages
+$locale['410'] = "Smiley aÃ±adido";
+$locale['411'] = "Smiley actualizado";
+$locale['412'] = "Smiley borrado";
+$locale['413'] = "No se ha podido actualizar el Smiley";
+$locale['414'] = "No se ha podido aÃ±adir el Smiley";
+$locale['415'] = "El cÃ³digo del Smiley ya estÃ¡ en uso.";
+$locale['416'] = "Â¿Borrar este Smiley?";
+// Smiley form
+$locale['420'] = "CÃ³digo:";
+$locale['421'] = "Imagen:";
+$locale['422'] = "Texto:";
+$locale['423'] = "Guardar'";
+$locale['424'] = "Vista Previa:";
+// Current Smileys
+$locale['430'] = "CÃ³digo";
+$locale['431'] = "Imagen";
+$locale['432'] = "Texto";
+$locale['433'] = "Opciones";
+$locale['434'] = "Editar";
+$locale['435'] = "Borrar";
+$locale['436'] = "No se han creado Smileys.";
+// Error Message
+$locale['437'] = "Introduce un cÃ³digo de Smiley";
+$locale['438'] = "seleccione un Smiley";
+$locale['439'] = "Introduce un texto de Smiley";
