cd84b5cbcf9f3f4556f7a56d7652c3e239d49fe7
Talocha <mateofds@hotmail.com>
2015-10-29 17:05:48 +0100

    Create phpinfo.php

---
+++ locale/Spanish/admin/phpinfo.php
@@ -0,0 +1,32 @@
+<?php
+$locale['400'] = "InformaciÃ³n de PHP";
+//General
+$locale['401'] = "InformaciÃ³n General";
+$locale['402'] = "Software del Servidor";
+$locale['403'] = "Software del Servidor Web";
+$locale['404'] = "VersiÃ³n de PHP";
+$locale['405'] = "Interfaz de PHP";
+$locale['406'] = "VersiÃ³n de MySQL";
+$locale['407'] = "VersiÃ³n de PHP-Fusion";
+$locale['408'] = "Prefijo de la BD";
+$locale['409'] = "Prefijo de la Cookie";
+$locale['410'] = "Navegador y SO";
+//PHP settings
+$locale['420'] = "ConfiguraciÃ³n de PHP";
+$locale['421'] = "SÃ­";
+$locale['422'] = "No";
+$locale['423'] = "Safemode";
+$locale['424'] = "RegisterGlobals";
+$locale['425'] = "Soporte para";
+$locale['426'] = "Transferencia de Archivos (TamaÃ±o MÃ¡x.)";
+$locale['428'] = "Mostrar Errores";
+$locale['429'] = "Funciones Desactivadas";
+$locale['430'] = "n/d";
+$locale['431'] = "VersiÃ³n";
+//Write permission
+$locale['440'] = "Permisos de Carpetas";
+$locale['441'] = "Puede Escribirse";
+$locale['442'] = "No Puede Escribirse";
+//PHPinfo
+$locale['450'] = "InformaciÃ³n Detallada";
+$locale['451'] = "No se han podido obtener los datos. Comprueba si la funciÃ³n phpinfo() estÃ¡ desactivada.";
