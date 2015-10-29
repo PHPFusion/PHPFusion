5e84121c9533d934739fcdd704191ab7291836e5
Talocha <mateofds@hotmail.com>
2015-10-29 19:09:56 +0100

    Create sitelinks.php

---
+++ locale/Spanish/admin/sitelinks.php
@@ -0,0 +1,43 @@
+<?php
+// News titles
+$locale['SL_0001'] = 'Enlaces del Sitio';
+
+$locale['SL_0010'] = "AÃ±adir Enlace del Sitio";
+$locale['SL_0011'] = "Editar Enlace del Sitio";
+$locale['SL_0012'] = "Enlaces del Sitio Existentes";
+// News messages
+$locale['SL_0015'] = "Enlace aÃ±adido";
+$locale['SL_0016'] = "Enlace actualizado";
+$locale['SL_0017'] = "Enlace borrado";
+// Site Links form
+$locale['SL_0020'] = "Nombre del Enlace:";
+$locale['SL_0021'] = "URL:";
+$locale['SL_0022'] = "Acceso:";
+$locale['SL_0023'] = "Orden:";
+$locale['SL_0024'] = "PosiciÃ³n:";
+$locale['SL_0025'] = "SÃ³lo panel de navegaciÃ³n";
+$locale['SL_0026'] = "Panel de navegaciÃ³n y subcabecera";
+$locale['SL_0027'] = "SÃ³lo subcabecera";
+$locale['SL_0028'] = "Abrir el enlace en una nueva ventana";
+$locale['SL_0029'] = "CategorÃ­a";
+$locale['SL_0030'] = "Icono";
+$locale['SL_0040'] = "Guardar";
+// Site Links list
+$locale['SL_0050'] = "Nombre del Enlace";
+$locale['SL_0051'] = "Acceso";
+$locale['SL_0052'] = "Orden";
+$locale['SL_0053'] = "Opciones";
+$locale['SL_0060'] = "Editar";
+$locale['SL_0061'] = "Borrar";
+$locale['SL_0062'] = "No se han creado enlaces del sitio.";
+
+$locale['SL_0070'] = "Icono";
+$locale['SL_0071'] = "Â¿nueva ventana?";
+$locale['SL_0072'] = "MenÃº";
+$locale['SL_0073'] = "Reordenar";
+
+// Delete Site Link category
+$locale['SL_0080'] = "Â¿Borrar este enlace?";
+// Error
+$locale['SL_0085'] = "Introduzca un nombre de enlace";
+$locale['SL_0086'] = "Introduzca una URL de enlace";
