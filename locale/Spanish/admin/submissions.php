18998236a284597ae27b83dc3c4efe9f6d94df8c
Talocha <mateofds@hotmail.com>
2015-10-29 19:41:30 +0100

    Create submissions.php

---
+++ locale/Spanish/admin/submissions.php
@@ -0,0 +1,133 @@
+<?php
+// Delete Submission
+$locale['400'] = "Borrar";
+$locale['401'] = "EnvÃ­o borrado";
+$locale['402'] = "Volver al MenÃº de EnvÃ­os";
+$locale['403'] = "Volver a AdministraciÃ³n";
+// Submission Options
+$locale['410'] = "EnvÃ­os";
+$locale['411'] = "Enlaces Enviados:";
+$locale['412'] = "Noticias Enviadas:";
+$locale['412b'] = "Blogs Enviados:";
+$locale['413'] = "ArtÃ­culos Enviados:";
+$locale['414'] = "No hay enlaces pendientes de verificaciÃ³n.";
+$locale['415'] = "No hay noticias pendientes de verificaciÃ³n.";
+$locale['415b'] = "No hay Blogs pendientes de verificaciÃ³n.";
+$locale['416'] = "No hay artÃ­culos pendientes de verificaciÃ³n.";
+$locale['417'] = "Ver";
+$locale['418'] = "Borrar";
+$locale['419'] = "Fotos Enviadas:";
+$locale['420'] = "No hay fotos pendientes de verificaciÃ³n.";
+$locale['421'] = "Descargas Enviadas:";
+$locale['422'] = "No hay descargas pendientes de verificaciÃ³n.";
+// Add/Delete Web Link Submissions
+$locale['430'] = "Publicar";
+$locale['431'] = "Enlace publicado";
+$locale['432'] = "Borrar";
+$locale['433'] = "Enlace borrado";
+$locale['434'] = "No se han creado categorÃ­as de enlaces web";
+// Web Link Submission Form
+$locale['440'] = "Ver";
+$locale['441'] = "El siguiente enlace ha sido enviado por ";
+$locale['442'] = " el ";
+$locale['443'] = "CategorÃ­a:";
+$locale['444'] = "Nombre:";
+$locale['445'] = "URL:";
+$locale['446'] = "DescripciÃ³n:";
+$locale['447'] = "Â¿Publicar o borrar este enlace?";
+$locale['448'] = "Publicar";
+$locale['449'] = "Borrar";
+// Add/Delete News Submissions
+$locale['490'] = "Publicar";
+$locale['491'] = "Noticia publicada";
+$locale['492'] = "Borrar";
+$locale['493'] = "Noticia borrada";
+$locale['490b'] = "Publicar EnvÃ­o de Blog";
+$locale['491b'] = "blog publicado";
+$locale['492b'] = "Eliminar Blog";
+$locale['493b'] = "El Blog se ha eliminado";
+// News Submission Form
+$locale['500'] = "Ver";
+$locale['501'] = "La siguiente noticia ha sido enviada por ";
+$locale['502'] = "Â¿Publicar o borrar esta noticia?";
+$locale['503'] = "Publicar";
+$locale['504'] = "Borrar";
+$locale['505'] = "TÃ­tulo:";
+$locale['506'] = "CategorÃ­a:";
+$locale['507'] = "- Ninguna -";
+$locale['508'] = "Noticia:";
+$locale['509'] = "Resumen:";
+$locale['510'] = "Vista Previa";
+$locale['500b'] = "Ver Blog de EnvÃ­o";
+$locale['501b'] = "El siguiente Blog ha sido enviada por ";
+$locale['502b'] = "Â¿Desea publicar o eliminar este artÃ­culo de Blog?";
+$locale['503b'] = "Publicar Blog";
+$locale['504b'] = "Borrar Blog";
+$locale['505b'] = "Asunto:";
+$locale['506b'] = "Category:";
+$locale['507b'] = "- Ninguno -";
+$locale['508b'] = "Blog:";
+$locale['509b'] = "Blog Fragmento:";
+$locale['510b'] = "Ver Blog";
+// Add/Delete Article Submissions
+$locale['530'] = "Publicar";
+$locale['531'] = "ArtÃ­culo publicado";
+$locale['532'] = "Borrar";
+$locale['533'] = "ArtÃ­culo borrado";
+// Article Submission Form
+$locale['540'] = "Ver";
+$locale['541'] = "El siguiente artÃ­culo ha sido enviado por ";
+$locale['542'] = "Â¿Publicar o borrar este artÃ­culo?";
+$locale['543'] = "Publicar";
+$locale['544'] = "Borrar";
+$locale['545'] = "CategorÃ­a:";
+$locale['546'] = "TÃ­tulo:";
+$locale['547'] = "Resumen:";
+$locale['548'] = "ArtÃ­culo:";
+$locale['549'] = "Vista Previa";
+// Add/Delete Photo Submissions
+$locale['580'] = "Publicar";
+$locale['581'] = "Foto publicada";
+$locale['582'] = "Borrar";
+$locale['583'] = "Foto borrada";
+// Photo Submission Form
+$locale['590'] = "Ver";
+$locale['591'] = "Pulsa aquÃ­ para ver la foto";
+$locale['592'] = "La siguiente foto ha sido enviada por ";
+$locale['593'] = "Â¿Publicar o borrar esta foto?";
+$locale['594'] = "Publicar";
+$locale['595'] = "Borrar";
+$locale['596'] = "TÃ­tulo:";
+$locale['597'] = "DescripciÃ³n:";
+$locale['598'] = "Ã¡lbum";
+// Add/Delete Download Submissions
+$locale['630'] = "Publicar";
+$locale['631'] = "La descarga ha sido publicada";
+$locale['632'] = "Borrar";
+$locale['633'] = "La descarga ha sido borrada";
+// Download Submission Form
+$locale['640'] = "Ver la Descarga";
+$locale['641'] = "Esta descarga ha sido enviada por ";
+$locale['642'] = "Â¿QuiÃ©res publicar o borrar esta descarga?";
+$locale['643'] = "Publcar";
+$locale['644'] = "Borrar";
+$locale['645'] = "TÃ­tulo:";
+$locale['646'] = "DescripciÃ³n:";
+$locale['646b'] = "Resumen:";
+$locale['647'] = "URL:";
+$locale['647b'] = "Archivo:";
+$locale['648'] = "CategorÃ­a:";
+$locale['649'] = "Licencia:";
+$locale['650'] = "S/O:";
+$locale['651'] = "VersiÃ³n:";
+$locale['652'] = "TamaÃ±o del Archivo:";
+$locale['653'] = "Imagen:";
+$locale['654'] = "Sitio Web:";
+$locale['655'] = "Copyright:";
+$locale['656'] = "Comentarios:";
+$locale['657'] = "Valoraciones:";
+$locale['658'] = "Borrar la Imagen";
+$locale['660'] = "%s esta funciÃ³n estÃ¡ desactivada.<br />Esta configuraciÃ³n sÃ³lo tendrÃ¡ efecto cuando la funciÃ³n vuelva a ser activada.";
+$locale['661'] = "Comentarios";
+$locale['662'] = "Valoraciones";
+$locale['663'] = "Comentarios y Valoraciones";
