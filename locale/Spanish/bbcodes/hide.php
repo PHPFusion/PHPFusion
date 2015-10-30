fbf73d67a612b68ea32c206d4dc784385000194f
Talocha <mateofds@hotmail.com>
2015-10-30 20:04:37 +0100

    Create hide.php

---
+++ locale/Spanish/bbcodes/hide.php
@@ -0,0 +1,4 @@
+<?php
+$locale['bb_hide'] = "Mensaje para Administradores";
+$locale['bb_hide_description'] = "Mensajes sÃ³lo visibles para los Administradores.";
+$locale['bb_hide_usage'] = "Texto sÃ³lo visible por los Administradores.";
