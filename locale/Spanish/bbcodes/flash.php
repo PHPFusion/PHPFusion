7f8d4793a165a4304e9f2f9377724c1961ceab58
Talocha <mateofds@hotmail.com>
2015-10-30 19:58:48 +0100

    Create flash.php

---
+++ locale/Spanish/bbcodes/flash.php
@@ -0,0 +1,5 @@
+<?php
+$locale['bb_flash_description'] = "Muestra una animaciÃ³n Flash desde la URL que se indique.";
+$locale['bb_flash_usage'] = "URL a la animaciÃ³n Flash.";
+$locale['bb_flash_width'] = "Ancho en px.";
+$locale['bb_flash_height'] = "Alto en px.";
