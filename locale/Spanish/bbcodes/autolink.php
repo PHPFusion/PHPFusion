503c1fe877b100fa38f2bf8e79334ae8418f3ab7
Talocha <mateofds@hotmail.com>
2015-10-30 19:39:23 +0100

    Create autolink.php

---
+++ locale/Spanish/bbcodes/autolink.php
@@ -0,0 +1,3 @@
+<?php
+$locale['bb_autolink_description'] = "Convierte los enlaces web o las direcciones de mail en hiperenlaces.";
+$locale['bb_autolink_usage'] = "Este cÃ³digo BB no tiene botÃ³n y DEBE estar situado por encima de los cÃ³digos BB de URL y Mail.";
