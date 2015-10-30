5acff9df7367387cbf00e8aae5f00092f49eec69
Talocha <mateofds@hotmail.com>
2015-10-30 20:01:51 +0100

    Create geshi.php

---
+++ locale/Spanish/bbcodes/geshi.php
@@ -0,0 +1,8 @@
+<?php
+$locale['bb_geshi_description'] = "Utilidad GeSHi para el resaltado de cÃ³digo.";
+$locale['bb_geshi_usage'] = "Texto del cÃ³digo a resaltar por medio de la Clase GeSHi.";
+$locale['bb_geshi_lang'] = "Nombre del leguaje del cÃ³digo.";
+$locale['bb_geshi_parser1'] = "Aviso del analizador";
+$locale['bb_geshi_parser2'] = "El cÃ³digo es demasiado grande para GeSHi. Se sustituye por la etiqueta de cÃ³digo.";
+$locale['bb_geshi_info'] = "Analizado en <TIME> segundos con GeSHi <VERSION>.";
+$locale['bb_geshi_save'] = "Descargar cÃ³digo fuente.";
