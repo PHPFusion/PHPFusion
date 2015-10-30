3baf005f0bd48f6b3e972c27d291a9b009b0d675
Talocha <mateofds@hotmail.com>
2015-10-30 19:52:14 +0100

    Create code.php

---
+++ locale/Spanish/bbcodes/code.php
@@ -0,0 +1,5 @@
+<?php
+$locale['bb_code_description'] = "Muestra texto en cÃ³digo.";
+$locale['bb_code_usage'] = "Algo de CÃ³digo.";
+$locale['bb_code_code'] = "CÃ³digo.";
+$locale['bb_code_save'] = "Descargar cÃ³digo fuente.";
