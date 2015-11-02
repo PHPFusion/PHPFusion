21a34affdd2085a1f629dbf1caf15952ff3b36f9
Talocha <mateofds@hotmail.com>
2015-11-02 19:39:41 +0100

    Create login.php

---
+++ locale/Spanish/permalinks/login.php
@@ -0,0 +1,3 @@
+<?php
+$locale['pl_login_title'] = "Inicio de sesiÃ³n / Cierre de sesiÃ³n";
+$locale['pl_login_desc'] = "Habilitar Enlaces permanentes para la informaciÃ³n Panel de usuario en el sitio.";
