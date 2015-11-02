8286a76bad65c47a1ea4c852b1d560dd70e12a5e
Talocha <mateofds@hotmail.com>
2015-11-02 10:27:39 +0100

    Create mail.php

---
+++ locale/Spanish/bbcodes/mail.php
@@ -0,0 +1,4 @@
+<?php
+$locale['bb_mail'] = "usuario@dominio.com";
+$locale['bb_mail_description'] = "Hace que se pueda pulsar la direcciÃ³n de mail.";
+$locale['bb_mail_usage'] = "DirecciÃ³n de email o texto a mostrar";
