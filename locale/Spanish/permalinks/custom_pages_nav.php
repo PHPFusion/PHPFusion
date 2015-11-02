8ca61f53460c059301a8df1f6b1da8b403822004
Talocha <mateofds@hotmail.com>
2015-11-02 11:27:26 +0100

    Create custom_pages_nav.php

---
+++ locale/Spanish/permalinks/custom_pages_nav.php
@@ -0,0 +1,3 @@
+<?php
+$locale['pl_cpnav_title'] = "NavegaciÃ³n de pÃ¡ginas personalizadas";
+$locale['pl_cpnav_desc'] = "Habilitar Enlaces permanentes para los enlaces de pÃ¡ginas personalizadas de navegaciÃ³n en el sitio.";
