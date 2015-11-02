0d707745467d31f439d1f17ae6c1dd20ffeffbfb
Talocha <mateofds@hotmail.com>
2015-11-02 10:41:35 +0100

    Create search.php

---
+++ locale/Spanish/bbcodes/search.php
@@ -0,0 +1,6 @@
+<?php
+$locale['bb_search_description'] = "Buscador interno del texto indicado";
+$locale['bb_search_usage'] = "Texto a buscar";
+$locale['bb_search_prefix'] = "Buscar ";
+$locale['bb_search_suffix'] = " en ";
+$locale['bb_search_where'] = "Donde";
