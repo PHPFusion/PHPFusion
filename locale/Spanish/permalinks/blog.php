72127b19d0f348784d7ed2792dc1d75802cd3667
Talocha <mateofds@hotmail.com>
2015-11-02 11:18:36 +0100

    Create blog.php

---
+++ locale/Spanish/permalinks/blog.php
@@ -0,0 +1,5 @@
+<?php
+$locale['pl_blog_title'] = "Blog";
+$locale['pl_blog_desc'] = "Habilitar Enlaces permanentes para Blog en el sitio.";
+$locale['pl_tags_001'] = "IdentificaciÃ³n del Blog artÃ­culo.";
+$locale['pl_tags_002'] = "TÃ­tulo o Tema del Blog artÃ­culo.";
