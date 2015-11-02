084038a64e9ea44f8f6d528ec7d0d324805e6682
Talocha <mateofds@hotmail.com>
2015-11-02 19:38:06 +0100

    Create forums.php

---
+++ locale/Spanish/permalinks/forums.php
@@ -0,0 +1,5 @@
+<?php
+$locale['pl_forums_title'] = "Foros";
+$locale['pl_forums_desc'] = "Habilitar Enlaces permanentes para los Enlaces Foro";
+$locale['pl_tags_001'] = "IdentificaciÃ³n Foro para un foro especÃ­fico.";
+$locale['pl_tags_002'] = "TÃ­tulo de un foro especÃ­fico.";
