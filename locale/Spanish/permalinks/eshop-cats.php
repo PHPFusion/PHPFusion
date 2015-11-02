d7c66bc2fec3e9f0a71030f53722628b9c4e0b51
Talocha <mateofds@hotmail.com>
2015-11-02 19:35:06 +0100

    Create eshop-cats.php

---
+++ locale/Spanish/permalinks/eshop-cats.php
@@ -0,0 +1,5 @@
+<?php
+$locale['pl_eshop_cats_title'] = "eShop CategorÃ­a";
+$locale['pl_eshop_cats_desc'] = "Habilitar Enlaces permanentes para la CategorÃ­a eShop en el sitio.";
+$locale['pl_tags_001'] = "IdentificaciÃ³n del eShop artÃ­culo CategorÃ­a.";
+$locale['pl_tags_002'] = "TÃ­tulo o Tema de la eShop artÃ­culo CategorÃ­a.";
