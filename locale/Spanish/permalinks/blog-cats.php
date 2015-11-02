2dcd02885e468131549437290d037d34c75ce7ca
Talocha <mateofds@hotmail.com>
2015-11-02 11:22:06 +0100

    Create blog-cats.php

---
+++ locale/Spanish/permalinks/blog-cats.php
@@ -0,0 +1,5 @@
+<?php
+$locale['pl_blog_cats_title'] = "Blog CategorÃ­a";
+$locale['pl_blog_cats_desc'] = "Habilitar Enlaces permanentes para el Blog de CategorÃ­a en el sitio.";
+$locale['pl_tags_001'] = "IdentificaciÃ³n del Blog artÃ­culo CategorÃ­a.";
+$locale['pl_tags_002'] = "TÃ­tulo o Tema del Blog artÃ­culo CategorÃ­a.";
