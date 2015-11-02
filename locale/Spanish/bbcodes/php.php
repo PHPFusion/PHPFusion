3c7f95cc18fd5c1a53af72644c0f178ff1f117f2
Talocha <mateofds@hotmail.com>
2015-11-02 10:33:01 +0100

    Create php.php

---
+++ locale/Spanish/bbcodes/php.php
@@ -0,0 +1,5 @@
+<?php
+$locale['bb_php'] = "PHP";
+$locale['bb_php_description'] = "Resalta el texto seleccionado como cÃ³digo PHP.";
+$locale['bb_php_usage'] = "CÃ³digo a resaltar como PHP.";
+$locale['bb_php_save'] = "Descargar el cÃ³digo fuente.";
