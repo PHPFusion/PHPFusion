81a7f8368c14473b6a8bf62453bff6a690d65b81
Talocha <mateofds@hotmail.com>
2015-11-02 19:33:41 +0100

    Create eshop.php

---
+++ locale/Spanish/permalinks/eshop.php
@@ -0,0 +1,5 @@
+<?php
+$locale['pl_eshop_title'] = "eShop";
+$locale['pl_eshop_desc'] = "Habilitar Enlaces permanentes para eShop en el sitio.";
+$locale['pl_tags_001'] = "IdentificaciÃ³n del artÃ­culo eShop.";
+$locale['pl_tags_002'] = "TÃ­tulo o Asunto del Tema eShop.";
