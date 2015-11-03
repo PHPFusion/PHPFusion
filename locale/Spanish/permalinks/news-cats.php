7036d9dcb3d80864a1e8a1dd2fd5d2b8994c1926
Talocha <mateofds@hotmail.com>
2015-11-03 08:22:04 +0100

    Create news-cats.php

---
+++ locale/Spanish/permalinks/news-cats.php
@@ -0,0 +1,5 @@
+<?php
+$locale['pl_news_cats_title'] = "Noticias CategorÃ­a";
+$locale['pl_news_cats_desc'] = "Habilitar Enlaces permanentes para las noticias CategorÃ­a en el sitio.";
+$locale['pl_tags_001'] = "ID del artÃ­culo CategorÃ­a Noticias.";
+$locale['pl_tags_002'] = "TÃ­tulo o Tema de las noticias artÃ­culo CategorÃ­a.";
