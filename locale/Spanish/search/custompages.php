8bbf73e271a013c8f6eebfc5e10086460a963822
Talocha <mateofds@hotmail.com>
2015-11-03 09:46:50 +0100

    Create custompages.php

---
+++ locale/Spanish/search/custompages.php
@@ -0,0 +1,7 @@
+<?php
+$locale['c400'] = "PÃ¡ginas a Medida";
+$locale['c401'] = "PÃ¡gina a Medida";
+$locale['c402'] = "PÃ¡ginas a Medida";
+$locale['c403'] = "encontrado(s) en el";
+$locale['c404'] = "tÃ­tulo de la pÃ¡gina";
+$locale['c405'] = "contenido de la pÃ¡gina";
