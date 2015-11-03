398f2f0ccf99097de836eab9609c4726203dba6d
Talocha <mateofds@hotmail.com>
2015-11-03 09:02:36 +0100

    Create articles.php

---
+++ locale/Spanish/search/articles.php
@@ -0,0 +1,7 @@
+<?php
+$locale['a400'] = "ArtÃ­culos";
+$locale['a401'] = "ArtÃ­culo";
+$locale['a402'] = "ArtÃ­culos";
+$locale['a403'] = "encontrado(s) en el";
+$locale['a404'] = "tÃ­tulo del artÃ­culo";
+$locale['a405'] = "texto del artÃ­culo";
