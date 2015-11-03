2b2ec6b5c68a45d6481c0112d9da1500e3723a30
Talocha <mateofds@hotmail.com>
2015-11-03 11:37:47 +0100

    Create articles.php

---
+++ infusions/articles/locale/Spanish/articles.php
@@ -0,0 +1,5 @@
+<?php
+$locale['400'] = "ArtÃ­culos";
+$locale['401'] = "No hay categorÃ­as de artÃ­culo definido";
+$locale['402'] = "Nuevo";
+$locale['403'] = "No hay artÃ­culos aÃ±adidos a esta categorÃ­a";
