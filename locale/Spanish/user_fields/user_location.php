abfe19272ba40c6679b96ac892474c59a62b37d4
Talocha <mateofds@hotmail.com>
2015-11-03 10:29:14 +0100

    Create user_location.php

---
+++ locale/Spanish/user_fields/user_location.php
@@ -0,0 +1,4 @@
+<?php
+$locale['uf_location'] = "LocalizaciÃ³n";
+$locale['uf_location_desc'] = "Permite a los usuarios indicar su localizaciÃ³n (paÃ­s, ciudad, etc.)";
+$locale['uf_location_error'] = "La localizaciÃ³n no puede dejarse vacÃ­a.";
