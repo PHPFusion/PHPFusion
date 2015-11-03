ff54d26f6559fbd6fdccc369adc86814aadfa91f
Talocha <mateofds@hotmail.com>
2015-11-03 12:20:38 +0100

    Create articles_admin.php

---
+++ infusions/articles/locale/Spanish/articles_admin.php
@@ -0,0 +1,108 @@
+<?php
+// Article titles
+$locale['articles_0000'] = "ArtÃ­culos actuales";
+$locale['articles_0001'] = "ArtÃ­culos";
+$locale['articles_0002'] = "AÃ±adir artÃ­culo";
+$locale['articles_0003'] = "Editar artÃ­culo";
+// Category titles
+$locale['articles_0020'] = "CategorÃ­as de artÃ­culo";
+$locale['articles_0021'] = "Agregar categorÃ­a artÃ­culo";
+$locale['articles_0022'] = "Editar categorÃ­a artÃ­culo";
+$locale['articles_0023'] = "Todas las entradas del artÃ­culo";
+$locale['articles_0024'] = "Actualmente exhibiendo %d de %d entradas del total del artÃ­culo";
+$locale['articles_0025'] = "Filtrar por:";
+$locale['articles_0026'] = "Filtro mostrar artÃ­culos de";
+$locale['articles_0027'] = "Editor de la categorÃ­a de artÃ­culo";
+// Article Settings
+$locale['articles_0030'] = "ConfiguraciÃ³n de artÃ­culo";
+$locale['articles_0031'] = "Las opciones de configuraciÃ³n para el sistema de artÃ­culo";
+$locale['articles_0032'] = "ArtÃ­culo artÃ­culos por pÃ¡gina";
+$locale['articles_0033'] = "Â¿Permite presentaciones de artÃ­culo?";
+$locale['articles_0034'] = "Â¿Requerir artÃ­culos extendidos?";
+
+// Article Submissions
+$locale['articles_0040'] = "las presentaciones";
+$locale['articles_0041'] = "Tema de presentaciÃ³n para revisiÃ³n";
+$locale['articles_0042'] = "actualmente no hay propuestas de artÃ­culo";
+$locale['articles_0043'] = "Lo sentimos, actualmente no aceptamos los envÃ­os de artÃ­culos en este sitio.";
+$locale['articles_0044'] = "Actualmente hay %s pendientes para su revisiÃ³n.";
+$locale['articles_0045'] = "Vista previa artÃ­culo";
+$locale['articles_0046'] = "Autor de la presentaciÃ³n";
+$locale['articles_0047'] = "Tiempo de presentaciÃ³n";
+$locale['articles_0048'] = "PresentaciÃ³n Id";
+$locale['articles_0049'] = "PresentaciÃ³n de artÃ­culo ha sido borrado";
+$locale['articles_0050'] = "ArtÃ­culo Presentado ha sido publicado";
+$locale['articles_0051'] = "ArtÃ­culo Presentado se guarda como borrador";
+$locale['articles_0052'] = "El artÃ­culo anterior fue presentado por ";
+$locale['articles_0053'] = "publicado por ";
+
+
+$locale['articles_0060'] = "Enviar artÃ­culo";
+$locale['articles_0061'] = "Gracias por enviar tu artÃ­culo";
+$locale['articles_0062'] = "Presentar otro artÃ­culo";
+$locale['articles_0063'] = "Utilice el siguiente formulario para enviar artÃ­culo. Su envÃ­o serÃ¡ revisado por un
+Administrator. ".fusion_get_settings('sitename')." se reserva el derecho de modificar o editar cualquier envÃ­o. ArtÃ­culo
+debe ser aplicable al contenido de este sitio. Se eliminarÃ¡n las presentaciones inadecuadas.";
+$locale['articles_0064'] = "Volver a ".fusion_get_settings('sitename');
+
+// Article messages
+$locale['articles_0100'] = "ArtÃ­culo aÃ±adido";
+$locale['articles_0101'] = "ArtÃ­culo actualizado";
+$locale['articles_0102'] = "ArtÃ­culo borrado";
+// Category messages
+$locale['articles_0150'] = "CategorÃ­a de artÃ­culo aÃ±adido";
+$locale['articles_0151'] = "CategorÃ­a de artÃ­culo actualizado";
+$locale['articles_0152'] = "CategorÃ­a de artÃ­culo no se puede eliminar";
+$locale['articles_0153'] = "Hay artÃ­culos o subcategorÃ­as vinculadas a esta categorÃ­a";
+$locale['articles_0154'] = "CategorÃ­a de artÃ­culo borrado";
+// Article form
+$locale['articles_0200'] = "Asunto:";
+$locale['articles_0201'] = "CategorÃ­a:";
+$locale['articles_0202'] = "Fragmento:";
+$locale['articles_0203'] = "ArtÃ­culo:";
+$locale['articles_0204'] = "Palabras clave";
+$locale['articles_0204a'] = "Pulsar intro despuÃ©s de cada palabras clave";
+$locale['articles_0205'] = "Guardar como borrador";
+$locale['articles_0206'] = "Saltos de lÃ­nea automÃ¡ticos";
+$locale['articles_0207'] = "Permitir comentarios";
+$locale['articles_0208'] = "Permitir calificaciones";
+$locale['articles_0209'] = "Salto de pÃ¡gina";
+$locale['articles_0210'] = "[borrador]";
+$locale['articles_0211'] = "Visibilidad";
+$locale['articles_0240'] = "Vista previa artÃ­culo";
+$locale['articles_0241'] = "Guardar artÃ­culo";
+$locale['articles_0242'] = "Publicar artÃ­culo";
+$locale['articles_0243'] = "Eliminar artÃ­culo";
+
+// Popup Error/Warning Messages
+$locale['articles_0250'] = "Por favor especifique un Asunto";
+$locale['articles_0251'] = "Â¿Eliminar este artÃ­culo?";
+$locale['articles_0252'] = "No hay categorÃ­as de artÃ­culo definido";
+$locale['articles_0253'] = "Debe definir al menos una categorÃ­a antes de poder agregar cualquier artÃ­culos";
+$locale['articles_0254'] = "Haga clic aquÃ­";
+$locale['articles_0255'] = " ir a artÃ­culo categorÃ­as";
+$locale['articles_0256'] = "%s estÃ¡n desactivados todos los elementos.<br />Esta opciÃ³n solo tendrÃ¡ efecto cuando se habilitan nuevamente.";
+$locale['articles_0257'] = "Por favor especifique palabras clave";
+// Category form
+$locale['articles_0300'] = "Nombre de la categorÃ­a:";
+$locale['articles_0301'] = "DescripciÃ³n de la categorÃ­a:";
+$locale['articles_0302'] = "ClasificaciÃ³n de CategorÃ­a:";
+$locale['articles_0303'] = "ID de artÃ­culo";
+$locale['articles_0304'] = "ArtÃ­culo Asunto";
+$locale['articles_0305'] = "Fecha del artÃ­culo";
+$locale['articles_0306'] = "Ascendente";
+$locale['articles_0307'] = "Descendente";
+$locale['articles_0308'] = "CategorÃ­a Superior:";
+$locale['articles_0309'] = "Guardar CategorÃ­a";
+// Current category options
+$locale['articles_0340'] = "CategorÃ­a";
+$locale['articles_0341'] = "Opciones";
+$locale['edit'] = "Editar";
+$locale['delete'] = "Eliminar";
+$locale['articles_0342'] = "No hay categorÃ­as de artÃ­culo definido";
+$locale['articles_0343'] = "No hay artÃ­culo definido";
+// Delete Article category
+$locale['articles_0350'] = "Â¿Desea eliminar esta categorÃ­a de artÃ­culo?";
+// Error Messages
+$locale['articles_0351'] = "Por favor, introduzca un nombre de categorÃ­a.";
+$locale['articles_0352'] = "Esta categorÃ­a ya existe.";
