6b12fa22728a12435cab7e6331f8366078551c4d
Talocha <mateofds@hotmail.com>
2015-11-03 10:14:55 +0100

    Create user_birthdate.php

---
+++ locale/Spanish/user_fields/user_birthdate.php
@@ -0,0 +1,4 @@
+<?php
+$locale['uf_birthdate'] = "Fecha de Nacimiento";
+$locale['uf_birthdate_desc'] = "Permite a los usuarios indicar su fecha de nacimiento";
+$locale['uf_birthdate_error'] = "La fecha de nacimiento no estÃ¡ configurada correctamente.";
