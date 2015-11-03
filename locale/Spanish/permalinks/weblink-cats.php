14b64fcff42fce8450f8d0b4a17c180d3ef9c4ea
Talocha <mateofds@hotmail.com>
2015-11-03 08:51:52 +0100

    Create weblink-cats.php

---
+++ locale/Spanish/permalinks/weblink-cats.php
@@ -0,0 +1,5 @@
+<?php
+$locale['pl_weblinks_cats_title'] = "Enlaces CategorÃ­a";
+$locale['pl_weblinks_cats_desc'] = "Habilitar Enlaces permanentes para la CategorÃ­a Enlaces en el sitio.";
+$locale['pl_tags_001'] = "IdentificaciÃ³n del artÃ­culo CategorÃ­a Enlaces.";
+$locale['pl_tags_002'] = "TÃ­tulo o tema del elemento de categorÃ­a de enlaces Web.";
