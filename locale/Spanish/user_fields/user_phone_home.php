7bf280907742ceff6de373db66f1250ae0502e35
Talocha <mateofds@hotmail.com>
2015-11-03 10:53:09 +0100

    Create user_phone_home.php

---
+++ locale/Spanish/user_fields/user_phone_home.php
@@ -0,0 +1,4 @@
+<?php
+$locale['uf_phone_home'] = "TelÃ©fono de casa";
+$locale['uf_phone_home_desc'] = "NÃºmero de telÃ©fono de usuario casero (fijo)";
+$locale['uf_phone_home_error'] = "TelÃ©fono particular del usuario no puede dejarse vacÃ­o.";
