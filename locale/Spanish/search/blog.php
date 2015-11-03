d54ecc337300170c683b19b12c1b40383c415a74
Talocha <mateofds@hotmail.com>
2015-11-03 09:07:27 +0100

    Create blog.php

---
+++ locale/Spanish/search/blog.php
@@ -0,0 +1,8 @@
+<?php
+$locale['n400'] = "Blog";
+$locale['n401'] = "artÃ­culo de blog";
+$locale['n402'] = "artÃ­culos de blog";
+$locale['n403'] = "encontrar en";
+$locale['n404'] = "Blog sujeto";
+$locale['n405'] = "Blog texto";
+$locale['n406'] = "texto extendido de Blog";
