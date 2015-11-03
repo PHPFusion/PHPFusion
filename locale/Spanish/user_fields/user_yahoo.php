c91b512b484cd89868043e788d325618b429f362
Talocha <mateofds@hotmail.com>
2015-11-03 11:30:29 +0100

    Create user_yahoo.php

---
+++ locale/Spanish/user_fields/user_yahoo.php
@@ -0,0 +1,4 @@
+<?php
+$locale['uf_yahoo'] = "Yahoo";
+$locale['uf_yahoo_desc'] = "Identificador de la mensajerÃ­a de Yahoo!";
+$locale['uf_yahoo_error'] = "El ID de Yahoo! no puede dejarse vacÃ­o.";
