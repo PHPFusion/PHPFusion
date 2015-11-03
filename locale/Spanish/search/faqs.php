30a2a6cc57433d2ad8cb9821da9c3ee3191f7bc6
Talocha <mateofds@hotmail.com>
2015-11-03 09:50:52 +0100

    Create faqs.php

---
+++ locale/Spanish/search/faqs.php
@@ -0,0 +1,7 @@
+<?php
+$locale['fq400'] = "Preguntas Frecuentes";
+$locale['fq401'] = "Pregunta Frecuente";
+$locale['fq402'] = "Preguntas Frecuentes";
+$locale['fq403'] = "encontrado en el";
+$locale['fq404'] = "texto de la pregunta";
+$locale['fq405'] = "texto de la respuesta";
