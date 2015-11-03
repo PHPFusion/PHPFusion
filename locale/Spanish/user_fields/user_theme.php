70fc87f9b61998a5ecbe97d8c85a710d2df65b4c
Talocha <mateofds@hotmail.com>
2015-11-03 11:24:33 +0100

    Create user_theme.php

---
+++ locale/Spanish/user_fields/user_theme.php
@@ -0,0 +1,4 @@
+<?php
+$locale['uf_theme'] = "Tema";
+$locale['uf_theme_desc'] = "Permite a los usuarios elegir el diseÃ±o (theme) del sitio web";
+$locale['uf_theme_error'] = "El diseÃ±o (theme) no puede dejarse vacÃ­o.";
