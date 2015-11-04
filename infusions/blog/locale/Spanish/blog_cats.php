f1a785cc9fcbd6b173ea1bd87cdac054916b0278
Talocha <mateofds@hotmail.com>
2015-11-04 11:38:31 +0100

    Create blog_cats.php

---
+++ infusions/blog/locale/Spanish/blog_cats.php
@@ -0,0 +1,9 @@
+<?php
+$locale['400'] = "CategorÃ­as del blog";
+$locale['401'] = "CategorÃ­a:";
+$locale['402'] = "NÃºmero de artÃ­culos:";
+$locale['403'] = "Blogs No-categorizados";
+$locale['404'] = "No hay artÃ­culos de blog en esta categorÃ­a.";
+$locale['405'] = "MÃ¡s";
+$locale['406'] = "Mostrar Todas las categorÃ­as";
+$locale['407'] = "No hay categorÃ­as definidas blog";
