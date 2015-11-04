f8b6ef38e8e3b5dfba8b390d292f9417b38e5513
Talocha <mateofds@hotmail.com>
2015-11-04 11:32:43 +0100

    Create blog_admin.php

---
+++ infusions/blog/locale/Spanish/blog_admin.php
@@ -0,0 +1,116 @@
+<?php
+// blog Submissions messages
+$locale['blog_0130'] = "Actualmente no hay envÃ­os en el blog";
+$locale['blog_0131'] = "RevisiÃ³n PresentaciÃ³n del Blog";
+$locale['blog_0132'] = "El anterior blog fue presentado por ";
+$locale['blog_0133'] = "Â¿Desea publicar o eliminar este artÃ­culo de blog?";
+$locale['blog_0134'] = "Publicar blog";
+$locale['blog_0135'] = "Borrar Blog";
+$locale['blog_0136'] = "PresentaciÃ³n para revisiÃ³n";
+$locale['blog_0137'] = "Actualmente hay %s pendientes de revisiÃ³n.";
+$locale['blog_0138'] = "Lo sentimos, por el momento no aceptamos ninguna presentaciÃ³n de blog en este sitio.";
+$locale['blog_0141'] = "Vista previa Blog";
+$locale['blog_0142'] = "Autor de la presentaciÃ³n";
+$locale['blog_0143'] = "Tiempo de presentaciÃ³n";
+$locale['blog_0144'] = "PresentaciÃ³n Id";
+$locale['blog_0145'] = "PresentaciÃ³n Borrada";
+$locale['blog_0146'] = "EnvÃ­os se ha publicado";
+$locale['blog_0147'] = "EnvÃ­os se guarda como borrador";
+$locale['blog_0148'] = "Publicado por ";
+// Blog titles
+$locale['blog_0400'] = "Blogs actuales";
+$locale['blog_0401'] = "AÃ±adir Blog";
+$locale['blog_0402'] = "Editar Blog";
+$locale['blog_0405'] = "Blog";
+$locale['blog_0406'] = "Ajustes de blog";
+$locale['blog_0407'] = "Blog Categorias";
+$locale['blog_0408'] = "Actualmente exhibiendo %d de %d total entradas de blog";
+$locale['blog_0409'] = "AÃ±adir CategorÃ­a Blog";
+// Blog messages
+$locale['blog_0410'] = "ArtÃ­culo blog aÃ±adido";
+$locale['blog_0411'] = "ArtÃ­culo blog actualizado";
+$locale['blog_0412'] = "ArtÃ­culo del blog eliminado";
+$locale['blog_0413'] = "Nombre de archivo de imagen no vÃ¡lido.";
+$locale['blog_0414'] = "Imagen debe ser inferior a %s.";
+$locale['blog_0415'] = "Imagen debe ser uno de los siguientes tipos de archivo %s.";
+$locale['blog_0416'] = "Imagen debe ser no mayor que %s pÃ­xeles.";
+$locale['blog_0417'] = "Error desconocido";
+// Blog form
+$locale['blog_0420'] = "Editar";
+$locale['blog_0421'] = "Eliminar";
+$locale['blog_0422'] = "Asunto:";
+$locale['blog_0423'] = "CategorÃ­a:";
+$locale['blog_0424'] = "sin categorizar";
+$locale['blog_0425'] = "Breve fragmento:";
+$locale['blog_0425a'] = "Breve IntroducciÃ³n";
+$locale['blog_0426'] = "Extendido:";
+$locale['blog_0426b'] = "Blog contenido";
+$locale['blog_0427'] = "Fecha de inicio:";
+$locale['blog_0428'] = "Fecha final:";
+$locale['blog_0429'] = "(opcional)";
+$locale['blog_0430'] = "Visibilidad:";
+$locale['blog_0431'] = "Guardar como borrador";
+$locale['blog_0432'] = "Hacer este Blog tema pegajoso";
+$locale['blog_0433'] = "Saltos de lÃ­nea automÃ¡ticos";
+$locale['blog_0434'] = "Permitir comentarios";
+$locale['blog_0435'] = "Permiten calificaciones";
+$locale['blog_0436'] = "Vista previa Blog";
+$locale['blog_0437'] = "Guardar Blog";
+$locale['blog_0438'] = "borrador";
+$locale['blog_0439'] = "Blog imagen:";
+$locale['blog_0440'] = "Max. tamaÃ±o de archivo %s / tipos de archivo permitidos GIF, JPEG or PNG.";
+$locale['blog_0441'] = "Salto de pÃ¡gina";
+$locale['blog_0442'] = "PosiciÃ³n de la imagen:";
+$locale['blog_0443'] = "Palabras clave:";
+$locale['blog_0444'] = "Pulsar intro despuÃ©s de cada palabras clave";
+// Blog Category
+// Blog Categories titles
+$locale['blog_0500'] = "Editar Blog categorÃ­a";
+$locale['blog_0501'] = "AÃ±adir Blog CategorÃ­a";
+$locale['blog_0502'] = "CategorÃ­as";
+// Blog Categories messages
+$locale['blog_0520'] = "CategorÃ­a blog aÃ±adido";
+$locale['blog_0521'] = "CategorÃ­a blog actualizado";
+$locale['blog_0522'] = "No se puede eliminar la categorÃ­a de blog";
+$locale['blog_0523'] = "Hay Blog artÃ­culos o subcategorÃ­as vinculadas a esta categorÃ­a";
+$locale['blog_0524b'] = "CategorÃ­a blog eliminado";
+// Blog Categories form
+$locale['blog_0530'] = "Nombre de la categorÃ­a:";
+$locale['blog_0531'] = "CategorÃ­a imagen:";
+$locale['blog_0532'] = "Guardar CategorÃ­a";
+$locale['blog_0533'] = "CategorÃ­a Superior:";
+$locale['blog_0535'] = "No hay categorÃ­as definidas blog";
+$locale['blog_0536'] = "Haga clic aquÃ­ para subir imÃ¡genes categorÃ­a";
+$locale['blog_0537'] = "Padres";
+// Popup Error/Warning Messages
+$locale['blog_0550'] = "Â¿Desea eliminar esta categorÃ­a de Blog?";
+// Error Messages
+$locale['blog_0560'] = "Por favor, introduzca un nombre de categorÃ­a.";
+$locale['blog_0561'] = "Esta categorÃ­a ya existe.";
+// Popup Error/Warning Messages
+$locale['blog_0450'] = "Por favor especifique un tema";
+$locale['blog_0451'] = "Â¿Borrar este Blog?";
+$locale['blog_0451b'] = "Â¿Desea eliminar esta categorÃ­a de Blog?";
+$locale['blog_0452'] = "%s estÃ¡n deshabilitados para todos los elementos. <br /> Este ajuste sÃ³lo tendrÃ¡ efecto cuando se habilitan de nuevo.";
+$locale['blog_0456'] = 'No hay ningÃºn Blog en esta categorÃ­a.';
+$locale['blog_0457'] = "Por favor especifique palabras clave";
+$locale['blog_0461'] = 'No hay categorÃ­as de Blog definidas.';
+// Filters
+$locale['blog_0458'] = "Filtrar por:";
+$locale['blog_0459'] = "mostrar por categorÃ­a";
+$locale['blog_0460'] = "Todas las entradas de blog";
+//Alignment
+$locale['left'] = "Izquierda";
+$locale['center'] = "Centro";
+$locale['right'] = "Derecha";
+$locale['blog_0600'] = "presentaciones de blog";
+$locale['blog_0601'] = "presentaciÃ³n imagen";
+$locale['blog_0602'] = "Â¿Requerido el blog extendida?";
+// Public Submissions Form
+$locale['blog_0700'] = "Enviar Blog";
+$locale['blog_0701'] = "Gracias por enviar tu Blog";
+$locale['blog_0702'] = "Enviar otro artÃ­culo Blog";
+$locale['blog_0703'] = "Utilice el siguiente formulario para presentar el Blog. Su envÃ­o serÃ¡ revisado por un
+Administrador. ".fusion_get_settings('sitename')." se reserva el derecho de modificar o editar cualquier envÃ­o. Blog
+debe ser aplicable al contenido de este sitio. Se eliminarÃ¡n las presentaciones inadecuadas.";
+$locale['blog_0704'] = "Volver a ".fusion_get_settings('sitename');
