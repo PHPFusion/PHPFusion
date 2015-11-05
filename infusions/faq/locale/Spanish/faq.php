35c6aec924b04bf1a771cfcf30bd42d0c3743faa
Talocha <mateofds@hotmail.com>
2015-11-05 07:07:38 +0100

    Create faq.php

---
+++ infusions/faq/locale/Spanish/faq.php
@@ -0,0 +1,7 @@
+<?php
+$locale['400'] = "FAQ (preguntas frecuentes)";
+$locale['401'] = "FAQ";
+$locale['402'] = "Arriba";
+// Notices
+$locale['410'] = "No hay FAQ categorÃ­as definidas.";
+$locale['411'] = "No se han aÃ±adido preguntas frecuentes a esta categorÃ­a.";
