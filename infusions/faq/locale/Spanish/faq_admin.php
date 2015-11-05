4e48ca77ac8bcb7af6ecf1d9b0db457c6e8a7015
Talocha <mateofds@hotmail.com>
2015-11-05 07:23:23 +0100

    Create faq_admin.php

---
+++ infusions/faq/locale/Spanish/faq_admin.php
@@ -0,0 +1,40 @@
+<?php
+$locale['faq_0100'] = "Preguntas frecuentes";
+$locale['faq_0101'] = "Actuales";
+$locale['faq_0102'] = "CategorÃ­as";
+$locale['faq_0102b'] = "AÃ±adir FAQ";
+$locale['faq_0102c'] = "Editar preguntas mÃ¡s frecuentes";
+$locale['faq_0102d'] = "Editar FAQ categorÃ­a";
+$locale['faq_0103'] = "Nombre de la categorÃ­a";
+$locale['faq_0104'] = "Conteo preguntas";
+$locale['faq_0105'] = "Id de la categorÃ­a";
+$locale['faq_0106'] = "Opciones";
+$locale['faq_0107'] = "Editar";
+$locale['faq_0108'] = "Borrar";
+$locale['faq_0109'] = "Â¿Eliminar esta categorÃ­a de FAQ?";
+$locale['faq_0110'] = "pregunta:";
+$locale['faq_0111'] = "respuesta:";
+$locale['faq_0112'] = "Â¿Borrar esta pregunta?";
+$locale['faq_0113'] = "No hay pregunta frecuente definida";
+$locale['faq_0114'] = "Listado de %d de %d total entradas";
+$locale['faq_0115'] = "Listado de %d de %d total categorÃ­as";
+$locale['faq_0116'] = "No hay ninguna categorÃ­a de FAQ definida";
+
+// Formulario categoria FAQ
+$locale['faq_0200'] = "Nombre de la categorÃ­a";
+$locale['faq_0201'] = "Rellene nombre de la categorÃ­a";
+$locale['faq_0202'] = "DescripciÃ³n de la categorÃ­a";
+$locale['faq_0203'] = "Guardar categorÃ­a";
+$locale['faq_0204'] = "CategorÃ­a guardada";
+$locale['faq_0205'] = "CategorÃ­a actualizada";
+$locale['faq_0206'] = "CategorÃ­a eliminada";
+$locale['faq_0207'] = "CategorÃ­a de preguntas frecuentes no se puede eliminar porque hay %d preguntas en esta categorÃ­a";
+// Formulario de preguntas frecuentes
+$locale['faq_0300'] = "CategorÃ­a";
+$locale['faq_0301'] = "Pregunta";
+$locale['faq_0302'] = "Respuesta";
+$locale['faq_0303'] = "Guardar FAQ";
+$locale['faq_0304'] = "No estÃ¡ disponible actualmente porque no hay ninguna categorÃ­a de FAQ definida. Haga clic <a href='%s'> aquÃ­</a> para agregar una categorÃ­a de FAQ ";
+$locale['faq_0305'] = "Agregado";
+$locale['faq_0306'] = "Actualizado";
+$locale['faq_0307'] = "Eliminado";
