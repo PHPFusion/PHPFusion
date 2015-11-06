06f03ff92659fe8060848ac6ffada2c287ac72af
Talocha <mateofds@hotmail.com>
2015-11-06 13:27:32 +0100

    Create forum_ranks.php

---
+++ infusions/forum/locale/Spanish/forum_ranks.php
@@ -0,0 +1,40 @@
+<?php
+// Forum Ranks Titles
+$locale['400'] = "AÃ±adir Rango";
+$locale['401'] = "Editar Rango";
+$locale['402'] = "Rangos Existentes";
+$locale['403'] = "Sistema de Rangos Desactivado";
+$locale['404'] = "Rangos del Foro";
+// News messages
+$locale['410'] = "Rango aÃ±adido";
+$locale['411'] = "Rango actualizado";
+$locale['412'] = "Rango borrado";
+$locale['413'] = "SÃ³lo puede aÃ±adirse un Ãºnico rango a los Administradores y Moderadores";
+$locale['414'] = "Por favor, introduzca un TÃ­tulo de rango";
+// Forum Ranks form
+$locale['420'] = "TÃ­tulo del Rango:";
+$locale['421'] = "Imagen del Rango:";
+$locale['422'] = "Mensajes del Rango:";
+$locale['423'] = "AplicaciÃ³n:";
+$locale['424'] = "Miembros";
+$locale['425'] = "Moderadores";
+$locale['426'] = "Administradores";
+$locale['427'] = "Super Administradores";
+$locale['428'] = "Guardar";
+$locale['429'] = "Tipo de Rango:";
+$locale['429a'] = "Especial";
+$locale['429b'] = "AdministraciÃ³n";
+$locale['429c'] = "NÃºmero de Mensajes";
+// Current Forum Ranks
+$locale['430'] = "TÃ­tulo";
+$locale['431'] = "AplicaciÃ³n";
+$locale['432'] = "Imagen";
+$locale['433'] = "Mensajes";
+$locale['434'] = "Opciones";
+$locale['435'] = "Editar";
+$locale['436'] = "Borrar";
+$locale['437'] = "No se han creado rangos del foro.";
+$locale['438'] = "Tipo/Mensajes";
+// Forum Ranks Disabled
+$locale['450'] = "Los rangos del foro estÃ¡n desactivados.<br />Puedes activarlos desde %s Panel de AdministraciÃ³n.";
+$locale['451'] = "Conf. Foro";
