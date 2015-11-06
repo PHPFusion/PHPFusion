8eb1f6f3d865d269c0d15fd11170770658c008e6
Frederick MC Chan <meangczac.chan@gmail.com>
2015-11-06 10:38:59 +0800

    Close #435

--- locale/English/global.php
+++ locale/English/global.php
@@ -245,7 +245,7 @@ $locale['global_194'] = "This account is currently suspended.";
 $locale['global_195'] = "This account has not been activated.";
 $locale['global_196'] = "Invalid username or password.";
 
-$locale['global_197'] = "Please wait while we transfer you...\n\n[ <a href='index.php'>Or click here if you do not wish to wait</a> ]";
+$locale['global_197'] = "Please wait while we transfer you...\n\n[ [LINK]Or click here if you do not wish to wait[/LINK] ]";
 
 $locale['global_198'] = "WARNING: INSTALLER DETECTED, PLEASE DELETE THE /INSTALL/ FOLDER IMMEDIATELY.";
 $locale['global_199'] = "WARNING: admin password not set, click [LINK]Edit Profile[/LINK] to set it.";
