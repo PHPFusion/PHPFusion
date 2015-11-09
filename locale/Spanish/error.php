d2025d431d65fe4510548f72bd90b7eb7d6c76cf
Talocha <mateofds@hotmail.com>
2015-11-09 15:20:37 +0100

    Update error.php

--- locale/Spanish/error.php
+++ locale/Spanish/error.php
@@ -1,7 +1,7 @@
 <?php
-$locale['err401'] = "Acceso no autorizado a esta pÃ¡gina!";
-$locale['err403'] = "Acceso prohibido a esta pÃ¡gina!";
-$locale['err404'] = "Esta pÃ¡gina no se encuentra en este servidor.";
+$locale['err401'] = "Acceso no autorizado a esta página!";
+$locale['err403'] = "Acceso prohibido a esta página!";
+$locale['err404'] = "Esta página no se encuentra en este servidor.";
 $locale['err500'] = "Error interno del servidor.";
 $locale['errunk'] = "Error desconocido";
 $locale['errret'] = "Volver al sitio Principal";
