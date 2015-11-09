c9328871b48ebb1aef0651902ba30a11517564fd
Talocha <mateofds@hotmail.com>
2015-11-09 15:14:14 +0100

    Update contact.php

--- locale/Spanish/contact.php
+++ locale/Spanish/contact.php
@@ -1,23 +1,23 @@
 <?php
 // Contact Form
 $locale['400'] = "Contactar";
-$locale['401'] = "Hay varias maneras que usted puede ponerse en contacto conmigo. Usted me puede enviar por correo electrÃ³nico directamente en<br />
-Puedes enviar un email a ".hide_email(fusion_get_settings('siteemail')).". Si estÃ¡s registrado, puedes enviar un<br />
+$locale['401'] = "Hay varias maneras que usted puede ponerse en contacto conmigo. Usted me puede enviar por correo electrónico directamente en<br />
+Puedes enviar un email a ".hide_email(fusion_get_settings('siteemail')).". Si estás registrado, puedes enviar un<br />
 <a href='messages.php?msg_send=1'>Mensaje Privado</a>.<br />
-Y tambiÃ©n puedes rellenar el siguiente formulario.";
+Y también puedes rellenar el siguiente formulario.";
 $locale['402'] = "Tu Nombre:";
 $locale['403'] = "Email:";
 $locale['404'] = "Asunto:";
 $locale['405'] = "Mensaje:";
 $locale['406'] = "Enviar";
-$locale['407'] = "CÃ³digo de ValidaciÃ³n:";
-$locale['408'] = "Introduce el CÃ³digo de ValidaciÃ³n:";
+$locale['407'] = "Código de Validación:";
+$locale['408'] = "Introduce el Código de Validación:";
 // Contact Errors
 $locale['420'] = "Debes indicar tu nombre";
-$locale['421'] = "Debes indicar tu direcciÃ³n de email";
+$locale['421'] = "Debes indicar tu dirección de email";
 $locale['422'] = "Debes indicar el asunto de tu mensaje";
 $locale['423'] = "Debes escribir el texto de tu mensaje";
-$locale['424'] = "Debes introducir el codigo de validaciÃ³n correcto";
+$locale['424'] = "Debes introducir el codigo de validación correcto";
 $locale['425'] = "Error Interno: No se ha podido enviar el mensaje.";
 // Message Sent
 $locale['440'] = "Tu mensaje ha sido enviado";
