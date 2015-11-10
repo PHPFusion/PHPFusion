bfa9fe8863910be4fc7487ad77fc4d7b86698dde
Talocha <mateofds@hotmail.com>
2015-11-10 11:08:17 +0100

    Update homepage.php

--- locale/Spanish/homepage.php
+++ locale/Spanish/homepage.php
@@ -1,12 +1,12 @@
 <?php
 // block titles
-$locale['home_0000'] = "Ãšltimas Noticias";
-$locale['home_0001'] = "Ãšltimos ArtÃ­culos";
-$locale['home_0002'] = "Ãšltimos Blogs";
-$locale['home_0003'] = "Ãšltimas Descargas";
+$locale['home_0000'] = "Últimas Noticias";
+$locale['home_0001'] = "Últimos Artículos";
+$locale['home_0002'] = "Últimos Blogs";
+$locale['home_0003'] = "Últimas Descargas";
 
 $locale['submit_0000'] = '%s Noticias';
-$locale['submit_0001'] = '%s ArtÃ­culos';
+$locale['submit_0001'] = '%s Artículos';
 $locale['submit_0002'] = '%s Descargas';
 $locale['submit_0003'] = '%s Fotos';
 $locale['submit_0004'] = '%s Enlaces';
@@ -14,13 +14,13 @@ $locale['submit_0005'] = '%s Blog';
 
 // no content messages
 $locale['home_0050'] = "No Hay Noticias para mostrar";
-$locale['home_0051'] = "No hay artÃ­culos para mostrar";
+$locale['home_0051'] = "No hay artículos para mostrar";
 $locale['home_0052'] = "No hay blogs para mostrar";
 $locale['home_0053'] = "No hay Descargas para mostrar";
 
 // common text
-$locale['home_0100'] = "Los mÃ³dulos de contenido no instalados";
-$locale['home_0101'] = "Instale al menos un mÃ³dulo de contenido de texto o cambiar la pÃ¡gina de inicio en configuraciÃ³n principal.";
+$locale['home_0100'] = "Los módulos de contenido no instalados";
+$locale['home_0101'] = "Instale al menos un módulo de contenido de texto o cambiar la página de inicio en configuración principal.";
 
 $locale['home_0102'] = "No categorizados";
 
