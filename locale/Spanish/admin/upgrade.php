96e49f9d92842cd87c804f239382f05f57e4c3de
Talocha <mateofds@hotmail.com>
2015-11-11 20:16:16 +0100

    Update upgrade.php

--- locale/Spanish/admin/upgrade.php
+++ locale/Spanish/admin/upgrade.php
@@ -1,18 +1,18 @@
 <?php
-$locale['400'] = "ActualizaciÃ³n";
-$locale['enable_maint_warning'] = "Por favor ingresa tu sitio web en modo de mantenimiento antes de continuar con el fin de evitar problemas que pueden surgir durante el proceso de actualizaciÃ³n.<br />
-					Usted puede ya sea ir a <a target='_blank' href='settings_security.php".$aidlink."'>ConfiguraciÃ³n de seguridad</a> activarla y haga clic en el botÃ³n de abajo.";
+$locale['400'] = "Actualización";
+$locale['enable_maint_warning'] = "Por favor ingresa tu sitio web en modo de mantenimiento antes de continuar con el fin de evitar problemas que pueden surgir durante el proceso de actualización.<br />
+					Usted puede ya sea ir a <a target='_blank' href='settings_security.php".$aidlink."'>Configuración de seguridad</a> activarla y haga clic en el botón de abajo.";
 $locale['enable_maint'] = "Habilitar Mantenimiento";
 // No upgrade
 $locale['401'] = "No hay disponibles actualizaciones de la base de datos.";
 // Stage 6 - User Field Categories
-$locale['420'] = "InformaciÃ³n de Contacto";
-$locale['421'] = "Otra InformaciÃ³n";
+$locale['420'] = "Información de Contacto";
+$locale['421'] = "Otra Información";
 $locale['422'] = "Opciones";
-$locale['423'] = "EstadÃ­sticas";
+$locale['423'] = "Estadísticas";
 $locale['430'] = "Cat. Campos de Usuario";
-$locale['432'] = "GestiÃ³n de Usuarios";
-$locale['434'] = "Elementos por PÃ¡gina";
+$locale['432'] = "Gestión de Usuarios";
+$locale['434'] = "Elementos por Página";
 $locale['435'] = "Seguridad";
 $locale['436'] = "Conf. Noticias";
 $locale['437'] = "Conf. Descargas";
@@ -21,25 +21,25 @@ $locale['440'] = "Enviar Descargas";
 $locale['441'] = "Mini Charla";
 $locale['442'] = "Archivo robots.txt";
 $locale['443'] = "Registro de Usuarios";
-$locale['444'] = "REinicio ContraseÃ±a Administrador";
+$locale['444'] = "REinicio Contraseña Administrador";
 $locale['445'] = "Registro de Errores";
 //Upgrade locales
-$locale['500'] = "Hay disponible una actualizaciÃ³n %s de la base de datos para esta instalaciÃ³n de PHP-Fusion.";
-$locale['501'] = "Pulsa el botÃ³n Actualizar para instalarla en el sistema.";
-$locale['502'] = "ActualizaciÃ³n de la base de datos completada.";
+$locale['500'] = "Hay disponible una actualización %s de la base de datos para esta instalación de PHP-Fusion.";
+$locale['501'] = "Pulsa el botón Actualizar para instalarla en el sistema.";
+$locale['502'] = "Actualización de la base de datos completada.";
 $locale['503'] = "mayor";
 $locale['504'] = "menor";
-$locale['505'] = "Porfavor, reemplace estas lÃ­neas en tu config.php!";
+$locale['505'] = "Porfavor, reemplace estas líneas en tu config.php!";
 // Cookie Prefix
-$locale['600'] = "Edita el archivo config.php y reemplaza su contenido con el siguiente cÃ³digo:";
+$locale['600'] = "Edita el archivo config.php y reemplaza su contenido con el siguiente código:";
 // Email Template Setup
 // Please do NOT translate the words between brackets [] !
-$locale['T001'] = "Plantillas de correo electrÃ³nico";
-$locale['T101'] = "NotificaciÃ³n de nuevo MP";
+$locale['T001'] = "Plantillas de correo electrónico";
+$locale['T101'] = "Notificación de nuevo MP";
 $locale['T102'] = "Usted tiene un nuevo mensaje privado de [USER] esperando en [SITENAME]";
-$locale['T103'] = "Hola [RECEIVER],\r\n Usted ha recibido un nuevo mensaje privado titulado [SUBJECT] De [USER] en [SITENAME]. Usted puede leer su mensaje privado en [SITEURL]messages.php\r\n\r\nMensaje: [MESSAGE]\r\n\r\nPara deshabilitar la notificaciÃ³n por correo electrÃ³nico a travÃ©s del panel de opciones de la pÃ¡gina de mensaje privado si ya no desea ser notificado de nuevos mensajes.\r\n\r\nSaludos,\r\n[SENDER].";
-$locale['T201'] = "NotificaciÃ³n sobre nuevos mensajes del Foro";
-$locale['T202'] = "NotificaciÃ³n de la respuesta del hilo - [SUBJECT]";
+$locale['T103'] = "Hola [RECEIVER],\r\n Usted ha recibido un nuevo mensaje privado titulado [SUBJECT] De [USER] en [SITENAME]. Usted puede leer su mensaje privado en [SITEURL]messages.php\r\n\r\nMensaje: [MESSAGE]\r\n\r\nPara deshabilitar la notificación por correo electrónico a través del panel de opciones de la página de mensaje privado si ya no desea ser notificado de nuevos mensajes.\r\n\r\nSaludos,\r\n[SENDER].";
+$locale['T201'] = "Notificación sobre nuevos mensajes del Foro";
+$locale['T202'] = "Notificación de la respuesta del hilo - [SUBJECT]";
 $locale['T203'] = "Hola [RECEIVER],\r\n\r\nUna respuesta ha sido publicada en el hilo del Foro \'[SUBJECT]\' que se sigue en [SITENAME]. Puede utilizar el siguiente enlace para ver la respuesta:\r\n\r\n[THREAD_URL]\r\n\r\nSi no desea ver este hilo puede hacer clic en el \'Dejar seguimiento de este hilo\' enlace situado en la parte superior de el hilo.\r\n\r\nSaludos,\r\n[SENDER].";
 $locale['T301'] = "Formulario de contacto";
 $locale['T302'] = "[SUBJECT]";
