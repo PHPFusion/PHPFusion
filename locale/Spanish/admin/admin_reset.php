cc382af4dfa82ed96f1d6563d188852809666b92
Talocha <mateofds@hotmail.com>
2015-11-11 19:48:44 +0100

    Update admin_reset.php

--- locale/Spanish/admin/admin_reset.php
+++ locale/Spanish/admin/admin_reset.php
@@ -1,21 +1,21 @@
 <?php
 // Infusion titles & description
-$locale['apw_title'] = "Reinicio de la ContraseÃ±a de Administrador";
-$locale['apw_desc'] = "Cambia las contraseÃ±as de usuario/administrador";
+$locale['apw_title'] = "Reinicio de la Contraseña de Administrador";
+$locale['apw_desc'] = "Cambia las contraseñas de usuario/administrador";
 $locale['apw_admin1'] = "Reiniciar Administrador";
-$locale['400'] = "Reiniciar la ContraseÃ±a de:";
+$locale['400'] = "Reiniciar la Contraseña de:";
 $locale['401'] = "Todos los Administradores";
-$locale['402'] = "SÃ³lo los Super Administradores";
-$locale['403'] = "SÃ³lo los Administradores";
+$locale['402'] = "Sólo los Super Administradores";
+$locale['403'] = "Sólo los Administradores";
 $locale['404'] = "Motivo del Reinicio:";
-$locale['405'] = "Reiniciar ambas contraseÃ±as (usuario y administrador)";
-$locale['406'] = "Reiniciar ContraseÃ±as";
-$locale['407'] = "Reinicio de ContraseÃ±a en ";
+$locale['405'] = "Reiniciar ambas contraseñas (usuario y administrador)";
+$locale['406'] = "Reiniciar Contraseñas";
+$locale['407'] = "Reinicio de Contraseña en ";
 $locale['408'] = "Hola [USER_NAME],
 
-Tu contraseÃ±a de administrador ha sido reiniciada en ".fusion_get_settings('siteurl').":
+Tu contraseña de administrador ha sido reiniciada en ".fusion_get_settings('siteurl').":
 
-ContraseÃ±a de Administrador: [NEW_ADMIN_PASS]
+Contraseña de Administrador: [NEW_ADMIN_PASS]
 
 El motivo de este reinicio es: [RESET_MESSAGE]
 
@@ -23,18 +23,18 @@ Saludos,
 [ADMIN]";
 $locale['409'] = "Hola [USER_NAME],
 
-Tus contraseÃ±as de usuario y de administrador han sido reiniciadas en ".fusion_get_settings('siteurl').":
+Tus contraseñas de usuario y de administrador han sido reiniciadas en ".fusion_get_settings('siteurl').":
 
-ContraseÃ±a de Usuario: [NEW_PASS]
-ContraseÃ±a de Administrador: [NEW_ADMIN_PASS]
+Contraseña de Usuario: [NEW_PASS]
+Contraseña de Administrador: [NEW_ADMIN_PASS]
 
 El motivo de este reinicio es: [RESET_MESSAGE]
 
 Saludos,
 [ADMIN]";
 $locale['410'] = "Resultados del Reinicio";
-$locale['411'] = "<b>Aviso:</b> No has indicado la contraseÃ±a de administrador o es incorrecta.";
-$locale['412'] = "ContraseÃ±a de Administrador:";
+$locale['411'] = "<b>Aviso:</b> No has indicado la contraseña de administrador o es incorrecta.";
+$locale['412'] = "Contraseña de Administrador:";
 $locale['415'] = "Registro de Reinicios";
 $locale['416'] = "ID de Registro";
 $locale['417'] = "Hora de Reinicio";
