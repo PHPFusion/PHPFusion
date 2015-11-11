5b5cf7e48a579aac1165b71aaac9b75fd0bcd06b
Talocha <mateofds@hotmail.com>
2015-11-11 19:50:15 +0100

    Update bbcodes.php

--- locale/Spanish/admin/bbcodes.php
+++ locale/Spanish/admin/bbcodes.php
@@ -1,25 +1,25 @@
 <?php
-$locale['400'] = "AdministraciÃ³n de CÃ³digos BB";
-$locale['401'] = "Comprobar los CÃ³digos BB";
+$locale['400'] = "Administración de Códigos BB";
+$locale['401'] = "Comprobar los Códigos BB";
 // Enabled BB Codes
-$locale['402'] = "CÃ³digos BB Activados";
+$locale['402'] = "Códigos BB Activados";
 $locale['403'] = "Nombre";
 $locale['404'] = "Icono";
-$locale['405'] = "DescripciÃ³n";
+$locale['405'] = "Descripción";
 $locale['406'] = "Uso";
 $locale['407'] = "Orden";
 $locale['408'] = "Bajar";
 $locale['409'] = "Subir";
 $locale['410'] = "Desactivar";
-$locale['411'] = "No hay cÃ³digos BB activados.";
+$locale['411'] = "No hay códigos BB activados.";
 // Disabled BB Codes
-$locale['413'] = "CÃ³digos BB Desactivados";
+$locale['413'] = "Códigos BB Desactivados";
 $locale['414'] = "Activar";
-$locale['415'] = "Nueva LÃ­nea";
-$locale['416'] = "No hay cÃ³digos BB desactivados.";
+$locale['415'] = "Nueva Línea";
+$locale['416'] = "No hay códigos BB desactivados.";
 // Test BB Codes
-$locale['417'] = "Resultado de la ComprobaciÃ³n de los CÃ³digos BB";
-$locale['418'] = "Desactivar Smileys en la comprobaciÃ³n";
+$locale['417'] = "Resultado de la Comprobación de los Códigos BB";
+$locale['418'] = "Desactivar Smileys en la comprobación";
 $locale['418a'] = "Mensaje de prueba";
 $locale['418b'] = "Por favor, introduzca un mensaje a probar BBcodes.";
 
