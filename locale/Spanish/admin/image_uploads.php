5d679117353f5c6e6c10416e7ad9bb351f6cdbac
Talocha <mateofds@hotmail.com>
2015-11-11 19:57:49 +0100

    Update image_uploads.php

--- locale/Spanish/admin/image_uploads.php
+++ locale/Spanish/admin/image_uploads.php
@@ -2,28 +2,28 @@
 // Delete Image
 $locale['400'] = "Borrar Imagen";
 $locale['401'] = "Imagen borrada";
-$locale['402'] = "Volver a la AdministraciÃ³n de ImÃ¡genes";
-$locale['403'] = "Volver a AdministraciÃ³n";
+$locale['402'] = "Volver a la Administración de Imágenes";
+$locale['403'] = "Volver a Administración";
 // Upload Image
 $locale['420'] = "Transferir";
 $locale['421'] = "Nombre del Archivo:";
-$locale['422'] = "ImÃ¡genes Principales";
-$locale['423'] = "ImÃ¡genes de ArtÃ­culos";
-$locale['424'] = "ImÃ¡genes de Noticias";
-$locale['425'] = "SÃ³lo se pueden transferir imÃ¡genes en formato GIF, JPG o PNG";
+$locale['422'] = "Imágenes Principales";
+$locale['423'] = "Imágenes de Artículos";
+$locale['424'] = "Imágenes de Noticias";
+$locale['425'] = "Sólo se pueden transferir imágenes en formato GIF, JPG o PNG";
 $locale['426'] = "Imagen transferida";
-$locale['427'] = "ImÃ¡genes de CategorÃ­as de Noticias";
-$locale['428'] = "Blog ImÃ¡genes";
-$locale['429'] = "Blog CategorÃ­as ImÃ¡genes";
+$locale['427'] = "Imágenes de Categorías de Noticias";
+$locale['428'] = "Blog Imágenes";
+$locale['429'] = "Blog Categorías Imágenes";
 // View Image
 $locale['440'] = "Ver Imagen";
 $locale['441'] = "Formato de archivo desconocido. No se puede mostrar la imagen.";
 $locale['442'] = "Borrar";
 // Image Management
-$locale['460'] = "ImÃ¡genes Existentes";
+$locale['460'] = "Imágenes Existentes";
 $locale['461'] = "Ver";
 $locale['462'] = "Borrar";
-$locale['463'] = "Este directorio estÃ¡ vacÃ­o";
-$locale['464'] = "Actualizar la lista de imÃ¡genes para el editor TinyMCE";
+$locale['463'] = "Este directorio está vacío";
+$locale['464'] = "Actualizar la lista de imágenes para el editor TinyMCE";
 // Popup Error/Warning Messages
-$locale['470'] = "Â¿Borrar esta imagen?";
+$locale['470'] = "¿Borrar esta imagen?";
