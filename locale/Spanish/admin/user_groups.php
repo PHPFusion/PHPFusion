f49178b6a73ba40cb10ec2c8f4088bbde9e49e95
Talocha <mateofds@hotmail.com>
2015-11-11 20:17:01 +0100

    Update user_groups.php

--- locale/Spanish/admin/user_groups.php
+++ locale/Spanish/admin/user_groups.php
@@ -1,10 +1,10 @@
 <?php
 // Edit/Save/Delete User Group Messages
 $locale['400'] = "Grupo actualizado";
-$locale['401'] = "Grupo aÃ±adido";
+$locale['401'] = "Grupo añadido";
 $locale['402'] = "Se han eliminado del grupo los usuarios seleccionados";
 $locale['403'] = "Se han eliminado del grupo todos los usuarios";
-$locale['404'] = "Se han aÃ±adido al grupo los usuarios seleccionados";
+$locale['404'] = "Se han añadido al grupo los usuarios seleccionados";
 $locale['405'] = "No se ha podido borrar el grupo de usuarios";
 $locale['406'] = "Hay usuarios asignados a este grupo";
 $locale['407'] = "Grupo borrado";
@@ -12,17 +12,17 @@ $locale['407'] = "Grupo borrado";
 $locale['420'] = "Grupos de Usuarios";
 $locale['421'] = "Editar";
 $locale['422'] = "Borrar";
-$locale['423'] = "Â¿Borrar este grupo de usuarios?";
+$locale['423'] = "¿Borrar este grupo de usuarios?";
 // Edit/Add User Group
 $locale['430'] = "Editar Grupo de Usuarios";
-$locale['431'] = "AÃ±adir Grupo de Usuarios";
+$locale['431'] = "Añadir Grupo de Usuarios";
 $locale['432'] = "Nombre del Grupo:";
-$locale['433'] = "DescripciÃ³n:";
+$locale['433'] = "Descripción:";
 $locale['434'] = "Guardar";
 // Search/Add Group Users
-$locale['440'] = "AÃ±adir Usuarios al Grupo";
-$locale['441'] = "Usa el siguiente formulario para aÃ±adir usuarios a este grupo.";
-$locale['442'] = "Puedes buscar mÃºltiples usuarios separÃ¡ndolos con una coma.";
+$locale['440'] = "Añadir Usuarios al Grupo";
+$locale['441'] = "Usa el siguiente formulario para añadir usuarios a este grupo.";
+$locale['442'] = "Puedes buscar múltiples usuarios separándolos con una coma.";
 $locale['443'] = "ID de Usuarios";
 $locale['444'] = "Nombres de Usuarios";
 $locale['445'] = "Buscar";
@@ -30,9 +30,9 @@ $locale['446'] = "Nombre de Usuario";
 $locale['447'] = "Tipo de Usuario";
 $locale['448'] = "Marcar Todos";
 $locale['449'] = "Desmarcar Todos";
-$locale['450'] = "AÃ±adir los usuarios seleccionados";
+$locale['450'] = "Añadir los usuarios seleccionados";
 $locale['451'] = "No se han encontrado usuarios.";
-$locale['452'] = "Nueva BÃºsqueda";
+$locale['452'] = "Nueva Búsqueda";
 // Current Group Users
 $locale['460'] = "Usuarios del Grupo Existentes";
 $locale['461'] = "Eliminar los usuarios seleccionados";
