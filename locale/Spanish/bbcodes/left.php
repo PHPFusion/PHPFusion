b7bfaddc12533c75b7d74cee0e9d564c90826e69
Talocha <mateofds@hotmail.com>
2015-11-11 20:44:31 +0100

    Update left.php

--- locale/Spanish/bbcodes/left.php
+++ locale/Spanish/bbcodes/left.php
@@ -1,3 +1,3 @@
 <?php
-$locale['bb_left_description'] = "Alinea el texto seleccionado a la izquierda.";
+$locale['bb_left_description'] = "Alinea el texto seleccionado a la Izquierda.";
 $locale['bb_left_usage'] = "Texto a alinear a la izquierda.";
