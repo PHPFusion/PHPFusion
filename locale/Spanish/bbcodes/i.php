8dfedbea48886f91b9630b9432ddfbdea908e109
Talocha <mateofds@hotmail.com>
2015-11-11 20:41:14 +0100

    Update i.php

--- locale/Spanish/bbcodes/i.php
+++ locale/Spanish/bbcodes/i.php
@@ -1,3 +1,3 @@
 <?php
-$locale['bb_i_description'] = "Muestra el texto seleccionado en cursiva.";
+$locale['bb_i_description'] = "Muestra el texto seleccionado en Cursiva.";
 $locale['bb_i_usage'] = "Texto en cursiva.";
