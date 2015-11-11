40dea04f5e28b8e2aa1fbf96c8615b654d58e10d
Talocha <mateofds@hotmail.com>
2015-11-11 20:09:58 +0100

    Update robots.php

--- locale/Spanish/admin/robots.php
+++ locale/Spanish/admin/robots.php
@@ -2,14 +2,14 @@
 // Titles
 $locale['400'] = "Archivo robots.txt";
 // Messages
-$locale['410'] = "Â¿EstÃ¡s seguro? Se aplicarÃ¡ al archivo robots.txt la configuraciÃ³n predeterminada.";
-$locale['411'] = "El archivo robots.txt no existe. Crea el archivo en el diretorio raÃ­z (root) y vuelve a intentarlo.";
+$locale['410'] = "¿Estás seguro? Se aplicará al archivo robots.txt la configuración predeterminada.";
+$locale['411'] = "El archivo robots.txt no existe. Crea el archivo en el diretorio raíz (root) y vuelve a intentarlo.";
 $locale['412'] = "Actualizado el archivo robots.txt.";
 $locale['413'] = "Se ha producido el siguinte error:";
 $locale['414'] = "El archivo robots.txt no tiene permisos de escritura.";
 $locale['415'] = "No se puede escribir en el archivo robots.txt.";
 // Edit form
 $locale['420'] = "Editar el archivo robots.txt";
-$locale['421'] = "InformaciÃ³n y ayuda sobre el archivo robots.txt.";
+$locale['421'] = "Información y ayuda sobre el archivo robots.txt.";
 $locale['422'] = "Guardar";
-$locale['423'] = "ConfiguraciÃ³n Predeterminada";
+$locale['423'] = "Configuración Predeterminada";
