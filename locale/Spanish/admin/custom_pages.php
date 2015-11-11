5d97c3922410517a619d3447bac763d425a3ec66
Talocha <mateofds@hotmail.com>
2015-11-11 19:52:23 +0100

    Update custom_pages.php

--- locale/Spanish/admin/custom_pages.php
+++ locale/Spanish/admin/custom_pages.php
@@ -1,29 +1,29 @@
 <?php
-$locale['cp_100'] = 'PÃ¡gina';
-$locale['cp_101'] = 'TÃ­tulo de la pÃ¡gina';
+$locale['cp_100'] = 'Página';
+$locale['cp_101'] = 'Título de la página';
 $locale['cp_102'] = 'Visibilidad';
 $locale['cp_103'] = 'Idioma';
 $locale['cp_104'] = 'Permitir comentarios';
 $locale['cp_105'] = 'Permitir Calificaciones';
 $locale['cp_106'] = 'Enlaces de sitio';
 // Custom Pages titles
-$locale['400'] = "AÃ±adir PÃ¡gina a Medida";
-$locale['401'] = "Editar PÃ¡gina a Medida";
-$locale['402'] = "PÃ¡ginas a Medida Existentes";
-$locale['403'] = "PÃ¡ginas Personalizadas";
+$locale['400'] = "Añadir Página a Medida";
+$locale['401'] = "Editar Página a Medida";
+$locale['402'] = "Páginas a Medida Existentes";
+$locale['403'] = "Páginas Personalizadas";
 // Custom Pages messages
-$locale['410'] = "PÃ¡gina aÃ±adida";
-$locale['411'] = "PÃ¡gina actualizada";
-$locale['412'] = "La URL a esta pÃ¡gina es: ";
-$locale['413'] = "PÃ¡gina borrada";
+$locale['410'] = "Página añadida";
+$locale['411'] = "Página actualizada";
+$locale['412'] = "La URL a esta página es: ";
+$locale['413'] = "Página borrada";
 // Custom Page form
 $locale['420'] = "Editar";
 $locale['421'] = "Borrar";
-$locale['422'] = "TÃ­tulo de la PÃ¡gina:";
+$locale['422'] = "Título de la Página:";
 $locale['423'] = "Acceso: ";
 $locale['424'] = "Contenido:";
-$locale['425'] = "ContraseÃ±a de Administrador:";
-$locale['426'] = "AÃ±adir un enlace al menÃº de navegaciÃ³n";
+$locale['425'] = "Contraseña de Administrador:";
+$locale['426'] = "Añadir un enlace al menú de navegación";
 $locale['427'] = "Activar comentarios";
 $locale['428'] = "Activar valoraciones";
 $locale['429'] = "Vista Previa";
@@ -31,11 +31,11 @@ $locale['430'] = "Guardar";
 $locale['431'] = "Pagebreak";
 $locale['432'] = "Palabras clave (usted necesita pulsa enter para cada palabra clave)";
 // Popup errors / warning messages
-$locale['450'] = "Â¿Borrar esta pÃ¡gina?";
-$locale['451'] = "Debes indicar un tÃ­tulo";
-$locale['452'] = "Debes indicar tu contraseÃ±a de administrador";
-$locale['453'] = "La contraseÃ±a de administrador no es correcta";
-$locale['454'] = "%s esta funciÃ³n estÃ¡ desactivada.<br />Esta configuraciÃ³n sÃ³lo tendrÃ¡ efecto cuando la funciÃ³n vuelva a ser activada.";
+$locale['450'] = "¿Borrar esta página?";
+$locale['451'] = "Debes indicar un título";
+$locale['452'] = "Debes indicar tu contraseña de administrador";
+$locale['453'] = "La contraseña de administrador no es correcta";
+$locale['454'] = "%s esta función está desactivada.<br />Esta configuración sólo tendrá efecto cuando la función vuelva a ser activada.";
 $locale['455'] = "Comentarios";
 $locale['456'] = "Valoraciones";
 $locale['457'] = "Comentarios y Valoraciones";
