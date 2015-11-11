d10a1378d1987abf16eb0deba7e94493895aa477
Talocha <mateofds@hotmail.com>
2015-11-11 20:45:20 +0100

    Update mail.php

--- locale/Spanish/bbcodes/mail.php
+++ locale/Spanish/bbcodes/mail.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_mail'] = "usuario@dominio.com";
-$locale['bb_mail_description'] = "Hace que se pueda pulsar la direcciÃ³n de mail.";
-$locale['bb_mail_usage'] = "DirecciÃ³n de email o texto a mostrar";
+$locale['bb_mail_description'] = "Hace que se pueda pulsar la dirección de mail.";
+$locale['bb_mail_usage'] = "Dirección de email o texto a mostrar";
