c422aaf3b377e3cec0287d3c2055c9a9b48d64cf
Talocha <mateofds@hotmail.com>
2015-11-11 20:12:49 +0100

    Update smileys.php

--- locale/Spanish/admin/smileys.php
+++ locale/Spanish/admin/smileys.php
@@ -1,25 +1,25 @@
 <?php
 // Smiley titles
 $locale['400'] = "Smileys Existentes";
-$locale['401'] = "AÃ±adir Smiley";
+$locale['401'] = "Añadir Smiley";
 $locale['402'] = "Editar Smiley";
 $locale['403'] = "Smileys";
 // Smiley messages
-$locale['410'] = "Smiley aÃ±adido";
+$locale['410'] = "Smiley añadido";
 $locale['411'] = "Smiley actualizado";
 $locale['412'] = "Smiley borrado";
 $locale['413'] = "No se ha podido actualizar el Smiley";
-$locale['414'] = "No se ha podido aÃ±adir el Smiley";
-$locale['415'] = "El cÃ³digo del Smiley ya estÃ¡ en uso.";
-$locale['416'] = "Â¿Borrar este Smiley?";
+$locale['414'] = "No se ha podido añadir el Smiley";
+$locale['415'] = "El código del Smiley ya está en uso.";
+$locale['416'] = "¿Borrar este Smiley?";
 // Smiley form
-$locale['420'] = "CÃ³digo:";
+$locale['420'] = "Código:";
 $locale['421'] = "Imagen:";
 $locale['422'] = "Texto:";
 $locale['423'] = "Guardar'";
 $locale['424'] = "Vista Previa:";
 // Current Smileys
-$locale['430'] = "CÃ³digo";
+$locale['430'] = "Código";
 $locale['431'] = "Imagen";
 $locale['432'] = "Texto";
 $locale['433'] = "Opciones";
@@ -27,6 +27,6 @@ $locale['434'] = "Editar";
 $locale['435'] = "Borrar";
 $locale['436'] = "No se han creado Smileys.";
 // Error Message
-$locale['437'] = "Introduce un cÃ³digo de Smiley";
+$locale['437'] = "Introduce un código de Smiley";
 $locale['438'] = "seleccione un Smiley";
 $locale['439'] = "Introduce un texto de Smiley";
