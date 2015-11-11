12fd3880d9ce6e1428b2886a8845dfafe28987bc
Frederick MC Chan <meangczac.chan@gmail.com>
2015-11-11 22:42:06 +0800

    Malay translation update

--- locale/Malay/admin/errors.php
+++ locale/Malay/admin/errors.php
@@ -1,26 +1,26 @@
 <?php
 // Titles
-$locale['400'] = "Error Log";
-$locale['401'] = "Take a look into the file";
+$locale['400'] = "Log Kesalahan";
+$locale['401'] = "Sila baca fail yang berkenaan";
 // Errors table
-$locale['410'] = "Error";
-$locale['411'] = "On Page";
-$locale['412'] = "User";
-$locale['413'] = "Date";
+$locale['410'] = "Kesalahan";
+$locale['411'] = "Dalam Halaman";
+$locale['412'] = "Ahli";
+$locale['413'] = "Tarikh";
 $locale['414'] = "Status";
-$locale['415'] = "Line:";
-$locale['416'] = "Level:";
+$locale['415'] = "Garis:";
+$locale['416'] = "Pangkat:";
 $locale['417'] = "IP:";
-$locale['418'] = "No errors in the log.";
-$locale['419'] = "File";
-$locale['420'] = "Details";
-$locale['421'] = "Source code";
-$locale['422'] = "Back to top";
+$locale['418'] = "Tiada rekod dalam log kesalahan.";
+$locale['419'] = "Fail";
+$locale['420'] = "Butir-butir";
+$locale['421'] = "Kod Sumber";
+$locale['422'] = "Balik ke atas";
 // Delete form
-$locale['440'] = "Delete all entries with status";
+$locale['440'] = "Padamkan semua rekod dengan status";
 // Status form
-$locale['450'] = "New";
-$locale['451'] = "Solved";
-$locale['452'] = "Ignored";
-$locale['453'] = "Apply";
-$locale['454'] = "Severity";
+$locale['450'] = "Baru";
+$locale['451'] = "Selesai";
+$locale['452'] = "Diabaikan";
+$locale['453'] = "Jalankan";
+$locale['454'] = "Keparahan";
