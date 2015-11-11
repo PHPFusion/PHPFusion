4f8071776948ffc88c43739e24f490b951c1b138
Talocha <mateofds@hotmail.com>
2015-11-11 20:37:49 +0100

    Update flash.php

--- locale/Spanish/bbcodes/flash.php
+++ locale/Spanish/bbcodes/flash.php
@@ -1,5 +1,5 @@
 <?php
-$locale['bb_flash_description'] = "Muestra una animaciÃ³n Flash desde la URL que se indique.";
-$locale['bb_flash_usage'] = "URL a la animaciÃ³n Flash.";
+$locale['bb_flash_description'] = "Muestra una animación Flash desde la URL que se indique.";
+$locale['bb_flash_usage'] = "URL a la animación Flash.";
 $locale['bb_flash_width'] = "Ancho en px.";
 $locale['bb_flash_height'] = "Alto en px.";
