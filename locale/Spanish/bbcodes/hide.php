6d74ccd992cad74326a290d2bbb34a84502216b7
Talocha <mateofds@hotmail.com>
2015-11-11 20:40:07 +0100

    Update hide.php

--- locale/Spanish/bbcodes/hide.php
+++ locale/Spanish/bbcodes/hide.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_hide'] = "Mensaje para Administradores";
-$locale['bb_hide_description'] = "Mensajes sÃ³lo visibles para los Administradores.";
-$locale['bb_hide_usage'] = "Texto sÃ³lo visible por los Administradores.";
+$locale['bb_hide_description'] = "Mensajes sólo visibles para los Administradores.";
+$locale['bb_hide_usage'] = "Texto sólo visible por los Administradores.";
