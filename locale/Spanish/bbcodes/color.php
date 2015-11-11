5ee5ab7b35f70a7d97dd38450efd0f10e1f32baf
Talocha <mateofds@hotmail.com>
2015-11-11 20:27:13 +0100

    Update color.php

--- locale/Spanish/bbcodes/color.php
+++ locale/Spanish/bbcodes/color.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_color_description'] = "Cambia el color del texto seleccionado.";
 $locale['bb_color_usage'] = "Cambia el color del texto seleccionado.";
-$locale['bb_color_hex'] = "Valor HEX del color con el sÃ­mbolo #.";
+$locale['bb_color_hex'] = "Valor HEX del color con el símbolo #.";
