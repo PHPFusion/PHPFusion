53a178cf819934e317a212d7950050b6abf99c95
Talocha <mateofds@hotmail.com>
2015-11-11 19:53:04 +0100

    Update db-backup.php

--- locale/Spanish/admin/db-backup.php
+++ locale/Spanish/admin/db-backup.php
@@ -1,12 +1,12 @@
 <?php
 // Database Restore
-$locale['400'] = "RestauraciÃ³n de la Base de Datos";
+$locale['400'] = "Restauracíon de la Base de Datos";
 $locale['401'] = "Error";
-$locale['402'] = "El formato del archivo de copia de seguridad no es vÃ¡lido";
+$locale['402'] = "El formato del archivo de copia de seguridad no es válido";
 $locale['403'] = "Cerrar";
 // Backup File Information
-$locale['410'] = "InformaciÃ³n del Archivo de Copia de Seguridad";
-$locale['411'] = "InformaciÃ³n del Archivo de RestauraciÃ³n";
+$locale['410'] = "Información del Archivo de Copia de Seguridad";
+$locale['411'] = "Información del Archivo de Restauración";
 $locale['412'] = "Nombre del Archivo:";
 $locale['413'] = "Fecha:";
 $locale['414'] = "Nombre de la BD:";
@@ -16,7 +16,7 @@ $locale['417'] = "Ver";
 $locale['418'] = "Cancelar";
 $locale['419'] = "Tablas";
 // Database Restore
-$locale['430'] = "Opciones de RestauraciÃ³n";
+$locale['430'] = "Opciones de Restauración";
 $locale['431'] = "Nombre del Archivo:";
 $locale['432'] = "Fecha:";
 $locale['433'] = "Crear Tablas:";
@@ -29,18 +29,18 @@ $locale['439'] = "Cancelar";
 $locale['440'] = "Tipos de Archivo Admitidos:";
 // Database Backup
 $locale['450'] = "Copia de Seguridad de la Base de Datos";
-$locale['451'] = "InformaciÃ³n de la Base de Datos";
-$locale['452'] = "TamaÃ±o Total de las Tablas:";
-$locale['453'] = "TamaÃ±o de la Tablas del NÃºcleo:";
+$locale['451'] = "Información de la Base de Datos";
+$locale['452'] = "Tamaño Total de las Tablas:";
+$locale['453'] = "Tamaño de la Tablas del Núcleo:";
 $locale['454'] = "Opciones de la Copia de Seguridad:";
 $locale['455'] = "Tipo de Copia de Seguridad:";
 $locale['456'] = "(Comprimida)";
 $locale['457'] = "Tablas de la Base de Datos";
-$locale['458'] = "NÃºcleo";
+$locale['458'] = "Núcleo";
 $locale['459'] = "Hacer Copia";
-$locale['460'] = "ContraseÃ±a de Administrador:";
-$locale['460b'] = "Por favor, introduzca su contraseÃ±a de administrador";
-$locale['461'] = "InformaciÃ³n Obligatoria";
+$locale['460'] = "Contraseña de Administrador:";
+$locale['460b'] = "Por favor, introduzca su contraseña de administrador";
+$locale['461'] = "Información Obligatoria";
 // Backup List
 $locale['480'] = "Restaurar Copia de Seguridad de la Base de Datos";
 $locale['481'] = "Nombre del Archivo:";
