5e0af11aeabe03afed137682f31f8a3fdd3c71af
Talocha <mateofds@hotmail.com>
2015-11-12 07:58:37 +0100

    Update blog-cats.php

--- locale/Spanish/permalinks/blog-cats.php
+++ locale/Spanish/permalinks/blog-cats.php
@@ -1,5 +1,5 @@
 <?php
-$locale['pl_blog_cats_title'] = "Blog CategorÃ­a";
-$locale['pl_blog_cats_desc'] = "Habilitar Enlaces permanentes para el Blog de CategorÃ­a en el sitio.";
-$locale['pl_tags_001'] = "IdentificaciÃ³n del Blog artÃ­culo CategorÃ­a.";
-$locale['pl_tags_002'] = "TÃ­tulo o Tema del Blog artÃ­culo CategorÃ­a.";
+$locale['pl_blog_cats_title'] = "Blog Categoría";
+$locale['pl_blog_cats_desc'] = "Habilitar Enlaces permanentes para el Blog de Categoría en el sitio.";
+$locale['pl_tags_001'] = "Identificación del Blog artículo Categoría.";
+$locale['pl_tags_002'] = "Título o Tema del Blog artículo Categoría.";
