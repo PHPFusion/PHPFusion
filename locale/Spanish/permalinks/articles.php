2ebb15e126939147cd199839d8a27d82bbc2cbde
Talocha <mateofds@hotmail.com>
2015-11-12 07:57:53 +0100

    Update articles.php

--- locale/Spanish/permalinks/articles.php
+++ locale/Spanish/permalinks/articles.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_articles_title'] = "ArtÃ­culos";
-$locale['pl_articles_desc'] = "Activar los Enlaces permanentes para los artÃ­culos en el sitio.";
+$locale['pl_articles_title'] = "Artículos";
+$locale['pl_articles_desc'] = "Habilitar los Enlaces permanentes para los artículos en el sitio.";
