ac92be4896da713e3caa5173b6245628a3b3c7f8
Talocha <mateofds@hotmail.com>
2015-11-12 08:08:21 +0100

    Update forum_threads.php

--- locale/Spanish/permalinks/forum_threads.php
+++ locale/Spanish/permalinks/forum_threads.php
@@ -1,7 +1,7 @@
 <?php
-$locale['pl_threads_title'] = "Hilos del Foro";
+$locale['pl_threads_title'] = "Temas del Foro";
 $locale['pl_threads_desc'] = "Habilitar Enlaces permanentes para los Temas del foro en el sitio.";
-$locale['pl_tags_001'] = "ID del hilo de rosca del hilo";
-$locale['pl_tags_002'] = "TÃ­tulo del Tema.";
-$locale['pl_tags_003'] = "IdentificaciÃ³n de la publicaciÃ³n del puesto en el hilo.";
-$locale['pl_tags_004'] = "Valor de la fila de inicio del hilo.";
+$locale['pl_tags_001'] = "ID del mensaje de rosca del Tema";
+$locale['pl_tags_002'] = "Título del Tema.";
+$locale['pl_tags_003'] = "Identificación de la publicación del puesto en el Tema.";
+$locale['pl_tags_004'] = "Valor de la fila de inicio del Tema.";
