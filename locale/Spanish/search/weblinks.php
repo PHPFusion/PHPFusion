88b589ed1705e71a6bda90cd6db85fb9a3cffdbf
Talocha <mateofds@hotmail.com>
2015-11-12 08:53:28 +0100

    Update weblinks.php

--- locale/Spanish/search/weblinks.php
+++ locale/Spanish/search/weblinks.php
@@ -1,6 +1,6 @@
 <?php
 $locale['w400'] = "Enlaces Web";
-$locale['w401'] = "Enlace Web";
+$locale['w401'] = "Enlace web";
 $locale['w402'] = "Enlaces Web";
 $locale['w403'] = "[NUEVO]";
 $locale['w404'] = "Fecha:";
