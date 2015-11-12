7e9666744a2067692dbc3299279d9bfeceaaf39c
Talocha <mateofds@hotmail.com>
2015-11-12 08:14:11 +0100

    Update news.php

--- locale/Spanish/permalinks/news.php
+++ locale/Spanish/permalinks/news.php
@@ -1,5 +1,5 @@
 <?php
 $locale['pl_news_title'] = "Noticias";
 $locale['pl_news_desc'] = "Habilitar Enlaces permanentes de las noticias en el sitio.";
-$locale['pl_tags_001'] = "IdentificaciÃ³n de la Noticia.";
-$locale['pl_tags_002'] = "TÃ­tulo o Tema de la Noticia.";
+$locale['pl_tags_001'] = "Identificación de la Noticia.";
+$locale['pl_tags_002'] = "Título o Tema de la Noticia.";
