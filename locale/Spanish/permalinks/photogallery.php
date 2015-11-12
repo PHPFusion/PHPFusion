795e04effb286f2a72e69003a09c2c975d630b58
Talocha <mateofds@hotmail.com>
2015-11-12 08:15:38 +0100

    Update photogallery.php

--- locale/Spanish/permalinks/photogallery.php
+++ locale/Spanish/permalinks/photogallery.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_gallery_title'] = "Ãlbumes de fotos";
-$locale['pl_gallery_desc'] = "Habilitar Enlaces permanentes para los enlaces de Ãlbumes de fotos en el sitio.";
+$locale['pl_gallery_title'] = "Álbumes de fotos";
+$locale['pl_gallery_desc'] = "Habilitar Enlaces permanentes para los enlaces de Álbumes de fotos en el sitio.";
