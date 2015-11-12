3e8e73c67242b2a37ed25dbd639d13bbc2a0bd6b
Talocha <mateofds@hotmail.com>
2015-11-12 09:14:50 +0100

    Update user_name_first.php

--- locale/Spanish/user_fields/user_name_first.php
+++ locale/Spanish/user_fields/user_name_first.php
@@ -1,4 +1,4 @@
 <?php
 $locale['uf_name_first'] = "Nombre real del usuario";
-$locale['uf_name_first_desc'] = "Nombre de usuario real (Juan, MarÃ­a etc..).";
-$locale['uf_name_first_error'] = "Nombre real de usuario no puede dejarse vacÃ­o.";
+$locale['uf_name_first_desc'] = "Nombre de usuario real (Juan, María etc..).";
+$locale['uf_name_first_error'] = "Nombre real de usuario no puede dejarse vacío.";
