32c5f525ec261ee973325ddef800ad864d0e4213
Talocha <mateofds@hotmail.com>
2015-11-12 12:32:51 +0100

    Update forum_ranks.php

--- infusions/forum/locale/Spanish/forum_ranks.php
+++ infusions/forum/locale/Spanish/forum_ranks.php
@@ -1,21 +1,21 @@
 <?php
 // Forum Ranks Titles
-$locale['400'] = "AÃ±adir Rango";
+$locale['400'] = "Añadir Rango";
 $locale['401'] = "Editar Rango";
 $locale['402'] = "Rangos Existentes";
 $locale['403'] = "Sistema de Rangos Desactivado";
 $locale['404'] = "Rangos del Foro";
 // News messages
-$locale['410'] = "Rango aÃ±adido";
+$locale['410'] = "Rango añadido";
 $locale['411'] = "Rango actualizado";
 $locale['412'] = "Rango borrado";
-$locale['413'] = "SÃ³lo puede aÃ±adirse un Ãºnico rango a los Administradores y Moderadores";
-$locale['414'] = "Por favor, introduzca un TÃ­tulo de rango";
+$locale['413'] = "Sólo puede añadirse un único rango a los Administradores y Moderadores";
+$locale['414'] = "Por favor, introduzca un Título de rango";
 // Forum Ranks form
-$locale['420'] = "TÃ­tulo del Rango:";
+$locale['420'] = "Título del Rango:";
 $locale['421'] = "Imagen del Rango:";
 $locale['422'] = "Mensajes del Rango:";
-$locale['423'] = "AplicaciÃ³n:";
+$locale['423'] = "Aplicación:";
 $locale['424'] = "Miembros";
 $locale['425'] = "Moderadores";
 $locale['426'] = "Administradores";
@@ -23,11 +23,11 @@ $locale['427'] = "Super Administradores";
 $locale['428'] = "Guardar";
 $locale['429'] = "Tipo de Rango:";
 $locale['429a'] = "Especial";
-$locale['429b'] = "AdministraciÃ³n";
-$locale['429c'] = "NÃºmero de Mensajes";
+$locale['429b'] = "Administración";
+$locale['429c'] = "Número de Mensajes";
 // Current Forum Ranks
-$locale['430'] = "TÃ­tulo";
-$locale['431'] = "AplicaciÃ³n";
+$locale['430'] = "Título";
+$locale['431'] = "Aplicación";
 $locale['432'] = "Imagen";
 $locale['433'] = "Mensajes";
 $locale['434'] = "Opciones";
@@ -36,5 +36,5 @@ $locale['436'] = "Borrar";
 $locale['437'] = "No se han creado rangos del foro.";
 $locale['438'] = "Tipo/Mensajes";
 // Forum Ranks Disabled
-$locale['450'] = "Los rangos del foro estÃ¡n desactivados.<br />Puedes activarlos desde %s Panel de AdministraciÃ³n.";
+$locale['450'] = "Los rangos del foro están desactivados.<br />Puedes activarlos desde %s Panel de Administración.";
 $locale['451'] = "Conf. Foro";
