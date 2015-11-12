20ec66520b90e8dbb7851c7cf09b0ec687335bfc
Talocha <mateofds@hotmail.com>
2015-11-12 08:13:33 +0100

    Update news-cats.php

--- locale/Spanish/permalinks/news-cats.php
+++ locale/Spanish/permalinks/news-cats.php
@@ -1,5 +1,5 @@
 <?php
-$locale['pl_news_cats_title'] = "Noticias CategorÃ­a";
-$locale['pl_news_cats_desc'] = "Habilitar Enlaces permanentes para las noticias CategorÃ­a en el sitio.";
-$locale['pl_tags_001'] = "ID del artÃ­culo CategorÃ­a Noticias.";
-$locale['pl_tags_002'] = "TÃ­tulo o Tema de las noticias artÃ­culo CategorÃ­a.";
+$locale['pl_news_cats_title'] = "Noticias Categoría";
+$locale['pl_news_cats_desc'] = "Habilitar Enlaces permanentes para las noticias Categoría en el sitio.";
+$locale['pl_tags_001'] = "ID del artículo Categoría Noticias.";
+$locale['pl_tags_002'] = "Título o Tema de las noticias artículo Categoría.";
