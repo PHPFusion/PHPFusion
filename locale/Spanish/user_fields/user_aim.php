ea12bad2bdafae77589233f95e2bde2bf96017c1
Talocha <mateofds@hotmail.com>
2015-11-12 09:08:13 +0100

    Update user_aim.php

--- locale/Spanish/user_fields/user_aim.php
+++ locale/Spanish/user_fields/user_aim.php
@@ -1,4 +1,4 @@
 <?php
 $locale['uf_aim'] = "AIM";
-$locale['uf_aim_desc'] = "Identificador de la mensajerÃ­a de AOL";
-$locale['uf_aim_error'] = "El ID de AIM no puede dejarse vacÃ­o.";
+$locale['uf_aim_desc'] = "Mensajería instantánea de AOL";
+$locale['uf_aim_error'] = "El ID de AIM no puede dejarse vacío.";
