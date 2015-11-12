807ea275c7e52c08e6e421dc0d359949a91b88e4
Talocha <mateofds@hotmail.com>
2015-11-12 08:44:27 +0100

    Update articles.php

--- locale/Spanish/search/articles.php
+++ locale/Spanish/search/articles.php
@@ -1,7 +1,7 @@
 <?php
-$locale['a400'] = "ArtÃ­culos";
-$locale['a401'] = "ArtÃ­culo";
-$locale['a402'] = "ArtÃ­culos";
+$locale['a400'] = "Artículos";
+$locale['a401'] = "Artículo";
+$locale['a402'] = "Artículos";
 $locale['a403'] = "encontrado(s) en el";
-$locale['a404'] = "tÃ­tulo del artÃ­culo";
-$locale['a405'] = "texto del artÃ­culo";
+$locale['a404'] = "título del artículo";
+$locale['a405'] = "texto del artículo";
