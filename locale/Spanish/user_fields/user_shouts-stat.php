5acb310c909fb522986592263179265e401aab9f
Talocha <mateofds@hotmail.com>
2015-11-12 09:19:00 +0100

    Update user_shouts-stat.php

--- locale/Spanish/user_fields/user_shouts-stat.php
+++ locale/Spanish/user_fields/user_shouts-stat.php
@@ -1,3 +1,3 @@
 <?php
 $locale['uf_shouts-stat'] = "Mensajes en la Mini Charla";
-$locale['uf_shouts-stat_desc'] = "Muestra el nÃºmero de mensajes publicados en la mini charla.";
+$locale['uf_shouts-stat_desc'] = "Muestra el número de mensajes publicados en la mini charla.";
