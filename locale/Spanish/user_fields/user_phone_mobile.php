df9c8983864487b6488d5979d65d9dd8237bb4e4
Talocha <mateofds@hotmail.com>
2015-11-12 09:17:16 +0100

    Update user_phone_mobile.php

--- locale/Spanish/user_fields/user_phone_mobile.php
+++ locale/Spanish/user_fields/user_phone_mobile.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_phone_mobile'] = "TelÃ©fono mÃ³vil";
-$locale['uf_phone_mobile_desc'] = "NÃºmero de telÃ©fono mÃ³vil de usuario";
-$locale['uf_phone_mobile_error'] = "NÃºmero de telÃ©fono mÃ³vil del usuario no puede dejarse vacÃ­o.";
+$locale['uf_phone_mobile'] = "Teléfono móvil";
+$locale['uf_phone_mobile_desc'] = "Número de teléfono móvil de usuario";
+$locale['uf_phone_mobile_error'] = "Número de teléfono móvil del usuario no puede dejarse vacío.";
