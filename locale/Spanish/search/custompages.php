a7f6b0dc6ec831413cb4fe5df210bd94eec279a9
Talocha <mateofds@hotmail.com>
2015-11-12 08:45:58 +0100

    Update custompages.php

--- locale/Spanish/search/custompages.php
+++ locale/Spanish/search/custompages.php
@@ -1,7 +1,7 @@
 <?php
-$locale['c400'] = "PÃ¡ginas a Medida";
-$locale['c401'] = "PÃ¡gina a Medida";
-$locale['c402'] = "PÃ¡ginas a Medida";
+$locale['c400'] = "Páginas a Medida";
+$locale['c401'] = "Página a Medida";
+$locale['c402'] = "Páginas a Medida";
 $locale['c403'] = "encontrado(s) en el";
-$locale['c404'] = "tÃ­tulo de la pÃ¡gina";
-$locale['c405'] = "contenido de la pÃ¡gina";
+$locale['c404'] = "título de la página";
+$locale['c405'] = "contenido de la página";
