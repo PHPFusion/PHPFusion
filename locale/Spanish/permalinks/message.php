6d666f3c6cbdb3b1f2d771163b3d5f37d1dbe32c
Talocha <mateofds@hotmail.com>
2015-11-12 08:12:45 +0100

    Update message.php

--- locale/Spanish/permalinks/message.php
+++ locale/Spanish/permalinks/message.php
@@ -1,3 +1,3 @@
 <?php
 $locale['pl_message_title'] = "Mensaje";
-$locale['pl_message_desc'] = "Habilitar Enlaces permanentes para los mensajes en el sitio.";
+$locale['pl_message_desc'] = "Habilitar Enlaces permanentes para los Mensajes en el sitio.";
