977bc9f86f9eb4a2fbc6a742345fe8dd0e6e4288
Talocha <mateofds@hotmail.com>
2015-11-12 07:27:41 +0100

    Update small2.php

--- locale/Spanish/bbcodes/small2.php
+++ locale/Spanish/bbcodes/small2.php
@@ -1,3 +1,3 @@
 <?php
-$locale['bb_small2_description'] = "Muestra el texto seleccionado como alternativo pequeÃ±o.";
-$locale['bb_small2_usage'] = "Texto como alternativo pequeÃ±o";
+$locale['bb_small2_description'] = "Muestra el texto seleccionado como alternativo pequeño.";
+$locale['bb_small2_usage'] = "Texto como alternativo pequeño";
