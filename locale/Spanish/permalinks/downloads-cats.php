734cbdce381f23409079cbd00849f133cbc0eaff
Talocha <mateofds@hotmail.com>
2015-11-12 08:01:43 +0100

    Update downloads-cats.php

--- locale/Spanish/permalinks/downloads-cats.php
+++ locale/Spanish/permalinks/downloads-cats.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_downloads_cat_title'] = "Descargas CategorÃ­a";
-$locale['pl_downloads_cat_desc'] = "Habilitar Enlaces permanentes para Descargas CategorÃ­a en el Sitio.";
+$locale['pl_downloads_cat_title'] = "Descargas Categoría";
+$locale['pl_downloads_cat_desc'] = "Habilitar Enlaces permanentes para Descargas Categoría en el Sitio.";
