d6b4c27ffbe6f37c8fa951fa0013cf76bf809e1a
Talocha <mateofds@hotmail.com>
2015-11-12 08:12:06 +0100

    Update login.php

--- locale/Spanish/permalinks/login.php
+++ locale/Spanish/permalinks/login.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_login_title'] = "Inicio de sesiÃ³n / Cierre de sesiÃ³n";
-$locale['pl_login_desc'] = "Habilitar Enlaces permanentes para la informaciÃ³n Panel de usuario en el sitio.";
+$locale['pl_login_title'] = "Inicio de sesión / Cierre de sesión";
+$locale['pl_login_desc'] = "Habilitar Enlaces permanentes para la información Panel de usuario en el sitio.";
