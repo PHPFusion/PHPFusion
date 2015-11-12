429194256e834a5ef7dcf6f6f256a4f774558967
Talocha <mateofds@hotmail.com>
2015-11-12 12:22:40 +0100

    Update blog.php

--- infusions/blog/locale/Spanish/blog.php
+++ infusions/blog/locale/Spanish/blog.php
@@ -2,21 +2,21 @@
 
 $locale['blog_1000'] = "Blog";
 $locale['blog_1001'] = "Blog Inicio";
-$locale['blog_1003'] = "Blog CategorÃ­a";
+$locale['blog_1003'] = "Blog Categoría";
 $locale['blog_1004'] = "Blog Archivo";
 $locale['blog_1005'] = "Blog Autor";
-$locale['blog_1006'] = "Leer mÃ¡s";
-$locale['blog_2001'] = "Blogs MÃ¡s Recientes";
-$locale['blog_2002'] = "Blog MÃ¡s Comentados";
-$locale['blog_2003'] = "Blog de alta VotaciÃ³n";
-$locale['blog_3000'] = "NingÃºn Blogs se ha puesto aÃºn";
-$locale['blog_3001'] = "Ninguna CategorÃ­a";
+$locale['blog_1006'] = "Leer más";
+$locale['blog_2001'] = "Blogs Más Recientes";
+$locale['blog_2002'] = "Blog Más Comentados";
+$locale['blog_2003'] = "Blog de alta Votación";
+$locale['blog_3000'] = "Ningún Blogs se ha puesto aún";
+$locale['blog_3001'] = "Ninguna Categoría";
 $locale['blog_3002'] = "Sin Post";
 $locale['blog_3003'] = "Sin Autor";
-$locale['testimonial_rank'] = "Soy el %s de este sitio web";
+$locale['testimonial_rank'] = "Soy %s de este sitio web";
 $locale['testimonial_location'] = "y actualmente vivo en %s";
-$locale['testimonial_join'] = "IngresÃ© a este sitio web en %s";
-$locale['testimonial_web'] = "TambiÃ©n ejecuto otro sitio web en %s";
+$locale['testimonial_join'] = "Ingresé a este sitio web en %s";
+$locale['testimonial_web'] = "También ejecuto otro sitio web en %s";
 $locale['testimonial_contact'] = "Si desea ponerse en contacto conmigo, pueden contactarme en %s";
-$locale['testimonial_email'] = "TambiÃ©n puede contactarme a mi direcciÃ³n de correo electrÃ³nico en %s";
+$locale['testimonial_email'] = "También puede contactarme a mi dirección de correo electrónico en %s";
 
