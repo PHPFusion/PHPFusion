e422a14df674cd50b4b9c53d24d73288ce16add3
Talocha <mateofds@hotmail.com>
2015-11-12 09:13:47 +0100

    Update user_location.php

--- locale/Spanish/user_fields/user_location.php
+++ locale/Spanish/user_fields/user_location.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_location'] = "LocalizaciÃ³n";
-$locale['uf_location_desc'] = "Permite a los usuarios indicar su localizaciÃ³n (paÃ­s, ciudad, etc.)";
-$locale['uf_location_error'] = "La localizaciÃ³n no puede dejarse vacÃ­a.";
+$locale['uf_location'] = "Localización";
+$locale['uf_location_desc'] = "Permite a los usuarios indicar su localización (país, ciudad, etc.)";
+$locale['uf_location_error'] = "La localización no puede dejarse vacía.";
