16511837fffb217b1f8e0d7f1ecce1775e20441d
Talocha <mateofds@hotmail.com>
2015-11-12 12:32:09 +0100

    Update forum_admin.php

--- infusions/forum/locale/Spanish/forum_admin.php
+++ infusions/forum/locale/Spanish/forum_admin.php
@@ -1,16 +1,16 @@
 <?php
 /* Common Forum Locales */
-$locale['forum_000'] = 'AÃ±adir Foro';
-$locale['forum_000a'] = 'ActualizaciÃ³n Foro';
+$locale['forum_000'] = 'Añadir Foro';
+$locale['forum_000a'] = 'Actualización Foro';
 $locale['forum_000b'] = 'Foro %s';
-$locale['forum_000c'] = 'Ã­ndice del Foro';
+$locale['forum_000c'] = 'índice del Foro';
 $locale['forum_001'] = 'Crear nuevo Foro';
 $locale['forum_002'] = 'Editar foro';
 $locale['forum_003'] = 'Borrar foro';
 $locale['forum_004'] = 'Mover foro';
 $locale['forum_005'] = 'Moderar foro';
 $locale['forum_006'] = 'Nombre del Foro';
-$locale['forum_007'] = 'DescripciÃ³n del foro';
+$locale['forum_007'] = 'Descripción del foro';
 $locale['forum_008'] = 'Padres del foro';
 $locale['forum_009'] = 'Tipo de foro';
 $locale['forum_010'] = 'Idioma de foro';
@@ -18,23 +18,23 @@ $locale['forum_011'] = 'Alias de foro';
 $locale['forum_012'] = 'Palabras clave (tiene que tocar la introduzca para cada palabra clave)';
 $locale['forum_013'] = 'Subir imagen';
 $locale['forum_014'] = 'URL de imagen';
-$locale['forum_015'] = 'Max. tamaÃ±o del archivo: %s y debe ser imagen tipo .png, .bmp, .jpg, .gif';
-$locale['forum_016'] = 'Si se carga archivo de imagen, no se utilizarÃ¡ la URL.';
+$locale['forum_015'] = 'Max. tamaño del archivo: %s y debe ser imagen tipo .png, .bmp, .jpg, .gif';
+$locale['forum_016'] = 'Si se carga archivo de imagen, no se utilizará la URL.';
 $locale['forum_017'] = 'Reglas del foro o Avisos';
 $locale['forum_018'] = 'Introduzca un nombre nuevo de foro';
 
 $locale['forum_019'] = 'Habilitar Unir Mensajes';
 $locale['forum_020'] = 'Permitir datos adjuntos en los mensajes';
-$locale['forum_021'] = 'Habilitar entrada rÃ¡pida';
+$locale['forum_021'] = 'Habilitar entrada rápida';
 $locale['forum_022'] = 'Habilitar Encuestas';
-$locale['forum_023'] = 'Habilitar VotaciÃ³n';
+$locale['forum_023'] = 'Habilitar Votación';
 $locale['forum_024'] = 'Mostrar usuarios participaron';
-$locale['forum_025'] = 'Copiar ConfiguraciÃ³n de permisos';
+$locale['forum_025'] = 'Copiar Configuración de permisos';
 $locale['forum_026'] = 'Bloquear Este Foro';
 $locale['forum_027'] = 'Anchura: %s px x altura: %s px';
 $locale['forum_028'] = 'Quitar imagen de foro';
 $locale['forum_029'] = 'Editar permisos';
-$locale['forum_030'] = 'ConfiguraciÃ³n de permisos del Foro';
+$locale['forum_030'] = 'Configuración de permisos del Foro';
 $locale['forum_031'] = 'Acceso foro';
 $locale['forum_032'] = 'Crear nuevos temas';
 $locale['forum_033'] = 'Publicar respuestas';
@@ -64,7 +64,7 @@ $locale['forum_056'] = 'Directorio Base';
 $locale['forum_057'] = 'Temas : ';
 $locale['forum_058'] = 'Subforos : ';
 $locale['forum_059'] = 'Mensajes : ';
-$locale['forum_060'] = 'Opciones de eliminaciÃ³n de Foro';
+$locale['forum_060'] = 'Opciones de eliminación de Foro';
 $locale['forum_061'] = 'Recuento de Mensajes de usuario en los foros';
 
 /* Ranks Styling */
@@ -77,23 +77,23 @@ $locale['forum_070'] = "Mensajes eliminados: ";
 $locale['forum_071'] = "Archivos adjuntos eliminados: ";
 $locale['forum_072'] = "Temas borrados: ";
 
-$locale['forum_080'] = "Temas por pÃ¡gina: ";
-$locale['forum_081'] = "Mensajes por pÃ¡gina: ";
+$locale['forum_080'] = "Temas por página: ";
+$locale['forum_081'] = "Mensajes por página: ";
 
 
 /* Permissions */
 $locale['forum_desc_000'] = 'Permisos Mensaje';
 $locale['forum_desc_001'] = 'Permisos Encuestas';
 $locale['forum_desc_002'] = 'Permisos Adjuntos';
-$locale['forum_desc_003'] = 'SelecciÃ³n de moderadores del Foro';
+$locale['forum_desc_003'] = 'Selección de moderadores del Foro';
 $locale['forum_desc_004'] = 'Respuestas foro';
 
 /* Navs */
-$locale['forum_root'] = 'Ã­ndice del Foro';
+$locale['forum_root'] = 'índice del Foro';
 $locale['forum_points'] = 'Puntos del Foro';
 
 /* Type */
-$locale['forum_opts_001'] = 'CategorÃ­a';
+$locale['forum_opts_001'] = 'Categoría';
 $locale['forum_opts_002'] = 'Foro';
 $locale['forum_opts_003'] = 'Enlace';
 $locale['forum_opts_004'] = 'Apoyo y Respuestas';
@@ -104,33 +104,33 @@ $locale['forum_error_2'] = 'Imagen debe ser inferior a %s.';
 $locale['forum_error_3'] = 'Imagen debe ser menor que %s.';
 $locale['forum_error_4'] = 'Imagen debe ser uno de los tipos de archivo siguientes: %s.';
 $locale['forum_error_5'] = 'Error desconocido';
-$locale['forum_error_6'] = 'La URL de Alias ya estÃ¡ siendo utilizada. Utilice otra direcciÃ³n Ãºnica.';
+$locale['forum_error_6'] = 'La URL de Alias ya está siendo utilizada. Utilice otra dirección única.';
 $locale['forum_error_7'] = 'Por favor, introduzca un nombre de foro.';
 $locale['forum_error_8'] = 'Por favor, elimine la imagen actual foro antes de asignar una nueva.';
-$locale['forum_error_9'] = 'DirecciÃ³n URL de la imagen especificada no existe. Por favor, compruebe su camino otra vez.';
+$locale['forum_error_9'] = 'Dirección URL de la imagen especificada no existe. Por favor, compruebe su camino otra vez.';
 /* Notices */
-$locale['forum_notice_1'] = 'Se han creado con Ã©xito una nueva categorÃ­a de foro.';
-$locale['forum_notice_2'] = 'Se han creado con Ã©xito un nuevo foro.';
-$locale['forum_notice_3'] = 'Se han creado con Ã©xito un nuevo enlace del foro.';
-$locale['forum_notice_4'] = 'Se han creado con Ã©xito un foro de soporte.';
+$locale['forum_notice_1'] = 'Se han creado con éxito una nueva categoría de foro.';
+$locale['forum_notice_2'] = 'Se han creado con éxito un nuevo foro.';
+$locale['forum_notice_3'] = 'Se han creado con éxito un nuevo enlace del foro.';
+$locale['forum_notice_4'] = 'Se han creado con éxito un foro de soporte.';
 $locale['forum_notice_5'] = 'Foro se elimina correctamente.';
 $locale['forum_notice_6'] = 'Foro movido hacia arriba.';
 $locale['forum_notice_7'] = 'Foro movido hacia abajo.';
-$locale['forum_notice_8'] = 'Imagen de Foro se borrÃ³ con Ã©xito.';
+$locale['forum_notice_8'] = 'Imagen de Foro se borró con éxito.';
 $locale['forum_notice_9'] = 'Detalles del Foro se han actualizado correctamente.';
 $locale['forum_notice_10'] = 'Permisos del Foro se han actualizado correctamente.';
 $locale['forum_notice_11'] = "Poda del Foro completada.";
 $locale['forum_notice_13'] = 'Foro ID %d ha sido ordenado y ahora lleva la orden de %d';
-$locale['forum_notice_na'] = 'AcciÃ³n de foro no vÃ¡lida';
+$locale['forum_notice_na'] = 'Acción de foro no válida';
 
-$locale['forum_admin_000'] = 'GestiÃ³n de Foro';
+$locale['forum_admin_000'] = 'Gestión de Foro';
 $locale['forum_admin_001'] = 'Rangos del Foro';
-$locale['forum_admin_002'] = 'ConfiguraciÃ³n del Foro';
+$locale['forum_admin_002'] = 'Configuración del Foro';
 
 
 /* Etc */
-$locale['delete_notice'] = 'Â¿Borrar el foro? Nota: Esta acciÃ³n es irreversible.';
+$locale['delete_notice'] = '¿Borrar el foro? Nota: Esta acción es irreversible.';
 $locale['531'] = 'Apagado';
-$locale['560'] = "No hay categorÃ­as de foro definidas";
+$locale['560'] = "No hay categorías de foro definidas";
 $locale['561'] = "No hay foros definidos";
 $locale['562'] = "Actualizar Orden Foro";
