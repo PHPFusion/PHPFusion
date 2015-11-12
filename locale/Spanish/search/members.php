cf5a830e103818942a986f24aebfb15a2376bde0
Talocha <mateofds@hotmail.com>
2015-11-12 08:49:30 +0100

    Update members.php

--- locale/Spanish/search/members.php
+++ locale/Spanish/search/members.php
@@ -2,4 +2,4 @@
 $locale['m400'] = "Miembros";
 $locale['m401'] = "Miembro";
 $locale['m402'] = "Miembros";
-$locale['m403'] = "Los perfiles de los usuarios sÃ³lo son visibles para los miembros registrados.";
+$locale['m403'] = "Los perfiles de los usuarios sólo son visibles para los miembros registrados.";
