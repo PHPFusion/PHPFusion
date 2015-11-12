7c12cb8942bbfba08c06a1a009009b328ad8c9dd
Talocha <mateofds@hotmail.com>
2015-11-12 08:09:34 +0100

    Update forums.php

--- locale/Spanish/permalinks/forums.php
+++ locale/Spanish/permalinks/forums.php
@@ -1,5 +1,5 @@
 <?php
 $locale['pl_forums_title'] = "Foros";
 $locale['pl_forums_desc'] = "Habilitar Enlaces permanentes para los Enlaces Foro";
-$locale['pl_tags_001'] = "IdentificaciÃ³n Foro para un foro especÃ­fico.";
-$locale['pl_tags_002'] = "TÃ­tulo de un foro especÃ­fico.";
+$locale['pl_tags_001'] = "Identificación Foro para un foro específico.";
+$locale['pl_tags_002'] = "Título de un foro específico.";
