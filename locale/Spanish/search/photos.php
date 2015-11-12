79621be2a76c9c829e519f3d42a9f16ca58f82e4
Talocha <mateofds@hotmail.com>
2015-11-12 08:50:26 +0100

    Update photos.php

--- locale/Spanish/search/photos.php
+++ locale/Spanish/search/photos.php
@@ -3,6 +3,6 @@ $locale['p400'] = "Fotos";
 $locale['p401'] = "Foto";
 $locale['p402'] = "Fotos";
 $locale['p403'] = "[NUEVA]";
-$locale['p404'] = "Ãlbum:";
+$locale['p404'] = "Álbum:";
 $locale['p405'] = "Fecha:";
 $locale['p406'] = "Visitas:";
