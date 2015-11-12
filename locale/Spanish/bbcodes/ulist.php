7d1fb02004e69f11d8cf9e46ded1a91341c9e1c5
Talocha <mateofds@hotmail.com>
2015-11-12 07:30:26 +0100

    Update ulist.php

--- locale/Spanish/bbcodes/ulist.php
+++ locale/Spanish/bbcodes/ulist.php
@@ -2,5 +2,5 @@
 $locale['bb_ulist_description'] = "Crea listas no ordenadas con el tipo indicado.";
 $locale['bb_ulist_usage'] = "Texto 1 :enter: Texto 2 :enter: etc.";
 $locale['bb_ulist_1'] = "Disco";
-$locale['bb_ulist_2'] = "CÃ­rculo";
+$locale['bb_ulist_2'] = "Círculo";
 $locale['bb_ulist_3'] = "Cuadrado";
