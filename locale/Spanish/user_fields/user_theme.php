f4c37f4d72e06ecd2abb879618721a53b9d8a565
Talocha <mateofds@hotmail.com>
2015-11-12 09:21:28 +0100

    Update user_theme.php

--- locale/Spanish/user_fields/user_theme.php
+++ locale/Spanish/user_fields/user_theme.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_theme'] = "Tema";
-$locale['uf_theme_desc'] = "Permite a los usuarios elegir el diseÃ±o (theme) del sitio web";
-$locale['uf_theme_error'] = "El diseÃ±o (theme) no puede dejarse vacÃ­o.";
+$locale['uf_theme'] = "Theme (Diseño)";
+$locale['uf_theme_desc'] = "Permite a los usuarios elegir el diseño (theme) del sitio web";
+$locale['uf_theme_error'] = "El diseño (theme) no puede dejarse vacío.";
