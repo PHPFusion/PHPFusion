3e1d642f9d64ed8adfb3a213c197d9402e634cdd
Talocha <mateofds@hotmail.com>
2015-11-12 12:28:52 +0100

    Update faq.php

--- infusions/faq/locale/Spanish/faq.php
+++ infusions/faq/locale/Spanish/faq.php
@@ -3,5 +3,5 @@ $locale['400'] = "FAQ (preguntas frecuentes)";
 $locale['401'] = "FAQ";
 $locale['402'] = "Arriba";
 // Notices
-$locale['410'] = "No hay FAQ categorÃ­as definidas.";
-$locale['411'] = "No se han aÃ±adido preguntas frecuentes a esta categorÃ­a.";
+$locale['410'] = "No hay FAQ categorías definidas.";
+$locale['411'] = "No se han añadido preguntas frecuentes a esta categoría.";
