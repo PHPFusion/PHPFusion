fc5c3f7c7e6837eb51b869ee61a40dd8a7dab7f4
Talocha <mateofds@hotmail.com>
2015-11-12 09:19:42 +0100

    Update user_sig.php

--- locale/Spanish/user_fields/user_sig.php
+++ locale/Spanish/user_fields/user_sig.php
@@ -1,4 +1,4 @@
 <?php
 $locale['uf_sig'] = "Firma";
-$locale['uf_sig_desc'] = "Permite a los usuarios incluir textos o imÃ¡genes como firma";
-$locale['uf_sig_error'] = "La firma no puede dejarse vacÃ­a.";
+$locale['uf_sig_desc'] = "Permite a los usuarios incluir textos o imágenes como firma";
+$locale['uf_sig_error'] = "La firma no puede dejarse vacía.";
