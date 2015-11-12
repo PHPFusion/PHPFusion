3744db714436919d070ecafc1083d8b44f7ec857
Talocha <mateofds@hotmail.com>
2015-11-12 08:17:54 +0100

    Update print.php

--- locale/Spanish/permalinks/print.php
+++ locale/Spanish/permalinks/print.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_print_title'] = "Imprimir pÃ¡ginas";
-$locale['pl_print_desc'] = "Habilitar Enlaces permanentes para la pÃ¡gina de impresiÃ³n.";
+$locale['pl_print_title'] = "Imprimir páginas";
+$locale['pl_print_desc'] = "Habilitar Enlaces permanentes para la página de impresión.";
