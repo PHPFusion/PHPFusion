d7f7d7c872da17d069aa710cf70d9d8930bc25fb
Talocha <mateofds@hotmail.com>
2015-11-12 09:17:55 +0100

    Update user_phone_work.php

--- locale/Spanish/user_fields/user_phone_work.php
+++ locale/Spanish/user_fields/user_phone_work.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_phone_work'] = "TelÃ©fono del trabajo (oficina)";
-$locale['uf_phone_work_desc'] = "NÃºmero de telÃ©fono del trabajo (oficina) de usuario";
-$locale['uf_phone_work_error'] = "NÃºmero de telÃ©fono del trabajo (oficina) de usuario no puede dejarse vacÃ­o.";
+$locale['uf_phone_work'] = "Teléfono del trabajo (oficina)";
+$locale['uf_phone_work_desc'] = "Número de teléfono del trabajo (oficina) de usuario";
+$locale['uf_phone_work_error'] = "Número de teléfono del trabajo (oficina) de usuario no puede dejarse vacío.";
