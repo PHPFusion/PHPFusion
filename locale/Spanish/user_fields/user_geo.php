b827fa26499e722d6a81f581a2f9e72007a62c02
Talocha <mateofds@hotmail.com>
2015-11-12 09:12:22 +0100

    Update user_geo.php

--- locale/Spanish/user_fields/user_geo.php
+++ locale/Spanish/user_fields/user_geo.php
@@ -1,4 +1,4 @@
 <?php
 $locale['uf_geo'] = "Usuario Geo";
-$locale['uf_geo_desc'] = "PaÃ­s, estado, ciudad, direcciÃ³n, cÃ³digo postal de nuestra DB de Geo";
-$locale['uf_geo_error'] = "Campos de Geo no pueden dejarse vacÃ­os.";
+$locale['uf_geo_desc'] = "País, estado, ciudad, dirección, código postal de nuestra DB de Geo";
+$locale['uf_geo_error'] = "Campos de Geo no pueden dejarse vacíos.";
