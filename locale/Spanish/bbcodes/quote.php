bd3efa3671e3ee06b9eae460142a4ddc35eebff7
Talocha <mateofds@hotmail.com>
2015-11-12 07:22:50 +0100

    Update quote.php

--- locale/Spanish/bbcodes/quote.php
+++ locale/Spanish/bbcodes/quote.php
@@ -4,4 +4,4 @@ $locale['bb_quote_usage'] = "Texto a citar.";
 $locale['bb_quote_expand'] = "Ver texto completo";
 $locale['bb_quote_collapse'] = "Ocultar texto completo";
 $locale['bb_quote'] = "Cita";
-$locale['bb_wrote'] = "escribiÃ³";
+$locale['bb_wrote'] = "escribió";
