7c2cd2d9f29eb6086d9011432a9e01a208f5ced7
Talocha <mateofds@hotmail.com>
2015-11-12 08:20:30 +0100

    Update search.php

--- locale/Spanish/permalinks/search.php
+++ locale/Spanish/permalinks/search.php
@@ -1,3 +1,3 @@
 <?php
 $locale['pl_search_title'] = "Buscar";
-$locale['pl_search_desc'] = "Habilitar Enlaces permanentes para el motor de bÃºsqueda en el sitio.";
+$locale['pl_search_desc'] = "Habilitar Enlaces permanentes para el motor de búsqueda en el sitio.";
