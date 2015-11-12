9ebc7ca510f41d15d45407d1283db3dba0c4a9d7
Talocha <mateofds@hotmail.com>
2015-11-12 08:44:57 +0100

    Update blog.php

--- locale/Spanish/search/blog.php
+++ locale/Spanish/search/blog.php
@@ -1,7 +1,7 @@
 <?php
 $locale['n400'] = "Blog";
-$locale['n401'] = "artÃ­culo de blog";
-$locale['n402'] = "artÃ­culos de blog";
+$locale['n401'] = "artículo de blog";
+$locale['n402'] = "artículos de blog";
 $locale['n403'] = "encontrar en";
 $locale['n404'] = "Blog sujeto";
 $locale['n405'] = "Blog texto";
