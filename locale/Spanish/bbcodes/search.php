46a02c52cdbe6a840b0d4e991864fcc03ca64b01
Talocha <mateofds@hotmail.com>
2015-11-12 07:24:15 +0100

    Update search.php

--- locale/Spanish/bbcodes/search.php
+++ locale/Spanish/bbcodes/search.php
@@ -1,5 +1,5 @@
 <?php
-$locale['bb_search_description'] = "Buscador interno del texto indicado";
+$locale['bb_search_description'] = "Buscador interno del Texto indicado";
 $locale['bb_search_usage'] = "Texto a buscar";
 $locale['bb_search_prefix'] = "Buscar ";
 $locale['bb_search_suffix'] = " en ";
