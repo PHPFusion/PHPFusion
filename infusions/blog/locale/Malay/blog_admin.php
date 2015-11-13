d8610515bd59b466b3078a4660ca7d0a77b5bbc3
Frederick MC Chan <meangczac.chan@gmail.com>
2015-11-13 18:58:06 +0800

    Blog - Malay locale update

--- infusions/blog/locale/Malay/blog_admin.php
+++ infusions/blog/locale/Malay/blog_admin.php
@@ -1,122 +1,123 @@
 <?php
 // blog Submissions messages
-$locale['blog_0130'] = "There are currently no blog submissions";
-$locale['blog_0131'] = "Review Blog Submission";
-$locale['blog_0132'] = "The above blog was submitted by ";
-$locale['blog_0133'] = "Do you want to publish or delete this blog item?";
-$locale['blog_0134'] = "Publish blog";
-$locale['blog_0135'] = "Delete blog";
-$locale['blog_0136'] = "Submission Subject for Review";
-$locale['blog_0137'] = "There are currently %s pending for your review.";
-$locale['blog_0138'] = "Sorry, we currently do not accept any blog submissions on this site.";
-$locale['blog_0141'] = "Preview Blog";
-$locale['blog_0142'] = "Submission Author";
-$locale['blog_0143'] = "Submission Time";
-$locale['blog_0144'] = "Submission Id";
-$locale['blog_0145'] = "Blog Submission has been deleted";
-$locale['blog_0146'] = "Blog Submissions has been published";
-$locale['blog_0147'] = "Blog Submissions is saved as draft";
-$locale['blog_0148'] = "Posted by ";
+$locale['blog_0130'] = "Tiada penyerahan blog";
+$locale['blog_0131'] = "Penyerahan Blog";
+$locale['blog_0132'] = "Blog ini diserah oleh ";
+$locale['blog_0133'] = "Adakah anda ingin terbit atau padam blog ini?";
+$locale['blog_0134'] = "Terbit Blog";
+$locale['blog_0135'] = "Padam Blog";
+$locale['blog_0136'] = "Tajuk serahan untuk pengesahan";
+$locale['blog_0137'] = "Kini, terdapat %s sedia untuk pengesahan dan penerimaan anda.";
+$locale['blog_0138'] = "Harap Maaf, situs ini tidak dapat menerima sebarang serahan Blog sementara ini.";
+$locale['blog_0141'] = "Papar Blog";
+$locale['blog_0142'] = "Pengarang Serahan";
+$locale['blog_0143'] = "Masa Serahan";
+$locale['blog_0144'] = "ID Serahan";
+$locale['blog_0145'] = "Serahan Blog telah dipadamkan";
+$locale['blog_0146'] = "Serahan Blog telah diterima dan diterbitkan";
+$locale['blog_0147'] = "Serahan Blog telah disimpan sebagai deraf";
+$locale['blog_0148'] = "Blog ini diserah oleh";
 // Blog titles
-$locale['blog_0400'] = "Current Blogs";
-$locale['blog_0401'] = "Add Blog";
-$locale['blog_0402'] = "Edit Blog";
+$locale['blog_0400'] = "Blog Terkini";
+$locale['blog_0401'] = "Tambah Blog";
+$locale['blog_0402'] = "Sunting Blog";
 $locale['blog_0405'] = "Blog";
-$locale['blog_0406'] = "Blog Settings";
-$locale['blog_0407'] = "Blog Categories";
-$locale['blog_0408'] = "Currently displaying %d of %d total blog entries";
+$locale['blog_0406'] = "Pengaturan Blog";
+$locale['blog_0407'] = "Kategori Blog";
+$locale['blog_0408'] = "Kini didapati %d daripada %d semua rekod blog";
 // Blog messages
-$locale['blog_0410'] = "Blog Item added";
-$locale['blog_0411'] = "Blog Item updated";
-$locale['blog_0412'] = "Blog Item deleted";
-$locale['blog_0413'] = "Invalid image filename.";
-$locale['blog_0414'] = "Image must be less than %s.";
-$locale['blog_0415'] = "Image must be one of the following file types %s.";
-$locale['blog_0416'] = "Image must be no bigger than %s pixels.";
-$locale['blog_0417'] = "Unknown error";
+$locale['blog_0410'] = "Item Blog telah ditambah";
+$locale['blog_0411'] = "Item Blog telah dikemaskinikan";
+$locale['blog_0412'] = "Item Blog telah dipadamkan";
+$locale['blog_0413'] = "Nama fail imej tidak sah";
+$locale['blog_0414'] = "Imej mesti lebih kurang daripada %s.";
+$locale['blog_0415'] = "Imej mesti satu daripada jenis %s.";
+$locale['blog_0416'] = "Imej mesti lebih kurang daripada %s pixel.";
+$locale['blog_0417'] = "Kesalahan tidak ditentui";
 // Blog form
-$locale['blog_0420'] = "Edit";
-$locale['blog_0421'] = "Delete";
+$locale['blog_0420'] = "Sunting";
+$locale['blog_0421'] = "Padam";
 $locale['blog_0422'] = "Subject:";
-$locale['blog_0423'] = "Category:";
-$locale['blog_0424'] = "Uncategorized";
-$locale['blog_0425'] = "Snippet:";
-$locale['blog_0425a'] = "Short Blog Introduction";
-$locale['blog_0426'] = "Extended:";
-$locale['blog_0426b'] = "Blog Content";
-$locale['blog_0427'] = "Start Date:";
-$locale['blog_0428'] = "End Date:";
+$locale['blog_0423'] = "Kategori:";
+$locale['blog_0424'] = "Tiada Kategori";
+$locale['blog_0425'] = "Ringkasan:";
+$locale['blog_0425a'] = "Pengenalan Blog";
+$locale['blog_0426'] = "Blog:";
+$locale['blog_0426b'] = "Kandungan Blog";
+$locale['blog_0427'] = "Tarikh Mula:";
+$locale['blog_0428'] = "Tarikh Lupus:";
 $locale['blog_0429'] = "(Optional)";
-$locale['blog_0430'] = "Visibility:";
-$locale['blog_0431'] = "Save as draft";
-$locale['blog_0432'] = "Make this Blog item sticky";
-$locale['blog_0433'] = "Automatic Line-Breaks";
-$locale['blog_0434'] = "Enable Comments";
-$locale['blog_0435'] = "Enable Ratings";
-$locale['blog_0436'] = "Preview Blog";
-$locale['blog_0437'] = "Save Blog";
-$locale['blog_0438'] = "Draft";
-$locale['blog_0439'] = "Blog Image:";
-$locale['blog_0440'] = "Max. filesize %s / Allowed filetypes GIF, JPEG or PNG.";
-$locale['blog_0441'] = "Pagebreak";
-$locale['blog_0442'] = "Image Position:";
-$locale['blog_0443'] = "Keywords:";
-$locale['blog_0444'] = "Hit enter after each keywords";
+$locale['blog_0430'] = "Keterlihatan:";
+$locale['blog_0431'] = "Simpan sebagai Deraf";
+$locale['blog_0432'] = "Lekitkan Item Blog ini";
+$locale['blog_0433'] = "Garis baru Automatik";
+$locale['blog_0434'] = "Guna Komen";
+$locale['blog_0435'] = "Guna Penilaian";
+$locale['blog_0436'] = "Papar Blog";
+$locale['blog_0437'] = "Simpan Blog";
+$locale['blog_0438'] = "Deraf";
+$locale['blog_0439'] = "Imej Blog:";
+$locale['blog_0440'] = "Saiz fail maks. %s / Jenis fail dibenarkan GIF, JPEG or PNG.";
+$locale['blog_0441'] = "Halaman baru";
+$locale['blog_0442'] = "Posisi Imej:";
+$locale['blog_0443'] = "Kata kunci:";
+$locale['blog_0444'] = "Asingkan kata kunci dengan butang Enter setiap perkataan";
 // Blog Category
 // Blog Categories titles
-$locale['blog_0500'] = "Edit Blog Category";
-$locale['blog_0501'] = "Add Blog Category";
-$locale['blog_0502'] = "Current Blog Categories";
+$locale['blog_0500'] = "Sunting Kategori Blog";
+$locale['blog_0501'] = "Tambah Kategori Blog";
+$locale['blog_0502'] = "Kategori Blog Terkini";
 // Blog Categories messages
-$locale['blog_0520'] = "Blog Category added";
-$locale['blog_0521'] = "Blog Category updated";
-$locale['blog_0522'] = "Blog Category cannot be deleted";
-$locale['blog_0523'] = "There are Blog items or Sub-Categories linked to this category";
-$locale['blog_0524b'] = "Blog Category deleted";
+$locale['blog_0520'] = "Kategori Blog ditambah";
+$locale['blog_0521'] = "Kategori Blog dikemaskinikan";
+$locale['blog_0522'] = "Kategori Blog tidak dapat dipadamkan";
+$locale['blog_0523'] = "Terdapat Blog atau sub-Kategori Blog dikaitkan dengan kategori ini";
+$locale['blog_0524b'] = "Kategori Blog dipadamkan";
 // Blog Categories form
-$locale['blog_0530'] = "Category Name:";
-$locale['blog_0531'] = "Category Image:";
-$locale['blog_0532'] = "Save Category";
-$locale['blog_0533'] = "Category Parent:";
-$locale['blog_0535'] = "No Blog Categories defined";
-$locale['blog_0536'] = "Click here to upload category images";
-$locale['blog_0537'] = "Parent";
+$locale['blog_0530'] = "Nama Kategori:";
+$locale['blog_0531'] = "Imej Kategori:";
+$locale['blog_0532'] = "Simpan Kategori";
+$locale['blog_0533'] = "Dasar Kategori";
+$locale['blog_0535'] = "Tiada Kategori Blog didapati";
+$locale['blog_0536'] = "Klik sini untuk muat naik imej kategori";
+$locale['blog_0537'] = "Asas";
 // Popup Error/Warning Messages
-$locale['blog_0550'] = "Delete this Blog category?";
+$locale['blog_0550'] = "Padamkan Kategori Blog ini?";
 // Error Messages
-$locale['blog_0560'] = "Please enter a category name.";
-$locale['blog_0561'] = "This category already exists.";
+$locale['blog_0560'] = "Sila isikan Tajuk Kategori.";
+$locale['blog_0561'] = "Nama Kategori ini telah diguna. Sila cuba dengan nama lain.";
 // Popup Error/Warning Messages
-$locale['blog_0450'] = "Please specify a subject";
-$locale['blog_0451'] = "Delete this Blog?";
-$locale['blog_0451b'] = "Delete this Blog category?";
-$locale['blog_0452'] = "%s are disabled for all items.<br />This setting will only take effect when they are enabled again.";
-$locale['blog_0456'] = 'There are no Blog in this category.';
-$locale['blog_0457'] = "Please specify keywords";
+$locale['blog_0450'] = "Tajuk tidak boleh dibiarkan kosong";
+$locale['blog_0451'] = "Padamkan blog ini?";
+$locale['blog_0451b'] = "Padamkan kategori blog ini?";
+$locale['blog_0452'] = "%s telah ditutup untuk semua item.\n\rPengaturan ini hanya akan dilaksanakan bila diguna kembali lagi.";
+$locale['blog_0456'] = "Tiada Blog didapati dalam kategori ini.";
+$locale['blog_0457'] = "Kata kunci tidak boleh dibiarkan kosong";
 // Filters
-$locale['blog_0458'] = "Filter by:";
-$locale['blog_0459'] = "Filter show category by";
-$locale['blog_0460'] = "All Blog Entries";
+$locale['blog_0458'] = "Penapis papar blog dengan";
+$locale['blog_0459'] = "Penapis papar kategori blog dengan";
+$locale['blog_0460'] = "Semua rekod Blog";
 //Alignment
-$locale['left'] = "Left";
-$locale['center'] = "Center";
-$locale['right'] = "Right";
-$locale['blog_0600'] = "Blog Submissions";
-$locale['blog_0601'] = "Submission Image";
-$locale['blog_0602'] = "Required extended blog?";
+$locale['left'] = "Kiri";
+$locale['center'] = "Tengah";
+$locale['right'] = "Kanan";
+$locale['blog_0600'] = "Penyerahan Blog";
+$locale['blog_0601'] = "Imej Penyerahan";
+$locale['blog_0602'] = "Perlukan kandungan blog?";
 // Public Submissions Form
-$locale['blog_0700'] = "Submit Blog";
-$locale['blog_0701'] = "Thank you for submitting your Blog";
-$locale['blog_0702'] = "Submit another Blog item";
-$locale['blog_0703'] = "Use the following form to submit Blog. Your submission will be reviewed by an
-Administrator. ".fusion_get_settings('sitename')." reserves the right to amend or edit any submission. Blog
-should be applicable to the content of this site. Unsuitable submissions will be deleted.";
-$locale['blog_0704'] = "Return to ".fusion_get_settings('sitename');
-$locale['601'] = "Thumb size:";
-$locale['602'] = "Photo size:";
-$locale['603'] = "Maximum photo size:";
-$locale['604'] = "Width x Height";
-$locale['605'] = "Maximum file size (bytes):";
+$locale['blog_0700'] = "Penyerahan Blog";
+$locale['blog_0701'] = "Terima Kasih atas penyerahan Blog anda";
+$locale['blog_0702'] = "Serah Blog sekali lagi";
+$locale['blog_0703'] = "Guna borang ini untuk serahan Blog. Pengesahan akan dibuat oleh Administrator situs selepas ini.
+".fusion_get_settings('sitename')." berhak untuk meminda atau mengubah sebarang penyerahan. Perkara
+hendaklah berpatutan dengan kandungan laman web ini. Hujahan yang tidak sesuai akan dipadamkan.";
+$locale['blog_0704'] = "Kembali ke ".fusion_get_settings('sitename');
+
+$locale['601'] = "Saiz Jejari:";
+$locale['602'] = "Saiz Gambar:";
+$locale['603'] = "Saiz Gamabar Maks.:";
+$locale['604'] = "Lebar x Tinggi";
+$locale['605'] = "Saiz fail Maks. (bytes):";
 
 // Blog & News Settings
 $locale['950'] = "Muat Naik Imej";
--- infusions/blog/locale/Malay/blog_cats.php
+++ infusions/blog/locale/Malay/blog_cats.php
@@ -1,9 +1,9 @@
 <?php
-$locale['400'] = "Blog Categories";
-$locale['401'] = "Category:";
-$locale['402'] = "Number of Items:";
-$locale['403'] = "None-Categorised Blogs";
-$locale['404'] = "There are no Blog items in this category.";
-$locale['405'] = "More";
-$locale['406'] = "Show All Categories";
-$locale['407'] = "No Blog Categories defined";
+$locale['400'] = "Kategori Blog";
+$locale['401'] = "Kategori:";
+$locale['402'] = "Nombor Bil. Item:";
+$locale['403'] = "Blog tanpa Kategori";
+$locale['404'] = "Tiada blog didapati dalam kategori ini.";
+$locale['405'] = "Lebih";
+$locale['406'] = "Paparkan Semua Kategori";
+$locale['407'] = "Tiada Kategori Blog didapati";
