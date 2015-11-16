ae3e7bd4d61e9fb17f2320df30f1e7b588f7a0d4
Frederick MC Chan <meangczac.chan@gmail.com>
2015-11-16 16:01:06 +0800

    Fix #470

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if ($settings['site_seo'] == "1") {
     if (!empty($filepath)) {
         require_once $filepath;
     } else {
-        if ($_SERVER['SCRIPT_NAME'] == fusion_get_settings("site_path").$settings['opening_page'] or $_SERVER['SCRIPT_NAME'] == fusion_get_settings("site_path")."index.php") {
+        if ($_SERVER['REQUEST_URI'] == fusion_get_settings("site_path").$settings['opening_page'] or $_SERVER['REQUEST_URI'] == fusion_get_settings("site_path")."index.php") {
             require_once $settings['opening_page'];
         } else {
             if (!$settings['debug_seo']) {
@@ -34,21 +34,6 @@ if ($settings['site_seo'] == "1") {
             }
         }
     }
-// We get no error pages at all with the index.php page check inclusion otherwise it works.
-    /*
-	if ($filepath != "" || FUSION_SELF == $settings['opening_page'] || FUSION_SELF == "home.php" || FUSION_SELF == "index.php") {
-		if ($filepath != "") {
-				require_once $filepath;
-			} else if (empty($settings['opening_page']) || $settings['opening_page'] == "index.php" || $settings['opening_page'] == "/") {
-				redirect("home.php");
-			} else {
-				redirect($settings['opening_page']);
-			}
-		} else {
-			redirect($settings['debug_seo'] == "0" ? $settings['siteurl']."error.php?code=404" : "");
-		}
-    */
-
 } else if (empty($settings['opening_page']) || $settings['opening_page'] == "index.php" || $settings['opening_page'] == "/") {
 	redirect("home.php");
 } else {
