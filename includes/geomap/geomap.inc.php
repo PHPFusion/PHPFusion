44dcdc246e728062f9affe1fc194ee16de47557e
Domi <domi@php-fusion.co.uk>
2015-11-30 11:05:55 +0100

    Add access check. updated listed countries and who did what
    Add access check. updated listed countries and who did what

--- includes/geomap/geomap.inc.php
+++ includes/geomap/geomap.inc.php
@@ -8,7 +8,8 @@
 | With Codes from PHP-Fusion Communities Authors
 +--------------------------------------------------------+
 | Registered in php-fusion.co.uk under the username of:
-| Domi (Sweden), Basti (Germany), Thomas-SVK (Slovakia),
+| Domi (Sweden) + Rest not covered by community, 
+| Basti (Germany), Thomas-SVK (Slovakia),
 | afoster (USA), Kamillo (Poland), Dimki (Greece),
 | Creatium (Lithuania), douwe_yntema (Netherlands),
 | JoiNNN (Romania), EphyxHU (Hungary), afaaro (Somalia),
@@ -22,6 +23,8 @@
 | copyright header is strictly prohibited without
 | written permission from the original author(s).
 +--------------------------------------------------------*/
+if (!defined("IN_FUSION")) { die("Access Denied"); }
+
 // Official Cut Off List : Bouvet Island, Christmas Island, Cocos-Keeling-Islands, French Metropolitan, French Guiana, French-Southern-Territories, Guadeloupe, Heard and Mc Donald Islands, Mayotte, "St. Helena","St. Pierre and Miquelon", Svalbard and Jan Mayen Islands,
 // Tokelau, United States Minor Outlying Islands, Western Sahara,
 $countries = array("Afghanistan", "Albania", "Algeria", "American-Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia", "Botswana", "Brazil", "British Indian Ocean", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo Republic", "Cook Islands", "Costa Rica", "Cote Divoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Equador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "South Korea", "North Korea", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauretania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Islands", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "USA", "Uruguay", "Uzbekistan", "Vatican City", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands", "Wallis and Futuna", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe");
@@ -289,6 +292,5 @@ foreach($my_status AS $arr=>$v) {
 }
 print_p($i);
 */
-// Total Countries Covered: 218 Countries
-// Total Cities Covered: 6,832 Cities.
-
+// Total Countries Covered: 250 Countries ( With the Geo Class )
+// Total States Covered: 6,832 States.
