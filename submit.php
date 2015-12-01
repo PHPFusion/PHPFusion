ba59045ec615cdb4b08734938e7a1a4b7811838a
Frederick MC Chan <meangczac.chan@gmail.com>
2015-12-01 16:22:13 +0800

    Fix submit.php not able to yield INPUT_GET - #505

--- submit.php
+++ submit.php
@@ -23,30 +23,42 @@ include LOCALE.LOCALESET."submit.php";
 if (!iMEMBER) {
 	redirect("index.php");
 }
-$stype = filter_input(INPUT_GET, 'stype') ? : '';
+
 $submit_info = array();
+
+$stype = "";
+
 $modules = array(
 	'n' => db_exists(DB_NEWS),
 	'p' => db_exists(DB_PHOTO_ALBUMS),
 	'a' => db_exists(DB_ARTICLES),
 	'd' => db_exists(DB_DOWNLOADS),
 	'l' => db_exists(DB_WEBLINKS),
-	'b' => db_exists(DB_BLOG));
-$sum = array_sum($modules);
-if (!$sum or empty($modules[$stype])) {
-	redirect("index.php");
+    'b' => db_exists(DB_BLOG)
+);
 
-} elseif ($stype === "l") {
-	include INFUSIONS."weblinks/weblink_submit.php";
-} elseif ($stype === "n") {
-	include INFUSIONS."news/news_submit.php";
-} elseif ($stype === "b") {
-	include INFUSIONS."blog/blog_submit.php";
-} elseif ($stype === "a") {
-	include INFUSIONS."articles/article_submit.php";
-} elseif ($stype === "p") {
-	include INFUSIONS."gallery/photo_submit.php";
-} elseif ($stype === "d") {
-	include INFUSIONS."downloads/download_submit.php";
+$sum = array_sum($modules);
+
+if ($sum or isset($_GET['stype']) && !empty($modules[$_GET['stype']])) {
+
+    $stype = $_GET['stype'];
+
+    if ($stype === "l") {
+        include INFUSIONS."weblinks/weblink_submit.php";
+    } elseif ($stype === "n") {
+        include INFUSIONS."news/news_submit.php";
+    } elseif ($stype === "b") {
+        include INFUSIONS."blog/blog_submit.php";
+    } elseif ($stype === "a") {
+        include INFUSIONS."articles/article_submit.php";
+    } elseif ($stype === "p") {
+        include INFUSIONS."gallery/photo_submit.php";
+    } elseif ($stype === "d") {
+        include INFUSIONS."downloads/download_submit.php";
+    } else {
+        redirect("index.php");
+    }
+} else {
+    redirect("index.php");
 }
 require_once THEMES."templates/footer.php";
