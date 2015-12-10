e84f91ea6387dad570f19527b302c8811f41e743
Frederick MC Chan <meangczac.chan@gmail.com>
2015-12-10 14:41:29 +0800

    Fix English locales. - Accidentally edit the wrong file.

--- infusions/weblinks/locale/English/weblinks_admin.php
+++ infusions/weblinks/locale/English/weblinks_admin.php
@@ -7,9 +7,9 @@ $locale['wl_0002'] = "Edit Web Link";
 $locale['wl_0003'] = "Current Web Links";
 $locale['wl_0004'] = "Web Link Categories";
 $locale['wl_0005'] = "Edit Web Link Category";
-$locale['wl_0006'] = "Configuration page for Weblinks";
+$locale['wl_0006'] = "Configuration page for Web Links";
 $locale['wl_0007'] = "There are currently %s pending for your review.";
-$locale['wl_0008'] = "There are currently no weblink submisisons";
+$locale['wl_0008'] = "There are currently no Web Link submisisons";
 // Web Link form
 $locale['wl_0100'] = "Web Link Name:";
 $locale['wl_0101'] = "Site name";
@@ -24,16 +24,16 @@ $locale['wl_0108'] = "Save Web Link";
 $locale['wl_0200'] = "Web Links";
 $locale['wl_0201'] = "Category";
 $locale['wl_0203'] = "URL";
-$locale['wl_0204'] = "Weblink Id";
+$locale['wl_0204'] = "Web Link Id";
 $locale['wl_0205'] = "Edit";
 $locale['wl_0206'] = "Delete";
 $locale['wl_0207'] = "There are no web links defined";
-$locale['wl_0208'] = "Weblink Actions";
+$locale['wl_0208'] = "Web Link Actions";
 // Web Link messages
 $locale['wl_0300'] = "Web Link added";
 $locale['wl_0301'] = "Web Link updated";
 $locale['wl_0302'] = "Web Link deleted";
-$locale['wl_0303'] = "Delete this Weblink?";
+$locale['wl_0303'] = "Delete this Web Link?";
 // Web Link Category messages
 $locale['wl_0304'] = "Web Link Category added";
 $locale['wl_0305'] = "Web Link Category updated";
@@ -41,52 +41,42 @@ $locale['wl_0306'] = "Web Link Category deleted";
 $locale['wl_0307'] = "Web Link Category cannot be deleted";
 $locale['wl_0308'] = "There are Web Links or Sub-Categories linked to this category";
 $locale['wl_0309'] = "This category already exists.";
-$locale['wl_0310'] = "Delete this weblink category?";
-
+$locale['wl_0310'] = "Delete this Web Link category?";
 // Filters
 $locale['wl_0400'] = "Filter by:";
 $locale['wl_0401'] = "Filter show category by";
-$locale['wl_0402'] = "All Weblink Entries";
-
-
-
-
+$locale['wl_0402'] = "All Web Link Entries";
 // Add/Edit Web Link Category Form
 $locale['wl_0700'] = "Category Name:";
 $locale['wl_0701'] =  "Please enter a category name";
 $locale['wl_0702'] = "Category Description:";
 $locale['wl_0703'] = "Parent Category";
 $locale['wl_0704'] = "Category Sorting:";
-$locale['wl_0705'] = "Weblink ID";
-$locale['wl_0706'] = "Weblink Name";
-$locale['wl_0707'] = "Weblink Date";
+$locale['wl_0705'] = "Web Link ID";
+$locale['wl_0706'] = "Web Link Name";
+$locale['wl_0707'] = "Web Link Date";
 $locale['wl_0708'] = "Ascending";
 $locale['wl_0709'] = "Descending";
-$locale['wl_0710'] = "Weblink Category Editor";
+$locale['wl_0710'] = "Web Link Category Editor";
 $locale['wl_0711'] = "Save Category";
-
-
 $locale['536'] = "No Web Link categories defined";
 $locale['537'] = "There are no Web Link categories defined";
 $locale['538'] = "You must define at least one category before you can add any Web Links";
 $locale['539'] = "Click here";
 $locale['540'] = " to go to Web Link Categories";
-
 // Submissions
-$locale['wl_0500'] = "Weblink Submissions";
-$locale['wl_0501'] = "Currently displaying %d of %d total weblink entries";
+$locale['wl_0500'] = "Web Link Submissions";
+$locale['wl_0501'] = "Currently displaying %d of %d total Web Link entries";
 $locale['wl_0503'] = "Submission Subject for Review";
 $locale['wl_0504'] = "Submission Author";
 $locale['wl_0505'] = "Submission Time";
 $locale['wl_0506'] = "Submission Id";
-
-$locale['wl_0507'] = "Weblink Submission has been deleted";
-$locale['wl_0508'] = "Publish Weblink";
-$locale['wl_0509'] = "Weblink Submissions has been published";
-$locale['wl_0510'] = "The above weblink was submitted by ";
+$locale['wl_0507'] = "Web Link Submission has been deleted";
+$locale['wl_0508'] = "Publish Web Link";
+$locale['wl_0509'] = "Web Link Submissions has been published";
+$locale['wl_0510'] = "The above Web Link was submitted by ";
 $locale['wl_0511'] = "Posted by ";
 $locale['wl_0512'] = "Delete Submission";
-
 // Public
 $locale['wl_0800'] = "Submit Link";
 // Submit Link Success
@@ -94,17 +84,18 @@ $locale['wl_0801'] = "Thank you for submitting your Link";
 $locale['wl_0802'] = "Submit another Link";
 $locale['wl_0803'] = "Return to [SITENAME]";
 // Submit Link Form
-$locale['wl_0804'] = "Use the following form to submit a Link. Your submission will be verified by an
-Administrator. Links deemed unsuitable will not be accepted.";
-$locale['wl_0805'] = "Link Category:";
-$locale['wl_0806'] = "Link Name:";
-$locale['wl_0807'] = "Link URL:";
-$locale['wl_0808'] = "Link Description:";
-$locale['wl_0809'] = "Sorry, we currently do not accept any weblink submissions on this site.";
+$locale['wl_0804'] = "Use the following form to submit Web Link. Your submission will be reviewed by an
+Administrator. [SITENAME] reserves the right to amend or edit any submission. Web Link
+should be applicable to the content of this site. Unsuitable submissions will be deleted.";
+$locale['wl_0805'] = "Web Link Category:";
+$locale['wl_0806'] = "Web Link Name:";
+$locale['wl_0807'] = "Web Link URL:";
+$locale['wl_0808'] = "Web Link Description:";
+$locale['wl_0809'] = "Sorry, we currently do not accept any Web Link submissions on this site.";
 // Settings
 $locale['wl_0600'] = "Settings";
-$locale['wl_0601'] = "Allow weblink submissions?";
-$locale['wl_0602'] = "Require link description?";
+$locale['wl_0601'] = "Allow Web Link submissions?";
+$locale['wl_0602'] = "Require Web Link description?";
 $locale['601'] = "Settings Updated";
 $locale['602'] = "Links";
 $locale['603'] = "Links per page";
