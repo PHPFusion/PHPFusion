d28e46946aec1f4d93a84f13a1a9a11830d39c27
Frederick MC Chan <meangczac.chan@gmail.com>
2015-12-10 14:05:12 +0800

    Malay Forum Locale

---
+++ infusions/forum/locale/Malay/forum_ranks.php
@@ -0,0 +1,40 @@
+<?php
+// Nama Pangkat Forum
+$locale['400'] = "Tambah Pangkat Ahli";
+$locale['401'] = "Sunting Pangkat Ahli";
+$locale['402'] = "Current Pangkat Ahli";
+$locale['403'] = "Sistem Pangkat ditutup";
+$locale['404'] = "Pangkat Ahli Forum";
+// Mesej Baru
+$locale['410'] = "Pangkat ditambah";
+$locale['411'] = "Pangkat dikemaskini";
+$locale['412'] = "Pangkat dipadam";
+$locale['413'] = "Hanya satu pangkat ahli boleh diberi untuk admins dan moderator";
+$locale['414'] = "Sila isikan nama Pangkat Ahli";
+// Borang Pangkat Forum
+$locale['420'] = "Nama Pangkat:";
+$locale['421'] = "Imej Pangkat:";
+$locale['422'] = "Bil Pos Pangkat:";
+$locale['423'] = "Untuk:";
+$locale['424'] = "Ahli";
+$locale['425'] = "Moderator";
+$locale['426'] = "Administrator";
+$locale['427'] = "Super Administrator";
+$locale['428'] = "Simpan Pangkat";
+$locale['429'] = "Jenis Pangkat:";
+$locale['429a'] = "Khas";
+$locale['429b'] = "Admin";
+$locale['429c'] = "Bilangan Pos";
+// Pangkat Forum Semasa
+$locale['430'] = "Tajuk";
+$locale['431'] = "Beri kepada";
+$locale['432'] = "Imej";
+$locale['433'] = "Pos";
+$locale['434'] = "Pilihan";
+$locale['435'] = "Sunting";
+$locale['436'] = "Padam";
+$locale['437'] = "Tiada pangkat ahli forum didapati";
+$locale['438'] = "Jenis/Bilangan";
+// Forum Pangkat Disabled
+$locale['450'] = "Pangkat Forum tidak digunakan.\nSila ke %s Admin Panel untuk membuka sistem.";
+$locale['451'] = "Pengaturan Forum";
