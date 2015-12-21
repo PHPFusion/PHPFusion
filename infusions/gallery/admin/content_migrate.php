e6f0cba80d8482172e3cf4189826fcf37ebbdde9
Domi <domi@php-fusion.co.uk>
2015-12-21 10:41:01 +0100

    Migrate Photoalbum content to new album
    To be tested sharp but perform well on my tests. If no changes come
    during the next few h, it works on my sharp upgrade tests aswell.

--- infusions/gallery/admin/content_migrate.php
+++ infusions/gallery/admin/content_migrate.php
@@ -21,29 +21,54 @@ require_once THEMES."templates/header.php";
 // use checkrights outside of Administration
 if (!checkrights("PH")) { redirect(BASEDIR."index.php"); }
 
-$directory = array();
-$dir = IMAGES."photoalbum/";
-$temp = opendir($dir);
-	while ($folder = readdir($temp)) {
-		if (!in_array($folder, array("..", ".", '.DS_Store', 'index.php'))) {
-			$directory[] = $folder;
-			print_r($folder);
-				rename(IMAGES."photoalbum/".$folder, INFUSIONS."gallery/photos/".$folder);
-		}
-	}
+/**
+ * Remove all files, subdirs and ultimatly the directory in a given dir
+ * @param $dir
+ */
+ 
+function rrmdir($dir) { 
+	if (is_dir($dir)) { 
+		$objects = scandir($dir); 
+			foreach ($objects as $object) { 
+				if ($object != "." && $object != "..") { 
+					if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object); 
+				} 
+			} 
+		reset($objects); 
+		rmdir($dir); 
+	} 
+}
 
-if (!empty($directory)) {
-	foreach($directory as $folder) {
-		if (file_exists(IMAGES."photoalbum/".$folder."/")) {
-			$dir = opendir(IMAGES."photoalbum/".$folder."/");
-			while ($subfolder = readdir($dir)) {
-				if (!in_array($subfolder, array("..", ".", '.DS_Store', 'index.php'))) {
-					if (is_file($subfolder)) {
-						rename(IMAGES."photoalbum/".$folder."/".$subfolder, INFUSIONS."gallery/photos/".$subfolder);
-					}
-				}
-			}
+/**
+ * Returns all photos inside the album into an array
+ * @param $album_id
+ */
+
+function move_photos($album_id) {
+	$result = dbquery("SELECT * FROM ".DB_PHOTOS." WHERE album_id='".$album_id."'");
+    if (dbrows($result)>0) {
+        while ($photo_data = dbarray($result)) {
+			rename(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_filename'], INFUSIONS."gallery/photos/".$photo_data['photo_filename']);
+			rename(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_thumb1'], INFUSIONS."gallery/photos/".$photo_data['photo_thumb1']);
+			rename(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_thumb2'], INFUSIONS."gallery/photos/".$photo_data['photo_thumb2']);
 		}
+    }
+}
+
+$result = dbquery("SELECT * FROM ".DB_PHOTO_ALBUMS);
+
+if (dbrows($result) > 0) {
+	while ($data = dbarray($result)) {
+		// Rename the album thumb here
+		rename(IMAGES."photoalbum/".$data['album_thumb'], INFUSIONS."gallery/photos/".$data['album_thumb']);
+
+		// Call the album directory rename function here
+		move_photos($data['album_id']);
 	}
 }
+
+
+//Remove the whole old photoalbum dir
+rrmdir(IMAGES."photoalbum");
+
 require_once THEMES."templates/footer.php";
