47335e4addb6e023995292d3f56f4e44df712c1c
Frederick MC Chan <meangczac.chan@gmail.com>
2015-12-23 22:03:28 +0800

    Fix #598 - Forum permissions, some admin html layout adjustment

--- includes/core_functions_include.php
+++ includes/core_functions_include.php
@@ -282,9 +282,7 @@ function check_admin_pass($password) {
 
 function redirect($location, $delay = FALSE, $script = FALSE, $debug = FALSE) {
 
-    $prefix = (fusion_get_settings("site_seo") == 1 && defined("IN_PERMALINK") && !isset($_GET['aid']) ? ROOT : "");
-
-    $location = stripinput($prefix.$location);
+    $location = (fusion_get_settings("site_seo") == 1 && defined("IN_PERMALINK") && !isset($_GET['aid']) ? stripinput(ROOT.$location) : $location);
 
     if ($debug == FALSE) {
 
