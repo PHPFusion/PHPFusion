3e9a07b15ae83b3822825be8aed9bb7b54e888b7
Frederick MC Chan <meangczac.chan@gmail.com>
2015-12-23 13:18:33 +0800

    Fixes some form_text function HTML errors - error when there are 2 buttons in the same field - updated function docs how to do it as intended

--- includes/dynamics/includes/form_text.php
+++ includes/dynamics/includes/form_text.php
@@ -30,12 +30,26 @@
  * in the input, usually a value from DB prev. saved
  * @param array  $options Various options
  * @return string
+ *
+ * To add an inline button (set prepend or append - respectively)
+ * register these options accordingly into the function
+ * $options['append_button'] = true
+ * $options['append_type'] = button or submit
+ * $options['append_form_value'] = the value of the button
+ * $options['append_class'] = your pick of .btn classes (bootstrap .btn-success, .btn-info, etc)
+ * $options['append_value'] = the label
+ * $options['append_button_name'] = your button name , default: p-submit-".$options['input_id']."
+ *
+ * To add a decorative labels (set prepend or append - respectively)
+ * $options['append_value'] = "your-value" - You can also insert HTML <i class='fa fa-something'></i> for glyphs
+ *
  */
+
 function form_text($input_name, $label = "", $input_value = "", array $options = array()) {
     global $defender, $locale;
+
     $title = $label ? stripinput($label) : ucfirst(strtolower(str_replace("_", " ", $input_name)));
-    $html = "";
-    //var_dump($options);
+
     $valid_types = array('text', 'number', 'password', 'email', 'url');
 
     $options += array(
@@ -55,12 +69,14 @@ function form_text($input_name, $label = "", $input_value = "", array $options =
         'autocomplete_off' => !empty($options['autocomplete_off']) && $options['autocomplete_off'] == 1 ? 1 : 0,
         'tip' => !empty($options['tip']) ? $options['tip'] : '',
         'append_button' => !empty($options['append_button']) ? $options['append_button'] : '',
+        'append_button_name' => !empty($options['append_button_name']) ? $options['append_button_name'] : "p-submit-".$options['input_id'],
         'append_value' => !empty($options['append_value']) ? $options['append_value'] : "",
         'append_form_value' => !empty($options['append_form_value']) ? $options['append_form_value'] : '',
         'append_size' => !empty($options['append_size']) ? $options['append_size'] : '',
         'append_class' => !empty($options['append_class']) ? $options['append_class'] : 'btn-default',
         'append_type' => !empty($options['append_type']) ? $options['append_type'] : 'submit',
         'prepend_button' => !empty($options['prepend_button']) ? $options['prepend_button'] : '',
+        'prepend_button_name' => !empty($options['append_button_name']) ? $options['append_button_name'] : "p-submit-".$options['input_id'],
         'prepend_value' => !empty($options['prepend_value']) ? $options['prepend_value'] : "",
         'prepend_form_value' => !empty($options['prepend_form_value']) ? $options['prepend_form_value'] : '',
         'prepend_size' => !empty($options['prepend_size']) ? $options['prepend_size'] : '',
@@ -101,26 +117,50 @@ function form_text($input_name, $label = "", $input_value = "", array $options =
         }
     }
 
-    $html .= "<div id='".$options['input_id']."-field' class='form-group ".$error_class.$options['class']." ".($options['icon'] ? 'has-feedback' : '')."'  ".($options['width'] && !$label ? "style='width: ".$options['width']."'" : '').">\n";
+    $html = "<div id='".$options['input_id']."-field' class='form-group ".$error_class.$options['class']." ".($options['icon'] ? 'has-feedback' : '')."'  ".($options['width'] && !$label ? "style='width: ".$options['width']."'" : '').">\n";
+
     $html .= ($label) ? "<label class='control-label ".($options['inline'] ? "col-xs-12 col-sm-3 col-md-3 col-lg-3 p-l-0" : '')."' for='".$options['input_id']."'>$label ".($options['required'] ? "<span class='required'>*</span>" : '')." ".($options['tip'] ? "<i class='pointer fa fa-question-circle' title='".$options['tip']."'></i>" : '')."</label>\n" : "";
+
     $html .= ($options['inline'] && $label) ? "<div class='col-xs-12 col-sm-9 col-md-9 col-lg-9'>\n" : "";
+
     $html .= ($options['append_button'] || $options['prepend_button'] || $options['append_value'] || $options['prepend_value']) ? "<div class='input-group'>\n" : "";
+
     if ($options['prepend_button'] && $options['prepend_type'] && $options['prepend_form_value'] && $options['prepend_class'] && $options['prepend_value']) {
-        $html .= "<span class='input-group-btn'>\n<button id='".$options['input_id']."-prepend-btn' name='p-submit-".$options['input_id']."' type='".$options['prepend_type']."' value='".$options['prepend_form_value']."' class='btn ".$options['prepend_size']." ".$options['prepend_class']."'>".$options['prepend_value']."</button></span>";
+
+        $html .= "<span class='input-group-btn'>\n";
+        $html .= "<button id='".$options['input_id']."-prepend-btn' name='".$options['prepend_button_name']."' type='".$options['prepend_type']."' value='".$options['prepend_form_value']."' class='btn ".$options['prepend_size']." ".$options['prepend_class']."'>".$options['prepend_value']."</button>\n";
+        $html .= "</span>\n";
+
     } elseif ($options['prepend_value']) {
+
         $html .= "<span class='input-group-addon' id='p-".$options['input_id']."-prepend'>".$options['prepend_value']."</span>\n";
+
     }
-    $html .= "<input type='".($options['type'] == "password" ? "password" : "text")."' data-type='".$options['type']."' class='form-control textbox ".($options['stacked'] ? "stacked" : "")."' ".($options['width'] ? "style='width:".$options['width'].";'" : '')." ".($options['max_length'] ? "maxlength='".$options['max_length']."'" : '')." name='$input_name' id='".$options['input_id']."' value='$input_value' placeholder='".$options['placeholder']."' ".($options['autocomplete_off'] ? "autocomplete='off'" : '')." ".($options['deactivate'] ? 'readonly' : '').">";
+
+    $html .= "<input type='".($options['type'] == "password" ? "password" : "text")."' data-type='".$options['type']."' class='form-control textbox ".($options['stacked'] ? "stacked" : "")."' ".($options['width'] ? "style='width:".$options['width'].";'" : '')." ".($options['max_length'] ? "maxlength='".$options['max_length']."'" : '')." name='".$input_name."' id='".$options['input_id']."' value='".$input_value."' placeholder='".$options['placeholder']."' ".($options['autocomplete_off'] ? "autocomplete='off'" : '')." ".($options['deactivate'] ? 'readonly' : '').">";
+
     if ($options['append_button'] && $options['append_type'] && $options['append_form_value'] && $options['append_class'] && $options['append_value']) {
-        $html .= "<span class='input-group-btn'>\n<button id='".$options['input_id']."-append-btn' name='p-submit-".$options['input_id']."' type='".$options['append_type']."' value='".$options['append_form_value']."' class='btn ".$options['append_size']." ".$options['append_class']."'>".$options['append_value']."</button></span>";
+
+        $html .= "<span class='input-group-btn'>\n";
+        $html .= "<button id='".$options['input_id']."-append-btn' name='".$options['append_button_name']."' type='".$options['append_type']."' value='".$options['append_form_value']."' class='btn ".$options['append_size']." ".$options['append_class']."'>".$options['append_value']."</button>\n";
+        $html .= "</span>\n";
+
     } elseif ($options['append_value']) {
+
         $html .= "<span class='input-group-addon' id='p-".$options['input_id']."-append'>".$options['append_value']."</span>\n";
+
     }
+
     $html .= ($options['icon']) ? "<div class='form-control-feedback' style='top:0;'><i class='glyphicon ".$options['icon']."'></i></div>\n" : "";
+
     $html .= $options['stacked'];
+
     $html .= ($options['append_button'] || $options['prepend_button'] || $options['append_value'] || $options['prepend_value']) ? "</div>\n" : "";
+
     $html .= $defender->inputHasError($input_name) ? "<div id='".$options['input_id']."-help' class='label label-danger p-5 display-inline-block'>".$options['error_text']."</div>" : "";
+
     $html .= ($options['inline'] && $label) ? "</div>\n" : "";
+
     $html .= "</div>\n";
 
     // Add input settings in the SESSION
