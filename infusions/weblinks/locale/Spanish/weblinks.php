91d4132bbf6bfcb5c395c2d10bdadb27205b2654
Domi <domi@php-fusion.co.uk>
2015-12-28 11:39:43 +0100

    move to locales folder
    move to locales folder

--- infusions/weblinks/locale/Spanish/weblinks.php
+++
@@ -1,9 +0,0 @@
-<?php
-$locale['400'] = "Enlaces Web";
-// Download File Information
-$locale['410'] = "Nuevo";
-$locale['411'] = "Añadido el:";
-$locale['412'] = "Vistas:";
-// Downloads Notices
-$locale['430'] = "No se han creado categorías de enlaces web";
-$locale['431'] = "No hay enlaces web en esta categoría";
