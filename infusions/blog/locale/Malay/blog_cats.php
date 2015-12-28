8f9e9ac0a2b035dc057f283a3709ce564c695be9
Domi <domi@php-fusion.co.uk>
2015-12-28 11:28:01 +0100

    move to locales folder
    move to locales folder

--- infusions/blog/locale/Malay/blog_cats.php
+++
@@ -1,9 +0,0 @@
-<?php
-$locale['400'] = "Kategori Blog";
-$locale['401'] = "Kategori:";
-$locale['402'] = "Nombor Bil. Item:";
-$locale['403'] = "Blog tanpa Kategori";
-$locale['404'] = "Tiada blog didapati dalam kategori ini.";
-$locale['405'] = "Lebih";
-$locale['406'] = "Paparkan Semua Kategori";
-$locale['407'] = "Tiada Kategori Blog didapati";
