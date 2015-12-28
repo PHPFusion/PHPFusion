03e76f4f342d547f77efd8780932a13d6f68e38e
Domi <domi@php-fusion.co.uk>
2015-12-28 11:17:17 +0100

    Move to locale folder
    Move to locale folder

--- infusions/articles/locale/Danish/articles.php
+++
@@ -1,5 +0,0 @@
-<?php
-$locale['400'] = "Artikler";
-$locale['401'] = "Der er ikke oprettet artikelkategorier endnu";
-$locale['402'] = "NY";
-$locale['403'] = "Der er endnu ikke tilføjet artikler til denne kategori";
