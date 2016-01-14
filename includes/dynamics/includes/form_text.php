acf5643be7b56fabc82d013d547eb0a262a5cf70
Frederick MC Chan <meangczac.chan@gmail.com>
2016-01-14 08:04:28 +0800

    Patch Close #667 - Added numkeys for number type when field type are numbers.

--- includes/dynamics/includes/form_text.php
+++ includes/dynamics/includes/form_text.php
@@ -187,7 +187,7 @@ function form_text($input_name, $label = "", $input_value = "", array $options =
     if ($options['type'] == 'number' && !defined('NUMBERS_ONLY_JS')) {
         define('NUMBERS_ONLY_JS', TRUE);
         add_to_jquery("$('input[data-type=number]').keypress(function(e) {
-		var key_codes = [46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 0, 8];
+		var key_codes = [96, 97, 98, 99, 100, 101, 102, 103, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 0, 8];
 		if (!($.inArray(e.which, key_codes) >= 0)) { e.preventDefault(); }
 		});\n");
     }
