e9d7fbf889aaffab480a9ce5498d6efdcf6b6440
Edmundas Jankauskas <jankauskas.edmundas@gmail.com>
2016-01-20 23:23:42 +0200

    Small update
    Translated News Infusion + DebonAir theme

---
+++ themes/debonair/locale/Lithuanian/locale.php
@@ -0,0 +1,109 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: debonair/locale/English/locale.php
+| Author: Frederick MC Chan (Hien)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+
+$locale['debonair_0200'] = "Debonair baneris";
+$locale['debonair_0201'] = "Debonair nustatymai";
+$locale['debonair_0203'] = "Pridėti naujų skaidrių";
+$locale['debonair_0204'] = "Redaguoti skaidres";
+$locale['debonair_0205'] = "Pavadinimas";
+$locale['debonair_0206'] = "Skaidrės pavadinimas";
+$locale['debonair_0207'] = "Aprašymas";
+$locale['debonair_0208'] = "Aprašomasis tekstas";
+$locale['debonair_0209'] = "Nuoroda";
+$locale['debonair_0210'] = "Nuotrauka";
+$locale['debonair_0211'] = "Eiliškumas";
+$locale['debonair_0212'] = "Banerių skaidrės išsaugotos";
+$locale['debonair_0213'] = "Nėra sukurtų skaidrių";
+$locale['debonair_0214'] = "Kalba";
+$locale['debonair_0215'] = "Matomumas";
+$locale['debonair_0216'] = "Pasirinkimai";
+$locale['debonair_0217'] = "Skaidrės ištrintos";
+$locale['debonair_0218'] = "Perkelti aukštyn";
+$locale['debonair_0219'] = "Perkelti žemyn";
+$locale['debonair_0220'] = "Skaidrių tvarka atnaujinta";
+
+// Widget Control
+$locale['debonair_0300'] = "Pagrindinio banerio įtraukimas";
+$locale['debonair_0301'] = "Pasirinkite puslapį, kuriame baneris bus rodomas. Nepasirinkti puslapiai naudos įprastinį banerį.";
+$locale['debonair_0302'] = "Nėra";
+$locale['debonair_0303'] = "Rodyti straipsnį";
+$locale['debonair_0304'] = "Rodyti naujienas";
+$locale['debonair_0305'] = "Rodyti tinklaraštį";
+$locale['debonair_0306'] = "Rodyti pridėtinį puslapį";
+$locale['debonair_0307'] = "Headerio 1 stulpelio ruošinys";
+$locale['debonair_0308'] = "Headerio 2 stulpelio ruošinys";
+$locale['debonair_0309'] = "Headerio 3 stulpelio ruošinys";
+$locale['debonair_0310'] = "Straipsnis %s";
+$locale['debonair_0311'] = "Naujiena %s";
+$locale['debonair_0312'] = "Tinklaraštis %s";
+$locale['debonair_0313'] = "Puslapis %s";
+$locale['debonair_0314'] = "Pridėtinis puslapis %s";
+$locale['debonair_0315'] = "Rodo ištrauką iš straipsnių, tinklaraščių, naujienų ir pridėtinių puslapių headerio stulpelių vietoje iš karto po baneriu.";
+$locale['debonair_0316'] = "Rodo ištraukos tipą apačioje virš footerio.";
+$locale['debonair_0317'] = "Žemutinio 1 stulpelio ruošinys";
+$locale['debonair_0318'] = "Žemutinio 2 stulpelio ruošinys";
+$locale['debonair_0319'] = "Žemutinio 3 stulpelio ruošinys";
+$locale['debonair_0320'] = "Žemutinio 4 stulpelio ruošinys";
+$locale['debonair_0321'] = "Facebook puslapio nuoroda";
+$locale['debonair_0322'] = "Twitter puslapio nuoroda";
+
+// Users Panel
+$locale['debonair_0400'] = "Paskutiniai nariai";
+$locale['debonair_0401'] = "Daugiau";
+$locale['debonair_0402'] = "Šiuo metu nėra narių";
+
+// Latest Blog
+$locale['debonair_0403'] = "Paskutiniai tinklaraščiai";
+$locale['debonair_0404'] = "Nėra jokių tinklaraščių";
+$locale['debonair_0405'] = "Prašome įrašyti oficialų PHP-Fusion Tinklaraščių Infusion";
+// Latest News
+$locale['debonair_0406'] = "Paskutinės naujienos";
+$locale['debonair_0407'] = "Nėra jokių naujienų";
+$locale['debonair_0408'] = "Prašome įrašyti oficialų PHP-Fusion Naujienų Infusion";
+// About Us
+$locale['debonair_0410'] = "Apie mus";
+$locale['debonair_0411'] = "El. paštas:";
+$locale['debonair_0412'] = "Prašome įrašyti oficialų PHP-Fusion Naujienų Infusion";
+// Latest Articles
+$locale['debonair_0413'] = "Paskutiniai straipsniai";
+$locale['debonair_0414'] = "Nėra jokių straipsnių";
+$locale['debonair_0415'] = "Prašome įrašyti oficialų PHP-Fusion Straipsnių Infusion";
+
+// Banner
+$locale['debonair_0500'] = "Sveiki atvykę į PHP-Fusion 9";
+$locale['debonair_0501'] = "Debonair yra itin švari <span>Web 3.0</span> verslo klasės tema jūsų tinklapiui";
+$locale['debonair_0502'] = "Temos plėtiniai";
+$locale['debonair_0502a'] = "Eikite į <span>Temų nustatymus</span>, kad galėtumėte atlikti pakeitimus.";
+$locale['debonair_0502c'] = "Nustatymus rasite Temų administravimas > Debonair > Valdyti temą > Įskiepių valdymas > Debonair baneris";
+$locale['debonair_0504'] = "Lean More";
+
+// Footer
+$locale['debonair_0505'] = "Pradinis";
+$locale['debonair_0506'] = "Apie us";
+$locale['debonair_0507'] = "Straipsniai";
+$locale['debonair_0508'] = "Naujienos";
+$locale['debonair_0509'] = "Tinklaraščiai";
+$locale['debonair_0510'] = "Mus rasite:";
+$locale['debonair_0511'] = "Facebook";
+$locale['debonair_0512'] = "Twitter";
+
+$locale['debonair_0600'] = "Nerasta arba pasibaigę";
+$locale['debonair_0601'] = "Plėtinys nenustatytas veikimui";
+$locale['debonair_0602'] = "Nėra jokių plėtinių";
+
+$locale['debonair_0700'] = "<strong>Naudojimas:</strong> reikia mažiausiai 2 skaidrių, kad prasidėtų jų rodymas";
