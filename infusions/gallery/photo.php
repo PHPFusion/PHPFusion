59dc14f368a7c98ea2df079aa827a7f37a5bc0bf
Domi <domi@php-fusion.co.uk>
2016-02-21 10:10:15 +0100

    Fixed a faulty check that made album not work with no blog installed

--- infusions/gallery/photo.php
+++ infusions/gallery/photo.php
@@ -16,7 +16,7 @@
 | written permission from the original author(s).
 +--------------------------------------------------------*/
 require_once file_exists('maincore.php') ? '../../maincore.php' : __DIR__."/../../maincore.php";
-if (!db_exists(DB_BLOG)) { redirect(BASEDIR."error.php?code=404"); }
+if (!db_exists(DB_PHOTOS)) { redirect(BASEDIR."error.php?code=404"); }
 
 require_once INCLUDES."infusions_include.php";
 $gallery_settings = get_settings("gallery");
