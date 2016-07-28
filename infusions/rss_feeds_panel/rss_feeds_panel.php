803a12c9d22d0d64fc55fff9158132671c9b23ca
Tomasz "jantom" Jankowski <jantom@jantom.info>
2016-07-28 18:45:17 +0200

    Add support for translating RSS images: - delete hardcoded images, adding option for loading files from /infusions/rss_feeds_panel/images/LANGUAGE in rss_feeds_panel.php - adding declaration for RSS images in ImageRepo.php

--- infusions/rss_feeds_panel/rss_feeds_panel.php
+++ infusions/rss_feeds_panel/rss_feeds_panel.php
@@ -7,6 +7,7 @@
 | Filename: rss_feeds_panel.php
 | Author: Robert Gaudyn (Wooya)
 | Co-Author: Joakim Falk (Falk)
+| Co-Author: Tomasz Jankowski (jantom)
 +--------------------------------------------------------+
 | This program is released as free software under the
 | Affero GPL license. You can redistribute it and/or
@@ -18,31 +19,43 @@
 +--------------------------------------------------------*/
 if (!defined("IN_FUSION")) { die("Access Denied"); }
 
+include INCLUDES."infusions_include.php";
+
 if (file_exists(INFUSIONS."rss_feeds_panel/locale/".LANGUAGE.".php")) {
 	include INFUSIONS."rss_feeds_panel/locale/".LANGUAGE.".php";
 } else {
 	include INFUSIONS."rss_feeds_panel/locale/English.php";
 }
 
+if (file_exists(INFUSIONS."rss_feeds_panel/images/".LANGUAGE)) {
+	set_image("rss_article" , INFUSIONS."rss_feeds_panel/images/".LANGUAGE."/rss_articles.gif");
+	set_image("rss_blog" , INFUSIONS."rss_feeds_panel/images/".LANGUAGE."/rss_blog.gif");
+	set_image("rss_downloads" , INFUSIONS."rss_feeds_panel/images/".LANGUAGE."/rss_downloads.gif");
+	set_image("rss_forums" , INFUSIONS."rss_feeds_panel/images/".LANGUAGE."/rss_forums.gif");
+	set_image("rss_news" , INFUSIONS."rss_feeds_panel/images/".LANGUAGE."/rss_news.gif");
+	set_image("rss_weblinks" , INFUSIONS."rss_feeds_panel/images/".LANGUAGE."/rss_weblinks.gif");
+		
+	}
+
 openside($locale['rss009']);
 echo "<p style='text-align: center;'>\n";
 if (db_exists(DB_NEWS)) {
-	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_news.php'><img class='img-responsive' src='".INFUSIONS."rss_feeds_panel/images/rss_news.gif' alt='".$locale['rss004']."'></a>\n";
+	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_news.php'>".get_image("rss_news",$locale['rss004'],"","","class='img-responsive'")."</a>\n";
 }
 if (db_exists(DB_BLOG)) {
-	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_blog.php'><img class='img-responsive' src='".INFUSIONS."rss_feeds_panel/images/rss_blog.gif' alt='".$locale['rss000']."'></a>\n";
+	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_blog.php'>".get_image("rss_blog",$locale['rss000'],"","","class='img-responsive'")."</a>\n";
 }
 if (db_exists(DB_ARTICLES) && db_exists(DB_ARTICLE_CATS)) {
-	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_articles.php'><img class='img-responsive' src='".INFUSIONS."rss_feeds_panel/images/rss_articles.gif' alt='".$locale['rss002']."'></a>\n";
+	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_articles.php'>".get_image("rss_article",$locale['rss002'],"","","class='img-responsive'")."</a>\n";
 }
 if (db_exists(DB_DOWNLOADS) && db_exists(DB_DOWNLOAD_CATS)) {
-	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_downloads.php'><img class='img-responsive' src='".INFUSIONS."rss_feeds_panel/images/rss_downloads.gif' alt='".$locale['rss003']."'></a>\n";
+	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_downloads.php'>".get_image("rss_downloads",$locale['rss003'],"","","class='img-responsive'")."</a>\n";
 }
 if (db_exists(DB_WEBLINKS) && db_exists(DB_WEBLINK_CATS)) {
-	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_weblinks.php'><img class='img-responsive' src='".INFUSIONS."rss_feeds_panel/images/rss_weblinks.gif' alt='".$locale['rss005']."'></a>\n";
+	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_weblinks.php'>".get_image("rss_weblinks",$locale['rss005'],"","","class='img-responsive'")."</a>\n";
 }
 if (db_exists(DB_FORUM_POSTS) && db_exists(DB_FORUMS)) {
-	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_forums.php'><img class='img-responsive' src='".INFUSIONS."rss_feeds_panel/images/rss_forums.gif' alt='".$locale['rss001']."'></a>\n";
+	echo "<a href='".INFUSIONS."rss_feeds_panel/feeds/rss_forums.php'>".get_image("rss_forums",$locale['rss001'],"","","class='img-responsive'")."</a>\n";
 }
 echo "</p>\n";
 closeside();
