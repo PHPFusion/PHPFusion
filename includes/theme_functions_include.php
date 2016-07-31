36ec4d01446a0de61c4a83fca1cabb453a0dcf10
Frederick MC Chan <meangczac.chan@gmail.com>
2016-07-31 17:51:29 +0800

    Colorbox function. Make it easy to use.

--- includes/theme_functions_include.php
+++ includes/theme_functions_include.php
@@ -816,6 +816,22 @@ if (!function_exists('display_avatar')) {
 	}
 }
 
+if (!function_exists('colorbox')) {
+    function colorbox($img_path, $img_title) {
+        if (!defined('COLORBOX')) {
+            define('COLORBOX', TRUE);
+            add_to_head("<link rel='stylesheet' href='".INCLUDES."jquery/colorbox/colorbox.css' type='text/css' media='screen' />");
+            add_to_head("<script type='text/javascript' src='".INCLUDES."jquery/colorbox/jquery.colorbox.js'></script>");
+            add_to_jquery("$('a[rel^=\"colorbox\"]').colorbox({ current: '',width:'80%',height:'80%'});");
+            return "
+            <a target='_blank' href='$img_path' title='$img_title' rel='colorbox'>
+                <img src='$img_path' class='img-responsive' alt='$img_title'/>
+            </a>
+            ";
+        }
+    }
+}
+
 /**
  * Thumbnail function
  * @param      $src
