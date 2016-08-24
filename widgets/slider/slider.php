579f2ee7571265447d40c3bc08cd133f7c0965f7
Frederick MC Chan <meangczac.chan@gmail.com>
2016-08-24 09:26:36 +0800

    Update Slider Widget to support Subdirectory Paths.

--- widgets/slider/slider.php
+++ widgets/slider/slider.php
@@ -84,7 +84,9 @@ class carouselWidget extends \PHPFusion\Page\PageModel implements \PHPFusion\Pag
                     $slides = self::$sliderData[$slider_counter];
                     ?>
                     <div class="item <?php echo($slider_counter == 0 ? 'active' : '') ?>">
-                        <img src="<?php echo IMAGES.$slides['slider_image_src'] ?>" alt="<?php echo $slides['slider_title'] ?>">
+                        <img
+                            src="<?php echo IMAGES.(!empty(self::$sliderOptions['slider_path']) ? self::$sliderOptions['slider_path'].DIRECTORY_SEPARATOR : '').$slides['slider_image_src'] ?>"
+                            alt="<?php echo $slides['slider_title'] ?>">
                         <div class="carousel-caption" style="display:block; top:0; padding-top:<?php echo $slides['slider_caption_offset'] ?>px;">
                             <?php echo(!empty($slides['slider_title']) ? "<h3 class='".$slides['slider_caption_align']."' style='font-size: ".$slides['slider_title_size']."px'>".$slides['slider_title']."</h3>" : '') ?>
                             <?php echo(!empty($slides['slider_description']) ? "<p class='".$slides['slider_caption_align']."'style='font-size: ".$slides['slider_desc_size']."px'>".$slides['slider_description']."</p>" : '') ?>
