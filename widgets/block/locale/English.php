96f70e3edc71f8f1e8b2f8b8e64f26aa79eaa2cf
Frederick MC Chan <meangczac.chan@gmail.com>
2016-08-24 16:16:01 +0800

    New Block Widget

---
+++ widgets/block/locale/English.php
@@ -0,0 +1,33 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------*
+| Filename: Block/locale/English.php
+| Author: Frederick MC Chan (Chan)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+
+$locale['0100'] = "Block Box";
+$locale['0101'] = "Text Block Plugin";
+$locale['0200'] = "Block Title";
+$locale['0201'] = "Block Description";
+$locale['0202'] = "Block Description";
+$locale['0203'] = "Default Position";
+$locale['0204'] = "Left Position";
+$locale['0205'] = "Center Position";
+$locale['0206'] = "Right Position";
+$locale['0207'] = "Class";
+$locale['0218'] = "Block Margins and Spacing";
+$locale['0219'] = "Block Margin";
+$locale['0220'] = "Block Padding";
+$locale['0221'] = "Save Block";
+$locale['0222'] = "Save and Close Block";
