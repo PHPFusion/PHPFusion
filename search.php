4c286d2bf04d10fd1f2347eaf14eb9c0bee794e7
Frederick MC Chan <meangczac.chan@gmail.com>
2016-09-01 09:59:26 +0800

    Search code update by Karrak

--- search.php
+++ search.php
@@ -97,7 +97,7 @@ $available[] = "all";
 // Format string stype
 if (isset($_GET['stype']) || isset($_POST['stype']) && in_array(isset($_GET['stype']), $available)) {
     if (isset($_GET['stype']) && in_array($_GET['stype'], $available) || isset($_POST['stype']) && in_array($_POST['stype'], $available)) {
-        $_GET['stype'] = isset($_POST['stype']) ? lcfirst($_POST['stype']) : lcfirst(fusion_get_settings('default_search'));
+        $_GET['stype'] = isset($_POST['stype']) ? lcfirst($_POST['stype']) : lcfirst(str_replace(".php", "", fusion_get_settings('default_search')));
     } else {
         $_GET['stype'] = "all";
     }
