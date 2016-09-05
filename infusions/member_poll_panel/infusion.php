4630063d59607c76a4ecb2994feee0c31da582cf
karrak1 <g.sebestyen@fusionjatek.hu>
2016-09-05 12:28:23 +0200

    broken link
    records broken link

--- infusions/member_poll_panel/infusion.php
+++ infusions/member_poll_panel/infusion.php
@@ -66,7 +66,7 @@ $inf_newtable[] = DB_POLLS." (
 ) ENGINE=MyISAM DEFAULT CHARSET=UTF8 COLLATE=utf8_unicode_ci";
 
 // Automatic enable of the latest articles panel
-$inf_insertdbrow[] = DB_PANELS." (panel_name, panel_filename, panel_content, panel_side, panel_order, panel_type, panel_access, panel_display, panel_status, panel_url_list, panel_restriction) VALUES('".$locale['setup_3407']."', 'member_poll_panel', '', '1', '5', 'file', '0', '0', '1', '', '0')";
+$inf_insertdbrow[] = DB_PANELS." (panel_name, panel_filename, panel_content, panel_side, panel_order, panel_type, panel_access, panel_display, panel_status, panel_url_list, panel_restriction) VALUES('".$locale['setup_3407']."', '".$inf_folder."', '', '1', '5', 'file', '0', '0', '1', '', '0')";
 
 // Position these links under Content Administration
 $inf_adminpanel[] = array(
@@ -82,12 +82,12 @@ $enabled_languages = makefilelist(LOCALE, ".|..", TRUE, "folders");
 if (!empty($enabled_languages)) {
     foreach ($enabled_languages as $language) {
         $locale = fusion_get_locale("", LOCALE.$language."/setup.php");
-        $mlt_insertdbrow[$language][] = DB_SITE_LINKS." (link_name, link_url, link_visibility, link_position, link_window, link_order, link_language) VALUES ('".$locale['setup_3022']."', '".INFUSIONS."member_poll_panel/polls_archive.php', '0', '1', '0', '2', '".$language."')";
-        $mlt_deldbrow[$language][] = DB_SITE_LINKS." WHERE link_url='".INFUSIONS."member_poll_panel/polls_archive.php' AND link_language='".$language."'";
+        $mlt_insertdbrow[$language][] = DB_SITE_LINKS." (link_name, link_url, link_visibility, link_position, link_window, link_order, link_language) VALUES ('".$locale['setup_3022']."', 'infusions/".$inf_folder."/polls_archive.php', '0', '1', '0', '2', '".$language."')";
+        $mlt_deldbrow[$language][] = DB_SITE_LINKS." WHERE link_url='infusions/".$inf_folder."/polls_archive.php' AND link_language='".$language."'";
     }
 } else {
-    $inf_insertdbrow[] = DB_SITE_LINKS." (link_name, link_url, link_visibility, link_position, link_window, link_order, link_language) VALUES ('".$locale['setup_3022']."', '".INFUSIONS."member_poll_panel/polls_archive.php', '0', '1', '0', '2', '".LANGUAGE."')";
-    $inf_deldbrow[] = DB_SITE_LINKS." WHERE link_url='".INFUSIONS."member_poll_panel/polls_archive.php' AND link_language='".LANGUAGE."'";
+    $inf_insertdbrow[] = DB_SITE_LINKS." (link_name, link_url, link_visibility, link_position, link_window, link_order, link_language) VALUES ('".$locale['setup_3022']."', 'infusions/".$inf_folder."/polls_archive.php', '0', '1', '0', '2', '".LANGUAGE."')";
+    $inf_deldbrow[] = DB_SITE_LINKS." WHERE link_url='infusions/".$inf_folder."/polls_archive.php' AND link_language='".LANGUAGE."'";
 }
 
 // Defuse cleaning	
