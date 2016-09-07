f2b075d5ca1f6cf3f8316a9ed1195485ca6cf815
Frederick MC Chan <meangczac.chan@gmail.com>
2016-09-07 10:11:06 +0800

    FusionTheme - ThemePack selector

--- themes/FusionTheme/theme_db.php
+++ themes/FusionTheme/theme_db.php
@@ -16,11 +16,14 @@
 | written permission from the original author(s).
 +--------------------------------------------------------*/
 $readme_file = '';
-$theme_folder = 'Nebula';
-$theme_title = 'Nebula';
+$theme_folder = 'FusionTheme';
+$theme_title = 'FusionTheme';
 $theme_screenshot = 'screenshot.jpg';
 $theme_author = 'PHP-Fusion Inc';
 $theme_web = 'https://www.phpfusion.co.uk';
 $theme_license = 'AGPL3';
 $theme_version = '1.00';
 $theme_description = 'Official theme release for PHP-Fusion 9 RC5';
+
+$theme_insertdbrow[] = DB_SETTINGS_THEME." (settings_name, settings_value, settings_theme) VALUES ('theme_pack', 'Nebula', '".$theme_folder."')";
+$theme_deldbrow[] = DB_SETTINGS_THEME." WHERE settings_theme='".$theme_folder."'";
