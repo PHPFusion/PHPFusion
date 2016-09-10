dd07630b57fb7d46f44ad55f8b20e928b4491837
Frederick MC Chan <meangczac.chan@gmail.com>
2016-09-10 15:26:55 +0800

    Update Avatar function (using absolute path)

--- includes/theme_functions_include.php
+++ includes/theme_functions_include.php
@@ -900,13 +900,13 @@ if (!function_exists('display_avatar')) {
             $userdata['user_id'] = 1;
         }
         $class = ($class) ? "class='$class'" : '';
-        $default_avatar = IMAGES."avatars/no-avatar.jpg";
-        $user_avatar = IMAGES."avatars/".$userdata['user_avatar'];
+        // Need a full path - or else Jquery script cannot use this function.
+        $default_avatar = fusion_get_settings('site_path')."images/avatars/no-avatar.jpg";
+        $user_avatar = fusion_get_settings('site_path')."images/avatars/".$userdata['user_avatar'];
         $hasAvatar = $userdata['user_avatar'] && file_exists($user_avatar) && $userdata['user_status'] != '5' && $userdata['user_status'] != '6';
         $imgTpl = "<img class='img-responsive $img_class %s' alt='".$userdata['user_name']."' data-pin-nopin='true' style='display:inline; max-width:$size; max-height:$size;' src='%s'>";
         $img = sprintf($imgTpl, $hasAvatar ? 'm-r-10' : 'm-r-10',
                        $hasAvatar ? $user_avatar : $default_avatar);
-
         return $link ? sprintf("<a $class title='".$userdata['user_name']."' href='".BASEDIR."profile.php?lookup=".$userdata['user_id']."'>%s</a>", $img) : $img;
     }
 }
