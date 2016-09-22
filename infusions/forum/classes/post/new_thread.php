afad00602225bf4f3eb27b89ca9443dc0e79dd29
Frederick MC Chan <meangczac.chan@gmail.com>
2016-09-22 08:08:08 +0800

    Set thread tags width full.

--- infusions/forum/classes/post/new_thread.php
+++ infusions/forum/classes/post/new_thread.php
@@ -608,7 +608,7 @@ class NewThread extends ForumServer {
                     'tags_field' => form_select('thread_tags[]', $locale['forum_tag_0100'], $thread_data['thread_tags'],
                                                 array(
                                                     'options' => $this->tag()->get_TagOpts(),
-                                                    'width' => '100%',
+                                                    'inner_width' => '100%',
                                                     'multiple' => TRUE,
                                                     'delimiter' => '.',
                                                     'max_select' => 3, // to do settings on this
