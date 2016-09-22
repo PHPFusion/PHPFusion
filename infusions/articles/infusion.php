4b7a5f1852494dad0e9ce725d4e2debe77380ef9
Frederick MC Chan <meangczac.chan@gmail.com>
2016-09-22 16:41:39 +0800

    Articles upgrade

--- infusions/articles/infusion.php
+++ infusions/articles/infusion.php
@@ -23,7 +23,7 @@ $locale = fusion_get_locale("", LOCALE.LOCALESET."setup.php");
 
 $inf_title = $locale['articles']['title'];
 $inf_description = $locale['articles']['description'];
-$inf_version = "9.00";
+$inf_version = "1.1";
 $inf_developer = "PHP Fusion Development Team";
 $inf_email = "info@php-fusion.co.uk";
 $inf_weburl = "https://www.php-fusion.co.uk";
