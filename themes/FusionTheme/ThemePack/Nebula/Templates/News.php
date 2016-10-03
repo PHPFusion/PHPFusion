08536dc7906e8762af6f9f07a3f9f715e8cbcac5
Frederick MC Chan <meangczac.chan@gmail.com>
2016-10-03 16:09:50 +0800

    Documentation and Implementations of New menu API

--- themes/FusionTheme/ThemePack/Nebula/Templates/News.php
+++ themes/FusionTheme/ThemePack/Nebula/Templates/News.php
@@ -1,4 +1,21 @@
 <?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) 2002 - 2011 PHP-Fusion Inc
+| http://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: /Nebula/Templates/News.php
+| Author: Hien (Frederick MC Chan)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+
 namespace ThemePack\Nebula\Templates;
 
 use ThemeFactory\Core;
