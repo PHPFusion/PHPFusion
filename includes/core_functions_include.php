8f2b7c9ace78438f938e95063fda5826eb54e324
karrak1 <g.sebestyen@fusionjatek.hu>
2016-10-06 11:46:13 +0200

    modify user level (102 -> -102)
    modify user level (102 -> -102)

--- includes/core_functions_include.php
+++ includes/core_functions_include.php
@@ -1121,7 +1121,7 @@ function blacklist($field) {
         $result = dbquery("SELECT user_id, user_level FROM ".DB_USERS." WHERE user_blacklist REGEXP('^\\\.{$userdata['user_id']}$|\\\.{$userdata['user_id']}\\\.|\\\.{$userdata['user_id']}$')");
         if (dbrows($result) > 0) {
             while ($data = dbarray($result)) {
-                if ($data['user_level'] < 102) {
+                if ($data['user_level'] > -102) {
                     $blacklist[] = $data['user_id']; // all users to filter
                 }
             }
