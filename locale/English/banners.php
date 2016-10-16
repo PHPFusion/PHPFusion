a49c71754c66d1360c98444ce68f1afdec1feae7
karrak1 <g.sebestyen@fusionjatek.hu>
2016-10-16 10:24:59 +0200

    Update banners.php

--- locale/English/banners.php
+++ locale/English/banners.php
@@ -1,19 +1,1 @@
-<?php
-$locale['BN_000'] = "Banners";
 
-$locale['BN_010'] = "Image Upload.";
-$locale['BN_011'] = "During Error Uploading Images.";
-$locale['BN_012'] = "Banner Updated in the database.";
-$locale['BN_013'] = "Banner deleted from the database.";
-$locale['BN_014'] = "Not Selected Banner.";
-$locale['BN_015'] = "Page Logo: folder/file_name.ext must be.";
-$locale['BN_016'] = "Are you sure you want to delete the page a logo?";
-$locale['BN_017'] = "Are you sure you want to delete this banner?";
-
-$locale['BN_020'] = "Uploading Images";
-$locale['BN_021'] = "Current Appearance";
-$locale['BN_022'] = "Submit";
-
-$locale['sitebanner'] = "Site Logo";
-$locale['sitebanner1'] = "Banner 1";
-$locale['sitebanner2'] = "Banner 2";
