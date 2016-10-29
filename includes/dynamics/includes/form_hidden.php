cfcb39f5faa421a087ed421cf3cefff1e5eaf064
karrak1 <g.sebestyen@fusionjatek.hu>
2016-10-29 17:21:42 +0200

    Update form_hidden.php

--- includes/dynamics/includes/form_hidden.php
+++ includes/dynamics/includes/form_hidden.php
@@ -43,7 +43,7 @@ function form_hidden($input_name, $label = "", $input_value = "", array $options
 
     if ($options['show_title']) {
         $html .= "<div id='".$options['input_id']."-field' class='form-group m-b-0 ".$options['class']." '>\n";
-        $html .= ($label) ? "<label class='control-label ".($options['inline'] ? "col-xs-12 col-sm-3 col-md-3 col-lg-3" : '')."' for='".$options['input_id']."'>$title ".($options['required'] ? "<span class='required'>*</span>" : '')."</label>\n" : '';
+        $html .= ($label) ? "<label class='control-label ".($options['inline'] ? "col-xs-12 col-sm-3 col-md-3 col-lg-3" : '')."' for='".$options['input_id']."'>"$title.($options['required'] ? "<span class='required'>&nbsp;*</span>" : '')."</label>\n" : '';
         $html .= $options['inline'] ? "<div class='col-xs-12 col-sm-9 col-md-9 col-lg-9'>\n" : '';
     }
     $html .= "<input type='hidden' name='$input_name' id='".$options['input_id']."' value='$input_value' ".($options['width'] ? "style='width:".$options['width']."'" : '')." ".($options['show_title'] ? "" : "readonly")." />\n";
