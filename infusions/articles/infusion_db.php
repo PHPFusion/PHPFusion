3c080ea2505ba081218682080db582bf69fc4dfd
karrak1 <g.sebestyen@fusionjatek.hu>
2016-10-30 15:06:50 +0100

    add articles infusion_db
    add articles infusion_db

---
+++ infusions/articles/infusion_db.php
@@ -0,0 +1,28 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: articles/infusion_db.php
+| Author: PHP-Fusion Development Team
+| Version: 9.2 prototype
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+if (!defined("IN_FUSION")) {
+    die("Access Denied");
+}
+if (defined("ADMIN_PANEL")) {
+    \PHPFusion\Admins::getInstance()->setAdminPageIcons("A", "<i class='admin-ico fa fa-fw fa-book'></i>");
+    \PHPFusion\Admins::getInstance()->setAdminPageIcons("AC", "<i class='admin-ico fa fa-fw fa-book'></i>");
+	\PHPFusion\Admins::getInstance()->setCommentType('A', $locale['A']);
+	\PHPFusion\Admins::getInstance()->setSubmitType('A', $locale['A']);
+	\PHPFusion\Admins::getInstance()->setLinkType('A', fusion_get_settings("siteurl")."infusions/articles/articles.php?article_id=%s");
+}
