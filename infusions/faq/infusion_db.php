f03b52fcaff87aac369f4b1624b5b91e9c895dd0
karrak1 <g.sebestyen@fusionjatek.hu>
2016-10-30 15:24:50 +0100

    Create infusion_db.php

---
+++ infusions/faq/infusion_db.php
@@ -0,0 +1,24 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: articles/infusion_db.php
+| Author: PHP-Fusion Development Team
+| Version: 9.2 prototype
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+if (!defined("IN_FUSION")) {
+    die("Access Denied");
+}
+if (defined("ADMIN_PANEL")) {
+    \PHPFusion\Admins::getInstance()->setAdminPageIcons("FQ", "<i class='admin-ico fa fa-fw fa-life-buoy'></i>");
+}
