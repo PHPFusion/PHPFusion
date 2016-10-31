7b9b082ff41eba8260df8d30da59243c42b3bbd8
karrak1 <g.sebestyen@fusionjatek.hu>
2016-10-31 08:22:08 +0100

    Update search_custompages_include_button.php

--- includes/search/search_custompages_include_button.php
+++ includes/search/search_custompages_include_button.php
@@ -18,7 +18,6 @@
 if (!defined("IN_FUSION")) {
     die("Access Denied");
 }
-include LOCALE.LOCALESET."search/custompages.php";
 $form_elements['custompages']['enabled'] = array("fields1", "fields2", "fields3", "order1", "order2", "chars");
 $form_elements['custompages']['disabled'] = array("datelimit", "sort");
 $form_elements['custompages']['display'] = array();
