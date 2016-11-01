7e7f669811e542a4f00ad3cb978d5cd0db047381
RobiNN1 <robokelcak@centrum.sk>
2016-11-01 09:48:00 +0100

    Fix width

--- includes/dynamics/includes/form_checkbox.php
+++ includes/dynamics/includes/form_checkbox.php
@@ -124,7 +124,7 @@ function form_checkbox($input_name, $label = '', $input_value = '0', array $opti
             $checkbox .= "<div class='m-b-10'>\n";
             $checkbox .= "<input id='".$options['input_id']."-$key' style='vertical-align: middle' name='$input_name' value='$key' type='".$options['type']."'
             ".($options['deactivate'] || $options['deactivate_key'] == $key ? 'disabled' : '')." ".($options['onclick'] ? 'onclick="'.$options['onclick'].'"' : '')." ".($input_value[$key] == TRUE || $default_checked && $key == FALSE ? 'checked' : '')." /> \n";
-            $checkbox .= "<label class='control-label m-r-10' for='".$options['input_id']."-$key' ".($options['width'] ? "style='width: ".$options['width']."'" : '').">".$value."</label>\n";
+            $checkbox .= "<label class='control-label m-r-10' for='".$options['input_id']."-$key' ".($options['inner_width'] ? "style='width: ".$options['inner_width']."'" : '').">".$value."</label>\n";
             $checkbox .= "</div>\n";
         }
     } else {
@@ -133,7 +133,7 @@ function form_checkbox($input_name, $label = '', $input_value = '0', array $opti
 
     $html = "<div id='".$options['input_id']."-field' class='$switch_class form-group clearfix ".$error_class.$options['class']."'>\n";
 
-    $html .= (!empty($label)) ? "<label class='control-label ".($options['inline'] ? "col-xs-12 col-sm-3 col-md-3 col-lg-3 p-l-0" : '')."' data-checked='".(!empty($input_value) ? "1" : "0")."'  for='".$options['input_id']."' ".($options['width'] ? "style='width: ".$options['width']."'" : '').">\n" : "";
+    $html .= (!empty($label)) ? "<label class='control-label ".($options['inline'] ? "col-xs-12 col-sm-3 col-md-3 col-lg-3 p-l-0" : '')."' data-checked='".(!empty($input_value) ? "1" : "0")."'  for='".$options['input_id']."' ".($options['inner_width'] ? "style='width: ".$options['inner_width']."'" : '').">\n" : "";
 
     $html .= ($options['reverse_label'] == TRUE) ? $checkbox : "";
 
