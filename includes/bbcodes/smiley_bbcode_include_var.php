5ffba738c298a77daccea2ef36a40679e9843114
Frederick MC Chan <meangczac.chan@gmail.com>
2016-11-02 17:05:55 +0800

    Prettify Smiley Dropdown.

--- includes/bbcodes/smiley_bbcode_include_var.php
+++ includes/bbcodes/smiley_bbcode_include_var.php
@@ -23,9 +23,11 @@ $__BBCODE__[] = array(
     "description" => $locale['bb_smiley_description'], "value" => "smiley", "bbcode_start" => "",
     "bbcode_end" => "", "usage" => $locale['bb_smiley_usage'],
     "onclick" => "return overlay(this, 'bbcode_smileys_list_".$textarea_name."', '".((isset($p_data['panel_side']) && $p_data['panel_side'] == 4) ? "bottomright" : "bottomleft")."');",
-    "onmouseover" => "", "onmouseout" => "",
-    "html_start" => "<div id='bbcode_smileys_list_".$textarea_name."' class='tbl1 bbcode-popup' style='display:none;border:1px solid black;position:absolute;overflow:auto;width:400px;height:auto;' onclick=\"overlayclose('bbcode_smileys_list_".$textarea_name."');\">",
-    "includejscript" => "", "calljscript" => "",
+    "onmouseover" => "",
+    "onmouseout" => "",
+    "html_start" => "<div id='bbcode_smileys_list_".$textarea_name."' class='bbcode-popup' style='display:none; border: 1px solid #ccc; position: absolute; overflow: auto; height: auto; padding: 6px 15px; background: #fff;' onclick=\"overlayclose('bbcode_smileys_list_".$textarea_name."');\">",
+    "includejscript" => "",
+    "calljscript" => "",
     "phpfunction" => "echo displaysmileys('$textarea_name', '$inputform_name');", "html_middle" => "",
     "html_end" => "</div>"
 );
