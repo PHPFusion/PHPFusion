84c941a8b8700198d6ecaa6f3d8fd78ce8ecebe6
karrak1 <g.sebestyen@fusionjatek.hu>
2016-11-02 18:54:13 +0100

    Update search_custompages_include_button.php

--- includes/search/search_custompages_include_button.php
+++ includes/search/search_custompages_include_button.php
@@ -18,14 +18,14 @@
 if (!defined("IN_FUSION")) {
     die("Access Denied");
 }
-	$form_elements = array(
-		'custompages' => array(
-					'enabled' => array('0' => 'fields1', '1' => 'fields2', '2' => 'fields3', '3' => 'order1', '4' => 'order2', '5' => 'chars'),
-					'disabled' => array('0' => 'datelimit', '1' => 'sort'),
-					'display' => array(),
-					'nodisplay' => array(),
-					)
-				);
+$form_elements += array(
+	'custompages' => array(
+				'enabled' => array('0' => 'fields1', '1' => 'fields2', '2' => 'fields3', '3' => 'order1', '4' => 'order2', '5' =>'chars'),
+				'disabled' => array('0' => 'datelimit', '1' => 'sort'),
+				'display' => array(),
+				'nodisplay' => array(),
+				)
+			);
 $radio_button['custompages'] = form_checkbox('stype', fusion_get_locale('c400', LOCALE.LOCALESET."search/custompages.php"), $_GET['stype'],
                                         array(
                                             'type'      => 'radio',
