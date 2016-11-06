058ff57934047ea295c57012cd7c0ed0d7a9696b
Frederick MC Chan <meangczac.chan@gmail.com>
2016-11-06 18:22:26 +0800

    makefilelist()  - filter add default

--- includes/multisite_include.php
+++ includes/multisite_include.php
@@ -18,55 +18,28 @@
 // Database table definitions
 define("DB_ADMIN", DB_PREFIX."admin");
 define("DB_ADMIN_RESETLOG", DB_PREFIX."admin_resetlog");
-define("DB_ARTICLE_CATS", DB_PREFIX."article_cats");
-define("DB_ARTICLES", DB_PREFIX."articles");
 define("DB_BBCODES", DB_PREFIX."bbcodes");
 define("DB_BLACKLIST", DB_PREFIX."blacklist");
-define("DB_BLOG", DB_PREFIX."blog");
-define("DB_BLOG_CATS", DB_PREFIX."blog_cats");
 define("DB_CAPTCHA", DB_PREFIX."captcha");
 define("DB_COMMENTS", DB_PREFIX."comments");
 define("DB_CUSTOM_PAGES", DB_PREFIX."custom_pages");
 define("DB_CUSTOM_PAGES_GRID", DB_PREFIX."custom_pages_grid");
 define("DB_CUSTOM_PAGES_CONTENT", DB_PREFIX."custom_pages_content");
-define("DB_DOWNLOAD_CATS", DB_PREFIX."download_cats");
-define("DB_DOWNLOADS", DB_PREFIX."downloads");
 define("DB_EMAIL_VERIFY", DB_PREFIX."email_verify");
 define("DB_EMAIL_TEMPLATES", DB_PREFIX."email_templates");
 define("DB_ERRORS", DB_PREFIX."errors");
-define("DB_FAQ_CATS", DB_PREFIX."faq_cats");
-define("DB_FAQS", DB_PREFIX."faqs");
 define("DB_FLOOD_CONTROL", DB_PREFIX."flood_control");
-define("DB_FORUM_ATTACHMENTS", DB_PREFIX."forum_attachments");
-define("DB_FORUM_POLL_OPTIONS", DB_PREFIX."forum_poll_options");
-define("DB_FORUM_POLL_VOTERS", DB_PREFIX."forum_poll_voters");
-define("DB_FORUM_POLLS", DB_PREFIX."forum_polls");
-define("DB_FORUM_POSTS", DB_PREFIX."forum_posts");
-define("DB_FORUM_RANKS", DB_PREFIX."forum_ranks");
-define("DB_FORUM_THREAD_NOTIFY", DB_PREFIX."forum_thread_notify");
-define("DB_FORUM_THREADS", DB_PREFIX."forum_threads");
-define("DB_FORUM_VOTES", DB_PREFIX."forum_votes");
-define("DB_FORUMS", DB_PREFIX."forums");
 define("DB_INFUSIONS", DB_PREFIX."infusions");
 define("DB_INFUSIONS_CAT", DB_PREFIX."infusions_cat");
 define("DB_LANGUAGE_TABLES", DB_PREFIX."mlt_tables");
 define("DB_LANGUAGE_SESSIONS", DB_PREFIX."language_sessions");
 define("DB_MESSAGES", DB_PREFIX."messages");
 define("DB_NEW_USERS", DB_PREFIX."new_users");
-
-define("DB_NEWS", DB_PREFIX."news");
-define("DB_NEWS_CATS", DB_PREFIX."news_cats");
-define("DB_NEWS_IMAGES", DB_PREFIX."news_gallery");
-
 define("DB_ONLINE", DB_PREFIX."online");
 define("DB_PANELS", DB_PREFIX."panels");
 define("DB_PERMALINK_REWRITE", DB_PREFIX."permalinks_rewrites");
 define("DB_PERMALINK_METHOD", DB_PREFIX."permalinks_method");
 define("DB_PERMALINK_ALIAS", DB_PREFIX."permalinks_alias");
-define("DB_PHOTO_ALBUMS", DB_PREFIX."photo_albums");
-define("DB_PHOTOS", DB_PREFIX."photos");
-define("DB_POLL_VOTES", DB_PREFIX."poll_votes");
-define("DB_POLLS", DB_PREFIX."polls");
 define("DB_RATINGS", DB_PREFIX."ratings");
 define("DB_SETTINGS", DB_PREFIX."settings");
 define("DB_SETTINGS_INF", DB_PREFIX."settings_inf");
@@ -80,6 +53,48 @@ define("DB_USER_FIELDS", DB_PREFIX."user_fields");
 define("DB_USER_GROUPS", DB_PREFIX."user_groups");
 define("DB_USER_LOG", DB_PREFIX."user_log");
 define("DB_USERS", DB_PREFIX."users");
+define("DB_THEME", DB_PREFIX."theme");
+
+/*
+ * Non-core infusions_db.php inclusion.
+ * These files are supposed to be infusion_db.php using Infusion SDK
+ */
+$inf_folder = makefilelist(INFUSIONS);
+if (!empty($inf_folder)) {
+    foreach($inf_folder as $folder) {
+        $files = makefilelist(INFUSIONS.$folder);
+        if (file_exists('infusion_db.php', $files)) {
+            $path = INFUSIONS.$folder."/infusion_db.php";
+            print_p($path);
+            include $path;
+        }
+    }
+}
+
+define("DB_ARTICLE_CATS", DB_PREFIX."article_cats");
+define("DB_ARTICLES", DB_PREFIX."articles");
+define("DB_BLOG", DB_PREFIX."blog");
+define("DB_BLOG_CATS", DB_PREFIX."blog_cats");
+define("DB_DOWNLOAD_CATS", DB_PREFIX."download_cats");
+define("DB_DOWNLOADS", DB_PREFIX."downloads");
+define("DB_FAQ_CATS", DB_PREFIX."faq_cats");
+define("DB_FAQS", DB_PREFIX."faqs");
+define("DB_FORUM_ATTACHMENTS", DB_PREFIX."forum_attachments");
+define("DB_FORUM_POLL_OPTIONS", DB_PREFIX."forum_poll_options");
+define("DB_FORUM_POLL_VOTERS", DB_PREFIX."forum_poll_voters");
+define("DB_FORUM_POLLS", DB_PREFIX."forum_polls");
+define("DB_FORUM_POSTS", DB_PREFIX."forum_posts");
+define("DB_FORUM_RANKS", DB_PREFIX."forum_ranks");
+define("DB_FORUM_THREAD_NOTIFY", DB_PREFIX."forum_thread_notify");
+define("DB_FORUM_THREADS", DB_PREFIX."forum_threads");
+define("DB_FORUM_VOTES", DB_PREFIX."forum_votes");
+define("DB_FORUMS", DB_PREFIX."forums");
+define("DB_NEWS", DB_PREFIX."news");
+define("DB_NEWS_CATS", DB_PREFIX."news_cats");
+define("DB_NEWS_IMAGES", DB_PREFIX."news_gallery");
+define("DB_PHOTO_ALBUMS", DB_PREFIX."photo_albums");
+define("DB_PHOTOS", DB_PREFIX."photos");
+define("DB_POLL_VOTES", DB_PREFIX."poll_votes");
+define("DB_POLLS", DB_PREFIX."polls");
 define("DB_WEBLINK_CATS", DB_PREFIX."weblink_cats");
 define("DB_WEBLINKS", DB_PREFIX."weblinks");
-define("DB_THEME", DB_PREFIX."theme");
