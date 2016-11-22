a05b0f9d57aa7c62d065a69619dffe0d99ecda10
Frederick MC Chan <meangczac.chan@gmail.com>
2016-11-22 15:47:21 +0800

    Small fixes

--- themes/templates/global/messages.php
+++ themes/templates/global/messages.php
@@ -116,7 +116,7 @@ if (!function_exists('display_inbox')) {
         <!---start_inbox_idx--->
         <div class="row m-t-20">
             <div class="hidden-xs hidden-sm col-md-3 col-lg-2">
-                <a class='btn btn-sm btn-primary btn-block text-white'
+                <a class='btn btn-primary btn-block text-white'
                    href='<?php echo $info['button']['new']['link'] ?>'>
                     <?php echo $info['button']['new']['name'] ?>
                 </a>
