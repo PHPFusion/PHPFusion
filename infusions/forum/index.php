86cc22c00fe42fa11379330c177b4756ccd800c9
Frederick MC Chan <meangczac.chan@gmail.com>
2016-12-01 11:50:08 +0800

    Upgrade Check - Fix forum upgrade file.

--- infusions/forum/index.php
+++ infusions/forum/index.php
@@ -5,7 +5,7 @@
 | https://www.php-fusion.co.uk/
 +--------------------------------------------------------+
 | Filename: forum/index.php
-| Author: Frederick MC Chan (Chan)
+| Author: PHP-Fusion Development Team
 +--------------------------------------------------------+
 | This program is released as free software under the
 | Affero GPL license. You can redistribute it and/or
@@ -19,15 +19,11 @@ require_once file_exists('maincore.php') ? 'maincore.php' : __DIR__."/../../main
 if (!db_exists(DB_FORUMS)) {
     redirect(BASEDIR."error.php?code=404");
 }
-require_once "infusion_db.php";
 require_once THEMES."templates/header.php";
 require_once INCLUDES."infusions_include.php";
 require_once FORUM_CLASS."autoloader.php";
-
 require_once INFUSIONS."forum/forum_include.php";
 include INFUSIONS."forum/templates/forum_main.php";
-
 $info = \PHPFusion\Forums\ForumServer::forum()->getForumInfo();
 render_forum($info);
-
 require_once THEMES."templates/footer.php";
