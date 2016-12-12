f4a0152b544acebe45cae6b8e5e8c4cb80010286
karrak1 <g.sebestyen@fusionjatek.hu>
2016-12-12 11:19:04 +0100

    Update settings_messages.php

--- administration/settings_messages.php
+++ administration/settings_messages.php
@@ -18,34 +18,32 @@
 require_once "../maincore.php";
 pageAccess("S7");
 require_once THEMES."templates/admin_header.php";
-include LOCALE.LOCALESET."admin/settings.php";
+$locale = fusion_get_locale('', LOCALE.LOCALESET.'admin/settings.php');
 \PHPFusion\BreadCrumbs::getInstance()->addBreadCrumb(['link' => ADMIN.'settings_messages.php'.fusion_get_aidlink(), 'title' => $locale['message_settings']]);
 $pm_settings = array(
-    "pm_inbox_limit" => fusion_get_settings("pm_inbox_limit"),
-    "pm_outbox_limit" => fusion_get_settings("pm_outbox_limit"),
-    "pm_archive_limit" => fusion_get_settings("pm_archive_limit"),
-    "pm_email_notify" => fusion_get_settings("pm_email_notify"),
-    "pm_save_sent" => fusion_get_settings("pm_save_sent"),
+    'pm_inbox_limit' => fusion_get_settings('pm_inbox_limit'),
+    'pm_outbox_limit' => fusion_get_settings('pm_outbox_limit'),
+    'pm_archive_limit' => fusion_get_settings('pm_archive_limit'),
+    'pm_email_notify' => fusion_get_settings('pm_email_notify'),
+    'pm_save_sent' => fusion_get_settings('pm_save_sent'),
 );
 
 if (isset($_POST['save_settings'])) {
-    foreach ($pm_settings as $key => $value) {
-        if (isset($_POST[$key])) {
-            $pm_settings[$key] = form_sanitizer($_POST[$key], $pm_settings[$key], $key);
-        } else {
-            $pm_settings[$key] = form_sanitizer($pm_settings[$key], $pm_settings[$key], $key);
-        }
-        if (defender::safe()) {
+    if (\defender::safe()) {
+    	foreach ($pm_settings as $key => $value) {
+        	if (isset($_POST[$key])) {
+        	    $pm_settings[$key] = form_sanitizer($_POST[$key], $pm_settings[$key], $key);
+        	} else {
+        	    $pm_settings[$key] = form_sanitizer($pm_settings[$key], $pm_settings[$key], $key);
+        	}
             dbquery("UPDATE ".DB_SETTINGS." SET settings_value='".$pm_settings[$key]."' WHERE settings_name='".$key."'");
-        }
-    }
-    if (defender::safe()) {
+    	}
         addNotice("success", $locale['900']);
-        redirect(FUSION_SELF.$aidlink);
+        redirect(FUSION_SELF.fusion_get_aidlink());
     }
 }
 opentable($locale['message_settings']);
-echo openform('settingsform', 'post', FUSION_SELF.$aidlink);
+echo openform('settingsform', 'post', FUSION_SELF.fusion_get_aidlink());
 echo "<div class='well'>".$locale['message_description']."</div>\n";
 echo "<div class='row'>";
 echo "<div class='col-xs-12 col-sm-6'>\n";
@@ -60,7 +58,7 @@ echo form_text('pm_inbox_limit', $locale['701'], $pm_settings['pm_inbox_limit'],
 echo form_text('pm_outbox_limit', $locale['702'], $pm_settings['pm_outbox_limit'], array(
     'type' => 'number',
     'max_length' => 2,
-    'width' => '100px',
+    'inner_width' => '100px',
     'inline' => TRUE
 ));
 echo form_text('pm_archive_limit', $locale['703'], $pm_settings['pm_archive_limit'], array(
