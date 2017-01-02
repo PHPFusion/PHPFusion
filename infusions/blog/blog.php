a36dc4a3f982dc4223264e8d5d8ff30cc3500256
Rizado <v.chubatyj@yandex.ru>
2017-01-02 19:41:22 +0300

    OpenGraph for blog
    Please notify me when his infusion will be rewrited with classes. Class
    for OpenGraph requires no changes. There is to change call of ogBlog()
    function is enough.

--- infusions/blog/blog.php
+++ infusions/blog/blog.php
@@ -27,6 +27,7 @@ if (file_exists(INFUSIONS."blog/locale/".LOCALESET."blog.php")) {
     include INFUSIONS."blog/locale/English/blog.php";
 }
 require_once INFUSIONS."blog/classes/Functions.php";
+require_once INFUSIONS."blog/classes/OpenGraphBlogs.php";
 require_once INFUSIONS."blog/templates/blog.php";
 require_once INCLUDES."infusions_include.php";
 
@@ -266,6 +267,7 @@ if (isset($_GET['readmore']) && isnum($_GET['readmore'])) {
                 $result2 = dbquery("UPDATE ".DB_BLOG." SET blog_reads=blog_reads+1 WHERE blog_id='".$_GET['readmore']."'");
                 $item['blog_reads']++;
             }
+            \PHPFusion\OpenGraphBlogs::ogBlog($_GET['readmore']);
         }
 
     } else {
