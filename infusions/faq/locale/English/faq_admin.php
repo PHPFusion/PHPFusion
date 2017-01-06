7e0419a7cde273c8ad0d15653bac6d83eb4cac4f
karrak1 <g.sebestyen@fusionjatek.hu>
2017-01-06 19:27:19 +0100

    Update faq_admin.php

--- infusions/faq/locale/English/faq_admin.php
+++ infusions/faq/locale/English/faq_admin.php
@@ -1,40 +1,107 @@
 <?php
-$locale['faq_0100'] = "FAQs";
-$locale['faq_0101'] = "Currrent FAQs";
-$locale['faq_0102'] = "FAQ Category";
-$locale['faq_0102b'] = "Add FAQ";
-$locale['faq_0102c'] = "Edit FAQ";
-$locale['faq_0102d'] = "Edit FAQ Category";
-$locale['faq_0103'] = "Category Name";
-$locale['faq_0104'] = "Questions Count";
-$locale['faq_0105'] = "Category Id";
-$locale['faq_0106'] = "Options";
-$locale['faq_0107'] = "Edit";
-$locale['faq_0108'] = "Delete";
-$locale['faq_0109'] = "Delete this FAQ Category?";
-$locale['faq_0110'] = "Question:";
-$locale['faq_0111'] = "Answer:";
-$locale['faq_0112'] = "Delete this Question?";
-$locale['faq_0113'] = "There are no Frequently Asked Question defined";
-$locale['faq_0114'] = "Listing %d of total %d FAQs entries";
-$locale['faq_0115'] = "Listing %d of total %d Categories";
-$locale['faq_0116'] = "There are no FAQ Category defined";
-$locale['faq_0117'] = "Categories";
-// Faq Category form
-$locale['faq_0200'] = "Category Name";
-$locale['faq_0201'] = "Fill in category name";
-$locale['faq_0202'] = "Category Description";
-$locale['faq_0203'] = "Save Category";
-$locale['faq_0204'] = "FAQ Category saved";
-$locale['faq_0205'] = "FAQ Category updated";
-$locale['faq_0206'] = "FAQ Category deleted";
-$locale['faq_0207'] = "FAQ Category cannot be deleted because there are %d questions in this category";
-// Faq Form
-$locale['faq_0300'] = "Category";
-$locale['faq_0301'] = "Question";
-$locale['faq_0302'] = "Answer";
-$locale['faq_0303'] = "Save FAQ";
-$locale['faq_0304'] = "FAQ is not available currently because there are no FAQ Category defined. Please click [LINK]here[/LINK] to add a FAQ category";
-$locale['faq_0305'] = "FAQ added";
-$locale['faq_0306'] = "FAQ updated";
-$locale['faq_0307'] = "FAQ deleted";
+// Faq titles
+$locale['faq_0000'] = "FAQs";
+$locale['faq_0001'] = "FAQs";
+
+$locale['faq_0002'] = "FAQs";
+$locale['faq_0003'] = "Add FAQ";
+$locale['faq_0004'] = "Edit FAQ";
+$locale['faq_0005'] = "Submissions";
+$locale['faq_0006'] = "Settings";
+
+// Messages - Faq
+$locale['faq_0030'] = "FAQ successfully created";
+$locale['faq_0031'] = "FAQ successfully updated";
+$locale['faq_0032'] = "FAQ successfully deleted";
+$locale['faq_0033'] = "FAQs successfully updated";
+$locale['faq_0034'] = "You must choose at least one FAQ.";
+$locale['faq_0035'] = "FAQ cannot be deleted";
+$locale['faq_0036'] = "There are FAQs or Subcategories in the Category";
+$locale['faq_0037'] = "FAQ successfully published";
+$locale['faq_0038'] = "FAQ publishing deleted";
+
+// Messages - Submissions
+$locale['faq_0060'] = "Submission successfully published";
+$locale['faq_0061'] = "Submission successfully saved as Draft";
+$locale['faq_0062'] = "Submission successfully deleted";
+$locale['faq_0063'] = "There are no new Submissions.";
+
+// Table - Faq
+$locale['faq_0100'] = "Question";
+$locale['faq_0102'] = "Draft";
+$locale['faq_0105'] = "Author";
+$locale['faq_0106'] = "Visibility";
+$locale['faq_0107'] = "Actions";
+
+// Table - Faq - Misc
+$locale['faq_0110'] = "Add FAQ";
+$locale['faq_0111'] = "Delete this FAQ?";
+$locale['faq_0112'] = "There are no FAQ defined.";
+$locale['faq_0113'] = "There are no FAQ, which match with the Searchoptions.";
+$locale['faq_0114'] = "Before you can create a FAQ, you must create a FAQ.";
+
+// Table - Faq - Sorting
+$locale['faq_0120'] = "Search";
+$locale['faq_0121'] = "Search";
+$locale['faq_0122'] = "Clear";
+$locale['faq_0123'] = "Choose Status";
+$locale['faq_0124'] = "All Status";
+$locale['faq_0125'] = "Choose Visibility";
+$locale['faq_0128'] = "Choose Language";
+$locale['faq_0129'] = "All Languages";
+$locale['faq_0130'] = "Choose Author";
+$locale['faq_0131'] = "All Authors";
+$locale['faq_0132'] = "Items per Page";
+
+// Table - Submissions
+$locale['faq_0200'] = "ID";
+$locale['faq_0201'] = "Question";
+$locale['faq_0202'] = "Submissioner";
+$locale['faq_0203'] = "Date";
+$locale['faq_0204'] = "Options";
+$locale['faq_0205'] = "Review Submission";
+
+// Form - Faq
+$locale['faq_0250'] = "Question";
+$locale['faq_0251'] = "Answer";
+$locale['faq_0251a'] = "The FAQs self.";
+$locale['faq_0252'] = "Category";
+$locale['faq_0253'] = "Visibility";
+$locale['faq_0254'] = "Date";
+$locale['faq_0255'] = "Published";
+$locale['faq_0256'] = "Enable automatic Linebreaks";
+$locale['faq_0257'] = "Update Publication Date";
+$locale['faq_0258'] = "More Settings";
+$locale['faq_0259'] = "More Informations";
+
+// Form - Faq - Errors
+$locale['faq_0270'] = "You must specify a FAQs.";
+$locale['faq_0271'] = "You must specify a Question.";
+$locale['faq_0272'] = "You must specify a FAQ.";
+$locale['faq_0274'] = "%s are disabled for all items.\n\rThis setting will only take effect when they are enabled again.";
+
+// Form - Submissions
+$locale['faq_0350'] = "The submission above was sent from {%SUBMISSION_AUTHOR%}.";
+$locale['faq_0351'] = "Submission Date: {%SUBMISSION_TIME%} - {%SUBMISSION_DATE%}.";
+$locale['faq_0352'] = "Published";
+
+// Settings
+$locale['faq_0400'] = "Settings for the FAQs System.";
+$locale['faq_0401'] = "Submissions";
+
+// Submissions - Titles
+$locale['faq_0900'] = "Submit FAQ";
+
+// Submissions - Success Submission
+$locale['faq_0910'] = "FAQ is successfully submitted.";
+$locale['faq_0911'] = "Thank you for the Submission.";
+$locale['faq_0912'] = "Submit other FAQs";
+$locale['faq_0913'] = "Back to [SITENAME]";
+
+// Submissions - Other
+$locale['faq_0920'] = "Use the following form to submit an FAQ. Your submission will be reviewed by an
+Administrator. [SITENAME] reserves the right to amend or edit any submission. FAQ
+should be applicable to the content of this site. Unsuitable submissions will be deleted.";
+$locale['faq_0921'] = "You must login to submit a FAQ.";
+$locale['faq_0922'] = "We do not accept new FAQs Submissions at the Moment.";
+$locale['faq_0923'] = "You cannot submit a FAQ, because there are no FAQ defined.";
