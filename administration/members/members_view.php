045f31112392f1f8bcb47db47ca6500f2151666e
karrak1 <g.sebestyen@fusionjatek.hu>
2017-01-17 17:25:13 +0100

    Update members_view.php

--- administration/members/members_view.php
+++ administration/members/members_view.php
@@ -28,13 +28,13 @@ class Members_View extends Members_Admin {
         <div id='filter_panel' style='display:none'>
             <div class='list-group-item'>
                 <div class='row'>
-                    <div class='col-xs-3'><strong>Display Results.</strong></div>
+                    <div class='col-xs-3'><strong>".self::$locale['ME_560']."</strong></div>
                     <div class='col-xs-9'>{%filter_options%}{%filter_extras%}</div>
                 </div>
             </div>
             <div class='list-group-item spacer-xs'>
                 <div class='row'>
-                    <div class='col-xs-3'><strong>Display User With Status</strong></div>
+                    <div class='col-xs-3'><strong>".self::$locale['ME_561']."</strong></div>
                     <div class='col-xs-9'>{%filter_status%}</div>
                 </div>
             </div>
