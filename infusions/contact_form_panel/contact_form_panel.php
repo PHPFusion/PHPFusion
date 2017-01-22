dc48508170eddb772124ad555dfccc97905a74a8
karrak1 <g.sebestyen@fusionjatek.hu>
2017-01-22 11:25:00 +0100

    add messages
    add messages

--- infusions/contact_form_panel/contact_form_panel.php
+++ infusions/contact_form_panel/contact_form_panel.php
@@ -97,8 +97,11 @@ if (!iADMIN) {
         $captcha = form_text('captcha_code', $locale['408'], '', ['required' => TRUE, 'autocomplete_off' => TRUE]);
     }
 }
+$message = str_replace("[SITE_EMAIL]", hide_email(fusion_get_settings('siteemail')), $locale['401']);
+$message = str_replace("[PM_LINK]", "<a href='messages.php?msg_send=1'>".$locale['global_121']."</a>", $message);
 
 $info = [
+    'prmessages'     => "<div class='text-center well'>".$message."</div>\n",
     'openform'     => openform('contactform', 'post', FUSION_SELF, ['max_tokens' => 1]),
     'mail_name'    => form_text('mailname', $locale['402'], $input['mailname'], ['required' => TRUE, 'error_text' => $locale['420'], 'max_length' => 64]),
     'email'        => form_text('email', $locale['403'], $input['email'], ['required' => TRUE, 'error_text' => $locale['421'], 'type' => 'email', 'max_length' => 64]),
@@ -115,6 +118,7 @@ require_once INFUSIONS.'contact_form_panel/templates/contact_form_panel.php';
 echo render_contact_panel($info);
 
 echo strtr(ob_get_clean(), [
+    '{%prmessages%}'      => $info['prmessages'],
     '{%open_form%}'       => $info['openform'],
     '{%mail_name_field%}' => $info['mail_name'],
     '{%email_field%}'     => $info['email'],
