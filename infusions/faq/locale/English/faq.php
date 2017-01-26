d3a86f615ae43554364539ef11a56bc7dbaa3f33
chan <meangczac.chan@gmail.com>
2017-01-27 00:00:52 +0800

    FAQ front end view (WIP for ratings and comments)

--- infusions/faq/locale/English/faq.php
+++ infusions/faq/locale/English/faq.php
@@ -2,5 +2,6 @@
 $locale['faqs_0000'] = "FAQ (Frequently Asked Questions)";
 $locale['faqs_0001'] = "Categories";
 $locale['faqs_0002'] = "Top";
+$locale['faqs_0003'] = "Uncategorized FAQs";
 // Notices
 $locale['faqs_0010'] = "No FAQ defined.";
