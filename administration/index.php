6e5ca4abb0a306ea1867c5572dca442f8faebed1
chan <meangczac.chan@gmail.com>
2017-01-27 19:35:20 +0800

    Fix API link_type, and comment_type un-defined

--- administration/index.php
+++ administration/index.php
@@ -113,7 +113,7 @@ $global_comments['rows'] = dbcount("('comment_id')", DB_COMMENTS);
 $_GET['c_rowstart'] = isset($_GET['c_rowstart']) && $_GET['c_rowstart'] <= $global_comments['rows'] ? $_GET['c_rowstart'] : 0;
 $comments_result = dbquery("SELECT c.*, u.user_id, u.user_name, u.user_status, u.user_avatar
 							FROM ".DB_COMMENTS." c LEFT JOIN ".DB_USERS." u on u.user_id=c.comment_name
-							ORDER BY comment_datestamp DESC LIMIT ".$_GET['c_rowstart'].", ".$settings['comments_per_page']."
+							ORDER BY comment_datestamp DESC LIMIT 5
 							");
 if ($global_comments['rows'] > $settings['comments_per_page']) {
     $global_comments['nav'] = makepagenav($_GET['c_rowstart'], $settings['comments_per_page'], $global_comments['rows'], 2);
@@ -131,8 +131,7 @@ $global_ratings['rows'] = dbcount("('rating_id')", DB_RATINGS);
 $_GET['r_rowstart'] = isset($_GET['r_rowstart']) && $_GET['r_rowstart'] <= $global_ratings['rows'] ? $_GET['r_rowstart'] : 0;
 $result = dbquery("SELECT r.*, u.user_id, u.user_name, u.user_status, u.user_avatar
 					FROM ".DB_RATINGS." r LEFT JOIN ".DB_USERS." u on u.user_id=r.rating_user
-					ORDER BY rating_datestamp DESC LIMIT ".$_GET['r_rowstart'].", ".$settings['comments_per_page']."
-					");
+					ORDER BY rating_datestamp DESC LIMIT 5");
 $global_ratings['data'] = array();
 if (dbrows($result) > 0) {
     while ($_ratdata = dbarray($result)) {
