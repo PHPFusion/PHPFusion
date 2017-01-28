2e159d4af59e570cba373a32baf4349abdab2026
karrak1 <g.sebestyen@fusionjatek.hu>
2017-01-28 15:43:50 +0100

    Update user_facebook.php

--- locale/English/user_fields/user_facebook.php
+++ locale/English/user_fields/user_facebook.php
@@ -1,5 +1,6 @@
 <?php
 $locale['uf_facebook'] = "Facebook";
 $locale['uf_facebook_desc'] = "Facebook Profile";
-$locale['uf_facebook_palceholder'] = "Link to your profile";
-$locale['uf_facebook_link'] = "Visit";
+$locale['uf_facebook_error'] = "Facebook can not be left empty.";
+$locale['uf_facebook_palceholder'] = "Link to your profile (e.g.: phfusion )";
+$locale['uf_facebook_tip'] = "https://www.facebook.com/ It must be involved after!! e.g.: phfusion";
