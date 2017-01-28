f8f2df29b82f079624a16224458fca2c2342fb95
karrak1 <g.sebestyen@fusionjatek.hu>
2017-01-28 15:45:51 +0100

    Update user_facebook_include.php

--- includes/user_fields/user_facebook_include.php
+++ includes/user_fields/user_facebook_include.php
@@ -21,20 +21,21 @@ if (!defined("IN_FUSION")) {
 
 // Display user field input
 if ($profile_method == "input") {
-    $options = array(
-        'inline' => TRUE,
-        'placeholder' => $locale['uf_facebook_palceholder']
-    );
+	$options = array('inline'	   => TRUE,
+                     'placeholder' => $locale['uf_facebook_palceholder'],
+					 'tip'		   => $locale['uf_facebook_tip'],
+					 'error_text'  => $locale['uf_facebook_error']
+					 );
 
     $user_fields = form_text('user_facebook', $locale['uf_facebook'], $field_value, $options);
 // Display in profile
 } elseif ($profile_method == "display") {
-     if ($field_value) {
-        $field_value = "<a href='".$field_value."' target='_blank'>".$locale['uf_facebook_link']."</a>";
-    }
-
+	if ($field_value) {
+	$field_value = !preg_match("@^http(s)?\:\/\/@i", $field_value) ? "https://www.facebook.com/".$field_value : $field_value;
+	$field_value = (fusion_get_settings('index_url_userweb') ? "" : "<!--noindex-->")."<a href='".$field_value."' title='".$field_value."' ".(fusion_get_settings('index_url_userweb') ? "" : "rel='nofollow' ")."target='_blank'><i class='fa fa-facebook-square fa-lg m-r-10'></i>".$locale['uf_facebook_link']."</a>".(fusion_get_settings('index_url_userweb') ? "" : "<!--/noindex-->");
+}
     $user_fields = array(
         'title' => $locale['uf_facebook'],
-        'value' => $field_value ? : ''
+        'value' => $field_value ?: ""
     );
 }
