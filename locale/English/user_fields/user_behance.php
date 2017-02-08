e7fc1af41969c7a53eb2b68e95e855c7e2760783
chan <meangczac.chan@gmail.com>
2017-02-09 07:16:08 +0800

    Adobe Behance User Field

---
+++ locale/English/user_fields/user_behance.php
@@ -0,0 +1,6 @@
+<?php
+$locale['uf_behance'] = "Behance";
+$locale['uf_behance_id'] = "Behance Username";
+$locale['uf_behance_desc'] = "Adobe Behance Account";
+$locale['uf_behance_error'] = "Behance Username can not be left empty.";
+
