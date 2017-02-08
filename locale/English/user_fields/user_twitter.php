ca0ee436b7ba5d50d315fdaf68c5d62f78f467ec
chan <meangczac.chan@gmail.com>
2017-02-09 07:17:44 +0800

    Twitter User Field

---
+++ locale/English/user_fields/user_twitter.php
@@ -0,0 +1,5 @@
+<?php
+$locale['uf_twitter'] = "Twitter";
+$locale['uf_twitter_id'] = "Twitter Username";
+$locale['uf_twitter_desc'] = "Twitter Account";
+$locale['uf_twitter_error'] = "Twitter Username can not be left empty.";
