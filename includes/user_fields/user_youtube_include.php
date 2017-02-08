8bab0f877f0b98911d8a118469e00ce593edcb80
chan <meangczac.chan@gmail.com>
2017-02-09 07:38:53 +0800

    Youtube User Field

--- includes/user_fields/user_youtube_include.php
+++ includes/user_fields/user_youtube_include.php
@@ -4,7 +4,7 @@
 | Copyright (C) PHP-Fusion Inc
 | https://www.php-fusion.co.uk/
 +--------------------------------------------------------+
-| Filename: user_aim_include.php
+| Filename: user_linkedin_include.php
 | Author: Digitanium
 +--------------------------------------------------------+
 | This program is released as free software under the
@@ -25,16 +25,16 @@ if ($profile_method == "input") {
             'inline'           => TRUE,
             'max_length'       => 16,
             'regex'            => '[a-z](?=[\w.]{3,31}$)\w*\.?\w*',
-            'regex_error_text' => $locale['uf_twitter_error_1'],
-            'error_text'       => $locale['uf_twitter_error'],
-            'placeholder'      => $locale['uf_twitter_id']
+            'regex_error_text' => $locale['uf_youtube_error_1'],
+            'error_text'       => $locale['uf_youtube_error'],
+            'placeholder'      => $locale['uf_youtube_id']
         ) + $options;
-    $user_fields = form_text('user_twitter', "<img src='".IMAGES."user_fields/social/twitter.svg' class='m-r-5' style='width:32px'>".$locale['uf_twitter'], $field_value, $options);
+    $user_fields = form_text('user_youtube', "<img src='".IMAGES."user_fields/social/youtube.svg' class='m-r-5' style='width:32px'>".$locale['uf_youtube'], $field_value, $options);
 // Display in profile
 } elseif ($profile_method == "display") {
     if ($field_value) {
-        $field_value = !preg_match("@^http(s)?\:\/\/@i", $field_value) ? "https://www.twitter.com/".$field_value : $field_value;
-        $field_value = (fusion_get_settings('index_url_userweb') ? "" : "<!--noindex-->")."<a href='".$field_value."' title='".$field_value."' ".(fusion_get_settings('index_url_userweb') ? "" : "rel='nofollow' ")."target='_blank'><img src='".IMAGES."user_fields/social/twitter.svg' class='m-r-5' style='width:32px'></a>".(fusion_get_settings('index_url_userweb') ? "" : "<!--/noindex-->");
+        $field_value = !preg_match("@^http(s)?\:\/\/@i", $field_value) ? "https://www.youtube.com/".$field_value : $field_value;
+        $field_value = (fusion_get_settings('index_url_userweb') ? '' : "<!--noindex-->")."<a href='".$field_value."' title='".$field_value."' ".(fusion_get_settings('index_url_userweb') ? "" : "rel='nofollow' ")."target='_blank'><img src='".IMAGES."user_fields/social/youtube.svg' style='width:32px;'></a>".(fusion_get_settings('index_url_userweb') ? "" : "<!--/noindex-->");
     }
-    $user_fields = array('title' => $locale['uf_twitter'], 'value' => $field_value ?: '');
+    $user_fields = array('title' => $locale['uf_linkedin'], 'value' => $field_value ?: '');
 }
--- locale/English/user_fields/user_twitter.php
+++ locale/English/user_fields/user_twitter.php
@@ -3,3 +3,4 @@ $locale['uf_twitter'] = "Twitter";
 $locale['uf_twitter_id'] = "Twitter Username";
 $locale['uf_twitter_desc'] = "Twitter Account";
 $locale['uf_twitter_error'] = "Twitter Username can not be left empty.";
+$locale['uf_twitter_error_1'] = "Twitter Username is Invalid. Please use a valid Twitter Username.";
