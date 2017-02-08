a2a056dae7e1b8e3e4b5db3d25d90f125ff88659
chan <meangczac.chan@gmail.com>
2017-02-09 07:16:29 +0800

    Deviantart User Field

---
+++ includes/user_fields/user_deviantart_include.php
@@ -0,0 +1,34 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: user_aim_include.php
+| Author: Digitanium
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+if (!defined("IN_FUSION")) {
+    die("Access Denied");
+}
+
+// Display user field input
+if ($profile_method == "input") {
+    $options = array(
+            'inline'      => TRUE,
+            'max_length'  => 16,
+            'error_text'  => $locale['uf_da_error'],
+            'placeholder' => $locale['uf_da_id']
+        ) + $options;
+    $user_fields = form_text('user_deviantart', "<img src='".IMAGES."user_fields/social/deviantart.svg' class='m-r-5' style='width:32px'>".$locale['uf_da'], $field_value, $options);
+// Display in profile
+} elseif ($profile_method == "display") {
+    $user_fields = array('title' => $locale['uf_da'], 'value' => $field_value ?: '');
+}
