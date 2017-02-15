986e5deab5794f0ef1d9bd5e78f1ed185e904d11
chan <meangczac.chan@gmail.com>
2017-02-15 18:02:14 +0800

    News Update 1.12  - Fixes news submissions on news gallery  - Added News_Preview Class.

--- infusions/news/news_submit.php
+++ infusions/news/news_submit.php
@@ -6,6 +6,7 @@
 +--------------------------------------------------------+
 | Filename: news_submit.php
 | Author: PHP-Fusion Development Team
+| Version: 1.12
 +--------------------------------------------------------+
 | This program is released as free software under the
 | Affero GPL license. You can redistribute it and/or
@@ -24,54 +25,67 @@ $news_settings = get_settings('news');
 $locale = fusion_get_locale('', [NEWS_ADMIN_LOCALE]);
 
 if (iMEMBER && $news_settings['news_allow_submission']) {
+
     $criteriaArray = [
-        'news_subject'  => '',
-        'news_cat'      => 0,
-        'news_snippet'  => '',
-        'news_body'     => '',
-        'news_language' => LANGUAGE,
-        'news_keywords' => '',
-        'news_ialign'   => '',
+        'news_subject'            => '',
+        'news_cat'                => 0,
+        'news_news'               => '',
+        'news_extended'           => '',
+        'news_language'           => LANGUAGE,
+        'news_keywords'           => '',
+        'news_image_full_default' => 0,
+        'news_image_font_default' => 0,
+        'news_image_align'        => '',
     ];
+
     if (isset($_POST['submit_news'])) {
+
         $submit_info['news_news'] = nl2br(parseubb(stripinput($_POST['news_news'])));
         $submit_info['news_body'] = nl2br(parseubb(stripinput($_POST['news_body'])));
         $criteriaArray = array(
             'news_subject'  => form_sanitizer($_POST['news_subject'], '', 'news_subject'),
             'news_cat'      => form_sanitizer($_POST['news_cat'], '', 'news_cat'),
-            'news_snippet'  => form_sanitizer($submit_info['news_news'], '', 'news_news'),
-            'news_body'     => form_sanitizer($submit_info['news_body'], '', 'news_body'),
+            'news_news'     => form_sanitizer($submit_info['news_news'], '', 'news_news'),
+            'news_extended' => form_sanitizer($submit_info['news_body'], '', 'news_body'),
             'news_language' => form_sanitizer($_POST['news_language'], '', 'news_language'),
             'news_keywords' => form_sanitizer($_POST['news_keywords'], '', 'news_keywords'),
         );
-        if ($news_settings['news_allow_submission_files']) {
-            if (isset($_FILES['news_image'])) {
+
+        if ($news_settings['news_allow_submission_files'] && \defender::safe()) {
+            if (!empty($_FILES['news_image'])) {
                 $upload = form_sanitizer($_FILES['news_image'], '', 'news_image');
                 if (!empty($upload)) {
-                    $criteriaArray['news_image'] = $upload['image_name'];
-                    $criteriaArray['news_image_t1'] = $upload['thumb1_name'];
-                    $criteriaArray['news_image_t2'] = $upload['thumb2_name'];
-                    $criteriaArray['news_ialign'] = (isset($_POST['news_ialign']) ? form_sanitizer($_POST['news_ialign'], 'pull-left', 'news_ialign') : 'pull-left');
-                } else {
-                    $criteriaArray['news_image'] = (isset($_POST['news_image']) ? $_POST['news_image'] : '');
-                    $criteriaArray['news_image_t1'] = (isset($_POST['news_image_t1']) ? $_POST['news_image_t1'] : '');
-                    $criteriaArray['news_image_t2'] = (isset($_POST['news_image_t2']) ? $_POST['news_image_t2'] : '');
-                    $criteriaArray['news_ialign'] = (isset($_POST['news_ialign']) ? form_sanitizer($_POST['news_ialign'], 'pull-left', 'news_ialign') : 'pull-left');
+                    if (!$upload['error']) {
+                        $data = array(
+                            'news_image_user'      => fusion_get_userdata('user_id'),
+                            'submit_id'            => dbnextid(DB_SUBMISSIONS),
+                            'news_image'           => $upload['image_name'],
+                            'news_image_t1'        => $upload['thumb1_name'],
+                            'news_image_t2'        => $upload['thumb2_name'],
+                            'news_image_datestamp' => TIME
+                        );
+                        $photo_id = dbquery_insert(DB_NEWS_IMAGES, $data, 'save', ['keep_session' => TRUE]);
+                        $criteriaArray['news_image_full_default'] = $photo_id;
+                        $criteriaArray['news_image_front_default'] = $photo_id;
+                        $criteriaArray['news_image_align'] = form_sanitizer($_POST['news_image_align'], '', 'news_image_align');
+                    }
                 }
             }
         }
+
         if (\defender::safe()) {
             $inputArray = array(
                 'submit_type'      => 'n',
                 'submit_user'      => fusion_get_userdata('user_id'),
                 'submit_datestamp' => TIME,
-                'submit_criteria'  => addslashes(serialize($criteriaArray))
+                'submit_criteria'  => \defender::encode($criteriaArray)
             );
             dbquery_insert(DB_SUBMISSIONS, $inputArray, 'save');
             addNotice('success', $locale['news_0701']);
             redirect(clean_request('submitted=n', array('submitted', 'stype'), TRUE));
         }
     }
+
     if (isset($_GET['submitted']) && $_GET['submitted'] == "n") {
         add_to_title($locale['global_200'].$locale['news_0400']);
         echo strtr(display_news_confirm_submissions(), [
@@ -82,25 +96,31 @@ if (iMEMBER && $news_settings['news_allow_submission']) {
         ]);
     } else {
         add_to_title($locale['global_200'].$locale['news_0400']);
+
         // Preview
         if (isset($_POST['preview_news'])) {
             $news_snippet = ($_POST['news_news'] ? parse_textarea($_POST['news_news']) : '');
             $news_body = ($_POST['news_body'] ? parse_textarea($_POST['news_body']) : '');
             $criteriaArray = array(
-                "news_subject"  => form_sanitizer($_POST['news_subject'], "", "news_subject"),
-                "news_language" => form_sanitizer($_POST['news_language'], "", "news_language"),
-                "news_ialign"   => form_sanitizer($_POST['news_ialign'], "", "news_ialign"),
-                "news_keywords" => form_sanitizer($_POST['news_keywords'], "", "news_keywords"),
-                "news_cat"      => form_sanitizer($_POST['news_cat'], 0, "news_cat"),
-                "news_snippet"  => form_sanitizer($_POST['news_news'], "", "news_news"),
-                "news_body"     => form_sanitizer($_POST['news_body'], "", "news_body"),
+                'news_subject'     => form_sanitizer($_POST['news_subject'], "", 'news_subject'),
+                'news_language'    => form_sanitizer($_POST['news_language'], "", 'news_language'),
+                'news_image_align' => form_sanitizer($_POST['news_image_align'], "", 'news_image_align'),
+                'news_keywords'    => form_sanitizer($_POST['news_keywords'], "", 'news_keywords'),
+                'news_cat'         => form_sanitizer($_POST['news_cat'], 0, 'news_cat'),
+                'news_news'        => form_sanitizer($_POST['news_news'], "", 'news_news'),
+                'news_extended'    => form_sanitizer($_POST['news_extended'], "", 'news_extended'),
             );
+            ob_start();
             echo strtr(display_news_submissions_preview(), [
                 '{%title%}'        => $locale['news_0400'],
                 '{%news_subject%}' => $criteriaArray['news_subject'],
                 '{%snippet%}'      => "<strong>".$locale['news_0203']."</strong><br/>".nl2br($news_snippet),
                 '{%full_text%}'    => "<strong>".$locale['news_0204']."</strong><br/>".nl2br($news_body),
             ]);
+            $modal = openmodal('news_preview', $locale['preview_news']);
+            $modal .= ob_get_clean();
+            $modal .= closemodal();
+            add_to_footer($modal);
         }
 
         $info = [
@@ -124,22 +144,33 @@ if (iMEMBER && $news_settings['news_allow_submission']) {
             ),
             'news_image_field'       => ($news_settings['news_allow_submission_files'] ? form_fileinput('news_image', $locale['news_0216'], '',
                 [
-                    'upload_path'  => IMAGES_N, 'max_width' => $news_settings['news_photo_max_w'],
-                    'max_height'   => $news_settings['news_photo_max_h'], 'max_byte' => $news_settings['news_photo_max_b'],
+                    'upload_path'      => IMAGES_N,
+                    'max_width'        => $news_settings['news_photo_max_w'],
+                    'max_height'       => $news_settings['news_photo_max_h'],
+                    'max_byte'         => $news_settings['news_photo_max_b'],
                     // set thumbnail
-                    'thumbnail'    => 1, 'thumbnail_w' => $news_settings['news_thumb_w'],
-                    'thumbnail_h'  => $news_settings['news_thumb_h'], 'thumbnail_folder' => 'thumbs', 'delete_original' => 0,
+                    'thumbnail'        => 1,
+                    'thumbnail_w'      => $news_settings['news_thumb_w'],
+                    'thumbnail_h'      => $news_settings['news_thumb_h'],
+                    'thumbnail_folder' => 'thumbs',
+                    'delete_original'  => 0,
                     // set thumbnail 2 settings
-                    'thumbnail2'   => 1, 'thumbnail2_w' => $news_settings['news_photo_w'],
-                    'thumbnail2_h' => $news_settings['news_photo_h'], 'type' => 'image', 'inline' => TRUE,
-                    'ext_tip'      => sprintf($locale['news_0217'], parsebytesize($news_settings['news_photo_max_b'])),
+                    'thumbnail2'       => 1,
+                    'thumbnail2_w'     => $news_settings['news_photo_w'],
+                    'thumbnail2_h'     => $news_settings['news_photo_h'],
+                    'type'             => 'image',
+                    'inline'           => TRUE,
+                    'ext_tip'          => sprintf($locale['news_0217'], parsebytesize($news_settings['news_photo_max_b'])),
                 ]
             ) : ''),
-            'news_image_align_field' => ($news_settings['news_allow_submission_files'] ? form_select('news_ialign', $locale['news_0218'], $criteriaArray['news_ialign'],
+            'news_image_align_field' => ($news_settings['news_allow_submission_files'] ? form_select('news_image_align', $locale['news_0218'], $criteriaArray['news_image_align'],
                 [
-                    'options'   => array(
-                        'pull-left' => $locale['left'], 'news-img-center' => $locale['center'], 'pull-right' => $locale['right']
-                    ), 'inline' => TRUE
+                    'options' => array(
+                        'pull-left'       => $locale['left'],
+                        'news-img-center' => $locale['center'],
+                        'pull-right'      => $locale['right']
+                    ),
+                    'inline'  => TRUE
                 ]
             ) : ''),
             'news_news_field'        => form_textarea('news_news', $locale['news_0203'], $criteriaArray['news_snippet'],
@@ -151,7 +182,7 @@ if (iMEMBER && $news_settings['news_allow_submission']) {
                     'form_name' => 'submit_form',
                 ]
             ),
-            'news_body_field'        => form_textarea('news_body', $locale['news_0203b'], $criteriaArray['news_body'],
+            'news_body_field'        => form_textarea('news_extended', $locale['news_0203b'], $criteriaArray['news_extended'],
                 [
                     'required'  => $news_settings['news_extended_required'] ? TRUE : FALSE,
                     'type'      => fusion_get_settings('tinymce_enabled') ? 'tinymce' : 'html',
