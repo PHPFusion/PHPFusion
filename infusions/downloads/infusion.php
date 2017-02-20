af826b3a2116b5cffe544d9f3dd929d688b634eb
chan <meangczac.chan@gmail.com>
2017-02-20 16:33:26 +0800

    Update Download Icon

--- infusions/downloads/infusion.php
+++ infusions/downloads/infusion.php
@@ -29,7 +29,7 @@ $inf_developer = "PHP Fusion Development Team";
 $inf_email = "info@php-fusion.co.uk";
 $inf_weburl = "https://www.php-fusion.co.uk";
 $inf_folder = "downloads";
-$inf_image = "download.png";
+$inf_image = "download.svg";
 
 // Multilanguage table for Administration
 $inf_mlt[] = array(
