f807a7bb9f0fe1305cc64a4e4f5951a4330e29f9
chan <meangczac.chan@gmail.com>
2017-02-20 08:43:40 +0800

    Add User Field - Vimeo

--- includes/user_fields/user_vimeo_include.php
+++ includes/user_fields/user_vimeo_include.php
@@ -4,7 +4,7 @@
 | Copyright (C) PHP-Fusion Inc
 | https://www.php-fusion.co.uk/
 +--------------------------------------------------------+
-| Filename: user_facebook_include.php
+| Filename: user_vimeo_include.php
 | Author: PHP-Fusion Development Team
 +--------------------------------------------------------+
 | This program is released as free software under the
@@ -19,23 +19,23 @@ if (!defined("IN_FUSION")) {
     die("Access Denied");
 }
 
-$icon = "<img src='".IMAGES."user_fields/social/facebook.svg'/>";
+$icon = "<img src='".IMAGES."user_fields/social/vimeo.svg'>\n";
 // Display user field input
 if ($profile_method == "input") {
-    $user_fields = form_text('user_facebook', $locale['uf_facebook'], $field_value, [
-        'inline'      => TRUE,
-        'placeholder' => $locale['uf_facebook_placeholder'],
-        'error_text'  => $locale['uf_facebook_error'],
-        'label_icon'  => $icon,
-        ] + $options);
+    $options = array(
+            'inline'           => TRUE,
+            'error_text'       => $locale['uf_vimeo_error'],
+            'regex_error_text' => $locale['uf_vimeo_error_1'],
+            'placeholder'      => $locale['uf_vimeo'],
+            'label_icon'       => $icon
+        ) + $options;
+    $user_fields = form_text('user_vimeo', $locale['uf_vimeo'], $field_value, $options);
 // Display in profile
 } elseif ($profile_method == "display") {
+    //https://vimeo.com/user1577007
     if ($field_value) {
-        $field_value = !preg_match("@^http(s)?\:\/\/@i", $field_value) ? "https://www.facebook.com/".$field_value : $field_value;
+        $field_value = !preg_match("@^http(s)?\:\/\/@i", $field_value) ? "https://www.vimeo.com/".$field_value : $field_value;
         $field_value = (fusion_get_settings('index_url_userweb') ? "" : "<!--noindex-->")."<a href='".$field_value."' title='".$field_value."' ".(fusion_get_settings('index_url_userweb') ? "" : "rel='nofollow' ")."target='_blank'>".$locale['uf_facebook_desc']."</a>".(fusion_get_settings('index_url_userweb') ? "" : "<!--/noindex-->");
     }
-    $user_fields = array(
-        'title' => $icon.$locale['uf_facebook'],
-        'value' => $field_value ? : ''
-    );
+    $user_fields = array('title' => $icon.$locale['uf_vimeo'], 'value' => $field_value ?: '');
 }
