fd3a28f5770bfea23d65ab5204f487e3df56403d
chan <meangczac.chan@gmail.com>
2017-03-03 22:35:57 +0800

    Add Install

---
+++ install.php
@@ -0,0 +1,22 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: install.php
+| Author: Frederick MC Chan (Chan)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+define('BASEDIR', '');
+define("IN_FUSION", true);
+require_once __DIR__.'/includes/autoloader.php';
+// Start the installer
+PHPFusion\Installer\Install_Core::getInstance()->install_phpfusion();
