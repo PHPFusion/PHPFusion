496a7e89a2f96785e4758760e608cc841e1c3044
chan <meangczac.chan@gmail.com>
2017-03-06 10:43:54 +0800

    Locale FAQ reindex keys (cleanup)

--- infusions/faq/locale/English/faq.php
+++ infusions/faq/locale/English/faq.php
@@ -1,5 +1,4 @@
 <?php
-// Faq titles
 $locale['faq_0000'] = "FAQs";
 $locale['faq_0001'] = "Categories";
 $locale['faq_0002'] = "Top";
@@ -11,8 +10,6 @@ $locale['faq_0007'] = "Add FAQ Category";
 $locale['faq_0008'] = "Edit FAQ Category";
 $locale['faq_0009'] = "FAQ Category";
 $locale['faq_0010'] = "Uncategorized";
-
-// Messages - Faq
 $locale['faq_0030'] = "FAQ successfully created";
 $locale['faq_0031'] = "FAQ successfully updated";
 $locale['faq_0032'] = "FAQ successfully deleted";
@@ -22,40 +19,29 @@ $locale['faq_0035'] = "FAQ cannot be deleted";
 $locale['faq_0036'] = "There are FAQs or Subcategories in the Category";
 $locale['faq_0037'] = "FAQ successfully published";
 $locale['faq_0038'] = "FAQ publishing deleted";
-
 $locale['faq_0039'] = "FAQ Category created";
 $locale['faq_0040'] = "FAQ Category updated";
 $locale['faq_0041'] = "FAQ Category deleted";
 $locale['faq_0042'] = "FAQ Category name must be unique";
-
-// Messages - Submissions
 $locale['faq_0060'] = "Submission successfully published";
 $locale['faq_0061'] = "Submission successfully saved as Draft";
 $locale['faq_0062'] = "Submission successfully deleted";
 $locale['faq_0063'] = "There are no new Submissions.";
-
-// Table - Faq
 $locale['faq_0100'] = "Question";
 $locale['faq_0102'] = "Draft";
 $locale['faq_0105'] = "Author";
 $locale['faq_0106'] = "Visibility";
 $locale['faq_0107'] = "Actions";
-
-// Table - Faq - Misc
 $locale['faq_0111'] = "Delete this FAQ?";
 $locale['faq_0112'] = "There are no FAQ defined.";
 $locale['faq_0113'] = "There are no FAQ, which match with the Searchoptions.";
 $locale['faq_0114'] = "Before you can create a FAQ, you must create a FAQ Category.";
-
 $locale['faq_0115'] = "Category Name";
 $locale['faq_0116'] = "Category Description";
 $locale['faq_0117'] = "Language";
 $locale['faq_0118'] = "Save Category";
 $locale['faq_0119'] = "Add Category";
-
-// Table - Faq - Sorting
 $locale['faq_0120'] = "Search";
-
 $locale['faq_0122'] = "Clear";
 $locale['faq_0123'] = "Choose Status";
 $locale['faq_0124'] = "All Status";
@@ -65,15 +51,11 @@ $locale['faq_0129'] = "All Languages";
 $locale['faq_0130'] = "Choose Author";
 $locale['faq_0131'] = "All Authors";
 $locale['faq_0132'] = "Items per Page";
-
-// Table - Submissions
 $locale['faq_0200'] = "ID";
 $locale['faq_0202'] = "Submissioner";
 $locale['faq_0203'] = "Date";
 $locale['faq_0204'] = "Options";
 $locale['faq_0205'] = "Review Submission";
-
-// Form - Faq
 $locale['faq_0251'] = "Answer";
 $locale['faq_0252'] = "Category";
 $locale['faq_0253'] = "The FAQs self.";
@@ -82,30 +64,20 @@ $locale['faq_0256'] = "Enable automatic Linebreaks";
 $locale['faq_0257'] = "Update Publication Date";
 $locale['faq_0258'] = "More Settings";
 $locale['faq_0259'] = "More Informations";
-
-// Form - Faq - Errors
 $locale['faq_0270'] = "You must specify a FAQs.";
 $locale['faq_0271'] = "You must specify a Question.";
 $locale['faq_0272'] = "You must specify a FAQ.";
-$locale['faq_0274'] = "%s are disabled for all items.\n\rThis setting will only take effect when they are enabled again.";
+$locale['faq_0274'] = "%s are disabled for all items.
 
-// Form - Submissions
+This setting will only take effect when they are enabled again.";
 $locale['faq_0350'] = "The submission above was sent from {%SUBMISSION_AUTHOR%}.";
 $locale['faq_0351'] = "Submission Date: {%SUBMISSION_TIME%} - {%SUBMISSION_DATE%}.";
-
-// Settings
 $locale['faq_0400'] = "Settings for the FAQs System.";
-
-// Submissions - Titles
 $locale['faq_0900'] = "Submit FAQ";
-
-// Submissions - Success Submission
 $locale['faq_0910'] = "FAQ is successfully submitted.";
 $locale['faq_0911'] = "Thank you for the Submission.";
 $locale['faq_0912'] = "Submit other FAQs";
 $locale['faq_0913'] = "Back to [SITENAME]";
-
-// Submissions - Other
 $locale['faq_0920'] = "Use the following form to submit an FAQ. Your submission will be reviewed by an
 Administrator. [SITENAME] reserves the right to amend or edit any submission. FAQ
 should be applicable to the content of this site. Unsuitable submissions will be deleted.";
