14f0a9e2d18c8001e54c2b57c54404d9f4772f92
chan <meangczac.chan@gmail.com>
2017-03-07 19:33:46 +0800

    Weblinks locale (cleanup)

--- infusions/weblinks/locale/English/weblinks_admin.php
+++ infusions/weblinks/locale/English/weblinks_admin.php
@@ -1,5 +1,4 @@
 <?php
-// Tabs and Breadcrumbs
 $locale['WLS_0001'] = "Web Links";
 $locale['WLS_0002'] = "Add Web Link";
 $locale['WLS_0003'] = "Edit Web Link";
@@ -8,8 +7,6 @@ $locale['WLS_0005'] = "Add Category";
 $locale['WLS_0006'] = "Edit Category";
 $locale['WLS_0007'] = "Submissions";
 $locale['WLS_0008'] = "Settings";
-
-// Messages - Web Link
 $locale['WLS_0030'] = "Web Link successfully created";
 $locale['WLS_0031'] = "Web Link successfully updated";
 $locale['WLS_0032'] = "Web Link successfully deleted";
@@ -17,8 +14,6 @@ $locale['WLS_0033'] = "Web Links successfully updated";
 $locale['WLS_0034'] = "You must choose at least one Web Link";
 $locale['WLS_0035'] = "Web Link successfully published";
 $locale['WLS_0036'] = "Web Link publishing deleted";
-
-// Messages - Web Link Cats
 $locale['WLS_0040'] = "Category successfully created";
 $locale['WLS_0041'] = "Category successfully updated";
 $locale['WLS_0042'] = "Category successfully deleted";
@@ -30,28 +25,20 @@ $locale['WLS_0047'] = "There are  Web Links or Subcategories in the Category";
 $locale['WLS_0048'] = "You must choose at least on Category";
 $locale['WLS_0049'] = "Category successfully published";
 $locale['WLS_0050'] = "Category publishing deleted";
-
-// Messages - Submissions
 $locale['WLS_0060'] = "Submission successfully published";
 $locale['WLS_0061'] = "Submission successfully deleted";
 $locale['WLS_0062'] = "There are no new Submissions.";
-
-// Table - Web Link
 $locale['WLS_0100'] = "Name";
 $locale['WLS_0101'] = "Category";
 $locale['WLS_0102'] = "Status";
 $locale['WLS_0103'] = "Visibility";
 $locale['WLS_0104'] = "Actions";
-
-// Table - Web Link - Misc
 $locale['WLS_0111'] = "Delete this Web Link?";
 $locale['WLS_0112'] = "There are no Weblink created.";
 $locale['WLS_0113'] = "There are no Web Links which match the Search options.";
 $locale['WLS_0114'] = "Before you can create a Web Link, you must create a Category.";
 $locale['WLS_0115'] = "Web Link is checked, broken links found %s.";
 $locale['WLS_0116'] = "Status Unpublished update.";
-
-// Table - Web Link - Sorting
 $locale['WLS_0120'] = "Subject";
 $locale['WLS_0121'] = "Search";
 $locale['WLS_0122'] = "Clear";
@@ -63,23 +50,15 @@ $locale['WLS_0127'] = "All Categories";
 $locale['WLS_0128'] = "Choose Language";
 $locale['WLS_0129'] = "All Languages";
 $locale['WLS_0132'] = "Items per Page";
-
-// Table - Web Link Cats
 $locale['WLS_0151'] = "Web Link";
-
-// Table - Web Link - Misc
 $locale['WLS_0161'] = "Delete this Category?";
 $locale['WLS_0162'] = "There are no Categories defined.";
-
-// Table - Submissions
 $locale['WLS_0200'] = "ID";
 $locale['WLS_0201'] = "Link Name";
 $locale['WLS_0202'] = "Submissioner";
 $locale['WLS_0203'] = "Date";
 $locale['WLS_0204'] = "Options";
 $locale['WLS_0205'] = "Review Submission";
-
-// Form - Web Link
 $locale['WLS_0252'] = "A Name of the Web Link.";
 $locale['WLS_0253'] = "Web Link URL";
 $locale['WLS_0254'] = "Descrition";
@@ -87,39 +66,23 @@ $locale['WLS_0255'] = "Please enter the description";
 $locale['WLS_0259'] = "Update Publication Date";
 $locale['WLS_0260'] = "More Informations";
 $locale['WLS_0261'] = "Web Links Verify";
-
-// Form - Web Link - Errors
 $locale['WLS_0270'] = "You must specify a Subject.";
-
-// Form - Web Link Cats
 $locale['WLS_0303'] = "Parent";
 $locale['WLS_0304'] = "Description";
-
-// Form - Web Link - Error
 $locale['WLS_0320'] = "You must specify a Name.";
 $locale['WLS_0321'] = "There exists a Category named this one.";
 $locale['WLS_0322'] = "You must specify a Description.";
-
-// Form - Submissions
 $locale['WLS_0350'] = "The submission above was sent from {%SUBMISSION_AUTHOR%}.";
 $locale['WLS_0351'] = "Submission Date: {%SUBMISSION_TIME%} - {%SUBMISSION_DATE%}.";
-
-// Settings
 $locale['WLS_0400'] = "Settings for the Web Link System in the Frontend and Backend.";
 $locale['WLS_0403'] = "Extended Web Link required";
-
-// Submissions - Titles
 $locale['WLS_0900'] = "Submit Weblink";
-
-// Submissions - Success Submission
 $locale['WLS_0910'] = "Web Link is successfully submitted.";
 $locale['WLS_0911'] = "Thank you for the Submission.";
 $locale['WLS_0912'] = "Submit other Web Link";
 $locale['WLS_0913'] = "Back to [SITENAME]";
-
-// Submissions - Other
 $locale['WLS_0920'] = "Use the following form to submit Web Link. Your submission will be reviewed by an
-Administrator. [SITENAME] reserves the right to amend or edit any submission. Web Link
-should be applicable to the content of this site. Unsuitable submissions will be deleted.";
+Administrator. [SITENAME] reserves the right to amend or edit any submission. Web Link should be applicable to the content of this site. 
+Unsuitable submissions will be deleted.";
 $locale['WLS_0922'] = "Sorry, we currently do not accept any Web Link submissions on this site.";
 $locale['WLS_0923'] = "You cannot submit a Web Link, because there are no Categories defined.";
