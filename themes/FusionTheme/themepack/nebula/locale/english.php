2c46bf7875b5e3fc707bf5fbd2a74f65ec9e23f5
karrak1 <g.sebestyen@fusionjatek.hu>
2017-03-21 17:26:29 +0100

    Create english.php

---
+++ themes/FusionTheme/themepack/nebula/locale/english.php
@@ -0,0 +1,3 @@
+$locale['NB_0000'] = "The Nebula is a PHP-Fusion 9's first FusionTheme Theme Framework made offering many content elements, styles and features and to better understand and learn to build content using the PHP-Fusion 9, without coding.";
+$locale['NB_0001'] = "About Us";
+$locale['NB_0002'] = "Recents posts";
