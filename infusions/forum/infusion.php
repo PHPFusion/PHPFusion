ddab775866d1dbdf1f342c50b32b073d45e21fdb
Frederick Chan Meang Czac <meangczac.chan@gmail.com>
2017-04-14 13:49:53 +0800

    Fix #1597

--- infusions/forum/infusion.php
+++ infusions/forum/infusion.php
@@ -300,8 +300,7 @@ if (function_exists("fusion_get_enabled_languages")) {
 if (!empty($enabled_languages)) {
     foreach ($enabled_languages as $language) {
 
-        $locale = fusion_get_locale("", LOCALE.$language."/setup.php");
-        $locale += fusion_get_locale("", INFUSIONS."forum/locale/".LOCALESET."/forum_tags.php");
+        $locale = fusion_get_locale("", [LOCALE.$language."/setup.php", FORUM.'locale/'.$language.'/forum_tags.php']);
 
         $mlt_insertdbrow[$language][] = DB_SITE_LINKS." (link_name, link_url, link_visibility, link_position, link_window, link_order, link_status, link_language) VALUES ('".$locale['setup_3304']."', 'infusions/forum/index.php', '0', '2', '0', '5', '1', '".$language."')";
         $mlt_insertdbrow[$language][] = DB_SITE_LINKS." (link_cat, link_name, link_url, link_visibility, link_position, link_window, link_order, link_status, link_language) VALUES ('{last_id}', '".$locale['setup_3324']."', 'infusions/forum/newthread.php', '0', '2', '-101', '1', '1', '".$language."')";
