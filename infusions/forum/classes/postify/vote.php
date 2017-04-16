4d975555b5ab8c331327b4372a2525d231c677ab
Frederick Chan Meang Czac <meangczac.chan@gmail.com>
2017-04-16 22:40:56 +0800

    #1628

--- infusions/forum/classes/postify/vote.php
+++ infusions/forum/classes/postify/vote.php
@@ -48,7 +48,6 @@ class Postify_Vote extends Forum_Postify {
             // i can upvote as many post but each post only once.
             $thread_data['thread_link'] = fusion_get_settings('siteurl')."infusions/forum/viewthread.php?forum_id=".$thread_data['forum_id']."&thread_id=".$thread_data['thread_id']."&pid=".$thread_data['thread_lastpostid']."#post_".$thread_data['thread_lastpostid'];
             $forum_index = dbquery_tree(DB_FORUMS, 'forum_id', 'forum_cat');
-
             if ($this->check_forum_access($forum_index, $_GET['forum_id'], $_GET['thread_id'])) {
                 $d = [
                     'forum_id'       => $thread_data['forum_id'],
@@ -141,7 +140,9 @@ class Postify_Vote extends Forum_Postify {
                 } else {
                     addNotice('danger', self::$locale['forum_0520a'], 'viewthread.php');
                 }
+                print_p(self::$default_redirect_link);
                 redirect(self::$default_redirect_link);
+
             } else {
                 redirect(FORUM.'index.php');
             }
