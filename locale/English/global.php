7deab106cceedbe4d34e9b638c735b17afef64e7
Frederick Chan Meang Czac <meangczac.chan@gmail.com>
2017-04-17 17:52:22 +0800

    Locale File Please Update -- global.php - add $locale['view'] = "View (translated)" for Fix #1649

--- locale/English/global.php
+++ locale/English/global.php
@@ -457,6 +457,7 @@ $locale['close'] = "Close";
 $locale['nopreview'] = "There is nothing to Preview";
 $locale['mark_as'] = "Mark As";
 $locale['preview'] = "Preview";
+$locale['view'] = "View";
 $locale['custom'] = "Custom";
 // Alignment
 $locale['left'] = "Left";
