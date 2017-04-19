ef496fba7a2c81391334909a24e18f2ebdb4e7e5
karrak1 <g.sebestyen@fusionjatek.hu>
2017-04-19 18:37:29 +0200

    localize geomap
    localize geomap

--- includes/geomap/form_geomap.json.php
+++ includes/geomap/form_geomap.json.php
@@ -45,7 +45,7 @@ require_once $level."maincore.php";
 require_once INCLUDES."geomap/geomap.inc.php";
 $id = (isset($_GET['id']) && ($_GET['id'])) ? form_sanitizer($_GET['id'], "") : '';
 //$id = "Malaysia";
-$states_array[] = array("id" => "Other", "text" => "Other States");
+$states_array[] = array("id" => "Other", "text" => $locale['states']);
 foreach ($states as $key => $value) {
 	if ($id == $key) {
 		if (!empty($value)) {
