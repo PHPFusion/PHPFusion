4d2da7117fafd16e0bc15153962598231c029005
RobiNN1 <robokelcak@centrum.sk>
2017-04-19 18:02:42 +0200

    Fix #1670

--- themes/templates/global/groups.php
+++ themes/templates/global/groups.php
@@ -25,7 +25,7 @@ if (!function_exists('render_user_group')) {
         $locale = fusion_get_locale('', LOCALE.LOCALESET."user_fields.php");
         opentable($locale['u057']);
         echo "<div class='text-center well'>";
-        echo "<h4>".($info['group_icon'] ? "<i class='".$info['group_icon']."'></i> " : "").$info['group_name']." ".format_word($info['total_rows'], $locale['fmt_member'])."</h4>\n";
+        echo "<h4>".(!empty($info['group_icon']) ? "<i class='".$info['group_icon']."'></i> " : "").(!empty($info['group_name']) ? $info['group_name'] : '')." ".format_word($info['total_rows'], $locale['fmt_member'])."</h4>\n";
         echo "</div>\n";
         $sort_plugin = fusion_sort_table('groupTbl');
         echo "<table id='groupTbl' class='table table-responsive table-hover $sort_plugin'>\n";
