20818acede29bb0954fa6d114ab902ef55efefb6
Frederick Chan Meang Czac <meangczac.chan@gmail.com>
2017-04-23 10:48:45 +0800

    Fix #1720

--- themes/templates/layout.php
+++ themes/templates/layout.php
@@ -70,8 +70,8 @@ if (function_exists("get_head_tags")) {
 if (!file_exists(INCLUDES.'jquery/jquery.min.js')) {
     echo "<script type='text/javascript' src='https://code.jquery.com/jquery-2.2.4.min.js'></script>\n";
 }
-echo "<script type='text/javascript' src='".fusion_get_settings('siteurl')."includes/jquery/jquery.min.js'><\/script>\n";
-echo "<script type='text/javascript' src='".INCLUDES."jscripts/jscript.js'></script>\n"; // Use .min.js only in production manually
+echo "<script type='text/javascript' src='".INCLUDES."jquery/jquery.min.js'></script>\n";
+echo "<script type='text/javascript' src='".INCLUDES."jscripts/jscript.js'></script>\n";
 echo "</head>\n";
 
 /**
