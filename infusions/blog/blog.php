e7c2353c05166670eb42bd80f9f1e763bbc6b028
Frederick Chan Meang Czac <meangczac.chan@gmail.com>
2017-04-24 21:32:10 +0800

    Fix Blog (Backslash/Slash/Image-Inserts-Textarea-NonSEO/Image-Inserts-Textarea-SEO)

--- infusions/blog/blog.php
+++ infusions/blog/blog.php
@@ -178,10 +178,6 @@ if (!empty($_GET['readmore']) && isnum($_GET['readmore'])) {
             }
             $item['blog_blog'] = parse_textarea($item['blog_blog'], FALSE, FALSE, TRUE, FALSE, $item['blog_breaks'] == "y" ? TRUE : FALSE);
             $item['blog_extended'] = parse_textarea($item['blog_extended'], FALSE, FALSE, TRUE, FALSE, $item['blog_breaks'] == "y" ? TRUE : FALSE);
-            if (defined('IN_PERMALINK')) {
-                $item['blog_blog'] = strtr($item['blog_blog'], [fusion_get_settings('site_path') => '']);
-                $item['blog_extended'] = strtr($item['blog_extended'], [fusion_get_settings('site_path') => '']);
-            }
 
             $item['blog_image_link'] = '';
             $item['blog_thumb_1_link'] = '';
@@ -461,10 +457,6 @@ if (!empty($_GET['readmore']) && isnum($_GET['readmore'])) {
 
             $blog_blog = parse_textarea($data['blog_blog'], FALSE, FALSE, TRUE, FALSE, $data['blog_breaks'] == 'y' ? TRUE : FALSE);
             $blog_extended = parse_textarea($data['blog_extended'], FALSE, FALSE, TRUE, FALSE, $data['blog_breaks'] == 'y' ? TRUE : FALSE);
-            if (defined('IN_PERMALINK')) {
-                $blog_blog = strtr($blog_blog, [fusion_get_settings('site_path') => '']);
-                $blog_extended = strtr($blog_extended, [fusion_get_settings('site_path') => '']);
-            }
 
             $cdata = array(
                 'blog_ialign'            => $data['blog_ialign'] == 'center' ? 'clearfix' : $data['blog_ialign'],
