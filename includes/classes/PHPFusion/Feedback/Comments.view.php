fbb293792c36f5c6ec69da90f96b37d287eef561
RobiNN <kelcakrobo@gmail.com>
2017-05-10 17:33:23 +0200

    Fix #1802

--- includes/classes/PHPFusion/Feedback/Comments.view.php
+++ includes/classes/PHPFusion/Feedback/Comments.view.php
@@ -92,8 +92,9 @@ if (!function_exists('display_comments_list')) {
                     <div class='comment_title'><!--comment_subject-->{%comment_subject%}<!--//comment_subject--></div><?php endif; ?>
                 <div class='comment_message'><!--comment_message-->{%comment_message%}<!--//comment_message--></div>
                 <div>
-                    <small>{%comment_reply_link%}
-                     <?php echo !empty($info['edit_link']) ? ' &middot; {%comment_edit_link%}' : '';
+                    <small><?php
+                     echo !empty($info['reply_link']) ? '{%comment_reply_link%}' : '';
+                     echo !empty($info['edit_link']) ? ' &middot; {%comment_edit_link%}' : '';
                      echo !empty($info['delete_link']) ? ' &middot; {%comment_delete_link%}' : ''; ?>
                      - <span class='comment_date'>{%comment_date%}</span></small>
                 </div>
