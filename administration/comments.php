eb4d59808ef7639694e22b9e7917c37cb8372454
karrak1 <g.sebestyen@fusionjatek.hu>
2017-05-23 12:53:09 +0200

    comments git

--- administration/comments.php
+++ administration/comments.php
@@ -32,61 +32,61 @@ if (!isset($_GET['comment_item_id']) || !isnum($_GET['comment_item_id'])) {
 \PHPFusion\BreadCrumbs::getInstance()->addBreadCrumb(['link'=> ADMIN.'comments.php'.fusion_get_aidlink(), "title"=> $locale['401']]);
 
 if (isset($_POST['save_comment']) && (isset($_GET['comment_id']) && isnum($_GET['comment_id']))) {
-    $comment_message = stripinput($_POST['comment_message']);
-    $result = dbquery("UPDATE ".DB_COMMENTS." SET comment_message='$comment_message' WHERE comment_id='".$_GET['comment_id']."'");
+    $comment_message = form_sanitizer($_POST['comment_message'], '', 'comment_message');
+    $result = dbquery("UPDATE ".DB_COMMENTS." SET comment_message='".$comment_message."' WHERE comment_id='".$_GET['comment_id']."'");
     addNotice('success', $locale['410']);
-    redirect("comments.php".$aidlink."&ctype=".$_GET['ctype']."&comment_item_id=".$_GET['comment_item_id']);
+    redirect(FUSION_REQUEST);
 }
 
 if ((isset($_GET['action']) && $_GET['action'] == "delete") && (isset($_GET['comment_id']) && isnum($_GET['comment_id']))) {
     $result = dbquery("DELETE FROM ".DB_COMMENTS." WHERE comment_id='".$_GET['comment_id']."'");
     addNotice('success', $locale['411']);
-    redirect("comments.php".$aidlink."&ctype=".$_GET['ctype']."&comment_item_id=".$_GET['comment_item_id']);
+    redirect(FUSION_REQUEST);
 }
 
 if ((isset($_GET['action']) && $_GET['action'] == "edit") && (isset($_GET['comment_id']) && isnum($_GET['comment_id']))) {
     $result = dbquery("SELECT comment_message FROM ".DB_COMMENTS." WHERE comment_id='".$_GET['comment_id']."'");
     if (dbrows($result)) {
-        require_once INCLUDES."bbcode_include.php";
+
         $data = dbarray($result);
         opentable($locale['400']);
-        $form_action = FUSION_SELF.$aidlink."&amp;comment_id=".$_GET['comment_id']."&amp;ctype=".$_GET['ctype']."&amp;comment_item_id=".$_GET['comment_item_id'];
+        $form_action = FUSION_SELF.fusion_get_aidlink()."&amp;comment_id=".$_GET['comment_id']."&amp;ctype=".$_GET['ctype']."&amp;comment_item_id=".$_GET['comment_item_id'];
         echo openform('settingsform', 'post', $form_action);
         echo form_textarea('comment_message', '', $data['comment_message'],
                            array('autosize' => TRUE, 'bbcode' => TRUE, 'preview' => TRUE, 'form_name' => 'settingsform'));
-        echo form_button('save_comment', $locale['421'], $locale['421'], array('class' => 'btn-primary btn-sm'));
+        echo form_button('save_comment', $locale['421'], $locale['421'], array('class' => 'btn-primary'));
         closeform();
         closetable();
     }
 }
 
-opentable($locale['401']);
-$i = 0;
-$result = dbquery("SELECT c.comment_id, c.comment_name, c.comment_message, c.comment_datestamp, c.comment_ip, u.user_id, u.user_name, u.user_status FROM ".DB_COMMENTS." c
-	LEFT JOIN ".DB_USERS." u
-	ON c.comment_name=u.user_id
-	WHERE c.comment_type='".$_GET['ctype']."' AND c.comment_item_id='".$_GET['comment_item_id']."' ORDER BY c.comment_datestamp ASC");
+opentable(\PHPFusion\Admins::getInstance()->getCommentType($_GET['ctype'])." ".$locale['401']);
+$ndata = [
+		':type'       => $_GET['ctype'],
+		':item_id'    => $_GET['comment_item_id'],
+		];
+$comments_query = "SELECT
+			c.comment_id, c.comment_name, c.comment_message, c.comment_datestamp, c.comment_ip, c.comment_type, u.user_id, u.user_name, u.user_status
+			FROM ".DB_COMMENTS." AS c
+			LEFT JOIN ".DB_USERS." AS u ON c.comment_name=u.user_id
+			WHERE c.comment_type=:type AND c.comment_item_id=:item_id
+			ORDER BY c.comment_datestamp ASC";
+$result = dbquery($comments_query, $ndata);
+
 if (dbrows($result)) {
     while ($data = dbarray($result)) {
         echo "<div class='list-group-item'>\n";
         echo "<div class='btn-group pull-right'>\n";
-        echo "<a class='btn btn-xs btn-default' href='".FUSION_SELF.$aidlink."&amp;action=edit&amp;comment_id=".$data['comment_id']."&amp;ctype=".$_GET['ctype']."&amp;comment_item_id=".$_GET['comment_item_id']."'>".$locale['430']."</a>";
-        echo "<a class='btn btn-xs btn-default' href='".FUSION_SELF.$aidlink."&amp;action=delete&amp;comment_id=".$data['comment_id']."&amp;ctype=".$_GET['ctype']."&amp;comment_item_id=".$_GET['comment_item_id']."' onclick=\"return confirm('".$locale['433']."');\">".$locale['431']."</a>";
+        echo "<a class='btn btn-xs btn-default' href='".FUSION_REQUEST."&amp;action=edit&amp;comment_id=".$data['comment_id']."'>".$locale['430']."</a>";
+        echo "<a class='btn btn-xs btn-default' href='".FUSION_REQUEST."&amp;action=delete&amp;comment_id=".$data['comment_id']."' onclick=\"return confirm('".$locale['433']."');\">".$locale['431']."</a>";
         echo "</div>\n";
-        echo "<span class='comment-name'>";
-        if ($data['user_name']) {
-            echo "<span class='strong'>".profile_link($data['comment_name'], $data['user_name'], $data['user_status'])."</span>";
-        } else {
-            echo $data['comment_name'];
-        }
-        echo "</span>\n<span>".$locale['global_071'].showdate("longdate",
-                                                              $data['comment_datestamp'])."</span><span class='label label-default m-l-10'>".$locale['432']." ".$data['comment_ip']."</span>\n<br />\n ";
-        echo "<div class='m-t-10'>\n".nl2br(parseubb(parsesmileys($data['comment_message'])))."<br />\n</div>\n";
+        echo "<strong>".($data['user_name'] ? profile_link($data['comment_name'], $data['user_name'], $data['user_status']) : $data['comment_name'])."</strong>\n";
+        echo "<span>".$locale['global_071'].showdate("longdate", $data['comment_datestamp'])."</span><span class='label label-default m-l-10'>".$locale['432']." ".$data['comment_ip']."</span>\n<br />\n ";
+        echo "<div class='m-t-10'>".nl2br(parseubb(parsesmileys($data['comment_message'])))."<hr>\n</div>\n";
         echo "</div>\n";
-        $i++;
     }
 } else {
-    echo "<div style='text-align:center'><br />".$locale['434']."<br /><br /></div>\n";
+    echo "<div class='alert alert-info text-center'>".$locale['434']."</div>\n";
 }
 closetable();
 
