25effe98e8d253bb1a80575184d82ed412bf6e1e
Frederick Chan Meang Czac <meangczac.chan@gmail.com>
2017-05-24 16:15:32 +0800

    Update user_gender

--- includes/user_fields/user_gender_include.php
+++ includes/user_fields/user_gender_include.php
@@ -19,28 +19,48 @@ if (!defined("IN_FUSION")) {
     die("Access Denied");
 }
 
-$gen_set = 1; //0 = Just text; 1 = Emotikum + text; 2 = text + image; 3 = Just images.
-$locale['uf_kep'] = ['0' => "",
-                     '1' => ["fa fa-times-circle-o", "fa fa-mars", "fa fa-venus", "fa fa-user-secret"], // Emotikum megadás
-                     '2' => ["no", "male", "female", "no"],  // image name
-                     '3' => ["no", "male", "female", "no"]  // image name
-];
+// Variation Customization
+$gen_set = 2; //0 = Just text; 1 = Emotikum + text; 2 = text + image; 3 = Just images.
+$with_secret = FALSE; // True for additiona unspecified option
+$input_type = 'form_checkbox'; // form_select or form_checkbox (up to you)
 
+// Definitions
+$locale['uf_kep'] = [
+    '0' => '',
+    '1' => ["fa fa-times-circle-o", "fa fa-mars", "fa fa-venus", "fa fa-user-secret"], // Emotikum megadás
+    '2' => ["no", "male", "female", "no"],  // image name
+    '3' => ["no", "male", "female", "no"]  // image name
+];
 for ($i = 0; $i < count($locale['uf_gender_sz']); $i++) {
-    $locale['uf_gender_szkep'][] = ($gen_set == 0 ? $locale['uf_gender_sz'][$i] : ($gen_set == 1 ? "<i class='".$locale['uf_kep'][$gen_set][$i]." fa-lg m-r-10'></i>".$locale['uf_gender_sz'][$i] :
-        ($gen_set == 2 ? $locale['uf_gender_sz'][$i] : "")." <img src='".IMAGES."user_fields/".$locale['uf_kep'][$gen_set][$i].".png' width='16' title='".$locale['uf_gender_sz'][$i]."' >"));
+    switch ($gen_set) {
+        case 3:
+            $value = "<img src='".IMAGES."user_fields/".$locale['uf_kep'][$gen_set][$i].".png' width='16' title='".$locale['uf_gender_sz'][$i]."'/>";
+            break;
+        case 1:
+            $value = "<i class='".$locale['uf_kep'][$gen_set][$i]." fa-lg m-r-10'></i>".$locale['uf_gender_sz'][$i];
+            break;
+        case 2:
+            $value = "<img src='".IMAGES."user_fields/".$locale['uf_kep'][$gen_set][$i].".png' width='16' title='".$locale['uf_gender_sz'][$i]."'/> ".$locale['uf_gender_sz'][$i];
+            break;
+        default:
+            $value = $locale['uf_gender_sz'][$i];
+    }
+    $locale['uf_gender_szkep'][] = $value;
+}
+if (!$with_secret) {
+    unset($locale['uf_gender_szkep'][count($locale['uf_gender_szkep']) - 1]);
 }
 
 if ($profile_method == "input") {
-    $options = array('inline'     => TRUE,
-                     'error_text' => $locale['uf_gender_error'],
-                     'options'    => $locale['uf_gender_sz'],
-                     'width'      => '200px',
-        ) + $options;
-
-    $user_fields = form_select('user_gender', $locale['uf_gender'], $field_value, $options);
-
-// Display user field input
+    $options = [
+            'type'       => 'radio',
+            'inline'     => TRUE,
+            'error_text' => $locale['uf_gender_error'],
+            'options'    => $locale['uf_gender_szkep'],
+            'width'      => '200px'
+        ] + $options;
+    $user_fields = $input_type('user_gender', $locale['uf_gender'], $field_value, $options);
+    // Display user field input
 } elseif ($profile_method == "display") {
     if ($field_value) {
         $user_fields = [
--- locale/English/user_fields/user_gender.php
+++ locale/English/user_fields/user_gender.php
@@ -2,4 +2,4 @@
 $locale['uf_gender'] = "Gender";
 $locale['uf_gender_desc'] = "The user can specify Gender";
 $locale['uf_gender_error'] = "Is Gender wrong or not specified.";
-$locale['uf_gender_sz'] = array("Not Specified", "Female", "Male", "- Secret -");
+$locale['uf_gender_sz'] = array("Not Specified", "Female", "Male", "Secret");
