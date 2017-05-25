f47a00b43081b20b1640df89cdde0e6a6b154195
karrak1 <g.sebestyen@fusionjatek.hu>
2017-05-25 19:00:59 +0200

    Profile Signature, add nl2br in parse #1832

--- includes/user_fields/user_sig_include.php
+++ includes/user_fields/user_sig_include.php
@@ -28,5 +28,5 @@ if ($profile_method == "input") {
     $user_fields = form_textarea('user_sig', $locale['uf_sig'], $field_value, $options);
 
 } elseif ($profile_method == "display") {
-    $user_fields = array('title' => $locale['uf_sig'], 'value' => $field_value ? parseubb(parsesmileys($field_value)) : fusion_get_locale('na'));
+    $user_fields = array('title' => $locale['uf_sig'], 'value' => $field_value ? nl2br(parseubb(parsesmileys($field_value))) : fusion_get_locale('na'));
 }
