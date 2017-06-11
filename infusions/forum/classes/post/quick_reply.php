46f3c28a84e84458f8332661df640d1d67081fa4
Frederick Chan Meang Czac <meangczac.chan@gmail.com>
2017-06-11 16:31:18 +0800

    #1845 - Forum 2.0 - Code Update for Theme Engine Template Integration

--- infusions/forum/classes/post/quick_reply.php
+++ infusions/forum/classes/post/quick_reply.php
@@ -15,15 +15,50 @@
 | copyright header is strictly prohibited without
 | written permission from the original author(s).
 +--------------------------------------------------------*/
+
 namespace PHPFusion\Forums\Post;
 
 use PHPFusion\Forums\ForumServer;
 
 class QuickReply extends ForumServer {
 
-    public static function display_quickReply($thread_data) {
+    public static function display_quickReply($info) {
+        $locale = fusion_get_locale();
+        $user_sig = fusion_get_userdata('user_sig');
+        $notify_options = ForumServer::get_forum_settings('thread_notify');
         require_once FORUM."templates/forum_input.php";
-        return display_quickReply($thread_data);
+
+        $options_field = form_checkbox('post_smileys', $locale['forum_0169'], '', array('class' => 'm-b-0', 'reverse_label' => TRUE));
+        if (!$user_sig) {
+            $options_field .= form_checkbox('post_showsig', $locale['forum_0170'], '1', array('class' => 'm-b-0', 'reverse_label' => TRUE));
+        }
+        if ($notify_options) {
+            $options_field .= form_checkbox('notify_me', $locale['forum_0171'], $info['user_tracked'], array('class' => 'm-b-0', 'reverse_label' => TRUE));
+        }
+
+        $info += [
+            'openform'    => openform('quick_reply_form', 'post', FORUM."viewthread.php?thread_id=".$info['thread_id'], array('class' => 'spacer-sm')),
+            'description' => $locale['forum_0168'],
+            'field'       => [
+                'message' => form_textarea('post_message', '', '',
+                    array(
+                        'placeholder' => $locale['forum_0601']."...",
+                        'bbcode'      => TRUE,
+                        'required'    => TRUE,
+                        'preview'     => TRUE,
+                        'form_name'   => 'quick_reply_form',
+                        'height'      => '250px'
+                    )),
+                'button'  => form_button('post_quick_reply', $locale['forum_0172'], $locale['forum_0172'], array('class' => 'btn-primary')),
+                'options' => $options_field
+            ]
+        ];
+
+        $html = openform('quick_reply_form', 'post', FORUM."viewthread.php?thread_id=".$info['thread_id']);
+        $html .= display_quick_reply($info);
+        $html .= closeform();
+
+        return $html;
     }
 
 }
