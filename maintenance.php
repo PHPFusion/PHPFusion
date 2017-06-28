859b8eaf805417a32e0fe710f618560307b0f3ea
Frederick Chan Meang Czac <meangczac.chan@gmail.com>
2017-06-28 16:32:44 +0800

    Fix maintenance script not redirecting if opening_page is a folder.

--- maintenance.php
+++ maintenance.php
@@ -24,7 +24,9 @@ if (!fusion_get_settings("maintenance")) {
 if (fusion_get_settings("site_seo") == 1 && !defined("IN_PERMALINK")) {
     \PHPFusion\Rewrite\Permalinks::getPermalinkInstance()->handle_url_routing("");
 }
+
 $locale = fusion_get_locale();
+
 if (!iMEMBER) {
     switch (fusion_get_settings('login_method')) {
         case "2" :
@@ -38,12 +40,13 @@ if (!iMEMBER) {
     }
     $user_name = isset($_POST['user_name']) ? form_sanitizer($_POST['user_name'], "", "user_name") : "";
     $user_password = isset($_POST['user_pass']) ? form_sanitizer($_POST['user_pass'], "", "user_pass") : "";
+    $path = BASEDIR.(!stristr(fusion_get_settings('opening_page'), '.php') ? fusion_get_settings('opening_page').'/index.php' : fusion_get_settings('opening_page'));
     $info = [
-        "open_form"            => openform('loginpageform', 'POST', fusion_get_settings('opening_page')),
-        "user_name"            => form_text('user_name', "", $user_name, ['placeholder' => $placeholder, 'inline' => TRUE]),
-        "user_pass"            => form_text('user_pass', "", $user_password, ['placeholder' => $locale['global_102'], 'type' => 'password', 'inline' => TRUE]),
-        "remember_me"          => form_checkbox('remember_me', $locale['global_103'], ""),
-        "login_button"         => form_button('login', $locale['global_104'], $locale['global_104'], ['class' => 'btn-primary btn-block m-b-20']),
+        "open_form"    => openform('loginpageform', 'POST', $path),
+        "user_name"    => form_text('user_name', "", $user_name, ['placeholder' => $placeholder, 'inline' => TRUE]),
+        "user_pass"    => form_text('user_pass', "", $user_password, ['placeholder' => $locale['global_102'], 'type' => 'password', 'inline' => TRUE]),
+        "remember_me"  => form_checkbox('remember_me', $locale['global_103'], ""),
+        "login_button" => form_button('login', $locale['global_104'], $locale['global_104'], ['class' => 'btn-primary btn-block m-b-20']),
         "registration_link"    => (fusion_get_settings('enable_registration')) ? "<p>".$locale['global_105']."</p>\n" : "",
         "forgot_password_link" => $locale['global_106'],
         "close_form"           => closeform()
@@ -66,7 +69,6 @@ echo "<meta name='description' content='".fusion_get_settings('description')."' 
 echo "<meta name='url' content='".fusion_get_settings('siteurl')."' />\n";
 echo "<meta name='keywords' content='".fusion_get_settings('keywords')."' />\n";
 echo "<meta name='image' content='".fusion_get_settings('siteurl').fusion_get_settings('sitebanner')."' />\n";
-
 // Load bootstrap stylesheets
 if (fusion_get_settings('bootstrap') == TRUE) {
     define('BOOTSTRAPPED', TRUE);
@@ -82,7 +84,6 @@ if (fusion_get_settings('bootstrap') == TRUE) {
         echo "<link href='".$theme_css."' rel='stylesheet' type='text/css' />\n";
     }
 }
-
 if (fusion_get_settings('entypo')) {
     echo "<link rel='stylesheet' href='".INCLUDES."fonts/entypo/entypo.css' type='text/css' />\n";
     echo "<link rel='stylesheet' href='".INCLUDES."fonts/entypo/entypo-codes.css' type='text/css' />\n";
@@ -91,22 +92,17 @@ if (fusion_get_settings('entypo')) {
     echo "<link rel='stylesheet' href='".INCLUDES."fonts/entypo/entypo-ie7-codes.css' type='text/css' />\n";
     echo "<link rel='stylesheet' href='".INCLUDES."fonts/entypo/animation.css' type='text/css' />\n";
 }
-
 if (fusion_get_settings('fontawesome')) {
     echo "<link rel='stylesheet' href='".INCLUDES."fonts/font-awesome/css/font-awesome.min.css' type='text/css' />\n";
 }
-
 if (!defined('NO_DEFAULT_CSS')) {
     echo "<link href='".THEMES."templates/default.css' rel='stylesheet' type='text/css' media='screen' />\n";
 }
-
 echo "<link href='".THEME."styles.css' rel='stylesheet' type='text/css' media='screen' />\n";
 echo render_favicons(IMAGES);
-
 if (function_exists("get_head_tags")) {
     echo get_head_tags();
 }
-
 echo "<script type='text/javascript' src='".INCLUDES."jquery/jquery.js'></script>\n";
 echo "</head>\n";
 
