fea67b529343128969b4497b91fce6cda7b50d35
RobiNN <RobiNN1@users.noreply.github.com>
2017-07-03 12:55:48 +0200

    Fix Undefined variable: filtered_value

--- infusions/forum/templates.php
+++ infusions/forum/templates.php
@@ -4,7 +4,7 @@
 | Copyright (C) PHP-Fusion Inc
 | https://www.php-fusion.co.uk/
 +--------------------------------------------------------+
-| Filename: forum/templates/forum_main.php
+| Filename: forum/templates.php
 | Author: PHP-Fusion Development Team
 +--------------------------------------------------------+
 | This program is released as free software under the
