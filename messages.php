f5c4c4a2cc06688753d46d53cfc87aab2e0144be
Frederick Chan Meang Czac <meangczac.chan@gmail.com>
2017-07-14 14:24:19 +0800

    Fix PM locale set

--- messages.php
+++ messages.php
@@ -23,6 +23,7 @@ require_once THEMES."templates/header.php";
 //include LOCALE.LOCALESET."messages.php";
 include THEMES."templates/global/messages.php";
 $message = new \PHPFusion\PrivateMessages();
+$message->locale = fusion_get_locale('', LOCALE.LOCALESET.'messages.php');
 $message->display_inbox();
 display_inbox($message->getInfo());
 require_once THEMES."templates/footer.php";
