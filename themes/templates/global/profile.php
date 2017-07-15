61ba881d6af1eb6686a93bff41d7724985e81c3f
karrak1 <g.sebestyen@fusionjatek.hu>
2017-07-15 16:03:42 +0200

    update profile.php

--- themes/templates/global/profile.php
+++ themes/templates/global/profile.php
@@ -124,10 +124,10 @@ if (!function_exists('display_user_profile')) {
                 <div class='clearfix'>{%basic_info%}</div>
                 <hr/>
                 <div class='clearfix'>{%extended_info%}</div>
+            	<div class='text-center'>{%buttons%}</div>
             </div>
             {%tab_footer%}
         </section>
-            <div class='clearfix text-center'>{%buttons%}</div>
         <!--userprofile_sub_idx-->
         <?php
     }
