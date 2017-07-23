a9d4252420674fe4a3dbefc6e9b8e2f54d713685
karrak1 <g.sebestyen@fusionjatek.hu>
2017-07-23 10:54:23 +0200

    image align git

--- infusions/news/classes/admin/controllers/news.php
+++ infusions/news/classes/admin/controllers/news.php
@@ -100,7 +100,7 @@ class NewsAdmin extends NewsAdminModel {
                 'news_allow_ratings'       => isset($_POST['news_allow_ratings']) ? "1" : "0",
                 'news_language'            => form_sanitizer($_POST['news_language'], '', 'news_language'),
                 'news_image_front_default' => 0,
-                'news_image_align'         => '',
+                'news_image_align'         => form_sanitizer($_POST['news_image_align'], 'pull-left', 'news_image_align'),
             );
 
             if (fusion_get_settings('tinymce_enabled') != 1) {
@@ -599,6 +599,8 @@ class NewsAdmin extends NewsAdminModel {
                     )
                 ).
                 form_select('news_image_align', self::$locale['news_0218'], $this->news_data['news_image_align'], array("options" => $alignOptions, 'inline' => FALSE, 'inner_width' => '100%'));
+        else:
+                echo form_hidden('news_image_align', '', $this->news_data['news_image_align']);
         endif;
         closeside();
 
