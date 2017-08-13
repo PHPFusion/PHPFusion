33ae13e004725aff692d0077b4b5a740e76f9b94
RobiNN <RobiNN1@users.noreply.github.com>
2017-08-13 12:18:33 +0200

    Update home, messages tpl

--- themes/templates/global/home.php
+++ themes/templates/global/home.php
@@ -29,15 +29,15 @@ if (!function_exists('display_home')) {
                 foreach ($content['data'] as $data) {
                     echo "<div class='".$classes." clearfix'>";
 
-                         if ($db_id !== DB_PREFIX.'articles') {
-                             echo '<a href="' . $data['url'] . '">';
-                             if (!empty($data['image']) && file_exists($data['image'])) {
-                                 echo '<img style="max-height: 180px;" class="center-x img-responsive" src="' . $data['image'] . '" alt="' . $data['title'] . '"/>';
-                             } else {
-                                 echo get_image('imagenotfound', $data['title'], 'max-height: 180px;');
-                             }
-                             echo '</a>';
-                         }
+                        if ($db_id !== DB_PREFIX.'articles') {
+                            echo '<a href="' . $data['url'] . '">';
+                            if (!empty($data['image']) && file_exists($data['image'])) {
+                                echo '<img style="max-height: 180px;" class="center-x img-responsive" src="' . $data['image'] . '" alt="' . $data['title'] . '"/>';
+                            } else {
+                                echo get_image('imagenotfound', $data['title'], 'max-height: 180px;');
+                            }
+                            echo '</a>';
+                        }
 
                         echo "<h4><a href='".$data['url']."'>".$data['title']."</a></h4>";
                         echo "<div class='small m-b-10'>".$data['meta']."</div>";
