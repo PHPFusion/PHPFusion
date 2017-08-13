16bc251bfc4e6ed6274d21951a8610759c778fb6
RobiNN <RobiNN1@users.noreply.github.com>
2017-08-13 12:17:30 +0200

    Update Blog tpl

--- infusions/blog/templates/blog.php
+++ infusions/blog/templates/blog.php
@@ -58,35 +58,35 @@ if (!function_exists('display_blog_item')) {
         add_to_head("<link rel='stylesheet' href='".INCLUDES."jquery/colorbox/colorbox.css' type='text/css' media='screen' />");
         add_to_head("<script type='text/javascript' src='".INCLUDES."jquery/colorbox/jquery.colorbox.js'></script>");
         add_to_footer('<script type="text/javascript">'.jsminify('
-			$(document).ready(function() {
-				$(".blog-image-overlay").colorbox({
-					transition: "elasic",
-					height:"100%",
-					width:"100%",
-					maxWidth:"98%",
-					maxHeight:"98%",
-					scrolling:false,
-					overlayClose:true,
-					close:false,
-					photo:true,
-					onComplete: function(result) {
-						$("#colorbox").live("click", function(){
-						$(this).unbind("click");
-						$.fn.colorbox.close();
-						});
-					},
-					onLoad: function () {
-					}
-			   });
-			});
-			').'</script>');
+            $(document).ready(function() {
+                $(".blog-image-overlay").colorbox({
+                    transition: "elasic",
+                    height:"100%",
+                    width:"100%",
+                    maxWidth:"98%",
+                    maxHeight:"98%",
+                    scrolling:false,
+                    overlayClose:true,
+                    close:false,
+                    photo:true,
+                    onComplete: function(result) {
+                        $("#colorbox").live("click", function(){
+                        $(this).unbind("click");
+                        $.fn.colorbox.close();
+                        });
+                    },
+                    onLoad: function () {
+                    }
+               });
+            });
+            ').'</script>');
         ob_start();
 
         $data = $info['blog_item'];
 
         echo "<div class='clearfix'>
-				<div class='btn-group pull-right'>
-				<a class='btn btn-default btn-sm' href='".$data['print_link']."' target='_blank'><i class='fa fa-print'></i> ".$locale['print']."</a>";
+                <div class='btn-group pull-right'>
+                <a class='btn btn-default btn-sm' href='".$data['print_link']."' target='_blank'><i class='fa fa-print'></i> ".$locale['print']."</a>";
         if ($data['admin_link']) {
             $admin_actions = $data['admin_link'];
             echo "<a class='btn btn-default btn-sm' href='".$admin_actions['edit']."'><i class='fa fa-pencil'></i> ".$locale['edit']."</a>\n";
@@ -94,11 +94,11 @@ if (!function_exists('display_blog_item')) {
         }
         echo "</div>";
         echo "<div class='overflow-hide'>
-				<h2 class='strong m-t-0 m-b-0'>".$data['blog_subject']."</h2>
-				<div class='blog-category'>".$data['blog_category_link']."</div>
-				<div class='m-t-20 m-b-20'>".$data['blog_post_author']." ".$data['blog_post_time']."</div>
-			</div>
-		</div>";
+                <h2 class='strong m-t-0 m-b-0'>".$data['blog_subject']."</h2>
+                <div class='blog-category'>".$data['blog_category_link']."</div>
+                <div class='m-t-20 m-b-20'>".$data['blog_post_author']." ".$data['blog_post_time']."</div>
+            </div>
+        </div>";
 
         echo "<div class='clearfix m-b-20'>\n";
         if ($data['blog_image']) {
@@ -128,28 +128,28 @@ if (!function_exists('display_blog_index')) {
             foreach ($info['blog_item'] as $blog_id => $data) {
                 echo (isset($_GET['cat_id'])) ? "<!--pre_blog_cat_idx-->\n" : "<!--blog_prepost_".$blog_id."-->\n";
                 echo "
-					<div class='clearfix m-b-20'>
-						<div class='row'>
-							<div class='col-xs-12 col-sm-4'>
-								<div class='pull-left m-r-5'>".$data['blog_user_avatar']."</div>
-								<div class='overflow-hide'>
-									".$data['blog_user_link']." <br/>
-									<span class='m-r-10 text-lighter'><i class='fa fa-comment-o fa-fw'></i> ".$data['blog_comments']."</span><br/>
-									<span class='m-r-10 text-lighter'><i class='fa fa-star-o fa-fw'></i> ".$data['blog_count_votes']."</span><br/>
-									<span class='m-r-10 text-lighter'><i class='fa fa-eye fa-fw'></i> ".$data['blog_reads']."</span><br/>
-								</div>
-							</div>
-							<div class='col-xs-12 col-sm-8'>
-								<h2 class='strong m-b-20 m-t-0'><a class='text-dark' href='".$data['blog_link']."'>".$data['blog_subject']."</a></h2>
-								<div class='display-block'>".$data['blog_category_link']."</div>
-								<div class='display-block'><i class='fa fa-clock-o m-r-5'></i> ".$locale['global_049']." ".timer($data['blog_datestamp'])."</div>
-								".($data['blog_image'] ? "<div class='blog-image m-10 ".$data['blog_ialign']."'>".$data['blog_image']."</div>" : '')."
-								<div class='m-t-20'>".$data['blog_blog']."<br/>".$data['blog_readmore_link']."</div>
-							</div>
-						</div>
-						<hr>
-					</div>
-				";
+                    <div class='clearfix m-b-20'>
+                        <div class='row'>
+                            <div class='col-xs-12 col-sm-3'>
+                                <div class='pull-left m-r-5'>".$data['blog_user_avatar']."</div>
+                                <div class='overflow-hide'>
+                                    ".$data['blog_user_link']." <br/>
+                                    <span class='m-r-10 text-lighter'><i class='fa fa-comment-o fa-fw'></i> ".$data['blog_comments']."</span><br/>
+                                    <span class='m-r-10 text-lighter'><i class='fa fa-star-o fa-fw'></i> ".$data['blog_count_votes']."</span><br/>
+                                    <span class='m-r-10 text-lighter'><i class='fa fa-eye fa-fw'></i> ".$data['blog_reads']."</span><br/>
+                                </div>
+                            </div>
+                            <div class='col-xs-12 col-sm-9'>
+                                <h2 class='strong m-b-20 m-t-0'><a class='text-dark' href='".$data['blog_link']."'>".$data['blog_subject']."</a></h2>
+                                <div class='display-block'>".$data['blog_category_link']."</div>
+                                <div class='display-block'><i class='fa fa-clock-o m-r-5'></i> ".$locale['global_049']." ".timer($data['blog_datestamp'])."</div>
+                                ".($data['blog_image'] ? "<div class='blog-image m-10 ".$data['blog_ialign']."'>".$data['blog_image']."</div>" : '')."
+                                <div class='m-t-20'>".$data['blog_blog']."<br/>".$data['blog_readmore_link']."</div>
+                            </div>
+                        </div>
+                        <hr>
+                    </div>
+                ";
 
                 echo (isset($_GET['cat_id'])) ? "<!--sub_blog_cat_idx-->" : "<!--sub_blog_idx-->\n";
             }
@@ -206,8 +206,8 @@ if (!function_exists('display_blog_menu')) {
                 if (!empty($archive_data)) {
                     foreach ($archive_data as $month => $a_data) {
                         echo "<li ".($a_data['active'] ? "class='active strong'" : '').">
-						<a href='".$a_data['link']."'>".$a_data['title']."</a> <span class='badge m-l-10'>".$a_data['count']."</span>
-						</li>\n";
+                        <a href='".$a_data['link']."'>".$a_data['title']."</a> <span class='badge m-l-10'>".$a_data['count']."</span>
+                        </li>\n";
                     }
                 }
                 $current_year = $year;
@@ -221,8 +221,8 @@ if (!function_exists('display_blog_menu')) {
         if (!empty($info['blog_author'])) {
             foreach ($info['blog_author'] as $author_id => $author_info) {
                 echo "<li ".($author_info['active'] ? "class='active strong'" : '').">
-					<a href='".$author_info['link']."'>".$author_info['title']."</a> <span class='badge m-l-10'>".$author_info['count']."</span>
-					</li>\n";
+                    <a href='".$author_info['link']."'>".$author_info['title']."</a> <span class='badge m-l-10'>".$author_info['count']."</span>
+                    </li>\n";
             }
         } else {
             echo "<li>".$locale['blog_3003']."</li>\n";
