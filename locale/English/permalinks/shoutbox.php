631cd1e43d33f9e4eafbea97ec5c1c676c237116
slb4life <slb4life2k7@hotmail.com>
2017-10-12 20:57:32 -0400

    Fix Double

--- locale/English/permalinks/shoutbox.php
+++ locale/English/permalinks/shoutbox.php
@@ -1,4 +1,4 @@
 <?php
 $locale['pl_shoutbox_title'] = "Shoutbox";
-$locale['pl_shoutbox_desc'] = "Enable Permalinks for the Shoutbox links in the site..";
+$locale['pl_shoutbox_desc'] = "Enable Permalinks for the Shoutbox links in the site.";
 
