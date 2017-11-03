d3ebd9b9513cf0a3f32b342f1eb41db7c86a87c0
Frederick Chan Meang Czac <meangczac.chan@gmail.com>
2017-11-03 15:04:49 +0800

    Fix margins

--- includes/dynamics/includes/form_geomap.php
+++ includes/dynamics/includes/form_geomap.php
@@ -101,7 +101,7 @@ function form_geo($input_name, $label = '', $input_value = FALSE, array $options
     $html .= "<input type='text' name='".$input_name."[]' class='form-control' id='".$input_id."-street2' value='".$input_value['1']."' placeholder='".$locale['street2']."' ".($options['deactivate'] == "1" ? "readonly" : '')." />";
     $html .= "</div>\n";
 
-    $html .= "<div class='col-xs-12 col-sm-5 col-md-5 col-lg-5 m-b-10'>\n";
+    $html .= "<div class='col-xs-12 col-sm-5 col-md-5 col-lg-5 m-b-20'>\n";
     $html .= "<select name='".$input_name."[]' id='$input_id-country' style='width:100%;'>\n";
     $html .= "<option value=''></option>";
     foreach ($countries as $arv => $countryname) { // outputs: key, value, class - in order
@@ -112,7 +112,7 @@ function form_geo($input_name, $label = '', $input_value = FALSE, array $options
     $html .= "</select>\n";
     $html .= (($options['required'] == 1 && \defender::inputHasError($input_name.'-'.$validation_key[2])) || \defender::inputHasError($input_name.'-'.$validation_key[2])) ? "<div id='".$options['input_id']."-country-help' class='label label-danger p-5 display-inline-block'>".$options['error_text_3']."</div>" : "";
     $html .= "</div>\n";
-    $html .= "<div class='col-xs-12 col-sm-7 col-md-7 col-lg-7 m-b-10'>\n";
+    $html .= "<div class='col-xs-12 col-sm-7 col-md-7 col-lg-7 m-b-20'>\n";
     $html .= "<div id='state-spinner' style='display:none;'>\n<img src='".fusion_get_settings('siteurl')."images/loader.svg'>\n</div>\n";
     $html .= "<input type='hidden' name='".$input_name."[]' id='$input_id-state' value='".$input_value['3']."' style='width:100%;' />\n";
     $html .= (($options['required'] == 1 && \defender::inputHasError($input_name.'-'.$validation_key[3])) || \defender::inputHasError($input_name.'-'.$validation_key[3])) ? "<div id='".$options['input_id']."-state-help' class='label label-danger p-5 display-inline-block'>".$options['error_text_4']."</div>" : "";
