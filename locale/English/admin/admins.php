eeba0b6eeb295bab3dbee5deee5311084145768d
karrak1 <g.sebestyen@fusionjatek.hu>
2017-11-29 17:23:14 +0100

    administrators locale git

--- locale/English/admin/admins.php
+++ locale/English/admin/admins.php
@@ -1,47 +1,47 @@
 <?php
 // Administrators messages
-$locale['ADM_400'] = "Adminisztrátor hozzáadva";
-$locale['ADM_401'] = "Adminisztrátor módosítva";
-$locale['ADM_402'] = "Adminisztrátor törölve";
+$locale['ADM_400'] = "Administrator added";
+$locale['ADM_401'] = "Administrator updated";
+$locale['ADM_402'] = "Administrator removed";
 // Search/Add Administrator
-$locale['ADM_410'] = "Adminisztrátor hozzáadása";
-$locale['ADM_411'] = "Hozzáadáshoz keresd meg a felhasználót az alábbi keresővel:";
-$locale['412'] = "Azonosító (ID)";
-$locale['ADM_413'] = "Felhasználónév";
-$locale['414'] = "Keresés";
-$locale['ADM_415'] = "Összes admin jog megadása";
-$locale['ADM_416'] = "Főadminisztrátori jog";
-$locale['417'] = "Hozzáadás";
-$locale['ADM_418'] = "Nem található ilyen tag.";
-$locale['ADM_419'] = "Új keresés";
+$locale['ADM_410'] = "Add Administrator";
+$locale['ADM_411'] = "Search for users to add using the form below.";
+$locale['412'] = "User ID";
+$locale['ADM_413'] = "User Name";
+$locale['414'] = "Search";
+$locale['ADM_415'] = "Apply all privileges";
+$locale['ADM_416'] = "Make Super Administrator";
+$locale['417'] = "Add Admin";
+$locale['ADM_418'] = "No users found.";
+$locale['ADM_419'] = "Search Again";
 // Current Administrators
-$locale['ADM_420'] = "Adminisztrátorok";
-$locale['ADM_421'] = "Felhasználónév";
-$locale['ADM_422'] = "Rang";
-$locale['ADM_423'] = "Lehetőségek";
-$locale['424'] = "Elsődleges főadmin";
-$locale['ADM_425'] = "Nincs admin joga";
-$locale['426'] = "Szerkesztés";
-$locale['427'] = "Törlés";
+$locale['ADM_420'] = "Administrators";
+$locale['ADM_421'] = "User Name";
+$locale['ADM_422'] = "Admin Level";
+$locale['ADM_423'] = "Options";
+$locale['424'] = "Primary Admin";
+$locale['ADM_425'] = "No rights assigned";
+$locale['426'] = "Edit";
+$locale['427'] = "Remove";
 // Edit Admin Rights
-$locale['ADM_440'] = "Adminisztrátori jogok";
-$locale['ADM_441'] = "Tartalom";
-$locale['ADM_442'] = "Felhasználók";
-$locale['ADM_443'] = "Rendszer";
-$locale['ADM_444'] = "Infusion";
-$locale['ADM_445'] = "Összes";
-$locale['446'] = "Egyik sem";
-$locale['447'] = "Admin jelszó:";
-$locale['ADM_448'] = "Admin jogok mentése";
-$locale['ADM_449'] = "Beállítások";
-$locale['ADM_450'] = "Biztonságosak";
+$locale['ADM_440'] = "Edit Admin Rights";
+$locale['ADM_441'] = "Content Admin";
+$locale['ADM_442'] = "User Admin";
+$locale['ADM_443'] = "System Admin";
+$locale['ADM_444'] = "Infusions";
+$locale['ADM_445'] = "Check all";
+$locale['446'] = "UnCheck all";
+$locale['447'] = "Admin Password:";
+$locale['ADM_448'] = "Save Admin Rights";
+$locale['ADM_449'] = "Settings";
+$locale['ADM_450'] = "Check secure";
 // Popup Error/Warning Messages
-$locale['460'] = "Biztosan el akarod venni a felhasználó admin jogait?";
-$locale['ADM_461'] = "Biztosan hozzá szeretnéd adni ezt az adminisztrátort?";
-$locale['ADM_462'] = "Ezek az admin jogok rossz kezekben potenciális biztonsági kockázatot jelentenek!";
-$locale['ADM_463'] = "Nincs kiválasztva művelet!";
+$locale['460'] = "Remove this administrator?";
+$locale['ADM_461'] = "Add this administrator?";
+$locale['ADM_462'] = "Giving these admin rights to a user is a potential security risk!";
+$locale['ADM_463'] = "No action selected!";
 // Remove Admin confirmation
-$locale['470'] = "Adminisztrátor törlése";
-$locale['471'] = "Ha törölni szeretnéd az adminisztrátort, add meg az admin jelszavadat:";
-$locale['472'] = "Törlés";
-$locale['473'] = "Mégse";
+$locale['470'] = "Delete Admin";
+$locale['471'] = "In order to remove this Administrator, please enter your admin password:";
+$locale['472'] = "Confirm";
+$locale['473'] = "Cancel";
