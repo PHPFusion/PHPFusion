d4f249d7a5d4cac4b7479d34cf8f965e5c8dd946
karrak1 <g.sebestyen@fusionjatek.hu>
2017-12-07 18:44:29 +0100

    del space

--- includes/dynamics/includes/form_text.php
+++ includes/dynamics/includes/form_text.php
@@ -174,7 +174,7 @@ function form_text($input_name, $label = "", $input_value = "", array $options =
         ').'</script>');
     }
 
-    $html = "<div id='".$options['input_id']."-field' class='form-group".($options['inline'] ? ' overflow-hide' : '').$error_class.$options['class']."' ".($options['icon'] ? ' has-feedback' : '')."'".($options['width'] && !$label ? " style='width: ".$options['width']."'" : '').">\n";
+    $html = "<div id='".$options['input_id']."-field' class='form-group".($options['inline'] ? ' overflow-hide' : '').$error_class.$options['class']."".($options['icon'] ? ' has-feedback' : '')."'".($options['width'] && !$label ? " style='width: ".$options['width']."'" : '').">\n";
     $html .= ($label) ? "<label class='control-label ".($options['inline'] ? "col-xs-12 col-sm-3 col-md-3 col-lg-3 p-l-0" : '')."' for='".$options['input_id']."'>".$options['label_icon'].$label.($options['required'] ? "<span class='required'>&nbsp;*</span>" : '')." ".($options['tip'] ? "<i class='pointer fa fa-question-circle' title='".$options['tip']."'></i>" : '')."</label>\n" : '';
     $html .= ($options['inline'] && $label) ? "<div class='col-xs-12 col-sm-9 col-md-9 col-lg-9'>\n" : "";
 
