280fb9e3036d6c2e62aed804ac3032c33ed82a1a
Chan <meang.czac@outlook.com>
2017-12-12 12:15:01 +0800

    single quote file for better compatibility

--- includes/jquery/admin-msg.js
+++ includes/jquery/admin-msg.js
@@ -1,9 +1,9 @@
 "use strict";
 function closeDiv() {
-    $("#close-message").fadeTo("slow", 0.01, function () {
-        $(this).slideUp("slow", function () {
+    $('#close-message').fadeTo('slow', 0.01, function () {
+        $(this).slideUp('slow', function () {
             $(this).hide()
         })
     })
 }
-window.setTimeout("closeDiv();", 5000);
+window.setTimeout('closeDiv();', 5000);
