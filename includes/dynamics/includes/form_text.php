3a3a8be71c0c1ce497513fb6c6723778e5dda594
Chan <meang.czac@outlook.com>
2018-01-09 11:15:31 +0800

    Remove 'stacked' class. Standardize to hardcode html.
    The stack class can be used better for inserting additional HTML, such as hidden inputs.
    
    It should not affect the way of the default element behavior, i.e. width, etc.
    
    This has a better control on Template for width control instead.
    ````
    <div class='clearfix'>
     <div class='display-inline-block'>{%text_field%}</div>
     <div class='display-inline-block'>{%text_field%}</div>
    </div>
    ````

--- includes/dynamics/includes/form_text.php
+++ includes/dynamics/includes/form_text.php
@@ -208,7 +208,7 @@ function form_text($input_name, $label = "", $input_value = "", array $options =
             $input_type = "text";
     }
 
-    $html .= "<input type='".$input_type."' data-type='".$input_type."' ".(!empty($options_data) ? implode(' ', $options_data) : '')." ".$min.$max.$step."class='form-control textbox ".($options['inner_class'] ? " ".$options['inner_class']." " : '').($options['stacked'] ? "stacked" : "")."' ".($options['inner_width'] ? "style='width:".$options['inner_width'].";'" : '')." ".($options['max_length'] ? "maxlength='".$options['max_length']."'" : '')." name='".$input_name."' id='".$options['input_id']."' value='".$input_value."'".($options['placeholder'] ? " placeholder='".$options['placeholder']."' " : '')."".($options['autocomplete_off'] ? " autocomplete='off'" : '')." ".($options['deactivate'] ? 'readonly' : '').">";
+    $html .= "<input type='".$input_type."' data-type='".$input_type."' ".(!empty($options_data) ? implode(' ', $options_data) : '')." ".$min.$max.$step."class='form-control textbox ".($options['inner_class'] ? " ".$options['inner_class']." " : '')."' ".($options['inner_width'] ? "style='width:".$options['inner_width'].";'" : '')." ".($options['max_length'] ? "maxlength='".$options['max_length']."'" : '')." name='".$input_name."' id='".$options['input_id']."' value='".$input_value."'".($options['placeholder'] ? " placeholder='".$options['placeholder']."' " : '')."".($options['autocomplete_off'] ? " autocomplete='off'" : '')." ".($options['deactivate'] ? 'readonly' : '').">";
 
     $html .= $options['password_strength'] == TRUE ? '<div class="pwstrength_viewport_progress"></div>' : '';
 
