7037b7bae08fa9c06d4fdb990a8b7f99fdfc4568
RobiNN <RobiNN1@users.noreply.github.com>
2018-01-12 19:53:38 +0100

    Update Home Page

--- infusions/homepage/templates/homepage.php
+++ infusions/homepage/templates/homepage.php
@@ -29,8 +29,8 @@ if (!function_exists('display_home')) {
                     .item {padding: 0;height: inherit;}
                     .item .thumb {float: left;height: 120px;overflow: hidden;margin-right: 10px;}
                     .item .thumb img {vertical-align: middle;object-fit: contain;width: 100%;max-width: 140px;-webkit-transform: scale(1.5);-ms-transform: scale(1.5);-o-transform: scale(1.5);transform: scale(1.5);margin-top: -10px;}
-                    @media (min-width: 768px) {
-                        .item .thumb {float: inherit;height: 200px;margin-right: inherit;}
+                    @media (min-width: 900px) {
+                        .item .thumb {float: inherit;height: 150px;margin-right: inherit;}
                         .item .thumb img {max-width: 100%;max-height: 100%;-webkit-transform: scale(1.5);-ms-transform: scale(1.5);-o-transform: scale(1.5);transform: scale(1.5);margin-top: -10px;}
                         .item .post .meta {margin: 0;padding: 3px 0 10px;font-size: 12px;}
                     }
@@ -44,15 +44,15 @@ if (!function_exists('display_home')) {
                             if (!empty($data['image'])) {
                                 echo '<figure class="thumb">';
                                     echo '<a href="'.$data['url'].'">';
-                                        echo '<img style="max-height: 200px;" class="img-responsive" src="'.$data['image'].'" alt="'.$data['title'].'"/>';
+                                        echo '<img style="max-height: 120px;" class="img-responsive" src="'.$data['image'].'" alt="'.$data['title'].'"/>';
                                     echo '</a>';
                                 echo '</figure>';
                             }
 
                             echo '<div class="post">';
                                 echo '<h4><a href="'.$data['url'].'">'.$data['title'].'</a></h4>';
-                                echo '<div class="small m-b-10">'.$data['meta'].'</div>';
-                                echo '<div class="overflow-hide">'.nl2br(trim_text(strip_tags($data['content']), 200)).'</div>';
+                                echo '<div class="small m-b-10 overflow-hide">'.$data['meta'].'</div>';
+                                echo '<div class="overflow-hide hidden-xs">'.nl2br(trim_text(strip_tags($data['content']), 200)).'</div>';
                             echo '</div>';
                         echo '</div>';
                     echo '</div>';
