54a447e71b1952712e8e025041cca6ae9e07967d
Chan <meang.czac@outlook.com>
2018-01-23 14:48:24 +0800

    Update user_gender UF. - Locale in string rather than array. - Fixed matches

--- includes/user_fields/user_gender_include.php
+++ includes/user_fields/user_gender_include.php
@@ -24,12 +24,26 @@ $gen_set = 2; //0 = Just text; 1 = Icon + text; 2 = text + image; 3 = Just image
 $with_secret = FALSE; // True for additiona unspecified option
 $input_type = 'form_checkbox'; // form_select or form_checkbox (up to you)
 
+/*
+ * Field Value
+ * 0    Unspecified
+ * 1    Male
+ * 2    Female
+ * 3    Unspecified
+ */
+
 // Definitions
 $locale['uf_kep'] = [
     '0' => '', // Text
-    '1' => ["fa fa-user-secret", "fa fa-mars", "fa fa-venus", "fa fa-user-secret"], // Icon
-    '2' => ["unspecified", "male", "female", "unspecified"],  // Image + text
-    '3' => ["unspecified", "male", "female", "unspecified"]  // Image
+    '1' => ['fa fa-user-secret', 'fa fa-mars', 'fa fa-venus', 'fa fa-user-secret'], // Icon
+    '2' => ['unspecified', 'male', 'female', 'unspecified'],  // Image + text
+    '3' => ['unspecified', 'male', 'female', 'unspecified']  // Image
+];
+$locale['uf_gender_sz'] = [
+    0 => $locale['uf_gender_00'],
+    1 => $locale['uf_gender_01'],
+    2 => $locale['uf_gender_02'],
+    3 => $locale['uf_gender_03'],
 ];
 
 for ($i = 0; $i < count($locale['uf_gender_sz']); $i++) {
@@ -48,6 +62,7 @@ for ($i = 0; $i < count($locale['uf_gender_sz']); $i++) {
     }
     $locale['uf_gender_szkep'][] = $value;
 }
+
 if (!$with_secret) {
     unset($locale['uf_gender_szkep'][count($locale['uf_gender_szkep']) - 1]);
 }
@@ -59,13 +74,16 @@ if ($profile_method == "input") {
             'error_text' => $locale['uf_gender_error'],
             'options'    => $locale['uf_gender_szkep'],
         ] + $options;
+
     $user_fields = $input_type('user_gender', $locale['uf_gender'], $field_value, $options);
-    // Display user field input
+
 } else if ($profile_method == "display") {
+
     if ($field_value) {
         $user_fields = [
             'title' => $locale['uf_gender'],
             'value' => $locale['uf_gender_szkep'][$field_value]
         ];
     }
+
 }
