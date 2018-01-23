80459ad42d85e276dd4958829e15c5cef2952885
Chan <meang.czac@outlook.com>
2018-01-23 14:48:50 +0800

    Updated grecaptcha locale.

--- locale/English/defender.php
+++ locale/English/defender.php
@@ -88,8 +88,8 @@ $locale['grecaptcha_placeholder_2'] = "SECRET-KEY from Google";
 $locale['no_keys'] = "It looks like you don't have any keys yet. Register and obtain your [RECAPTCHA_LINK]public key here[/RECAPTCHA_LINK]";
 // here
 //$locale['review_keys'] = "<a href='https://www.google.com/recaptcha/admin' target='_BLANK'><button class='button'>Review your keys at Google reCAPTCHA</button></a>";
-$locale['grecaptcha_0100'] = "Public Key";
-$locale['grecaptcha_0101'] = "Private Key";
+$locale['grecaptcha_0100'] = "Site Key";
+$locale['grecaptcha_0101'] = "Secret Key";
 $locale['grecaptcha_0102'] = "Theme";
 $locale['grecaptcha_0102a'] = "Light";
 $locale['grecaptcha_0102b'] = "Dark";
