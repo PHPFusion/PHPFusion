5ac4bd42e05821c2dcfe82209a956c3fe983cdda
RobiNN <RobiNN1@users.noreply.github.com>
2018-01-30 23:09:56 +0100

    Fix home page

--- infusions/home_panel/templates/home_panel.php
+++ infusions/home_panel/templates/home_panel.php
@@ -28,7 +28,7 @@ if (!function_exists('display_home')) {
                     figure {margin: 0;padding: 0;border: 0;font: inherit;vertical-align: baseline;display: block;}
                     .item {padding: 0;height: inherit;}
                     .item .thumb {float: left;height: 120px;overflow: hidden;margin-right: 10px;}
-                    .item .thumb img {vertical-align: middle;object-fit: contain;width: 100%;max-width: 140px;-webkit-transform: scale(1.5);-ms-transform: scale(1.5);-o-transform: scale(1.5);transform: scale(1.5);}
+                    .item .thumb img {vertical-align: middle;object-fit: contain;width: 100%;max-width: 140px;-webkit-transform: scale(1.5);-ms-transform: scale(1.5);-o-transform: scale(1.5);transform: scale(1.5);margin-top: 10px;}
                     @media (min-width: 900px) {
                         .item .thumb {float: inherit;height: 150px;margin-right: inherit;}
                         .item .thumb img {max-width: 100%;max-height: 100%;-webkit-transform: scale(1.5);-ms-transform: scale(1.5);-o-transform: scale(1.5);transform: scale(1.5);margin-top: 15px;}
