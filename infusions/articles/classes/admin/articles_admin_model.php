e98d7de6bf094a210c9bf3514b99bce1d8bf5898
karrak1 <g.sebestyen@fusionjatek.hu>
2018-06-07 14:07:57 +0200

    article locale fix

--- infusions/articles/classes/admin/articles_admin_model.php
+++ infusions/articles/classes/admin/articles_admin_model.php
@@ -26,19 +26,19 @@ class ArticlesAdminModel extends ArticlesServer {
      * @var array
      */
     protected $default_article_data = [
-        "article_id"             => 0,
-        "article_draft"          => 0,
-        "article_snippet"        => "",
-        "article_article"        => "",
-        "article_datestamp"      => TIME,
-        "article_keywords"       => "",
-        "article_breaks"         => "n",
-        "article_allow_comments" => 1,
-        "article_allow_ratings"  => 1,
-        "article_language"       => LANGUAGE,
-        "article_visibility"     => 0,
-        "article_subject"        => "",
-        "article_cat"            => 0
+        'article_id'             => 0,
+        'article_draft'          => 0,
+        'article_snippet'        => '',
+        'article_article'        => '',
+        'article_datestamp'      => TIME,
+        'article_keywords'       => '',
+        'article_breaks'         => 'n',
+        'article_allow_comments' => 1,
+        'article_allow_ratings'  => 1,
+        'article_language'       => LANGUAGE,
+        'article_visibility'     => 0,
+        'article_subject'        => '',
+        'article_cat'            => 0
     ];
 
     public static function get_articleAdminLocale() {
@@ -47,8 +47,7 @@ class ArticlesAdminModel extends ArticlesServer {
             if (file_exists(LOCALE.LOCALESET."admin/settings.php")) {
                 $admin_locale_path = LOCALE.LOCALESET."admin/settings.php";
             }
-            $locale = fusion_get_locale("", ARTICLE_ADMIN_LOCALE);
-            $locale += fusion_get_locale("", $admin_locale_path);
+            $locale = fusion_get_locale('', [ARTICLE_ADMIN_LOCALE, $admin_locale_path]);
             self::$admin_locale = $locale;
         }
 
