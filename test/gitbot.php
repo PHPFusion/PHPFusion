95807f9cfb170d2d60ea15540c0407fd01b33365
Chan <meang.czac@outlook.com>
2018-06-29 09:29:07 +0800

    Add Gitbot Test Commit.

---
+++ test/gitbot.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__."/../maincore.php";
+require_once THEMES."templates/header.php";
+
+echo "Gitbot Push Test to Discord Channel";
+
+require_once THEMES."templates/footer.php";
