07e21c6c16a640bc57bea81bf40c26e23b1a98ac
RobiNN <RobiNN1@users.noreply.github.com>
2018-06-29 14:32:09 +0200

    Delete gitbot.php

--- test/gitbot.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__."/../maincore.php";
-require_once THEMES."templates/header.php";
-
-echo "Gitbot Push Test to Discord Channel";
-
-require_once THEMES."templates/footer.php";
