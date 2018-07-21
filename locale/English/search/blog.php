98ba3c0efd0c2131db783a3e42e6524a461115eb
karrak1 <g.sebestyen@fusionjatek.hu>
2018-07-21 11:25:03 +0200

    blog search remov

---
+++ locale/English/search/blog.php
@@ -0,0 +1,8 @@
+<?php
+$locale['b400'] = "Blog";
+$locale['b401'] = "Blog item";
+$locale['b402'] = "Blog items";
+$locale['b403'] = "found in";
+$locale['b404'] = "Blog subject";
+$locale['b405'] = "Blog text";
+$locale['b406'] = "extended Blog text";
