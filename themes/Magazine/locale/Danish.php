c4c4b048a1a28e64aab2f3c5d96d9d56b174ec6e
Helmuth Mikkelsen <helmuthm@gmail.com>
2018-08-16 11:39:17 +0200

    added Danish.php to Magazine
    Magazine/locale/Danish.php

---
+++ themes/Magazine/locale/Danish.php
@@ -0,0 +1,29 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: Magazine/locale/Danish.php
+| Author: Helmuth Mikkelsen (helmuth)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['MG_description'] = 'Officielt tema til PHP-Fusion Andromeda 9.0.3';
+$locale['created_by']  = 'Oprettet af';
+$locale['MG_001'] = 'Hej, ';
+// Templates
+$locale['MG_101'] = 'Læs mere';
+$locale['MG_102'] = 'Populære nyheder';
+// Widget
+$locale['MG_201'] = 'Tema inbdstillinger er blevet opdateret.';
+$locale['MG_202'] = 'GitHub URL';
+$locale['MG_203'] = 'Facebook URL';
+$locale['MG_204'] = 'Twitter URL';
+$locale['MG_205'] = 'Google Plus URL';
