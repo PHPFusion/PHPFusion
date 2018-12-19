618eb4fe96477f46586c8a4e927a456cd54f8023
robinn1 <RobiNN1@users.noreply.github.com>
2018-12-19 15:46:45 +0100

    Fix token.php

--- includes/defender/token.php
+++ includes/defender/token.php
@@ -173,7 +173,7 @@ class Token extends \defender {
      * @return string
      */
     public static function generate_token($form_id = 'phpfusion', $max_tokens = 5, $file = '') {
-        $form_id = !empty($form_id) ? $form_id : 'phpfusion';
+        $form_id = !isset($form_id) ? $form_id : 'phpfusion';
         // resets remote file every callback
         $remote_file = ($file ? $file : '');
         \defender::getInstance()->set_RemoteFile($remote_file);
@@ -195,7 +195,6 @@ class Token extends \defender {
                 array_shift($_SESSION['csrf_tokens'][self::pageHash($file)][$form_id]);
             }
         } else {
-
             $token_ring = $_SESSION['csrf_tokens'][self::pageHash($file)][$form_id];
             $ring = array_rand($token_ring, 1);
             $token = $token_ring[$ring];
