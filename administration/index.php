3a7e2811be3503e209ada293c7e24419f41922e4
Falk <falk@php-fusion.co.uk>
2019-01-03 22:30:47 +0100

    Admin do not need class now. Just need to move the Adminbreadcrumbs out too

--- administration/index.php
+++ administration/index.php
@@ -21,6 +21,7 @@ if (!iADMIN || $userdata['user_rights'] == "" || !defined("iAUTH") || !isset($_G
 }
 
 require_once THEMES."templates/admin_header.php";
+include LOCALE.LOCALESET."admin/main.php";
 
 if (!isset($_GET['pagenum']) || !isnum($_GET['pagenum'])) {
     $_GET['pagenum'] = 0;
@@ -28,232 +29,161 @@ if (!isset($_GET['pagenum']) || !isnum($_GET['pagenum'])) {
 
 $admin_images = TRUE;
 
-$members_count = dbarray(dbquery("SELECT
-    (SELECT COUNT(user_id) FROM ".DB_USERS." WHERE user_status<='1' OR user_status='3' OR user_status='5') AS members_registered,
-    (SELECT COUNT(user_id) FROM ".DB_USERS." WHERE user_status='2') AS members_unactivated,
-    (SELECT COUNT(user_id) FROM ".DB_USERS." WHERE user_status='4') AS members_security_ban,
-    (SELECT COUNT(user_id) FROM ".DB_USERS." WHERE user_status='5') AS members_canceled
-"));
-
 // Members stats
-$members['registered'] = $members_count['members_registered'];
-$members['unactivated'] = $members_count['members_unactivated'];
-$members['security_ban'] = $members_count['members_security_ban'];
-$members['cancelled'] = $members_count['members_canceled'];
-
-if ($settings['enable_deactivation'] == "1") {
-    $time_overdue = time() - (86400 * $settings['deactivation_period']);
-    $members['inactive'] = dbcount("(user_id)", DB_USERS, "user_lastvisit<'$time_overdue' AND user_actiontime='0' AND user_joined<'$time_overdue' AND user_status='0'");
+$members_registered = dbcount("(user_id)", DB_USERS, "user_status<='1' OR user_status='3' OR user_status='5'");
+$members_unactivated = dbcount("(user_id)", DB_USERS, "user_status='2'");
+$members_security_ban = dbcount("(user_id)", DB_USERS, "user_status='4'");
+$members_canceled = dbcount("(user_id)", DB_USERS, "user_status='5'");
+$members['registered'] = dbcount("(user_id)", DB_USERS, "user_status<='1' OR user_status='3' OR user_status='5'");
+$members['unactivated'] = dbcount("(user_id)", DB_USERS, "user_status='2'");
+$members['security_ban'] = dbcount("(user_id)", DB_USERS, "user_status='4'");
+$members['cancelled'] = dbcount("(user_id)", DB_USERS, "user_status='5'");
+if (fusion_get_settings("enable_deactivation") == "1") {
+	$time_overdue = time()-(86400*fusion_get_settings("deactivation_period"));
+	$members['inactive'] = dbcount("(user_id)", DB_USERS, "user_lastvisit<'$time_overdue' AND user_actiontime='0' AND user_joined<'$time_overdue' AND user_status='0'");
 }
-
-// Get Core Infusion's stats
-$forum = [];
+// Get Core Infusion´s stats
 if (db_exists(DB_FORUMS)) {
-    $f_count = dbarray(dbquery("SELECT
-        (SELECT COUNT(forum_id) FROM ".DB_PREFIX."forums) AS forums,
-        (SELECT COUNT(thread_id) FROM ".DB_PREFIX."threads) AS threads,
-        (SELECT COUNT(post_id) FROM ".DB_PREFIX."posts) AS posts,
-        (SELECT COUNT(user_id) FROM ".DB_USERS." WHERE user_posts > '0') AS user_posts
-    "));
-    $forum['count'] = $f_count['forums'];
-    $forum['thread'] = $f_count['threads'];
-    $forum['post'] = $f_count['posts'];
-    $forum['users'] = $f_count['user_posts'];
+	$forum['count'] = dbcount("('forum_id')", DB_FORUMS);
+	$forum['thread'] = dbcount("('post_id')", DB_THREADS);
+	$forum['post'] = dbcount("('post_id')", DB_POSTS);
+	$forum['users'] = dbcount("('user_id')", DB_USERS, "user_posts > '0'");
 }
-
-$download = [];
 if (db_exists(DB_DOWNLOADS)) {
-    $d_count = dbarray(dbquery("SELECT
-        (SELECT COUNT(download_id) FROM ".DB_PREFIX."downloads) AS items,
-        (SELECT COUNT(comment_id) FROM ".DB_COMMENTS." WHERE comment_type='D') AS comments,
-        (SELECT COUNT(submit_id) FROM ".DB_SUBMISSIONS." WHERE submit_type='d') AS submissions
-    "));
-    $download['download'] = $d_count['items'];
-    $download['comment'] = $d_count['comments'];
-    $download['submit'] = $d_count['submissions'];
+	$download['download'] = dbcount("('download_id')", DB_DOWNLOADS);
+	$download['comment'] = dbcount("('comment_id')", DB_COMMENTS, "comment_type='d'");
+	$download['submit'] = dbcount("(submit_id)", DB_SUBMISSIONS, "submit_type='d'");
 }
-
-$articles = [];
 if (db_exists(DB_ARTICLES)) {
-    $a_count = dbarray(dbquery("SELECT
-        (SELECT COUNT(article_id) FROM ".DB_PREFIX."articles) AS items,
-        (SELECT COUNT(comment_id) FROM ".DB_COMMENTS." WHERE comment_type='A') AS comments,
-        (SELECT COUNT(submit_id) FROM ".DB_SUBMISSIONS." WHERE submit_type='a') AS submissions
-    "));
-    $articles['article'] = $a_count['items'];
-    $articles['comment'] = $a_count['comments'];
-    $articles['submit'] = $a_count['submissions'];
+	$articles['article'] = dbcount("('article_id')", DB_ARTICLES);
+	$articles['comment'] = dbcount("('comment_id')", DB_COMMENTS, "comment_type='A'");
+	$articles['submit'] = dbcount("(submit_id)", DB_SUBMISSIONS, "submit_type='a'");
 }
-
-$weblinks = [];
 if (db_exists(DB_WEBLINKS)) {
-    $w_count = dbarray(dbquery("SELECT
-        (SELECT COUNT(weblink_id) FROM ".DB_PREFIX."weblinks) AS items,
-        (SELECT COUNT(submit_id) FROM ".DB_SUBMISSIONS." WHERE submit_type='l') AS submissions
-    "));
-    $weblinks['weblink'] = $w_count['items'];
-    $weblinks['submit'] = $w_count['submissions'];
+	$weblinks['weblink'] = dbcount("('weblink_id')", DB_WEBLINKS);
+	$weblinks['comment'] = dbcount("('comment_id')", DB_COMMENTS, "comment_type='L'");
+	$weblinks['submit'] = dbcount("(submit_id)", DB_SUBMISSIONS, "submit_type='l'");
 }
-
-$news = [];
 if (db_exists(DB_NEWS)) {
-    $n_count = dbarray(dbquery("SELECT
-        (SELECT COUNT(news_id) FROM ".DB_PREFIX."news) AS items,
-        (SELECT COUNT(comment_id) FROM ".DB_COMMENTS." WHERE comment_type='N') AS comments,
-        (SELECT COUNT(submit_id) FROM ".DB_SUBMISSIONS." WHERE submit_type='n') AS submissions
-    "));
-    $news['news'] = $n_count['items'];
-    $news['comment'] = $n_count['comments'];
-    $news['submit'] = $n_count['submissions'];
+	$news['news'] = dbcount("('news_id')", DB_NEWS);
+	$news['comment'] = dbcount("('comment_id')", DB_COMMENTS, "comment_type='n'");
+	$news['submit'] = dbcount("(submit_id)", DB_SUBMISSIONS, "submit_type='n'");
 }
-
-$blog = [];
 if (db_exists(DB_BLOG)) {
-    $b_count = dbarray(dbquery("SELECT
-        (SELECT COUNT(blog_id) FROM ".DB_PREFIX."blog) AS items,
-        (SELECT COUNT(comment_id) FROM ".DB_COMMENTS." WHERE comment_type='B') AS comments,
-        (SELECT COUNT(submit_id) FROM ".DB_SUBMISSIONS." WHERE submit_type='b') AS submissions
-    "));
-    $blog['blog'] = $b_count['items'];
-    $blog['comment'] = $b_count['comments'];
-    $blog['submit'] = $b_count['submissions'];
+	$blog['blog'] = dbcount("('blog_id')", DB_BLOG);
+	$blog['comment'] = dbcount("('comment_id')", DB_COMMENTS, "comment_type='b'");
+	$blog['submit'] = dbcount("(submit_id)", DB_SUBMISSIONS, "submit_type='b'");
 }
-
-$photos = [];
 if (db_exists(DB_PHOTOS)) {
-    $p_count = dbarray(dbquery("SELECT
-        (SELECT COUNT(photo_id) FROM ".DB_PREFIX."photos) AS items,
-        (SELECT COUNT(comment_id) FROM ".DB_COMMENTS." WHERE comment_type='P') AS comments,
-        (SELECT COUNT(submit_id) FROM ".DB_SUBMISSIONS." WHERE submit_type='p') AS submissions
-    "));
-    $photos['photo'] = $p_count['items'];
-    $photos['comment'] = $p_count['comments'];
-    $photos['submit'] = $p_count['submissions'];
+	$photos['photo'] = dbcount("('photo_id')", DB_PHOTOS);
+	$photos['comment'] = dbcount("('comment_id')", DB_COMMENTS, "comment_type='P'");
+	$photos['submit'] = dbcount("(submit_id)", DB_SUBMISSIONS, "submit_type='p'");
 }
-
-$comments_type = [
-    'C'  => $locale['272a'],
-    'UP' => $locale['UP']
-];
-$comments_type += \PHPFusion\Admins::getInstance()->getCommentType();
-
-$submit_type = [];
-$submit_type += \PHPFusion\Admins::getInstance()->getSubmitType();
-
-$submit_link = [];
-$submit_link += \PHPFusion\Admins::getInstance()->getSubmitLink();
-
-$submit_data = [];
-$submit_data += \PHPFusion\Admins::getInstance()->getSubmitData();
-
-$link_type = [
-    'C'  => $settings['siteurl']."viewpage.php?page_id=%s",
-    'UP' => $settings['siteurl']."profile.php?lookup=%s"
-];
-$link_type += \PHPFusion\Admins::getInstance()->getLinkType();
-
+$comments_type = array(
+	'N' => $locale['269'],
+	'D' => $locale['268'],
+	'P' => $locale['272'],
+	'A' => $locale['270'],
+	'B' => $locale['269b'],
+	'C' => $locale['272a'],
+	'PH' => $locale['261'],
+);
+$submit_type = array(
+	'n' => $locale['269'],
+	'd' => $locale['268'],
+	'p' => $locale['272'],
+	'a' => $locale['270'],
+	'l' => $locale['271'],
+	'b' => $locale['269b'],
+);
+$link_type = array(
+	'N' => fusion_get_settings("siteurl")."news.php?readmore=%s",
+	'D' => fusion_get_settings("siteurl")."downloads.php?download_id=%s",
+	'P' => fusion_get_settings("siteurl")."photogallery.php?photo_id=%s",
+	'A' => fusion_get_settings("siteurl")."articles.php?article_id=%s",
+	'B' => fusion_get_settings("siteurl")."blog.php?readmore=%s",
+	'C' => fusion_get_settings("siteurl")."viewpage.php?page_id=%s",
+	'PH' => fusion_get_settings("siteurl")."photogallery.php?photo_id=%s",
+);
 // Infusions count
 $infusions_count = dbcount("(inf_id)", DB_INFUSIONS);
-$global_infusions = [];
+$global_infusions = array();
 if ($infusions_count > 0) {
-    $inf_result = dbquery("SELECT *
-        FROM ".DB_INFUSIONS."
-        ORDER BY inf_id ASC
-    ");
-    while ($_inf = dbarray($inf_result)) {
-        $global_infusions[$_inf['inf_id']] = $_inf;
-    }
+	$inf_result = dbquery("SELECT * FROM ".DB_INFUSIONS." ORDER BY inf_id ASC");
+	while ($_inf = dbarray($inf_result)) {
+		$global_infusions[$_inf['inf_id']] = $_inf;
+	}
 }
-
 // Latest Comments
-$global_comments = [];
 $global_comments['rows'] = dbcount("('comment_id')", DB_COMMENTS);
 $_GET['c_rowstart'] = isset($_GET['c_rowstart']) && $_GET['c_rowstart'] <= $global_comments['rows'] ? $_GET['c_rowstart'] : 0;
 $comments_result = dbquery("SELECT c.*, u.user_id, u.user_name, u.user_status, u.user_avatar
-    FROM ".DB_COMMENTS." c
-    LEFT JOIN ".DB_USERS." u on u.user_id=c.comment_name
-    ORDER BY comment_datestamp DESC LIMIT ".$_GET['c_rowstart'].", 5
-");
-
-if ($global_comments['rows'] > 10) {
-    $global_comments['comments_nav'] = makepagenav($_GET['c_rowstart'], 10, $global_comments['rows'], 2, FUSION_SELF.$aidlink.'&amp;pagenum=0&amp;', 'c_rowstart');
+							FROM ".DB_COMMENTS." c LEFT JOIN ".DB_USERS." u on u.user_id=c.comment_name
+							ORDER BY comment_datestamp DESC LIMIT ".$_GET['c_rowstart'].", ".$settings['comments_per_page']."
+							");
+if ($global_comments['rows'] > $settings['comments_per_page']) {
+	$global_comments['nav'] = makepagenav($_GET['c_rowstart'], $settings['comments_per_page'], $global_comments['rows'], 2);
 }
-
-$global_comments['data'] = [];
-
+$global_comments['data'] = array();
 if (dbrows($comments_result)) {
-    while ($_comdata = dbarray($comments_result)) {
-        $global_comments['data'][] = $_comdata;
-    }
+	while ($_comdata = dbarray($comments_result)) {
+		$global_comments['data'][] = $_comdata;
+	}
 } else {
-    $global_comments['nodata'] = $locale['254c'];
+	$global_comments['nodata'] = $locale['254c'];
 }
-
 // Latest Ratings
-$global_ratings = [];
 $global_ratings['rows'] = dbcount("('rating_id')", DB_RATINGS);
 $_GET['r_rowstart'] = isset($_GET['r_rowstart']) && $_GET['r_rowstart'] <= $global_ratings['rows'] ? $_GET['r_rowstart'] : 0;
 $result = dbquery("SELECT r.*, u.user_id, u.user_name, u.user_status, u.user_avatar
-    FROM ".DB_RATINGS." r
-    LEFT JOIN ".DB_USERS." u on u.user_id=r.rating_user
-    ORDER BY rating_datestamp DESC LIMIT ".$_GET['r_rowstart'].", 5
-");
-
-$global_ratings['data'] = [];
+					FROM ".DB_RATINGS." r LEFT JOIN ".DB_USERS." u on u.user_id=r.rating_user
+					ORDER BY rating_datestamp DESC LIMIT ".$_GET['r_rowstart'].", ".$settings['comments_per_page']."
+					");
+$global_ratings['data'] = array();
 if (dbrows($result) > 0) {
-    while ($_ratdata = dbarray($result)) {
-        $global_ratings['data'][] = $_ratdata;
-    }
+	while ($_ratdata = dbarray($result)) {
+		$global_ratings['data'][] = $_ratdata;
+	}
 } else {
-    $global_ratings['nodata'] = $locale['254b'];
+	$global_ratings['nodata'] = $locale['254b'];
 }
-
-if ($global_ratings['rows'] > 10) {
-    $global_ratings['ratings_nav'] = makepagenav($_GET['r_rowstart'], 10, $global_comments['rows'], 2, FUSION_SELF.$aidlink.'&amp;pagenum=0&amp;', 'r_rowstart');
+if ($global_ratings['rows'] > $settings['comments_per_page']) {
+	$global_ratings['ratings_nav'] = makepagenav($_GET['r_rowstart'], $settings['comments_per_page'], $global_ratings['rows'], 2);
 }
-
 // Latest Submissions
-$global_submissions = [];
 $global_submissions['rows'] = dbcount("('submit_id')", DB_SUBMISSIONS);
 $_GET['s_rowstart'] = isset($_GET['s_rowstart']) && $_GET['s_rowstart'] <= $global_submissions['rows'] ? $_GET['s_rowstart'] : 0;
 $result = dbquery("SELECT s.*, u.user_id, u.user_name, u.user_status, u.user_avatar
-    FROM ".DB_SUBMISSIONS." s
-    LEFT JOIN ".DB_USERS." u on u.user_id=s.submit_user
-    ORDER BY submit_datestamp DESC LIMIT ".$_GET['s_rowstart'].", 5
-");
-
-$global_submissions['data'] = [];
-
+				FROM ".DB_SUBMISSIONS." s LEFT JOIN ".DB_USERS." u on u.user_id=s.submit_user
+				ORDER BY submit_datestamp DESC LIMIT ".$_GET['s_rowstart'].", ".$settings['comments_per_page']."
+				");
+$global_submissions['data'] = array();
 if (dbrows($result) > 0 && checkrights('SU')) {
-    while ($_subdata = dbarray($result)) {
-        $global_submissions['data'][] = $_subdata;
-    }
+	while ($_subdata = dbarray($result)) {
+		$global_submissions['data'][] = $_subdata;
+	}
 } else {
-    $global_submissions['nodata'] = $locale['254a'];
+	$global_submissions['nodata'] = $locale['254a'];
 }
-
-if ($global_submissions['rows'] > 10) {
-    $global_submissions['submissions_nav'] = makepagenav($_GET['s_rowstart'], 10, $global_submissions['rows'], 2, FUSION_SELF.$aidlink.'&amp;pagenum=0&amp;', 's_rowstart');
+if ($global_submissions['rows'] > $settings['comments_per_page']) {
+	$global_submissions['submissions_nav'] = "<span class='pull-right text-smaller'>".makepagenav($_GET['s_rowstart'], $settings['comments_per_page'], $global_submissions['rows'], 2)."</span>\n";
 }
-
 // Icon Grid
 if (isset($_GET['pagenum']) && isnum($_GET['pagenum'])) {
-
-    $result = dbquery("SELECT * FROM ".DB_ADMIN." WHERE admin_page='".$_GET['pagenum']."' AND admin_language='".LANGUAGE."' ORDER BY admin_title");
-    $admin_icons['rows'] = dbrows($result);
-    $admin_icons['data'] = [];
-    if (dbrows($result)) {
-        while ($_idata = dbarray($result)) {
-            if (checkrights($_idata['admin_rights']) && $_idata['admin_link'] != "reserved") {
-                // Current locale file have the admin title definitions paired by admin_rights.
-                if ($_idata['admin_page'] !== 5) {
-                    $_idata['admin_title'] = isset($locale[$_idata['admin_rights']]) ? $locale[$_idata['admin_rights']] : $_idata['admin_title'];
-                }
-                $admin_icons['data'][] = $_idata;
-            }
-        }
-    }
+	$result = dbquery("SELECT * FROM ".DB_ADMIN." WHERE admin_page='".$_GET['pagenum']."' ORDER BY admin_title");
+	$admin_icons['rows'] = dbrows($result);
+	$admin_icons['data'] = array();
+	if (dbrows($result)) {
+		while ($_idata = dbarray($result)) {
+			if (checkrights($_idata['admin_rights']) && $_idata['admin_link'] != "reserved") {
+				// Current locale file have the admin title definitions paired by admin_rights.
+				if ($_idata['admin_page'] !== 5) {
+					$_idata['admin_title'] = isset($locale[$_idata['admin_rights']]) ? $locale[$_idata['admin_rights']] : $_idata['admin_title'];
+				}
+				$admin_icons['data'][] = $_idata;
+			}
+		}
+	}
 }
-
 render_admin_dashboard();
 require_once THEMES."templates/footer.php";
