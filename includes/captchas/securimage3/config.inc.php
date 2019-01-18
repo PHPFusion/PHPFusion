8a54ffa2a6ae1a97329ddfc2b626a7b5cac434ff
Falk <falk@php-fusion.co.uk>
2019-01-18 19:44:04 +0100

    need session check

--- includes/captchas/securimage3/config.inc.php
+++ includes/captchas/securimage3/config.inc.php
@@ -2,6 +2,8 @@
 require_once __DIR__.'../../../../maincore.php';
 require_once __DIR__.'../../../../config.php';
 
+@session_start();
+
 return [
     'session_name'           => COOKIE_PREFIX.'session',
     'wordlist_file_encoding' => 'UTF-8'
