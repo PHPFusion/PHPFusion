89bd4e5b38a669d54dffecadc81501b8b64fc289
robinn1 <kelcakrobo@gmail.com>
2019-02-05 17:12:38 +0100

    Update pdo_functions_include.php

--- includes/db_handlers/pdo_functions_include.php
+++ includes/db_handlers/pdo_functions_include.php
@@ -149,7 +149,9 @@ function dbconnect($db_host, $db_user, $db_pass, $db_name, $db_port = 3306) {
     $db_select = TRUE;
     $halt_on_error = TRUE;
     try {
-        $pdo = dbconnection(new PDO("mysql:host=".$db_host.";dbname=".$db_name.";charset=utf8mb4;port=".$db_port, $db_user, $db_pass));
+        $pdo = dbconnection(new PDO("mysql:host=".$db_host.";dbname=".$db_name.";charset=utf8mb4;port=".$db_port, $db_user, $db_pass, [
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'"
+        ]));
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
     } catch (PDOException $error) {
