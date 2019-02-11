81dd8c74da7316d2d44206fd464cd1bb70d0f6ef
Falk <falk@php-fusion.co.uk>
2019-02-11 11:31:54 +0100

    Hide the Filemanager operating dir

--- includes/filemanager/config/config.php
+++ includes/filemanager/config/config.php
@@ -418,7 +418,7 @@ $config = array(
     // Hidden files and folders
     //**********************
     // set the names of any folders you want hidden (eg "hidden_folder1", "hidden_folder2" ) Remember all folders with these names will be hidden (you can set any exceptions in config.php files on folders)
-    'hidden_folders'                          => array(),
+    'hidden_folders'                          => array("thumbs"),
     // set the names of any files you want hidden. Remember these names will be hidden in all folders (eg "this_document.pdf", "that_image.jpg" )
     'hidden_files'                            => array( 'config.php' ),
 
