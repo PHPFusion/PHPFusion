f5eebc56684d02136f3400f20249084844dd932f
robinn1 <RobiNN1@users.noreply.github.com>
2019-03-15 23:15:23 +0100

    2.0

--- infusion.php
+++ infusion.php
@@ -22,7 +22,7 @@ $locale = fusion_get_locale("", LOCALE.LOCALESET."setup.php");
 // Infusion general information
 $inf_title = $locale['weblinks']['title'];
 $inf_description = $locale['weblinks']['description'];
-$inf_version = "1.2";
+$inf_version = "2.0";
 $inf_developer = "PHP Fusion Development Team";
 $inf_email = "info@php-fusion.co.uk";
 $inf_weburl = "https://www.php-fusion.co.uk";
