6ae8e101501615b01b4c473ca7fb6aae6e34f1f1
deviance.c <meangczac.chan@gmail.com>
2019-05-11 13:21:39 +0800

    Demo new sanitizer() function without using $_POST - removed print_p debugger previous commit.

--- test/defender/sanitizer.php
+++ test/defender/sanitizer.php
@@ -6,8 +6,13 @@ require_once THEMES.'templates/header.php';
  */
 if (post('save')) {
     $data = [
-        'input_string' => sanitizer('input_string', 'No input string', 'input_string'),
-        'input_name_arr' => sanitizer(['input_name_arr'], 'No input array', 'input_name_arr'),
+        'input_string'    => sanitizer('input_string', 'No input string', 'input_string'),
+        'input_name_arr'  => sanitizer(['input_name_arr'], 'No input array', 'input_name_arr'),
+        'all_names'       => sanitizer(['name'], 'No input array'), // can't target error since this is all 4 fields
+        'name_prefix'     => sanitizer(['name', 'name_prefix'], 'No input', 'name[name_prefix]'), // targeting name prefix specifically
+        'name_firstname'  => sanitizer(['name', 'first_name'], 'No input', 'name[first_name]'), // targeting first_name specifically
+        'name_middlename' => sanitizer(['name', 'middle_name'], 'No input', 'name[middle_name]'), // targeting middle_name prefix specifically
+        'name_lastname'   => sanitizer(['name', 'last_name'], 'No input', 'name[last_name]'), // targeting last_name prefix specifically
     ];
     print_p($data);
 }
@@ -16,12 +21,20 @@ echo "<div class='spacer-md'>\n<h3>\nTest Sanitizer\n</h3>\n";
 echo openform('inputform', 'post').
     form_text('input_string', 'Enter anything', '').
     form_select('input_name_arr[]', 'Select Groups', '', [
-        'options'=>fusion_get_groups(),
-        'multiple' => TRUE,
-        'width' => '100%',
+        'options'     => fusion_get_groups(),
+        'multiple'    => TRUE,
+        'width'       => '100%',
         'inner_width' => '100%',
-        ]).
-    form_button('save', 'Test sanitization', 'save', ['class'=>'btn-primary']).
+    ]).
+    "<div class='spacer-sm'>
+        <div class='display-flex'>".
+    form_select('name[name_prefix]', 'Salutations', '', ['required' => TRUE, 'class' => 'display-inline-block', 'inner_width' => '100px', 'options' => ['Mr' => 'Mr.', 'Ms' => 'Ms.', 'Dr' => 'Dr.']]).
+    form_text('name[first_name]', 'First Name', '', ['required' => TRUE, 'class' => 'display-inline-block']).
+    form_text('name[middle_name]', 'Middle Name', '', ['required' => TRUE, 'class' => 'display-inline-block']).
+    form_text('name[last_name]', 'Last Name', '', ['required' => TRUE, 'class' => 'display-inline-block']).
+    "</div>
+    </div>".
+    form_button('save', 'Test sanitization', 'save', ['class' => 'btn-primary']).
     closeform();
 echo "</div>\n";
 echo "</div>\n";
