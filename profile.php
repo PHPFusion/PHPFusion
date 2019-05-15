3d2f50975ff805ccd184953b464eb3da0b9628d5
deviance.c <meangczac.chan@gmail.com>
2019-05-15 11:25:27 +0800

    Profile page development update

--- profile.php
+++ profile.php
@@ -15,54 +15,29 @@
 | copyright header is strictly prohibited without
 | written permission from the original author(s).
 +--------------------------------------------------------*/
-require_once __DIR__.'/maincore.php';
-require_once THEMES.'templates/header.php';
-$locale = fusion_get_locale('', LOCALE.LOCALESET."user_fields.php");
+require_once __DIR__."/maincore.php";
+require_once THEMES."templates/header.php";
+$locale = fusion_get_locale("", LOCALE.LOCALESET."user_fields.php");
 $settings = fusion_get_settings();
-if (isset($_GET['lookup']) && isnum($_GET['lookup'])) {
+$profile_id = get("lookup", FILTER_VALIDATE_INT);
+$group_id = get("group_id", FILTER_VALIDATE_INT);
+if ($profile_id) {
+    /*
+     * Show user profile
+     */
     require_once THEMES."templates/global/profile.php";
-    if (!iMEMBER && $settings['hide_userprofiles'] == 1) {
-        redirect(BASEDIR."index.php");
-    }
-    $user_status = " AND (user_status='0' OR user_status='3' OR user_status='7')";
-    if (iADMIN) {
-        $user_status = "";
-    }
-    $user_data = [];
-    $result = dbquery("SELECT u.*, s.suspend_reason
-        FROM ".DB_USERS." u
-        LEFT JOIN ".DB_SUSPENDS." s ON u.user_id=s.suspended_user
-        WHERE user_id=:uid".$user_status."
-        ORDER BY suspend_date DESC
-        LIMIT 1", [':uid' => intval($_GET['lookup'])]);
-    if (dbrows($result)) {
-        $user_data = dbarray($result);
-    } else {
-        redirect(BASEDIR."index.php");
-    }
-    set_title($locale['u103'].$locale['global_201'].$user_data['user_name']);
-    if (iADMIN && checkrights("UG") && $_GET['lookup'] != $user_data['user_id']) {
-        if ((isset($_POST['add_to_group'])) && (isset($_POST['user_group']) && isnum($_POST['user_group']))) {
-            if (!preg_match("(^\.{$_POST['user_group']}$|\.{$_POST['user_group']}\.|\.{$_POST['user_group']}$)", $user_data['user_groups'])) {
-                $result = dbquery("UPDATE ".DB_USERS." SET user_groups='".$user_data['user_groups'].".".$_POST['user_group']."' WHERE user_id='".$_GET['lookup']."'");
-            }
-            redirect(FUSION_SELF."?lookup=".$_GET['lookup']);
-        }
-    }
     $userFields = new PHPFusion\UserFields();
-    $userFields->user_data = $user_data;
     $userFields->show_admin_options = TRUE;
     $userFields->method = 'display';
     $userFields->display_profile_output();
 
-    PHPFusion\OpenGraph::ogUserProfile($_GET['lookup']);
-
-} else if (isset($_GET['group_id']) && isnum($_GET['group_id'])) {
+} else if ($group_id) {
     /*
      * Show group
      */
-    \PHPFusion\UserGroups::getInstance()->setGroup($_GET['group_id'])->showGroup();
+    \PHPFusion\UserGroups::getInstance()->setGroup($group_id)->showGroup();
 } else {
     redirect(BASEDIR."index.php");
 }
-require_once THEMES.'templates/footer.php';
+
+require_once THEMES."templates/footer.php";
--- themes/templates/global/profile.php
+++ themes/templates/global/profile.php
@@ -298,6 +298,139 @@ if (!function_exists('display_profile_form')) {
     }
 }
 
+if (!function_exists('display_profile')) {
+    function display_profile($info) {
+
+        $locale = fusion_get_locale();
+        add_to_head("<link href='".THEMES."templates/global/css/profile.css' rel='stylesheet'/>");
+        $tpl = \PHPFusion\Template::getInstance('user_profile');
+        $tpl->set_template(__DIR__.'/tpl/user_profile.html');
+        $tpl->set_locale($locale);
+
+        foreach($info['profile_pages'] as $profile_page) {
+            $tpl->set_block('profile_nav', $profile_page);
+        }
+        $tpl->set_tag("profile_content", $info['profile_content']);
+        $tpl->set_tag('opentable', fusion_get_function('opentable', ''));
+        $tpl->set_tag('closetable', fusion_get_function('closetable'));
+
+        $user_name = '';
+        $user_avatar = '';
+        $user_level = '';
+        $basic_info = '';
+
+        // Basic User Information
+        if (!empty($info['core_field'])) {
+            // Core field put on top
+            $basic_info = '';
+            foreach ($info['core_field'] as $field_id => $field_data) {
+                // Sets data to core field block
+                $tpl->set_block($field_id, $field_data);
+                $skip = [
+                    'profile_user_avatar',
+                    'profile_user_name',
+                    'profile_user_level',
+                    'profile_user_group'
+                ];
+                if (!in_array($field_id, $skip)) {
+                    $tpl->set_block('user_core_fields', $field_data);
+                }
+
+                // old method
+                switch ($field_id) {
+                    case 'profile_user_group':
+                        if (!empty($field_data['value'])) {
+                            foreach ($field_data['value'] as $groups) {
+                                $tpl->set_block('user_groups', $groups);
+                            }
+                        } else {
+                            $tpl->set_block('user_group_na', []);
+                        }
+                        break;
+                    case 'profile_user_avatar':
+                        $avatar['user_id'] = $info['user_id'];
+                        $avatar['user_name'] = $info['user_name'];
+                        $avatar['user_avatar'] = $field_data['value'];
+                        $avatar['user_status'] = $field_data['status'];
+                        $user_avatar = display_avatar($avatar, '130px', 'profile-avatar', FALSE, 'img-responsive');
+                        break;
+                    case 'profile_user_name':
+                        $user_name = $field_data['value'];
+                        break;
+                    case 'profile_user_level':
+                        $user_level = $field_data['value'];
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        //User Fields Module Information
+        if (!empty($info['user_field'])) {
+            // first we need to identify the wrapper
+            foreach ($info['user_field'] as $catID => $categoryData) {
+                $tpl2 = \PHPFusion\Template::getInstance('user_fields');
+                $tpl2->set_template(__DIR__.'/tpl/user_profile_fields.html');
+                if (!empty($categoryData['fields'])) {
+                    foreach ($categoryData['fields'] as $_id => $_fields) {
+                        if (!empty($_fields)) {
+                            if (isset($_fields['type']) && $_fields['type'] == 'social') {
+                                $tpl->set_block('social_icons', $_fields);
+                            } else {
+                                $block_type = ($_fields['title'] ? 'user_fields_inline' : 'user_fields');
+                                $tpl2->set_block($block_type, [
+                                    'id'    => $_id,
+                                    'icon'  => !empty($_fields['icon']) ? $_fields['icon'] : '',
+                                    'title' => $_fields['title'],
+                                    'value' => $_fields['value']
+                                ]);
+                            }
+                        }
+                    }
+                    if (!empty($categoryData['title'])) {
+                        $tpl2->set_block('user_fields_cat', ['category_title' => $categoryData['title']]);
+                    }
+                    $tpl->set_block('fields_block', ["fields" => $tpl2->get_output()]);
+                }
+            }
+        } else {
+            $info['no_fields'] = $locale['uf_108'];
+        }
+
+        // Tabs
+        if (!empty($info['section'])) {
+            foreach ($info['section'] as $page_section) {
+                $tab_title['title'][$page_section['id']] = $page_section['name'];
+                $tab_title['id'][$page_section['id']] = $page_section['id'];
+                $tab_title['icon'][$page_section['id']] = $page_section['icon'];
+            }
+        }
+
+        $tpl->set_tag('tab_header', (isset($tab_title) ? opentab($tab_title, $info['section_id'], 'profile_tab', TRUE, FALSE, 'section') : ''));
+        $tpl->set_tag('user_name', $user_name);
+        $tpl->set_tag('user_avatar', $user_avatar);
+        $tpl->set_tag('user_level', $user_level);
+
+        if (!empty($info['user_admin'])) {
+            $tpl->set_block('user_admin', $info['user_admin']);
+        }
+        if (!empty($info['group_admin'])) {
+            $tpl->set_block('group_admin', $info['group_admin']);
+        }
+        if (!empty($info['buttons'])) {
+            $tpl->set_block('buttons', $info['buttons']);
+        }
+
+        $tpl->set_tag('basic_info', $basic_info);
+        $tpl->set_tag('tab_footer', (isset($tab_title) ? closetab() : ''));
+        $tpl->set_tag('no_fields', (!empty($info['no_fields']) ? $info['no_fields'] : ''));
+
+        return $tpl->get_output();
+
+    }
+}
+
 /**
  * Profile display view
  * @param $info (array) - prepared responsive fields
@@ -310,11 +443,9 @@ if (!function_exists('display_profile_form')) {
 if (!function_exists('display_user_profile')) {
     function display_user_profile($info) {
         $locale = fusion_get_locale();
-        add_to_head("<link href='".THEMES."templates/global/css/profile.css' rel='stylesheet'/>");
-
         $tpl = \PHPFusion\Template::getInstance('user_profile');
         $tpl->set_template(__DIR__.'/tpl/user_profile.html');
-        $tpl->set_locale(fusion_get_locale());
+        $tpl->set_locale($locale);
 
         $tpl->set_tag('opentable', fusion_get_function('opentable', ''));
         $tpl->set_tag('closetable', fusion_get_function('closetable'));
@@ -412,7 +543,7 @@ if (!function_exists('display_user_profile')) {
             }
         }
 
-        $tpl->set_tag('tab_header', (isset($tab_title) ? opentab($tab_title, $_GET['section'], 'profile_tab', TRUE, FALSE, 'section') : ''));
+        $tpl->set_tag('tab_header', (isset($tab_title) ? opentab($tab_title, $info['section_id'], 'profile_tab', TRUE, FALSE, 'section') : ''));
         $tpl->set_tag('user_name', $user_name);
         $tpl->set_tag('user_avatar', $user_avatar);
         $tpl->set_tag('user_level', $user_level);
