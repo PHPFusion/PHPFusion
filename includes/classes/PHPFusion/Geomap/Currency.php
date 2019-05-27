e107c7e35071c43e363a3b438b94c21ed66f607d
deviance.c <meangczac.chan@gmail.com>
2019-05-27 20:13:49 +0800

    Add Geomap folder with Ajax PHP

---
+++ includes/classes/PHPFusion/Geomap/Currency.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__.'/../../../../../maincore.php';
+
+/**
+ * $q - iso2 country code
+ */
+if (!empty($_REQUEST['q'])) {
+    $_REQUEST['q'] = stripinput($_REQUEST['q']);
+    $result = \PHPFusion\Geomap::get_Currency($_REQUEST['q']);
+    if (!empty($result)) {
+        header('Content-Type: application/json');
+        echo json_encode($result);
+    }
+}
