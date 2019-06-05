91605aaf732989034147348ab6ad04e9af8088f5
robinn1 <kelcakrobo@gmail.com>
2019-06-05 18:23:48 +0200

    Update files

--- themes/Atom9/locale/English.php
+++ themes/Atom9/locale/English.php
@@ -1,21 +1,4 @@
 <?php
-/*-------------------------------------------------------+
-| PHP-Fusion Content Management System
-| Copyright (C) PHP-Fusion Inc
-| https://www.php-fusion.co.uk/
-+--------------------------------------------------------+
-| Filename: Atom9/locale/English.php
-| Author: Frederick MC Chan (Chan)
-| Author: RobiNN
-+--------------------------------------------------------+
-| This program is released as free software under the
-| Affero GPL license. You can redistribute it and/or
-| modify it under the terms of this license which you
-| can read by viewing the included agpl.txt or online
-| at www.gnu.org/licenses/agpl.html. Removal of this
-| copyright header is strictly prohibited without
-| written permission from the original author(s).
-+--------------------------------------------------------*/
 // Footer
 $locale['A9_001'] = 'Follow us on:';
 $locale['A9_002'] = 'About Us';
--- widgets/slider/locale/English.php
+++ widgets/slider/locale/English.php
@@ -1,20 +1,4 @@
 <?php
-/*-------------------------------------------------------+
-| PHP-Fusion Content Management System
-| Copyright (C) PHP-Fusion Inc
-| https://www.php-fusion.co.uk/
-+--------------------------------------------------------+
-| Filename: slider/locale/English.php
-| Author: Frederick MC Chan (Chan)
-+--------------------------------------------------------+
-| This program is released as free software under the
-| Affero GPL license. You can redistribute it and/or
-| modify it under the terms of this license which you
-| can read by viewing the included agpl.txt or online
-| at www.gnu.org/licenses/agpl.html. Removal of this
-| copyright header is strictly prohibited without
-| written permission from the original author(s).
-+--------------------------------------------------------*/
 $locale['SLDW_0100'] = "Carousel";
 $locale['SLDW_0101'] = "Slider Plugin";
 $locale['SLDW_0200'] = "Slide Deleted";
