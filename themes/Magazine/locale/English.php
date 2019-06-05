a27843da14c07f89ba2df80b831d04ec673ffb89
robinn1 <kelcakrobo@gmail.com>
2019-06-05 18:33:21 +0200

    Update files

--- themes/Magazine/locale/English.php
+++ themes/Magazine/locale/English.php
@@ -1,20 +1,4 @@
 <?php
-/*-------------------------------------------------------+
-| PHP-Fusion Content Management System
-| Copyright (C) PHP-Fusion Inc
-| https://www.php-fusion.co.uk/
-+--------------------------------------------------------+
-| Filename: Magazine/locale/English.php
-| Author: RobiNN
-+--------------------------------------------------------+
-| This program is released as free software under the
-| Affero GPL license. You can redistribute it and/or
-| modify it under the terms of this license which you
-| can read by viewing the included agpl.txt or online
-| at www.gnu.org/licenses/agpl.html. Removal of this
-| copyright header is strictly prohibited without
-| written permission from the original author(s).
-+--------------------------------------------------------*/
 $locale['MG_description'] = 'Official theme for PHP-Fusion Andromeda 9.03';
 $locale['created_by']  = 'Created by';
 $locale['MG_001'] = 'Hi, ';
