b18dff8409b034ff8c89a6db69975b1edbf4a503
RobiNN <kelcakrobo@gmail.com>
2019-08-17 22:36:56 +0200

    Update contact.php

--- themes/templates/global/contact.php
+++ themes/templates/global/contact.php
@@ -22,19 +22,21 @@
  *
  * @return string
  */
-function render_contact_form($info) {
-    $locale = fusion_get_locale();
+if (!function_exists("render_contact_form")) {
+    function render_contact_form($info) {
+        $locale = fusion_get_locale();
 
-    $tpl = \PHPFusion\Template::getInstance('contact');
-    $tpl->set_template(__DIR__.'/tpl/contact.html');
-    $tpl->set_tag('message', $info['message']);
-    $tpl->set_tag('input_mailname', form_text('mailname', $locale['CT_402'], $info['input']['mailname'], ['required' => TRUE, 'error_text' => $locale['CT_420'], 'max_length' => 64]));
-    $tpl->set_tag('input_email', form_text('email', $locale['CT_403'], $info['input']['email'], ['required' => TRUE, 'error_text' => $locale['CT_421'], 'type' => 'email', 'max_length' => 64]));
-    $tpl->set_tag('input_subject', form_text('subject', $locale['CT_404'], $info['input']['subject'], ['required' => TRUE, 'error_text' => $locale['CT_422'], 'max_length' => 64]));
-    $tpl->set_tag('input_message', form_textarea('message', $locale['CT_405'], $info['input']['message'], ['required' => TRUE, 'error_text' => $locale['CT_423'], 'max_length' => 128]));
-    if (iGUEST) $tpl->set_block('input_captcha', ['captcha' => $info['captcha'], 'captcha_code' => $info['captcha_code']]);
-    $tpl->set_tag('input_button', form_button('sendmessage', $locale['CT_406'], $locale['CT_406'], ['class' => 'btn-primary', 'icon' => 'fa fa-send-o']));
+        $tpl = \PHPFusion\Template::getInstance('contact');
+        $tpl->set_template(__DIR__.'/tpl/contact.html');
+        $tpl->set_tag('message', $info['message']);
+        $tpl->set_tag('input_mailname', form_text('mailname', $locale['CT_402'], $info['input']['mailname'], ['required' => TRUE, 'error_text' => $locale['CT_420'], 'max_length' => 64]));
+        $tpl->set_tag('input_email', form_text('email', $locale['CT_403'], $info['input']['email'], ['required' => TRUE, 'error_text' => $locale['CT_421'], 'type' => 'email', 'max_length' => 64]));
+        $tpl->set_tag('input_subject', form_text('subject', $locale['CT_404'], $info['input']['subject'], ['required' => TRUE, 'error_text' => $locale['CT_422'], 'max_length' => 64]));
+        $tpl->set_tag('input_message', form_textarea('message', $locale['CT_405'], $info['input']['message'], ['required' => TRUE, 'error_text' => $locale['CT_423'], 'max_length' => 128]));
+        if (iGUEST)
+            $tpl->set_block('input_captcha', ['captcha' => $info['captcha'], 'captcha_code' => $info['captcha_code']]);
+        $tpl->set_tag('input_button', form_button('sendmessage', $locale['CT_406'], $locale['CT_406'], ['class' => 'btn-primary', 'icon' => 'fa fa-send-o']));
 
-    return fusion_get_function('opentable', $locale['CT_400']).$tpl->get_output().fusion_get_function('closetable');
-
+        return fusion_get_function('opentable', $locale['CT_400']).$tpl->get_output().fusion_get_function('closetable');
+    }
 }
