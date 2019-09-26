0e3ab8c7d7631a781bde26f3350592f0c2fb620e
RobiNN <kelcakrobo@gmail.com>
2019-09-26 21:56:42 +0200

    Fix search

--- includes/bbcodes/search_bbcode_include_var.php
+++ includes/bbcodes/search_bbcode_include_var.php
@@ -52,7 +52,14 @@ if (!function_exists("generate_search_opts")) {
                             if (preg_match("/_include.php/i", $file)) {
                                 $name = '';
                                 $search_name = explode("_", $file);
-                                $locale += fusion_get_locale('', INFUSIONS.$infusions_to_check.'/locale/'.LOCALESET."search/".$search_name[1].".php");
+
+                                if (file_exists(INFUSIONS.$infusions_to_check.'/locale/'.LOCALESET."search/".$search_name[1].".php")) {
+                                    $locale_file = INFUSIONS.$infusions_to_check.'/locale/'.LOCALESET."search/".$search_name[1].".php";
+                                } else {
+                                    $locale_file = INFUSIONS.$infusions_to_check."/locale/English/search/".$search_name[1].".php";
+                                }
+
+                                $locale += fusion_get_locale('', $locale_file);
                                 foreach ($locale as $key => $value) {
                                     if (preg_match("/400/i", $key)) {
                                         $name = $key;
@@ -60,7 +67,7 @@ if (!function_exists("generate_search_opts")) {
                                 }
 
                                 if (isset($locale[$name])) {
-                                    $generated .= "<input type='button' value='".$locale[$name]."' class='button btn btn-link btn-block btn-xs' onclick=\"addText('".$textarea_name."', '[search=".$search_name[1]."]', '[/search]', '".$inputform_name."');return false;\"/>";
+                                    $generated .= "<input type='button' value='".(!empty($locale[$name]) ? $locale[$name] : $name)."' class='button btn btn-link btn-block btn-xs' onclick=\"addText('".$textarea_name."', '[search=".$search_name[1]."]', '[/search]', '".$inputform_name."');return false;\"/>";
                                 }
                             }
                         }
