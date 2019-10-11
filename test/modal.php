162a0c90b357aa271a1c2f908edbcb414da4180b
Deviance <meangczac.chan@gmail.com>
2019-10-11 08:18:08 +0800

    Select

--- test/modal.php
+++ test/modal.php
@@ -13,10 +13,20 @@ $options = [
     6 => 'Strawberries',
     7 => 'Kiwi'
 ];
-$modal_1 = openmodal('first', 'This is the first modal').lorem_ipsum(300).form_select('plug', 'Plugin', '', ['id' => 'drop1', 'inline'=>TRUE, 'options'=>$options]).closemodal();
-$modal_2 = openmodal('second', 'This is the Second modal').lorem_ipsum(300).form_select('plug2', 'Plugin 2', '', ['id' => 'drop2', 'inline'=>TRUE, 'options'=>$options]).closemodal();
+$modal_1 = openmodal('first', 'This is the first modal').lorem_ipsum(300).form_select('plug', 'Plugin', '', ['input_id' => 'drop1', 'inline'=>TRUE, 'options'=>$options]).closemodal();
+$modal_2 = openmodal('second', 'This is the Second modal').lorem_ipsum(300).form_select('plug2', 'Plugin 2', '', ['input_id' => 'drop2', 'inline'=>TRUE, 'options'=>$options]).closemodal();
 
 add_to_footer($modal_1);
 add_to_footer($modal_2);
+echo "<style>#plug2 {
+z-index: 9999999999999999999999;
+}</style>";
+add_to_jquery("
+ $('select').each(function () {
+                $(this).select2({
+                    dropdownParent: $(this).parent()
+                });
+            });
+");
 
 require_once THEMES.'templates/footer.php';
