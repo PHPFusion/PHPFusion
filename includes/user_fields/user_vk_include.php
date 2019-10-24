906ded9d023d182293446a06b2ae2d1b7b41c199
Marcel Pol <marcel@timelord.nl>
2019-10-24 13:30:52 +0200

    User Fields should disable referrer on external links.

--- includes/user_fields/user_vk_include.php
+++ includes/user_fields/user_vk_include.php
@@ -31,7 +31,7 @@ if ($profile_method == "input") {
     $link = '';
     if ($field_value) {
         $link = !preg_match("@^https\:\/\/@i", $field_value) ? "https://vk.com/".$field_value : $field_value;
-        $field_value = (fusion_get_settings('index_url_userweb') ? "" : "<!--noindex-->")."<a href='".$link."' title='".$field_value."' ".(fusion_get_settings('index_url_userweb') ? "" : "rel='nofollow' ")."target='_blank'>".$locale['uf_vk_desc']."</a>".(fusion_get_settings('index_url_userweb') ? "" : "<!--/noindex-->");
+        $field_value = (fusion_get_settings('index_url_userweb') ? "" : "<!--noindex-->")."<a href='".$link."' title='".$field_value."' ".(fusion_get_settings('index_url_userweb') ? "" : "rel='nofollow noopener noreferrer' ")."target='_blank'>".$locale['uf_vk_desc']."</a>".(fusion_get_settings('index_url_userweb') ? "" : "<!--/noindex-->");
     }
     $user_fields = [
         'icon'  => $icon,
