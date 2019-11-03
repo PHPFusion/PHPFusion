1a8af6455fde7c8708a55ad1baaf4b12428cf41e
Deviance <meangczac.chan@gmail.com>
2019-11-03 22:22:56 +0800

    Update Forum

--- submit.php
+++ submit.php
@@ -15,18 +15,24 @@
 | copyright header is strictly prohibited without
 | written permission from the original author(s).
 +--------------------------------------------------------*/
+
+use PHPFusion\Admins;
+
 require_once __DIR__.'/maincore.php';
-if (!iMEMBER) {
-    redirect(BASEDIR.'index.php');
+if ( !iMEMBER ) {
+    redirect( BASEDIR.'index.php' );
 }
 require_once THEMES.'templates/header.php';
-$modules = \PHPFusion\Admins::getInstance()->getSubmitData();
+$modules = Admins::getInstance()->getSubmitData();
 
-$_GET['stype'] = !empty($_GET['stype']) && isset($modules[$_GET['stype']]) ? $_GET['stype'] : "";
-
-if (!empty($modules) && $_GET['stype']) {
-    require_once $modules[$_GET['stype']]['link'];
-} else {
-    redirect(BASEDIR.'index.php');
+$stype = get( 'stype' );
+if ( !isset( $modules[ $stype ] ) ) {
+    $stype = '';
 }
+if ( !empty( $modules ) && $stype ) {
+    require_once $modules[ $stype ]['link'];
+} else {
+    redirect( BASEDIR.'index.php' );
+}
+
 require_once THEMES.'templates/footer.php';
