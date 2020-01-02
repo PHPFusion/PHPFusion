02085dbc145f2c883447367b56b37a77cec4408e
karrak1 <g.sebestyen@fusionjatek.hu>
2020-01-02 15:20:57 +0100

    check_post() test

--- administration/settings_messages.php
+++ administration/settings_messages.php
@@ -17,88 +17,88 @@
 +--------------------------------------------------------*/
 require_once __DIR__.'/../maincore.php';
 require_once THEMES.'templates/admin_header.php';
-pageAccess("S7");
+pageAccess( "S7" );
 
 $settings = fusion_get_settings();
 
-$locale = fusion_get_locale('', LOCALE.LOCALESET.'admin/settings.php');
+$locale = fusion_get_locale( '', LOCALE.LOCALESET.'admin/settings.php' );
 
-add_breadcrumb(['link' => ADMIN.'settings_messages.php'.fusion_get_aidlink(), 'title' => $locale['message_settings']]);
+add_breadcrumb( [ 'link' => ADMIN.'settings_messages.php'.fusion_get_aidlink(), 'title' => $locale['message_settings'] ] );
 
-if (post('save_settings')) {
+if ( check_post( 'save_settings' ) ) {
     $inputData = [
-        'pm_inbox_limit'   => sanitizer('pm_inbox_limit', '20', 'pm_inbox_limit'),
-        'pm_outbox_limit'  => sanitizer('pm_outbox_limit', '20', 'pm_outbox_limit'),
-        'pm_archive_limit' => sanitizer('pm_archive_limit', '20', 'pm_archive_limit'),
-        'pm_email_notify'  => (post('pm_email_notify') ? sanitizer('pm_email_notify', '1', 'pm_email_notify') : 0),
-        'pm_save_sent'     => (post('pm_save_sent') ? sanitizer('pm_save_sent', '1', 'pm_save_sent') : 0)
+        'pm_inbox_limit'   => sanitizer( 'pm_inbox_limit', '20', 'pm_inbox_limit' ),
+        'pm_outbox_limit'  => sanitizer( 'pm_outbox_limit', '20', 'pm_outbox_limit' ),
+        'pm_archive_limit' => sanitizer( 'pm_archive_limit', '20', 'pm_archive_limit' ),
+        'pm_email_notify'  => ( check_post( 'pm_email_notify' ) ? sanitizer( 'pm_email_notify', '1', 'pm_email_notify' ) : 0 ),
+        'pm_save_sent'     => ( check_post( 'pm_save_sent' ) ? sanitizer( 'pm_save_sent', '1', 'pm_save_sent' ) : 0 )
     ];
 
-    if (fusion_safe()) {
-        foreach ($inputData as $settings_name => $settings_value) {
+    if ( fusion_safe() ) {
+        foreach ( $inputData as $settings_name => $settings_value ) {
             $data = [
                 'settings_name'  => $settings_name,
                 'settings_value' => $settings_value
             ];
-            dbquery_insert(DB_SETTINGS, $data, 'update', ['primary_key' => 'settings_name']);
+            dbquery_insert( DB_SETTINGS, $data, 'update', [ 'primary_key' => 'settings_name' ] );
         }
 
-        addNotice('success', $locale['900']);
-        redirect(FUSION_REQUEST);
+        addNotice( 'success', $locale['900'] );
+        redirect( FUSION_REQUEST );
     }
 }
 
-if (post('delete-messages')) {
-    dbquery("TRUNCATE TABLE ".DB_MESSAGES);
-    addNotice('success', $locale['712']);
-    redirect(FUSION_REQUEST);
+if ( check_post( 'delete-messages' ) ) {
+    dbquery( "TRUNCATE TABLE ".DB_MESSAGES );
+    addNotice( 'success', $locale['712'] );
+    redirect( FUSION_REQUEST );
 }
 
-opentable($locale['message_settings']);
-echo openform('messagesfrm', 'post');
+opentable( $locale['message_settings'] );
+echo openform( 'messagesfrm', 'post' );
 echo "<p>".$locale['message_description']."</p>\n";
 echo "<hr/>\n";
 echo "<div class='".grid_row()."'>\n";
-echo "<div class='".grid_column_size(100, 20)."'>\n";
+echo "<div class='".grid_column_size( 100, 20 )."'>\n";
 echo "<h4 class='m-0'>".$locale['message_settings']."</h4>";
-echo "</div>\n<div class='".grid_column_size(100, 70)."'>\n";
-echo form_checkbox('pm_email_notify', $locale['709'], $settings['pm_email_notify'], ['reverse_label' => TRUE]);
-echo form_checkbox('pm_save_sent', $locale['710'], $settings['pm_save_sent'], ['reverse_label' => TRUE]);
+echo "</div>\n<div class='".grid_column_size( 100, 70 )."'>\n";
+echo form_checkbox( 'pm_email_notify', $locale['709'], $settings['pm_email_notify'], [ 'reverse_label' => TRUE ] );
+echo form_checkbox( 'pm_save_sent', $locale['710'], $settings['pm_save_sent'], [ 'reverse_label' => TRUE ] );
 echo "</div>\n</div>\n";
 echo "<hr/>\n";
 
-echo form_text('pm_inbox_limit', $locale['701'], $settings['pm_inbox_limit'], [
+echo form_text( 'pm_inbox_limit', $locale['701'], $settings['pm_inbox_limit'], [
     'type'        => 'number',
     'max_length'  => 2,
     'ext_tip'     => $locale['704'],
     'inner_width' => '100px',
     'inline'      => TRUE
-]);
-echo form_text('pm_outbox_limit', $locale['702'], $settings['pm_outbox_limit'], [
+] );
+echo form_text( 'pm_outbox_limit', $locale['702'], $settings['pm_outbox_limit'], [
     'type'        => 'number',
     'max_length'  => 2,
     'inner_width' => '100px',
     'inline'      => TRUE
-]);
-echo form_text('pm_archive_limit', $locale['703'], $settings['pm_archive_limit'], [
+] );
+echo form_text( 'pm_archive_limit', $locale['703'], $settings['pm_archive_limit'], [
     'type'        => 'number',
     'max_length'  => 2,
     'inner_width' => '100px',
     'inline'      => TRUE
-]);
+] );
 echo "<hr/>\n";
 
 // Danger zone
 echo "<div class='".grid_row()."'>\n";
-echo "<div class='".grid_column_size(100, 20)."'>\n";
-echo "</div>\n<div class='".grid_column_size(100, 70)."'>\n";
-openform('delete-pm', 'post', FUSION_REQUEST);
+echo "<div class='".grid_column_size( 100, 20 )."'>\n";
+echo "</div>\n<div class='".grid_column_size( 100, 70 )."'>\n";
+openform( 'delete-pm', 'post', FUSION_REQUEST );
 fusion_confirm_exit();
-add_to_jquery("$('#delete-messages').bind('click', function() { return confirm('".$locale['713']."'); });");
-echo form_button('delete-messages', $locale['714'], $locale['714'], ['class' => 'btn-danger', 'icon' => 'fa fa-trash-o']);
+add_to_jquery( "$('#delete-messages').bind('click', function() { return confirm('".$locale['713']."'); });" );
+echo form_button( 'delete-messages', $locale['714'], $locale['714'], [ 'class' => 'btn-danger', 'icon' => 'fa fa-trash-o' ] );
 echo "</div>\n</div>\n";
 
-echo form_button('save_settings', $locale['750'], $locale['750'], ['class' => 'btn-success']);
+echo form_button( 'save_settings', $locale['750'], $locale['750'], [ 'class' => 'btn-success' ] );
 echo closeform();
 closetable();
 require_once THEMES.'templates/footer.php';
