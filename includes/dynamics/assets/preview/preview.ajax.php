6164f1df0564ec21fee87c27b77ac67ee6bedc77
Deviance <meangczac.chan@gmail.com>
2020-01-03 21:19:50 +0800

    Fixes issues discovered by SEC Consult, Malaysia.

--- includes/dynamics/assets/preview/preview.ajax.php
+++ includes/dynamics/assets/preview/preview.ajax.php
@@ -15,87 +15,67 @@
 | copyright header is strictly prohibited without
 | written permission from the original author(s).
 +--------------------------------------------------------*/
+
+use PHPFusion\ImageRepo;
+
 require_once __DIR__."../../../../../maincore.php";
 require_once THEMES."templates/render_functions.php";
 $locale = fusion_get_locale();
+$text = strip_scripts( post( 'text' ) );
+$editor = post( 'editor' );
+$mode = post( 'mode' );
 // filter to relative path conversion
 echo "<div class='preview-response clearfix p-20'>\n";
+
 // Set get_image paths based on URI. This is ajax request file. It doesn't return a standard BASEDIR.
-$prefix_ = '';
-$text = post('text');
-$text = stripinput($text);
-$editor = post('editor');
-$url = post('url');
-$mode = post('mode');
-$attach = post('post_attach');
-
-if ($attach) {
-    require_once INFUSIONS.'forum/forum_include.php';
-}
-
-if (!fusion_get_settings("site_seo") && $url) {
-    $uri = pathinfo($url);
-    $count = substr($url, -1) == "/" ? substr_count($uri['dirname'], "/") : substr_count($uri['dirname'], "/") - 1;
+$prefix_ = "";
+if ( !fusion_get_settings( "site_seo" ) && check_post( 'url' ) ) {
+    $url = post( 'url' );
+    $uri = pathinfo( $url );
+    $count = substr( $url, -1 ) == "/" ? substr_count( $uri['dirname'], "/" ) : substr_count( $uri['dirname'], "/" ) - 1;
     $prefix_ = str_repeat("../", ($count >= 0) ? $count : 0);
     foreach (cache_smileys() as $smiley) {
         $smiley_path = fusion_get_settings('siteurl')."images/smiley/".$smiley['smiley_image'];
-        \PHPFusion\ImageRepo::setImage("smiley_".$smiley['smiley_text'], $smiley_path);
+        ImageRepo::setImage( "smiley_".$smiley['smiley_text'], $smiley_path );
     }
 }
-
-if ($editor == 'html') {
-    if ($attach) {
-        $text = parse_attach($text);
-    }
-    $text = htmlspecialchars($text);
-    $text = parsesmileys(nl2br(html_entity_decode(stripslashes($text))));
-
-    if ($mode == 'admin') {
-        $images = str_replace('../../../', '', IMAGES);
-        $text = str_replace(IMAGES, $images, $text);
-        if (defined('IMAGES_N')) {
-            $text = str_replace(IMAGES_N, $images, $text);
+switch ( $editor ) {
+    case 'html':
+        $text = htmlspecialchars( $text );
+        $text = parsesmileys( nl2br( html_entity_decode( stripslashes( $text ) ) ) );
+        if ( $mode == 'admin' ) {
+            $images = str_replace( '../../../', '', IMAGES );
+            $text = str_replace( IMAGES, $images, $text );
+            if ( defined( 'IMAGES_N' ) ) {
+                $text = str_replace( IMAGES_N, $images, $text );
+            }
+            $text = parse_imageDir( $text, $prefix_."images/" );
         }
-        $text = parse_imageDir($text, $prefix_."images/");
-    }
-
-
-    echo nl2br(html_entity_decode($text, ENT_QUOTES, $locale['charset'])) ?: "<p class='text-center'>".$locale['nopreview']."</p>\n";
-
-} else if ($editor == 'bbcode') {
-    if ($attach) {
-        $text = parse_attach($text);
-    }
-    $text = htmlspecialchars($text);
-    $text = parseubb(parsesmileys($text));
-    if ($mode == 'admin') {
-        $images = str_replace('../../../', '', IMAGES);
-        $text = str_replace(IMAGES, $images, $text);
-        if (defined('IMAGES_N')) {
-            $text = str_replace(IMAGES_N, $images, $text);
+        echo nl2br( html_entity_decode( $text, ENT_QUOTES, $locale['charset'] ) ) ?: "<p class='text-center'>".$locale['nopreview']."</p>\n";
+        break;
+    case 'bbcode':
+        $text = htmlspecialchars( $text );
+        $text = parseubb( parsesmileys( $text ) );
+        if ( $mode == 'admin' ) {
+            $images = str_replace( '../../../', '', IMAGES );
+            $text = str_replace( IMAGES, $images, $text );
+            if ( defined( 'IMAGES_N' ) ) {
+                $text = str_replace( IMAGES_N, $images, $text );
+            }
+            $text = parse_imageDir( $text, $prefix_."images/" );
         }
-        $text = parse_imageDir($text, $prefix_."images/");
-    }
-
-
-    echo nl2br(html_entity_decode($text, ENT_QUOTES, $locale['charset'])) ?: "<p class='text-center'>".$locale['nopreview']."</p>\n";
-
-} else {
-    if ($attach) {
-        $text = parse_attach($text);
-    }
-    $text = htmlspecialchars($text);
-    $text = parsesmileys($text);
-    if ($mode == 'admin') {
-        $images = str_replace('../../../', '', IMAGES);
-        $text = str_replace(IMAGES, $images, $text);
-        if (defined('IMAGES_N')) {
-            $text = str_replace(IMAGES_N, $images, $text);
+        echo nl2br( html_entity_decode( $text, ENT_QUOTES, $locale['charset'] ) ) ?: "<p class='text-center'>".$locale['nopreview']."</p>\n";
+        break;
+    default:
+        $text = htmlspecialchars( $text );
+        $text = parsesmileys( $text );
+        if ( $mode == 'admin' ) {
+            $images = str_replace( '../../../', '', IMAGES );
+            $text = str_replace( IMAGES, $images, $text );
+            if ( defined( 'IMAGES_N' ) ) {
+                $text = str_replace( IMAGES_N, $images, $text );
+            }
         }
-    }
-
-
-    echo parse_imageDir(nl2br(html_entity_decode($text, ENT_QUOTES, $locale['charset']))) ?: "<p class='text-center'>".$locale['nopreview']."</p>\n";
+        echo parse_imageDir( nl2br( html_entity_decode( $text, ENT_QUOTES, $locale['charset'] ) ) ) ?: "<p class='text-center'>".$locale['nopreview']."</p>\n";
 }
-
 echo "</div>\n";
