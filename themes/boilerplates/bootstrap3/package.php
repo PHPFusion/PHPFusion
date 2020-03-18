dc2a3d531393164f07405fea8334411130d90026
Frederick MC Chan <meangczac.chan@gmail.com>
2020-03-18 21:44:01 +0800

    Bootstrap 3 boilerplate package

--- themes/boilerplates/bootstrap3/package.php
+++ themes/boilerplates/bootstrap3/package.php
@@ -1,30 +1,8 @@
 <?php
+require_once BOILERPLATES.'bootstrap3/index.php';
 
-function fusion_load_bootstrap() {
-    if (fusion_get_settings('bootstrap') || defined('BOOTSTRAP')) {
-        $_themes = THEMES;
-        if (!empty('CDN')) {
-            $_themes = CDN.'themes/';
-        }
+// Add the script to head tag
+fusion_add_hook('fusion_boiler_header', 'bootstrap3');
 
-        echo "<meta http-equiv='X-UA-Compatible' content='IE=edge'/>\n";
-        echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'/>\n";
-        echo "<link rel='stylesheet' href='".$_themes."templates/boilers/bootstrap3/css/bootstrap.min.css'/>\n";
-        echo "<link rel='stylesheet' href='".$_themes."templates/boilers/bootstrap3/css/bootstrap-submenu.min.css'/>\n";
-
-        if (fusion_get_locale('text-direction') == 'rtl') {
-            echo "<link rel='stylesheet' href='".$_themes."templates/boilers/bootstrap3/css/bootstrap-rtl.min.css'/>\n";
-        }
-
-        add_to_footer("<script src='".$_themes."templates/boilers/bootstrap3/js/bootstrap.min.js'></script>");
-        add_to_footer("<script src='".$_themes."templates/boilers/bootstrap3/js/bootstrap-submenu.min.js'></script>");
-
-        add_to_jquery("
-            $('[data-submenu]').submenupicker();
-            // Fix select2 on modal - http://stackoverflow.com/questions/13649459/twitter-bootstrap-multiple-modal-error/15856139#15856139
-            $.fn.modal.Constructor.prototype.enforceFocus = function () {};
-        ");
-    }
-}
-
-fusion_add_hook('start_boiler', 'fusion_load_bootstrap');
+// Set the boilerpalte template files to system
+fusion_add_hook('fusion_boiler_paths', 'change_to_bs3_twig');
