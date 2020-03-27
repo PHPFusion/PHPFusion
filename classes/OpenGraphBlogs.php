95c586392733c8739acbf23c56763d6f6fccea7d
RobiNN <kelcakrobo@gmail.com>
2020-03-27 21:25:13 +0100

    Fix opengraph

--- classes/OpenGraphBlogs.php
+++ classes/OpenGraphBlogs.php
@@ -22,7 +22,7 @@ class OpenGraphBlogs extends OpenGraph {
         $settings = fusion_get_settings();
         $info = [];
 
-        $result = dbquery("SELECT `blog_subject`, `blog_blog`, `blog_keywords`, `blog_image_t1` FROM `".DB_BLOG."` WHERE `blog_id` = :blogid", [':blogid' => $blog_id]);
+        $result = dbquery("SELECT blog_subject, blog_blog, blog_keywords, blog_image_t1, blog_image_t2 FROM ".DB_BLOG." WHERE blog_id = :blogid", [':blogid' => $blog_id]);
         if (dbrows($result)) {
             $data = dbarray($result);
             $info['url'] = $settings['siteurl'].'infusions/blog/blog.php?readmore='.$blog_id;
@@ -30,8 +30,10 @@ class OpenGraphBlogs extends OpenGraph {
             $info['title'] = $data['blog_subject'].' - '.$settings['sitename'];
             $info['description'] = $data['blog_blog'] ? fusion_first_words(strip_tags(html_entity_decode($data['blog_blog'])), 50) : $settings['description'];
             $info['type'] = 'article';
-            if (!empty($data['blog_image_t1'])) {
+            if (!empty($data['blog_image_t1']) && file_exists(IMAGES_B_T.$data['blog_image_t1'])) {
                 $info['image'] = $settings['siteurl'].'infusions/blog/images/thumbs/'.$data['blog_image_t1'];
+            } else if (!empty($data['blog_image_t2']) && file_exists(IMAGES_B_T.$data['blog_image_t2'])) {
+                $info['image'] = $settings['siteurl'].'infusions/blog/images/thumbs/'.$data['blog_image_t2'];
             } else {
                 $info['image'] = defined('THEME_ICON') ? THEME_ICON.'mstile-150x150.png' : $settings['siteurl'].'images/favicons/mstile-150x150.png';
             }
@@ -44,7 +46,7 @@ class OpenGraphBlogs extends OpenGraph {
         $settings = fusion_get_settings();
         $info = [];
 
-        $result = dbquery("SELECT `blog_cat_name`, `blog_cat_image` FROM `".DB_BLOG_CATS."` WHERE `blog_cat_id` = :cat_id", [':cat_id' => $cat_id]);
+        $result = dbquery("SELECT blog_cat_name, blog_cat_image FROM ".DB_BLOG_CATS." WHERE blog_cat_id = :cat_id", [':cat_id' => $cat_id]);
         if (dbrows($result)) {
             $data = dbarray($result);
             $info['url'] = $settings['siteurl'].'infusions/blog/blog.php?readmore='.$cat_id;
@@ -52,7 +54,7 @@ class OpenGraphBlogs extends OpenGraph {
             $info['title'] = $data['blog_cat_name'].' - '.$settings['sitename'];
             $info['description'] = $settings['description'];
             $info['type'] = 'website';
-            if (!empty($data['blog_cat_image'])) {
+            if (!empty($data['blog_cat_image']) && file_exists(IMAGES_BC.$data['blog_cat_image'])) {
                 $info['image'] = $settings['siteurl'].'infusions/blog/blog_cats/'.$data['blog_cat_image'];
             } else {
                 $info['image'] = defined('THEME_ICON') ? THEME_ICON.'mstile-150x150.png' : $settings['siteurl'].'images/favicons/mstile-150x150.png';
