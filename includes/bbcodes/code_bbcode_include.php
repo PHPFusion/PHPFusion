95472d094990cf75801088971280d82fc7985855
RobiNN <kelcakrobo@gmail.com>
2020-05-10 01:53:20 +0200

    Fix Code BBcode

--- includes/bbcodes/code_bbcode_include.php
+++ includes/bbcodes/code_bbcode_include.php
@@ -27,7 +27,8 @@ if (preg_match_all('#\[code(=(.*?))?\](.*?)\[/code\]#si', $text) ||
 ) {
     $text = preg_replace_callback(
         "#\[code(=(?P<lang>.*?))?\](?P<code>.*?)\[/code\]#si",
-        function ($m) use (&$i) {
+        function ($m) {
+            static $i = 0;
             $locale = fusion_get_locale();
             $pid = get('pid', FILTER_VALIDATE_INT);
             $tid = get('thread_id', FILTER_VALIDATE_INT);
@@ -43,7 +44,7 @@ if (preg_match_all('#\[code(=(.*?))?\](.*?)\[/code\]#si', $text) ||
                     ':pid' => (int) $pid
                     ]);
                     $data = dbarray($result);
-                    $code_save = '<a class="pull-right text-dark text-uppercase small" href="'.INCLUDES.'bbcodes/code_bbcode_save.php?thread_id='.$data['thread_id'].'&amp;post_id='.$data['post_id'].'&amp;code_id='.$i.'">Download</a>&nbsp;&nbsp;';
+                    $code_save = '<a class="pull-right text-dark text-uppercase small" href="'.INCLUDES.'bbcodes/code_bbcode_save.php?thread_id='.$data['thread_id'].'&amp;post_id='.$data['post_id'].'&amp;code_id='.$i.'">'.$locale['bb_code_save'].'</a>';
                 }
             }
             $i++;
--- includes/bbcodes/code_bbcode_save.php
+++ includes/bbcodes/code_bbcode_save.php
@@ -18,17 +18,6 @@
 require "../../maincore.php";
 require INCLUDES."class.httpdownload.php";
 
-function unstripinput($text) {
-    if (QUOTES_GPC) {
-        $text = stripslashes($text);
-    }
-    $search = ["\n", "&amp;", "&quot;", "&#39;", "&#92;", "&quot;", "&#39;", "&lt;", "&gt;"];
-    $replace = ["\r\n", "&", "\"", "'", "\\", '\"', "\'", "<", ">"];
-    $text = str_replace($search, $replace, $text);
-
-    return $text;
-}
-
 if ((isset($_GET['thread_id']) && isnum($_GET['thread_id'])) && (isset($_GET['post_id']) && isnum($_GET['post_id'])) && (isset($_GET['code_id']) && isnum($_GET['code_id']))) {
     $result = dbquery("SELECT fp.*, ff.* FROM ".DB_FORUM_POSTS." AS fp
         INNER JOIN ".DB_FORUMS." AS ff ON ff.forum_id=fp.forum_id
@@ -36,9 +25,9 @@ if ((isset($_GET['thread_id']) && isnum($_GET['thread_id'])) && (isset($_GET['po
     if (dbrows($result)) {
         $data = dbarray($result);
         $text = $data['post_message'];
-        preg_match_all("#\[code](.*?)\[/code\]#si", $text, $matches, PREG_PATTERN_ORDER);
+        preg_match_all("#\[code](.*?)\[/code]#si", $text, $matches, PREG_PATTERN_ORDER);
         if (isset($matches[1][$_GET['code_id']])) {
-            $text = unstripinput($matches[1][$_GET['code_id']]);
+            $text = html_entity_decode(stripslashes($matches[1][$_GET['code_id']]), ENT_QUOTES, fusion_get_locale('charset'));
             $filename = "code_".$_GET['thread_id']."_".$_GET['post_id']."_".$_GET['code_id'].".txt";
             $object = new PHPFusion\httpdownload;
             $object->set_bydata($text);
