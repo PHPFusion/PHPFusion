320a6c5797d2b235cd26dd22102cb0309930b1c9
RobiNN <kelcakrobo@gmail.com>
2020-05-24 16:40:51 +0200

    Update panels.php

--- administration/panels.php
+++ administration/panels.php
@@ -592,7 +592,6 @@ class PanelsAdministration {
         echo "<div id='panel_url_list-grp'>\n";
         echo form_textarea('panel_url_list', self::$locale['462'], $this->data['panel_url_list'], [
             'inline'   => FALSE,
-            'descript' => fusion_get_settings('allow_php_exe') == 1 ? 1 : 0
             //'required' => TRUE
         ]);
         echo "<div class='text-smaller'>".self::$locale['463']." <br />
@@ -616,10 +615,11 @@ class PanelsAdministration {
 
         echo "<div id='pgrp'>\n";
         echo form_textarea('panel_content', self::$locale['455'], $this->data['panel_content'], [
-            'html'      => fusion_get_settings("allow_php_exe") ? FALSE : TRUE,
+            'html'      => $settings['allow_php_exe'] ? FALSE : TRUE,
             'form_name' => 'panel_form',
             'autosize'  => TRUE,
-            'preview'   => fusion_get_settings("allow_php_exe") ? FALSE : TRUE,
+            'preview'   => $settings['allow_php_exe'] ? FALSE : TRUE,
+            'descript'  => $settings['allow_php_exe'] ? FALSE : TRUE
         ]);
         echo "</div>\n";
 
