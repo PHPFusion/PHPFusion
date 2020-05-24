9fe01daf239145660ce1bdb38fb6a45f27b9ef54
RobiNN <kelcakrobo@gmail.com>
2020-05-24 17:25:55 +0200

    Close #2292

--- templates/downloads.php
+++ templates/downloads.php
@@ -37,6 +37,8 @@ if (!function_exists('render_downloads')) {
         if ($info["get"]["download_id"] && !empty($info['download_item'])) {
             $data = $info['download_item'];
 
+            echo '<div class="download-item">';
+
             echo "<div class='clearfix'>\n";
             echo "<div class='btn-group pull-right m-t-15'>";
             if ($data['admin_link']) {
@@ -119,7 +121,10 @@ if (!function_exists('render_downloads')) {
 
             echo $data['download_show_comments'];
             echo $data['download_allow_ratings'] ? "<a id='rate'>\n</a>\n".$data['download_show_ratings'] : '';
+
+            echo '</div>';
         } else {
+            echo '<div class="downloads-index">';
             if (!empty($info['download_cat_description'])) {
                 echo "<div class='display-block'>\n";
                 echo $info['download_cat_description'];
@@ -127,11 +132,11 @@ if (!function_exists('render_downloads')) {
             }
 
             echo "<!--pre_download_cat-->\n";
-            echo "<div class='list-group'>\n";
+            echo "<div class='list-group downloads-index'>\n";
             if (!empty($info['download_item'])) {
                 foreach ($info['download_item'] as $download_id => $data) {
                     $download_title = $data['download_title'];
-                    echo "<div class='list-group-item clearfix'>\n";
+                    echo "<div class='list-group-item clearfix download-index-item'>\n";
                     echo "<div class='pull-right'>\n";
 
                     if ($dl_settings['download_stats']) {
@@ -174,6 +179,7 @@ if (!function_exists('render_downloads')) {
             }
             echo "</div>\n";
             echo "<!--sub_download_cat-->";
+            echo '</div>';
         }
 
         closetable();
