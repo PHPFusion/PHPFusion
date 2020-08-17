c2b82d5fadffe25a6e8d7fa28526a4972a239a81
RobiNN <kelcakrobo@gmail.com>
2020-08-17 19:36:03 +0200

    Update copyright header

--- admin/blog_cat.php
+++ admin/blog_cat.php
@@ -2,7 +2,7 @@
 /*-------------------------------------------------------+
 | PHP-Fusion Content Management System
 | Copyright (C) PHP-Fusion Inc
-| https://www.php-fusion.co.uk/
+| https://www.phpfusion.com/
 +--------------------------------------------------------+
 | Filename: admin/blog_cat.php
 | Author: Frederick MC Chan (Chan)
