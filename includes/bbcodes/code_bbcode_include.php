3fae3a8e3a16adb1a9a03322b75d96913d8a4ea6
RobiNN <kelcakrobo@gmail.com>
2020-08-29 22:47:40 +0200

    Update code_bbcode_include.php

--- includes/bbcodes/code_bbcode_include.php
+++ includes/bbcodes/code_bbcode_include.php
@@ -17,15 +17,15 @@
 +--------------------------------------------------------*/
 defined('IN_FUSION') || exit;
 
-add_to_head('<link rel="stylesheet" href="'.INCLUDES.'bbcodes/code/prism.css" type="text/css"/>');
-add_to_footer('<script src="'.INCLUDES.'bbcodes/code/prism.js"></script>');
-
 if (preg_match_all('#\[code(=(.*?))?\](.*?)\[/code\]#si', $text) ||
     preg_match_all('#```(.*?)```#si', $text) ||
     preg_match_all('#`(.*?)`#si', $text) ||
     preg_match_all('#\[php\](.*?)\[/php\]#si', $text) ||
     preg_match_all('#\[geshi=(.*?)\](.*?)\[/geshi\]#si', $text)
 ) {
+    add_to_head('<link rel="stylesheet" href="'.INCLUDES.'bbcodes/code/prism.css" type="text/css"/>');
+    add_to_footer('<script src="'.INCLUDES.'bbcodes/code/prism.js"></script>');
+
     $text = preg_replace_callback(
         "#\[code(=(?P<lang>.*?))?\](?P<code>.*?)\[/code\]#si",
         function ($m) {
