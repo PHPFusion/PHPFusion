07e00a0f879d4c448399b46015748e25f6b5d086
RobiNN <kelcakrobo@gmail.com>
2020-10-22 21:57:40 +0200

    Update photo_functions_include.php

--- includes/photo_functions_include.php
+++ includes/photo_functions_include.php
@@ -4,7 +4,7 @@
 | Copyright (C) PHP-Fusion Inc
 | https://www.phpfusion.com/
 +--------------------------------------------------------+
-| Filename: photo_functions_include
+| Filename: photo_functions_include.php
 | Author: Core Development Team (coredevs@phpfusion.com)
 +--------------------------------------------------------+
 | This program is released as free software under the
