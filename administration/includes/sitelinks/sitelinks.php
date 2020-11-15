e4fe9459787eae1340b740369a11a2f4adbaab4f
deviance <meangczac.chan@gmail.com>
2020-11-15 22:58:20 +0800

    Fixes counting

--- administration/includes/sitelinks/sitelinks.php
+++ administration/includes/sitelinks/sitelinks.php
@@ -66,8 +66,9 @@ function display_sitelinks() {
         }
 
         $table = DB_SITE_LINKS." sl LEFT JOIN ".DB_SITE_LINKS." sl2 ON (sl2.link_cat=sl.link_id)";
-        $count_sel = "(sl.link_id)";
+
         $column_sel = "sl.*, count(sl2.link_id) 'link_count'";
+
         if ($search) {
             $count_cond = "(".implode(" OR ", $columns).")";
             $sql_cond .= " AND $count_cond";
@@ -77,20 +78,23 @@ function display_sitelinks() {
 
         $list = [];
 
-        $max_rows = dbcount($count_sel, $table, $count_cond);
+        $count_query = "SELECT ".$column_sel." FROM ".$table.whitespace($sql_cond)." GROUP BY sl.link_id";
+
+        $max_rows = dbrows(dbquery($count_query));
 
         $sql_query = "SELECT ".$column_sel." FROM ".$table.whitespace($sql_cond)." GROUP BY sl.link_id".whitespace($orderby).whitespace($rowsearch);
 
         $result = dbquery($sql_query);
+
         if ($rows = dbrows($result)) {
 
             $i = 1;
 
             while ($data = dbarray($result)) {
-                //print_p($data);
 
                 // automatic link order repair
                 $link_order = $data["link_order"];
+
                 if (isset($ordering) && in_array("link_order asc", $ordering)) {
                     if ($data["link_order"] !== $i) {
                         dbquery("UPDATE ".DB_SITE_LINKS." SET link_order=:oid WHERE link_id=:lid", [":lid" => $data["link_id"], ":oid" => $i]);
@@ -112,8 +116,6 @@ function display_sitelinks() {
                     $link_name = "<a href='".ADMIN."site_links.php".$aidlink."&section=links&refs=".$refs."&cat=".$data["link_id"]."'>".$data["link_name"]."</a>";
                 }
 
-                //https://main.test/administration/site_links.php??aid=2af8083961791c57&section=links&refs=&cat=0
-                //".form_checkbox("link_id[]", "", '', ["input_id" => "link_id-".$data["link_id"], "value" => $data['link_id'], "class" => 'm-0'])."
                 $list[] = [
                     "DT_RowId"        => $data["link_id"],
                     "link_checkbox"   => "<div class='display-flex-row'>
