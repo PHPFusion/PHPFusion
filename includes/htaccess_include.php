55142252091a016c1976c0b2b275b3f771af4975
RobiNN <kelcakrobo@gmail.com>
2020-11-18 12:47:45 +0100

    Update htaccess_include.php

--- includes/htaccess_include.php
+++ includes/htaccess_include.php
@@ -48,12 +48,6 @@ function write_htaccess() {
     $htc .= "    deny from all".PHP_EOL;
     $htc .= "</Files>".PHP_EOL.PHP_EOL;
 
-    $htc .= "# Secure .inc files".PHP_EOL;
-    $htc .= "<Files *.inc>".PHP_EOL;
-    $htc .= "    Order allow,deny".PHP_EOL;
-    $htc .= "    deny from all".PHP_EOL;
-    $htc .= "</Files>".PHP_EOL.PHP_EOL;
-
     $htc .= "# Protect config.php".PHP_EOL;
     $htc .= "<Files config.php>".PHP_EOL;
     $htc .= "    order allow,deny".PHP_EOL;
@@ -66,38 +60,6 @@ function write_htaccess() {
     $htc .= "ErrorDocument 403 ".$site_path."error.php?code=403".PHP_EOL;
     $htc .= "ErrorDocument 404 ".$site_path."error.php?code=404".PHP_EOL;
     $htc .= "ErrorDocument 500 ".$site_path."error.php?code=500".PHP_EOL.PHP_EOL;
-
-    $htc .= "# Cache images for 7 days to soften network load".PHP_EOL;
-    $htc .= "<ifModule mod_headers.c>".PHP_EOL;
-    $htc .= '    <filesMatch "\\.(ico|pdf|flv|jpg|jpeg|png|gif|swf|ttf|otf|woff|woff2|eot|svg)$">'.PHP_EOL;
-    $htc .= '        Header append Vary: Accept-Encoding'.PHP_EOL;
-    $htc .= '        Header set Cache-Control "max-age=2592000, public"'.PHP_EOL;
-    $htc .= "    </filesMatch>".PHP_EOL;
-    $htc .= '    <filesMatch "\\.(css|js)$">'.PHP_EOL;
-    $htc .= '        Header set Cache-Control "max-age=604800, public"'.PHP_EOL;
-    $htc .= "    </filesMatch>".PHP_EOL;
-    $htc .= '    <filesMatch "\\.(html|htm|php)$">'.PHP_EOL;
-    $htc .= '        Header set Cache-Control "max-age=1, private, must-revalidate"'.PHP_EOL;
-    $htc .= "    </filesMatch>".PHP_EOL;
-    $htc .= "</ifModule>".PHP_EOL.PHP_EOL;
-
-    $htc .= '# Compress files'.PHP_EOL;
-    $htc .= '<ifModule mod_deflate.c>'.PHP_EOL;
-    $htc .= '    <filesMatch "\.(jpg|jpeg|png|gif|ico|svg|css|js|json|x?html?|php)$">'.PHP_EOL;
-    $htc .= '        SetOutputFilter DEFLATE'.PHP_EOL;
-    $htc .= '    </filesMatch>'.PHP_EOL;
-    $htc .= '</ifModule>'.PHP_EOL.PHP_EOL;
-
-    $htc .= "# This force image to not be able to be used as other matter".PHP_EOL;
-    $htc .= "<filesMatch \"(?i).jpe?g$\">".PHP_EOL;
-    $htc .= "    ForceType image/jpeg".PHP_EOL;
-    $htc .= "</filesMatch>".PHP_EOL;
-    $htc .= "<filesMatch \"(?i).gif$\">".PHP_EOL;
-    $htc .= "    ForceType image/gif".PHP_EOL;
-    $htc .= "</filesMatch>".PHP_EOL;
-    $htc .= "<filesMatch \"(?i).png$\">".PHP_EOL;
-    $htc .= "    ForceType image/png".PHP_EOL;
-    $htc .= "</filesMatch>".PHP_EOL.PHP_EOL;
 
     if ($settings_seo == 1) {
         // Rewrite settings
