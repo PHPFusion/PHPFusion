9026085918682921c3611f90b4bfb7d3858fe940
RobiNN <kelcakrobo@gmail.com>
2020-12-03 17:53:21 +0100

    Update settings.php

--- locale/English/admin/settings.php
+++ locale/English/admin/settings.php
@@ -106,7 +106,7 @@ $locale['608'] = "GD2";
 $locale['609'] = "Development Mode";
 $locale['610'] = "Update checker";
 $locale['611'] = "Number Delimiter";
-$locale['612'] = "Number Separator";
+$locale['612'] = "Thousands separator";
 
 $locale['649'] = "Prohibited user names:";
 $locale['650'] = "Miscellaneous";
