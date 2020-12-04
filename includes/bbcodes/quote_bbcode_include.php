84f6a27b2f52a3679d550a1b772f01c0c5464420
RobiNN <kelcakrobo@gmail.com>
2020-12-04 18:50:39 +0100

    Update quote_bbcode_include.php

--- includes/bbcodes/quote_bbcode_include.php
+++ includes/bbcodes/quote_bbcode_include.php
@@ -36,68 +36,6 @@ for ($i = 0; $i < $qcount; $i++) {
     //Replace default quotes
     $text = preg_replace('#\[quote\](.*?)\[/quote\]#si', $before.$locale['bb_quote'].$endbefore.'$1'.$after, $text); //replace default quote //HTML
     //Replace extended quotes
-    $text = preg_replace('#'.$exta.'(.*?)\[/quote\]#si', $before.'<a class=\'quote-link\' rel=\'$2\' href=\'viewthread.php?pid=$2\' target=\'_blank\'>$1 '.$locale['bb_wrote'].': <span class=\'goto-post-arrow\'>&uarr;</span></a>'.$endbefore.'$3'.$after, $text); //replace quote with valid name and post //HTML
+    $text = preg_replace('#'.$exta.'(.*?)\[/quote\]#si', $before.'<a class=\'quote-link\' href=\'#post_$2\'>$1 '.$locale['bb_wrote'].': <span class=\'goto-post-arrow\'><i class="fa fa-arrow-up"></i></span></a>'.$endbefore.'$3'.$after, $text); //replace quote with valid name and post //HTML
     $text = preg_replace('#'.$extb.'(.*?)\[/quote\]#si', $before.'$1'.$endbefore.'$2'.$after, $text); //replace quote with valid name and no post //HTML
-}
-
-// Add once only
-if (!defined('bbcode_quote_js')) {
-    define('bbcode_quote_js', TRUE);
-    add_to_footer("<script type='text/javascript'>".jsminify("$(function() {
-        /*
-         * Extended Quote BBcode for PHP-Fusion
-         * with jQuery Quote Collapse
-         * Author: JoiNNN
-         */
-        var quoteColHeight = 184,        // 0 - disables quote collapse
-            colCls         = 'collapsed',// class when collapsed
-            expCls         = 'expanded'; // class when expanded
-
-        if (quoteColHeight > 0) {
-        // On page load
-        jQuery('.quote').each(function() {
-            var quote = jQuery(this),
-                block = quote.find('.blockquote').first();
-
-            //On load add expand link if quote is long enough
-            if (block.height() > quoteColHeight) {
-                quote.addClass(colCls);
-                quote.find('.citation').first().prepend('<a href=\"#\" class=\"toggle-quote ' + colCls + ' flright\">".$locale['bb_quote_expand']."</a>');
-                block.css({'height': quoteColHeight, 'overflow': 'hidden'});
-            }
-        });
-
-        // On click
-        jQuery('.toggle-quote').click(function(e) {
-            e.preventDefault();
-            var toggler = jQuery(this),
-                quote   = toggler.parent().parent(),
-                block   = quote.find('.blockquote').first();
-
-            if (block.height() > quoteColHeight) {
-                block.stop().animate({'height': quoteColHeight + 'px'}, 200);
-                toggler.html('".$locale['bb_quote_expand']."');
-                toggler.removeClass(expCls).addClass(colCls);
-                quote.removeClass(expCls).addClass(colCls);
-            } else {
-                block.stop().animate({'height': block[0].scrollHeight + 'px'}, 200, function() {
-                    jQuery(this).css({'height': 'auto'});
-                });
-                toggler.html('".$locale['bb_quote_collapse']."');
-                toggler.removeClass(colCls).addClass(expCls);
-                quote.removeClass(colCls).addClass(expCls);
-            }
-        });
-
-        // Scroll to quoted post if is on the same page as the quote, instead of opening a new tab
-        jQuery('.quote-link').click(function(e) {
-            var pid = jQuery(this).attr('rel');
-            if (jQuery('#post_' + pid).length) {
-                var target =jQuery('#post_' + pid).offset().top;
-                jQuery('html, body').animate({scrollTop:target}, 200);
-                e.preventDefault();
-            }
-        });
-        }
-    });")."</script>");
 }
