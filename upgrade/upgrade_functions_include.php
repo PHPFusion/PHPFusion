9af8ff182a1d6c8a3b593f8f77ce2d3491b8795d
RobiNN <kelcakrobo@gmail.com>
2020-12-05 01:55:06 +0100

    Fix upgrade

--- upgrade/upgrade_functions_include.php
+++ upgrade/upgrade_functions_include.php
@@ -37,19 +37,13 @@ if (!function_exists('rrmdir')) {
     }
 }
 
-/*
- * Get all detected language in the system
- */
-function fusion_get_detected_languages() {
-    static $detected_languages = NULL;
-    if ($detected_languages === NULL) {
-        $all_languages = makefilelist(LOCALE, ".svn|.|..", TRUE, "folders");
-        foreach ($all_languages as $language_name) {
-            $detected_languages[$language_name] = $language_name;
+function fusion_rename($origin, $target) {
+    if ($origin != "." && $origin != ".." && !is_dir($origin)) {
+        if (TRUE !== @rename($origin, $target)) {
+            copy($origin, $target);
+            unlink($origin);
         }
     }
-
-    return (array)$detected_languages;
 }
 
 /**
@@ -62,13 +56,13 @@ function move_photos($album_id) {
     if (dbrows($result) > 0) {
         while ($photo_data = dbarray($result)) {
             if (file_exists(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_filename'])) {
-                rename(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_filename'], INFUSIONS."gallery/photos/".$photo_data['photo_filename']);
+                fusion_rename(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_filename'], INFUSIONS."gallery/photos/".$photo_data['photo_filename']);
             }
             if (file_exists(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_thumb1'])) {
-                rename(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_thumb1'], INFUSIONS."gallery/photos/".$photo_data['photo_thumb1']);
+                fusion_rename(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_thumb1'], INFUSIONS."gallery/photos/thumbs/".$photo_data['photo_thumb1']);
             }
             if (file_exists(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_thumb2'])) {
-                rename(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_thumb2'], INFUSIONS."gallery/photos/".$photo_data['photo_thumb2']);
+                fusion_rename(IMAGES."photoalbum/album_".$album_id."/".$photo_data['photo_thumb2'], INFUSIONS."gallery/photos/thumbs/".$photo_data['photo_thumb2']);
             }
         }
     }
