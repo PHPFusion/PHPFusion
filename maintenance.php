4f418648e32429a8fcd39dc4bb4b14caa393d6ad
RobiNN <kelcakrobo@gmail.com>
2021-01-07 21:40:00 +0100

    Update maintenance tpl

--- maintenance.php
+++ maintenance.php
@@ -68,7 +68,7 @@ if (file_exists(INCLUDES."header_includes.php")) {
 }
 require_once INCLUDES."theme_functions_include.php";
 require_once THEMES."templates/render_functions.php";
-include THEMES."templates/global/maintenance.php";
+include THEMES."templates/global/maintenance.tpl.php";
 
 header("Content-Type: text/html; charset=".$locale['charset']."");
 echo "<!DOCTYPE html>\n";
