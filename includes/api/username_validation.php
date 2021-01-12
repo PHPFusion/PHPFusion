6af3c1a07e2cf15bc744e15279054f6371bc103c
RobiNN <kelcakrobo@gmail.com>
2021-01-12 14:47:28 +0100

    Update username_validation.php

--- includes/api/username_validation.php
+++ includes/api/username_validation.php
@@ -21,8 +21,8 @@ function xhttp_usernamecheck() {
     $input_string = (string)filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
     $result = [];
     if (!empty($input_string)) {
-        $name_active = dbcount("(user_id)", DB_USERS, "user_name='".$input_string."'");
-        $name_inactive = dbcount("(user_code)", DB_NEW_USERS, "user_name='".$input_string."'");
+        $name_active = dbcount("(user_id)", DB_USERS, "user_name=:name", [':name' => $input_string]);
+        $name_inactive = dbcount("(user_code)", DB_NEW_USERS, "user_name=:name", [':name' => $input_string]);
         $check_string = preg_match("/^[\p{Latin}\p{Arabic}\p{Cyrillic}\p{Han}\p{Hebrew}a-zA-Z\p{N}]+\h?[\p{N}\p{Latin}\p{Arabic}\p{Cyrillic}\p{Han}\p{Hebrew}a-zA-Z]*$/um", $input_string);
 
         if ($name_active == 0 && $name_inactive == 0 && $check_string) {
