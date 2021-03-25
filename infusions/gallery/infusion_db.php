e1f6c96664fab8d710bb3b34816bda05cb951d0a
RobiNN <kelcakrobo@gmail.com>
2021-03-25 19:40:01 +0100

    Delete redundant gallery rights

--- infusions/gallery/infusion_db.php
+++ infusions/gallery/infusion_db.php
@@ -53,9 +53,7 @@ if (!defined("GALLERY_ADMIN_LOCALE")) {
 // Admin Settings
 \PHPFusion\Admins::getInstance()->setAdminPageIcons("PH", "<i class='admin-ico fa fa-fw fa-camera-retro'></i>");
 \PHPFusion\Admins::getInstance()->setCommentType('P', fusion_get_locale('272', LOCALE.LOCALESET."admin/main.php"));
-\PHPFusion\Admins::getInstance()->setCommentType('PH', fusion_get_locale('261', LOCALE.LOCALESET."admin/main.php"));
 \PHPFusion\Admins::getInstance()->setLinkType('P', fusion_get_settings("siteurl")."infusions/gallery/gallery.php?photo_id=%s");
-\PHPFusion\Admins::getInstance()->setLinkType('PH', fusion_get_settings("siteurl")."infusions/gallery/gallery.php?photo_id=%s");
 
 $inf_settings = get_settings('gallery');
 if (!empty($inf_settings['gallery_allow_submission']) && $inf_settings['gallery_allow_submission']) {
