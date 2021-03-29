fdf5322be2bfe808b69984f147e46adb01ee02a9
RobiNN <kelcakrobo@gmail.com>
2021-03-29 23:44:30 +0200

    Update panels_updater.php

--- administration/panels_updater.php
+++ administration/panels_updater.php
@@ -16,19 +16,20 @@
 | written permission from the original author(s).
 +--------------------------------------------------------*/
 require_once __DIR__.'/../maincore.php';
-$locale = fusion_get_locale("", LOCALE.LOCALESET."admin/panels.php");
+$locale = fusion_get_locale("", LOCALE.LOCALESET.'admin/panels.php');
 pageAccess("P");
 
-if (isset($_GET['listItem']) && is_array($_GET['listItem'])) {
+if (check_get(['listItem']) && get(['listItem'])) {
     $sql_side = "";
-    if (isset($_GET['panel_side']) && isnum($_GET['panel_side'])) {
-        $sql_side = ", panel_side='".$_GET['panel_side']."'";
+    if (check_get('panel_side') && get('panel_side', FILTER_SANITIZE_NUMBER_INT)) {
+        $sql_side = ", panel_side='".get('panel_side')."'";
     }
-    foreach ($_GET['listItem'] as $position => $item) {
+    foreach (get(['listItem']) as $position => $item) {
         if (isnum($position) && isnum($item)) {
             dbquery("UPDATE ".DB_PANELS." SET panel_order='".($position + 1)."'".$sql_side." WHERE panel_id='".$item."'");
         }
     }
+
     header("Content-Type: text/html; charset=".$locale['charset']."\n");
     echo "<div id='close-message'><div class='admin-message alert alert-info m-t-10'>".$locale['488']."</div></div>";
 }
