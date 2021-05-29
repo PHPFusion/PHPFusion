18efd39637fbc7a2efbc5c4d8f3a78284b9583f9
RobiNN <kelcakrobo@gmail.com>
2021-05-29 18:42:22 +0200

    Fix homepage

--- themes/templates/global/homepage.tpl.php
+++ themes/templates/global/homepage.tpl.php
@@ -78,7 +78,7 @@ if (!function_exists('display_home')) {
             ');
 
             // Push News to top
-            if (defined('NEWS_EXISTS')) {
+            if (!empty($info[DB_NEWS])) {
                 $temp = [DB_NEWS => $info[DB_NEWS]];
                 unset($info[DB_NEWS]);
                 $info = $temp + $info;
