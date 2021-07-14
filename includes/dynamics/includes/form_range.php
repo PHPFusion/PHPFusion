9167a243d69fb6871c19ab2a1f189535791f0ea2
deviance <meangczac.chan@gmail.com>
2021-07-14 23:43:54 +0800

    Adds fixes on form_range

--- includes/dynamics/includes/form_range.php
+++ includes/dynamics/includes/form_range.php
@@ -1,6 +1,28 @@
 <?php
-
-
+/*-------------------------------------------------------+
+| PHPFusion Content Management System
+| Copyright (C) PHP Fusion Inc
+| https://phpfusion.com/
++--------------------------------------------------------+
+| Filename: form_range.php
+| Author: PHPFusion Development Team
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+/**
+ * @param        $input_name
+ * @param string $label
+ * @param string $input_value
+ * @param array  $options
+ *
+ * @return string
+ */
 function form_range($input_name, $label = "", $input_value = "", array $options = []) {
     
     $locale = fusion_get_locale();
