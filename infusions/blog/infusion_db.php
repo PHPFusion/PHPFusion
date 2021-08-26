3f4cfad406631fc1f897dc9fe6b326f03e9d6584
RobiNN <kelcakrobo@gmail.com>
2021-08-26 17:27:54 +0200

    Update blog

--- infusions/blog/infusion_db.php
+++ infusions/blog/infusion_db.php
@@ -17,60 +17,47 @@
 +--------------------------------------------------------*/
 defined('IN_FUSION') || exit;
 
-if (!defined("BLOG_LOCALE")) {
-    if (file_exists(INFUSIONS."blog/locale/".LOCALESET."blog.php")) {
-        define("BLOG_LOCALE", INFUSIONS."blog/locale/".LOCALESET."blog.php");
-    } else {
-        define("BLOG_LOCALE", INFUSIONS."blog/locale/English/blog.php");
-    }
-}
-if (!defined("BLOG_ADMIN_LOCALE")) {
-    if (file_exists(INFUSIONS."blog/locale/".LOCALESET."blog_admin.php")) {
-        define("BLOG_ADMIN_LOCALE", INFUSIONS."blog/locale/".LOCALESET."blog_admin.php");
-    } else {
-        define("BLOG_ADMIN_LOCALE", INFUSIONS."blog/locale/English/blog_admin.php");
-    }
-}
+use PHPFusion\Admins;
 
-if (!defined("IMAGES_B")) {
-    define("IMAGES_B", INFUSIONS."blog/images/");
-}
-if (!defined("IMAGES_B_T")) {
-    define("IMAGES_B_T", INFUSIONS."blog/images/thumbs/");
-}
-if (!defined("IMAGES_BC")) {
-    define("IMAGES_BC", INFUSIONS."blog/blog_cats/");
-}
-if (!defined("DB_BLOG")) {
-    define("DB_BLOG", DB_PREFIX."blog");
-}
-if (!defined("DB_BLOG_CATS")) {
-    define("DB_BLOG_CATS", DB_PREFIX."blog_cats");
-}
+// Locales
+define('BLOG_LOCALE', fusion_get_inf_locale_path('blog.php', INFUSIONS.'blog/locale/'));
+define('BLOG_ADMIN_LOCALE', fusion_get_inf_locale_path('blog_admin.php', INFUSIONS.'blog/locale/'));
+
+// Paths
+const IMAGES_B = INFUSIONS.'blog/images/';
+const IMAGES_B_T = INFUSIONS.'blog/images/thumbs/';
+const IMAGES_BC = INFUSIONS.'blog/blog_cats/';
+
+// Database
+const DB_BLOG = DB_PREFIX.'blog';
+const DB_BLOG_CATS = DB_PREFIX.'blog_cats';
 
 // Admin Settings
-\PHPFusion\Admins::getInstance()->setAdminPageIcons("BLOG", "<i class='admin-ico fa fa-fw fa-graduation-cap'></i>");
-\PHPFusion\Admins::getInstance()->setCommentType('B', fusion_get_locale('BLOG', LOCALE.LOCALESET."admin/main.php"));
-\PHPFusion\Admins::getInstance()->setLinkType('B', fusion_get_settings("siteurl")."infusions/blog/blog.php?readmore=%s");
+Admins::getInstance()->setAdminPageIcons("BLOG", "<i class='admin-ico fa fa-fw fa-graduation-cap'></i>");
+Admins::getInstance()->setCommentType('B', fusion_get_locale('BLOG', LOCALE.LOCALESET."admin/main.php"));
+Admins::getInstance()->setLinkType('B', fusion_get_settings("siteurl")."infusions/blog/blog.php?readmore=%s");
 
 $inf_settings = get_settings('blog');
-if ((!empty($inf_settings['blog_allow_submission']) && $inf_settings['blog_allow_submission']) && (!empty($inf_settings['blog_submission_access']) && checkgroup($inf_settings['blog_submission_access']))) {
-    \PHPFusion\Admins::getInstance()->setSubmitData('b', [
+if (
+    (!empty($inf_settings['blog_allow_submission']) && $inf_settings['blog_allow_submission']) &&
+    (!empty($inf_settings['blog_submission_access']) && checkgroup($inf_settings['blog_submission_access']))
+) {
+    Admins::getInstance()->setSubmitData('b', [
         'infusion_name' => 'blog',
         'link'          => INFUSIONS."blog/blog_submit.php",
         'submit_link'   => "submit.php?stype=b",
         'submit_locale' => fusion_get_locale('BLOG', LOCALE.LOCALESET."admin/main.php"),
         'title'         => fusion_get_locale('blog_submit', LOCALE.LOCALESET."submissions.php"),
-        'admin_link'    => INFUSIONS."blog/blog_admin.php".fusion_get_aidlink()."&amp;section=submissions&amp;submit_id=%s"
+        'admin_link'    => INFUSIONS."blog/blog_admin.php".fusion_get_aidlink()."&section=submissions&submit_id=%s"
     ]);
 }
 
-\PHPFusion\Admins::getInstance()->setFolderPermissions('blog', [
+Admins::getInstance()->setFolderPermissions('blog', [
     'infusions/blog/images/'        => TRUE,
     'infusions/blog/images/thumbs/' => TRUE
 ]);
 
-\PHPFusion\Admins::getInstance()->setCustomFolder('BLOG', [
+Admins::getInstance()->setCustomFolder('BLOG', [
     [
         'path'  => IMAGES_B,
         'URL'   => fusion_get_settings('siteurl').'infusions/blog/images/',
@@ -88,11 +75,13 @@ if (defined('BLOG_EXISTS')) {
         $locale = fusion_get_locale();
 
         if (fusion_get_settings('comments_enabled') == 1) {
-            $comments_query = "(SELECT COUNT(c1.comment_id) FROM ".DB_COMMENTS." c1 WHERE c1.comment_item_id = bl.blog_id AND c1.comment_type = 'BL') AS comments_count,";
+            $comments_query = "(SELECT COUNT(c1.comment_id) FROM ".DB_COMMENTS." c1
+                WHERE c1.comment_item_id = bl.blog_id AND c1.comment_type = 'BL') AS comments_count,";
         }
 
         if (fusion_get_settings('ratings_enabled') == 1) {
-            $ratings_query = "(SELECT COUNT(r1.rating_id) FROM ".DB_RATINGS." r1 WHERE r1.rating_item_id = bl.blog_id AND r1.rating_type = 'BL') AS ratings_count,";
+            $ratings_query = "(SELECT COUNT(r1.rating_id) FROM ".DB_RATINGS." r1
+                WHERE r1.rating_item_id = bl.blog_id AND r1.rating_type = 'BL') AS ratings_count,";
         }
 
         $result = dbquery("SELECT
@@ -116,7 +105,8 @@ if (defined('BLOG_EXISTS')) {
             WHERE (".time()." > bl.blog_start OR bl.blog_start = 0)
             AND bl.blog_draft = 0
             AND (".time()." < bl.blog_end OR bl.blog_end = 0)
-            AND ".groupaccess('bl.blog_visibility')." ".(multilang_table("BL") ? "AND ".in_group('blog_language', LANGUAGE) : "")."
+            AND ".groupaccess('bl.blog_visibility')." ".(multilang_table("BL") ? "
+            AND ".in_group('blog_language', LANGUAGE) : "")."
             GROUP BY bl.blog_id
             ORDER BY bl.blog_datestamp DESC LIMIT ".$limit
         );
