afe9be640085b4ec87221a9f1f28ee4ba2fcc4e7
RobiNN <kelcakrobo@gmail.com>
2021-09-04 18:27:40 +0200

    Update quick_reply.php

--- infusions/forum/classes/post/quick_reply.php
+++ infusions/forum/classes/post/quick_reply.php
@@ -54,6 +54,7 @@ class QuickReply extends ForumServer {
                         'input_id'    => 'file_attachments',
                         'upload_path' => INFUSIONS.'forum/attachments/',
                         'type'        => 'object',
+                        'template'    => 'modern',
                         'preview_off' => TRUE,
                         'multiple'    => TRUE,
                         'inline'      => FALSE,
