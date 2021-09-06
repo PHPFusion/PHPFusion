3f669b746d42acbbea777666c6ee1eda1de21db3
RobiNN <kelcakrobo@gmail.com>
2021-09-07 01:17:44 +0200

    Update PageList.php

--- includes/classes/PHPFusion/Page/Composer/PageList.php
+++ includes/classes/PHPFusion/Page/Composer/PageList.php
@@ -239,12 +239,11 @@ class PageList extends PageAdmin {
             ]);
             echo "</div>\n";
             echo "<div class='display-inline-block'>\n";
-            echo form_select("page_access[]", "", $filter_values['page_access'],
+            echo form_select("page_access", "", $filter_values['page_access'],
                 [
                     "allowclear"  => TRUE,
                     "placeholder" => "- ".self::$locale['page_0111']." -",
-                    "options"     => fusion_get_groups(),
-                    'multiple'    => TRUE
+                    "options"     => fusion_get_groups()
                 ]
             );
             echo "</div>\n";
