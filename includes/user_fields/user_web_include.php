89a66d9bb110049d678e5c57b867600df50c5fef
karrak1 <g.sebestyen64@gmail.com>
2021-10-30 07:23:23 +0200

    Update user_web_include.php

--- includes/user_fields/user_web_include.php
+++ includes/user_fields/user_web_include.php
@@ -34,7 +34,7 @@ if ($profile_method == "input") {
 } else if ($profile_method == "display") {
     $link = '';
     if ($field_value) {
-        $link = !preg_match("@^http(s)?\:\/\/@i", $field_value) ? "https://www.github.com/".$field_value : $field_value;
+        $link = !preg_match("@^http(s)?\:\/\/@i", $field_value) ? "http://".$field_value : $field_value;
         $field_value = (fusion_get_settings('index_url_userweb') ? "" : "<!--noindex-->")."<a href='".$link."' title='".$field_value."' ".(fusion_get_settings('index_url_userweb') ? "" : "rel='nofollow noopener noreferrer' ")."target='_blank'>".$locale['uf_web_001']."</a>".(fusion_get_settings('index_url_userweb') ? "" : "<!--/noindex-->");
     }
     $user_fields = [
