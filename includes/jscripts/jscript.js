826fbcdbae30cc8d4ab0d3b7740565e4952c2a0c
deviance <meangczac.chan@gmail.com>
2022-07-28 10:47:09 +0800

    Fixes #2393 Thank you @karrak, and commit will fix the reported error.
    Seems like bbcode.js did not load for some reason, I wanted to start something modular for the bbcode part, but for now it's just a function. Let's merge the small function with jscripts.js for now.

--- includes/jscripts/jscript.js
+++ includes/jscripts/jscript.js
@@ -124,6 +124,21 @@ function addText(textarea, text1, text2, formname) {
 }
 
 /**
+ * Executes PHPFusion BBcode Editor Action
+ */
+let bbcode_smileys = function () {
+    $(document).on("click", "[data-action='bbcode_smileys']", function (e) {
+        e.preventDefault();
+        let bbcode = $(this).data('smiley'),
+            editor = $(this).closest('.panel-txtarea').find('textarea');
+        if (editor.length === 1) {
+            let curPos = editor[0].selectionStart, curVal = $(editor).val();
+            $(editor).val(curVal.slice(0, curPos) + bbcode + curVal.slice(curPos));
+        }
+    });
+}
+
+/**
  * insertText
  *
  * @param f
@@ -364,7 +379,8 @@ function setChecked(frmName, chkName, val) {
  * Run time execution
  */
 function onload_events() {
-    resize_forum_imgs()
+    resize_forum_imgs();
+    bbcode_smileys();
 }
 
 window.onload = onload_events;
