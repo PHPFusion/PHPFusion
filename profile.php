b896f7ec2a7f5573bdfd7404475fc97f965bd7d6
karrak1 <g.sebestyen64@gmail.com>
2025-03-13 18:08:11 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -15,59 +15,61 @@
 | copyright header is strictly prohibited without
 | written permission from the original author(s).
 +--------------------------------------------------------*/
-require_once __DIR__.'/maincore.php';
-require_once THEMES.'templates/header.php';
-$locale = fusion_get_locale('', LOCALE.LOCALESET."user_fields.php");
+require_once __DIR__ . '/maincore.php';
+require_once FUSION_HEADER;
+$locale = fusion_get_locale( '', LOCALE . LOCALESET . "user_fields.php" );
 $settings = fusion_get_settings();
-if (isset($_GET['lookup']) && isnum($_GET['lookup'])) {
-    require_once THEMES."templates/global/profile.tpl.php";
+if ( check_get( 'lookup' ) && get( 'lookup', FILTER_VALIDATE_INT ) ) {
+    require_once THEMES . 'templates/global/profile.tpl.php';
 
-    if (!iMEMBER && $settings['hide_userprofiles'] == 1 || user_blacklisted($_GET['lookup'])) {
-        redirect(BASEDIR."index.php");
+    if ( !iMEMBER && $settings['hide_userprofiles'] == 1 || user_blacklisted( get( 'lookup' ) ) ) {
+        redirect( BASEDIR . 'index.php' );
     }
-    $user_status =$user_status = " AND (u.user_status = '0' OR u.user_status = '3' OR u.user_status = '7')";
-    if (iADMIN) {
-        $user_status = "";
-    }
+
+    $user_status = iADMIN ? '' : " AND (u.user_status = '0' OR u.user_status = '3' OR u.user_status = '7')";
+
     $user_data = [];
-    $result = dbquery("SELECT u.*, s.suspend_reason
-        FROM ".DB_USERS." u
-        LEFT JOIN ".DB_SUSPENDS." s ON u.user_id=s.suspended_user
-        WHERE user_id=:uid".$user_status."
+    $result = dbquery( "SELECT u.*, s.suspend_reason
+        FROM " . DB_USERS . " AS u
+        LEFT JOIN " . DB_SUSPENDS . " AS s ON u.user_id = s.suspended_user
+        WHERE user_id = :uid" . $user_status . "
         ORDER BY suspend_date DESC
-        LIMIT 1", [':uid' => (int)get( 'lookup' )] );
-    if (dbrows($result)) {
-        $user_data = dbarray($result);
+        LIMIT 1", [':uid' => (int)get( 'lookup' )]
+    );
+    if ( dbrows( $result ) ) {
+        $user_data = dbarray( $result );
     } else {
-        redirect(BASEDIR."index.php");
+        redirect( BASEDIR . 'index.php' );
     }
-    set_title($locale['u103'].$locale['global_201'].$user_data['user_name']);
-    if (iADMIN && checkrights("UG") && $_GET['lookup'] != $user_data['user_id']) {
-        if ((isset($_POST['add_to_group'])) && (isset($_POST['user_group']) && isnum($_POST['user_group']))) {
-            if (!preg_match("(^\.{$_POST['user_group']}$|\.{$_POST['user_group']}\.|\.{$_POST['user_group']}$)", $user_data['user_groups'])) {
-                $result = dbquery("UPDATE ".DB_USERS." SET user_groups='".$user_data['user_groups'].".".$_POST['user_group']."'
-                    WHERE user_id='".$_GET['lookup']."'
-                ");
+
+    set_title( $locale['u103'] . $locale['global_201'] . $user_data['user_name'] );
+
+    if (iADMIN && checkrights( "UG" ) && get( 'lookup' ) != $user_data['user_id'] ) {
+        if ( ( check_post( 'add_to_group' ) ) && ( check_post( 'user_group' ) && post( 'user_group', FILTER_VALIDATE_INT ) ) ) {
+            if ( !preg_match( "(^\.{$_POST['user_group']}$|\.{$_POST['user_group']}\.|\.{$_POST['user_group']}$)", $user_data['user_groups'] ) ) {
+                dbquery( "UPDATE " . DB_USERS . " SET user_groups = '" . $user_data['user_groups'] . "." . post( 'user_group' ) . "'
+                    WHERE user_id = '" . get( 'lookup' ) . "'
+                " );
             }
-            redirect(FUSION_SELF."?lookup=".$_GET['lookup']);
+            redirect( FUSION_SELF . "?lookup=" . get( 'lookup' ) );
         }
     }
+
     $userFields = new PHPFusion\UserFields();
     $userFields->userData = $user_data;
     $userFields->showAdminOptions = TRUE;
     $userFields->method = 'display';
-    $userFields->plugin_folder = [INCLUDES."user_fields/", INFUSIONS];
-    $userFields->plugin_locale_folder = LOCALE.LOCALESET."user_fields/";
+    $userFields->plugin_folder = [INCLUDES . "user_fields/", INFUSIONS];
+    $userFields->plugin_locale_folder = LOCALE . LOCALESET . "user_fields/";
     $userFields->displayProfileOutput();
 
-    PHPFusion\OpenGraph::ogUserProfile($_GET['lookup']);
-
-} else if (isset($_GET['group_id']) && isnum($_GET['group_id'])) {
+    \PHPFusion\OpenGraph::ogUserProfile( get( 'lookup' ) );
+} else if ( check_get( 'group_id' ) && get( 'group_id', FILTER_VALIDATE_INT ) ) {
     /*
      * Show group
      */
-    \PHPFusion\UserGroups::getInstance()->setGroup($_GET['group_id'])->showGroup();
+    \PHPFusion\UserGroups::getInstance()->setGroup( get( 'group_id' ) )->showGroup();
 } else {
-    redirect(BASEDIR."index.php");
+    redirect( BASEDIR . 'index.php' );
 }
-require_once THEMES.'templates/footer.php';
+require_once FUSION_FOOTER;
