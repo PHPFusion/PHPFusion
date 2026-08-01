b39e1940e1244c0d09a3e9f18988e4bca86ddaa6
karrak1 <g.sebestyen64@gmail.com>
2026-08-01 15:11:04 +0200

    Update settings_registration.php

--- administration/contents/settings_registration.php
+++ administration/contents/settings_registration.php
@@ -37,27 +37,29 @@ $contents = [
 function pf_post() {
     $locale = fusion_get_locale();
 
-    $inputData = [
-        'login_method'        => sanitizer( 'login_method', '0', 'login_method' ),
-        'enable_registration' => post( 'enable_registration' ) ? 1 : 0,
-        'email_verification'  => post( 'email_verification' ) ? 1 : 0,
-        'admin_activation'    => post( 'admin_activation' ) ? 1 : 0,
-        'enable_terms'        => post( 'enable_terms' ) ? 1 : 0,
-        'gateway'             => post( 'gateway' ) ? 1 : 0,
-        'gateway_method'      => sanitizer( 'gateway_method', 0, 'gateway_method' )
-    ];
+    if ( admin_post( 'savesettings' ) ) {
+        $inputData = [
+            'login_method'        => sanitizer( 'login_method', '0', 'login_method' ),
+            'enable_registration' => post( 'enable_registration' ) ? 1 : 0,
+            'email_verification'  => post( 'email_verification' ) ? 1 : 0,
+            'admin_activation'    => post( 'admin_activation' ) ? 1 : 0,
+            'enable_terms'        => post( 'enable_terms' ) ? 1 : 0,
+            'gateway'             => post( 'gateway' ) ? 1 : 0,
+            'gateway_method'      => sanitizer( 'gateway_method', 0, 'gateway_method' )
+        ];
 
-    if ( fusion_safe() ) {
-        dbquery( "BEGIN" );
-        foreach ( $inputData as $settings_name => $settings_value ) {
-            dbquery( "UPDATE " . DB_SETTINGS . " SET settings_value=:settings_value WHERE settings_name=:settings_name", [
-                ':settings_value' => $settings_value,
-                ':settings_name'  => $settings_name
-            ] );
+        if ( fusion_safe() ) {
+            dbquery( "BEGIN" );
+            foreach ( $inputData as $settings_name => $settings_value ) {
+                dbquery( "UPDATE " . DB_SETTINGS . " SET settings_value=:settings_value WHERE settings_name=:settings_name", [
+                    ':settings_value' => $settings_value,
+                    ':settings_name'  => $settings_name
+                ] );
+            }
+            dbquery( "COMMIT" );
+            addnotice( 'success', $locale['900'] );
+            redirect( FUSION_REQUEST );
         }
-        dbquery( "COMMIT" );
-        addnotice( 'success', $locale['900'] );
-        redirect( FUSION_REQUEST );
     }
 }
 
